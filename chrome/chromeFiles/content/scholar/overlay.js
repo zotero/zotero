233a2414e70ee8ae483e7c1b780df0f52ba7dbb1
David Norton <david@nortoncrew.com>
2006-05-30 22:06:33 +0000

    Some temporary viewing in-browser.

---
+++ chrome/chromeFiles/content/scholar/overlay.js
@@ -0,0 +1,93 @@
+var ScholarPane = new function()
+{
+	
+	var foldersView;
+	var itemsView;
+	var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+	
+	this.init = init;
+	this.newItem = newItem;
+	this.newFolder = newFolder;
+	this.folderSelected = folderSelected;
+	this.itemSelected = itemSelected;
+	this.deleteSelection = deleteSelection;
+	this.search = search;
+	
+	function init()
+	{
+		foldersView = new Scholar.TreeView(0); //pass params here?
+		document.getElementById('folders-tree').view = foldersView;
+		itemsView = new Scholar.ItemTreeView(0);
+		document.getElementById('items-tree').view = itemsView;
+	
+		var addMenu = document.getElementById('tb-add').firstChild;
+		var itemTypes = Scholar.ItemTypes.getTypes();
+		for(var i = 0; i<itemTypes.length; i++)
+		{
+			var menuitem = document.createElement("menuitem");
+			menuitem.setAttribute("label", Scholar.getString("itemTypes."+itemTypes[i]['name']));
+			menuitem.setAttribute("oncommand","ScholarPane.newItem("+itemTypes[i]['id']+")");
+			addMenu.appendChild(menuitem);
+		}
+	}
+	
+	function newItem(typeID)
+	{
+		alert("new item of type: "+typeID);
+	}
+	
+	function newFolder()
+	{
+		alert("new folder");
+	}
+	
+	function folderSelected()
+	{
+		if(foldersView.selection.count == 1 && foldersView.selection.currentIndex != -1)
+		{
+			itemsView = new Scholar.ItemTreeView(foldersView._getItemAtRow(foldersView.selection.currentIndex).getID());
+			document.getElementById('items-tree').view = itemsView;
+		}
+		else if(foldersView.selection.count == 0)
+		{
+			itemsView = new Scholar.ItemTreeView(0);
+			document.getElementById('items-tree').view = itemsView;
+		}
+		else
+		{
+			document.getElementById('items-tree').view = null;
+		}
+		
+	}
+	
+	function itemSelected()
+	{
+		var editButton = document.getElementById('metadata-pane-edit-button');
+				
+		if(itemsView && itemsView.selection.count == 1)
+		{
+			var item = itemsView._getItemAtRow(itemsView.selection.currentIndex);
+			
+			document.getElementById('content').loadURI('chrome://scholar/content/view.xul?url='+encodeURIComponent('http://www.google.com/search?q='+encodeURIComponent('"'+item.getField("title")+'"')+'&btnI'));
+		}
+		else
+		{
+			
+		}
+
+	}
+	
+	function deleteSelection()
+	{
+		if(itemsView && itemsView.selection.count > 0 && confirm("Are you sure you want to delete the selected items?"))
+			itemsView.deleteSelection();
+	}
+	
+	function search()
+	{
+		//TO DO: reload items tree with a search instead of a root folder
+		alert(document.getElementById('tb-search').value);
+	}
+}
+
+window.addEventListener("load", function(e) { ScholarPane.init(e); }, false);
