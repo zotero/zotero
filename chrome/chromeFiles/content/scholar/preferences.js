27025befef9333bc49c0f7235e9a08d715bed53a
David Norton <david@nortoncrew.com>
2006-06-21 22:31:52 +0000

    Added "Disable automatic scraper updates" preference. (Up to Dan to implement this)

--- chrome/chromeFiles/content/scholar/preferences.js
+++ chrome/chromeFiles/content/scholar/preferences.js
@@ -1,25 +1,25 @@
 var prefManager = Components.classes["@mozilla.org/preferences-service;1"]
                                 .getService(Components.interfaces.nsIPrefBranch);
-var whateverBox;
+var autoUpdateBox;
 
 /*
 	To add a new preference:
-		* modify defaults/prefs.js
-		* add a control to prefs.xul
-		* in this document:
-			1) add var above
-			2) add lines to init() function
-			3) add line to accept() function
+		1) modify defaults/preferences/scholar.js
+		2) in this document:
+			a) add var above
+			b) add lines to init() function
+			c) add line to accept() function
+		3) add a control to prefs.xul
 */
 
 function init()
 {	
-	whateverBox = document.getElementById('whateverBox');
-	whateverBox.checked = prefManager.getBoolPref('extensions.scholar.whatever');
+	autoUpdateBox = document.getElementById('autoUpdateBox');
+	autoUpdateBox.checked = prefManager.getBoolPref('extensions.scholar.automaticScraperUpdates');
 
 }
 
 function accept()
 {
-	prefManager.setBoolPref('extensions.scholar.whatever', whateverBox.checked);
+	prefManager.setBoolPref('extensions.scholar.automaticScraperUpdates', autoUpdateBox.checked);
 }
