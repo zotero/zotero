720960feb9e92ac9547dbe2766bca760c10f9a09
Dan Stillman <dstillman@zotero.org>
2006-06-24 08:28:37 +0000

    Addresses #5, Add as many item types as possible
    New item types from Elena

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- 22
+-- 23
 
     DROP TABLE IF EXISTS version;
     CREATE TABLE version (
@@ -178,9 +178,18 @@
     );
     
     
-    -- Some sample data
     INSERT INTO itemTypes VALUES (1,'book');
-    INSERT INTO itemTypes VALUES (2,'journalArticle');
+    INSERT INTO itemTypes VALUES (2,'bookSection');
+    INSERT INTO itemTypes VALUES (3,'journalArticle');
+    INSERT INTO itemTypes VALUES (4,'magazineArticle');
+    INSERT INTO itemTypes VALUES (5,'newspaperArticle');
+    INSERT INTO itemTypes VALUES (6,'thesis');
+    INSERT INTO itemTypes VALUES (7,'letter');
+    INSERT INTO itemTypes VALUES (8,'manuscript');
+    INSERT INTO itemTypes VALUES (9,'interview');
+    INSERT INTO itemTypes VALUES (10,'film');
+    INSERT INTO itemTypes VALUES (11,'artwork');
+    INSERT INTO itemTypes VALUES (12,'website');
     
     INSERT INTO "fieldFormats" VALUES(1, '.*', 0);
     INSERT INTO "fieldFormats" VALUES(2, '[0-9]*', 1);
@@ -199,40 +208,112 @@
     INSERT INTO fields VALUES (11,'ISBN',NULL);
     INSERT INTO fields VALUES (12,'publication',NULL);
     INSERT INTO fields VALUES (13,'ISSN',NULL);
+    INSERT INTO fields VALUES (14,'date',NULL);
+    INSERT INTO fields VALUES (15,'section',NULL);
+    INSERT INTO fields VALUES (16,'thesisType',NULL);
+    INSERT INTO fields VALUES (17,'accessionNumber',NULL);
     
-    INSERT INTO itemTypeFields VALUES (1,1,1);
-    INSERT INTO itemTypeFields VALUES (1,2,2);
-    INSERT INTO itemTypeFields VALUES (1,3,3);
-    INSERT INTO itemTypeFields VALUES (1,4,4);
-    INSERT INTO itemTypeFields VALUES (1,5,5);
-    INSERT INTO itemTypeFields VALUES (1,6,6);
-    INSERT INTO itemTypeFields VALUES (1,7,7);
-    INSERT INTO itemTypeFields VALUES (1,8,8);
-    INSERT INTO itemTypeFields VALUES (1,9,9);
-    INSERT INTO itemTypeFields VALUES (1,10,10);
-    INSERT INTO itemTypeFields VALUES (1,11,11);
-    INSERT INTO itemTypeFields VALUES (2,1,1);
-    INSERT INTO itemTypeFields VALUES (2,2,2);
-    INSERT INTO itemTypeFields VALUES (2,12,3);
-    INSERT INTO itemTypeFields VALUES (2,4,4);
-    INSERT INTO itemTypeFields VALUES (2,5,5);
-    INSERT INTO itemTypeFields VALUES (2,10,6);
-    INSERT INTO itemTypeFields VALUES (2,13,7);
+    INSERT INTO fields VALUES (19,'archiveLocation',NULL);
+    INSERT INTO fields VALUES (20,'medium',NULL);
+    INSERT INTO fields VALUES (21,'distributor',NULL);
     
+    INSERT INTO fields VALUES (23,'url',NULL);
+    INSERT INTO fields VALUES (24,'type',NULL);
+    
+    INSERT INTO "itemTypeFields" VALUES(1, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(1, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(1, 3, 3);
+    INSERT INTO "itemTypeFields" VALUES(1, 4, 4);
+    INSERT INTO "itemTypeFields" VALUES(1, 5, 5);
+    INSERT INTO "itemTypeFields" VALUES(1, 6, 6);
+    INSERT INTO "itemTypeFields" VALUES(1, 7, 7);
+    INSERT INTO "itemTypeFields" VALUES(1, 8, 8);
+    INSERT INTO "itemTypeFields" VALUES(1, 9, 9);
+    INSERT INTO "itemTypeFields" VALUES(1, 10, 10);
+    INSERT INTO "itemTypeFields" VALUES(1, 11, 11);
+    INSERT INTO "itemTypeFields" VALUES(3, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(3, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(3, 12, 3);
+    INSERT INTO "itemTypeFields" VALUES(3, 4, 4);
+    INSERT INTO "itemTypeFields" VALUES(3, 5, 5);
+    INSERT INTO "itemTypeFields" VALUES(3, 10, 6);
+    INSERT INTO "itemTypeFields" VALUES(3, 13, 7);
+    INSERT INTO "itemTypeFields" VALUES(2, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(2, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(2, 12, 3);
+    INSERT INTO "itemTypeFields" VALUES(2, 3, 4);
+    INSERT INTO "itemTypeFields" VALUES(2, 4, 5);
+    INSERT INTO "itemTypeFields" VALUES(2, 5, 6);
+    INSERT INTO "itemTypeFields" VALUES(2, 6, 7);
+    INSERT INTO "itemTypeFields" VALUES(2, 7, 8);
+    INSERT INTO "itemTypeFields" VALUES(2, 8, 9);
+    INSERT INTO "itemTypeFields" VALUES(2, 9, 10);
+    INSERT INTO "itemTypeFields" VALUES(2, 10, 11);
+    INSERT INTO "itemTypeFields" VALUES(2, 11, 12);
+    INSERT INTO "itemTypeFields" VALUES(4, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(4, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(4, 12, 3);
+    INSERT INTO "itemTypeFields" VALUES(4, 14, 4);
+    INSERT INTO "itemTypeFields" VALUES(4, 10, 5);
+    INSERT INTO "itemTypeFields" VALUES(4, 13, 6);
+    INSERT INTO "itemTypeFields" VALUES(5, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(5, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(5, 12, 3);
+    INSERT INTO "itemTypeFields" VALUES(5, 6, 4);
+    INSERT INTO "itemTypeFields" VALUES(5, 14, 5);
+    INSERT INTO "itemTypeFields" VALUES(5, 15, 6);
+    INSERT INTO "itemTypeFields" VALUES(5, 10, 7);
+    INSERT INTO "itemTypeFields" VALUES(5, 13, 8);
+    INSERT INTO "itemTypeFields" VALUES(6, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(6, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(6, 8, 3);
+    INSERT INTO "itemTypeFields" VALUES(6, 16, 4);
+    INSERT INTO "itemTypeFields" VALUES(6, 9, 5);
+    INSERT INTO "itemTypeFields" VALUES(6, 10, 6);
+    INSERT INTO "itemTypeFields" VALUES(6, 17, 7);
+    INSERT INTO "itemTypeFields" VALUES(7, 2, 1);
+    INSERT INTO "itemTypeFields" VALUES(7, 24, 2);
+    INSERT INTO "itemTypeFields" VALUES(7, 14, 3);
+    INSERT INTO "itemTypeFields" VALUES(7, 19, 4);
+    INSERT INTO "itemTypeFields" VALUES(8, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(8, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(8, 24, 3);
+    INSERT INTO "itemTypeFields" VALUES(8, 7, 4);
+    INSERT INTO "itemTypeFields" VALUES(8, 14, 5);
+    INSERT INTO "itemTypeFields" VALUES(8, 19, 6);
+    INSERT INTO "itemTypeFields" VALUES(9, 2, 1);
+    INSERT INTO "itemTypeFields" VALUES(9, 14, 2);
+    INSERT INTO "itemTypeFields" VALUES(9, 20, 3);
+    INSERT INTO "itemTypeFields" VALUES(9, 19, 4);
+    INSERT INTO "itemTypeFields" VALUES(10, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(10, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(10, 21, 3);
+    INSERT INTO "itemTypeFields" VALUES(10, 9, 4);
+    INSERT INTO "itemTypeFields" VALUES(11, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(11, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(11, 24, 3);
+    INSERT INTO "itemTypeFields" VALUES(11, 14, 4);
+    INSERT INTO "itemTypeFields" VALUES(12, 1, 1);
+    INSERT INTO "itemTypeFields" VALUES(12, 2, 2);
+    INSERT INTO "itemTypeFields" VALUES(12, 14, 3);
+    INSERT INTO "itemTypeFields" VALUES(12, 23, 4);
+    
+    
+    -- Some sample data
     INSERT INTO "items" VALUES(1, 1, 'Online connections: Internet interpersonal relationships', '2006-03-12 05:24:40', '2006-03-12 05:24:40');
     INSERT INTO "items" VALUES(2, 1, 'Computer-Mediated Communication: Human-to-Human Communication Across the Internet', '2006-03-12 05:25:50', '2006-03-12 05:25:50');
-    INSERT INTO "items" VALUES(3, 2, 'Residential propinquity as a factor in marriage selection', '2006-03-12 05:26:37', '2006-03-12 05:26:37');
+    INSERT INTO "items" VALUES(3, 3, 'Residential propinquity as a factor in marriage selection', '2006-03-12 05:26:37', '2006-03-12 05:26:37');
     INSERT INTO "items" VALUES(4, 1, 'Connecting: how we form social bonds and communities in the Internet age', '2006-03-12 05:27:15', '2006-03-12 05:27:15');
     INSERT INTO "items" VALUES(5, 1, 'Male, Female, Email: The Struggle for Relatedness in a Paranoid Society', '2006-03-12 05:27:36', '2006-03-12 05:27:36');
-    INSERT INTO "items" VALUES(6, 2, 'Social Implications of Sociology', '2006-03-12 05:27:53', '2006-03-12 05:27:53');
+    INSERT INTO "items" VALUES(6, 3, 'Social Implications of Sociology', '2006-03-12 05:27:53', '2006-03-12 05:27:53');
     INSERT INTO "items" VALUES(7, 1, 'Social Pressures in Informal Groups: A Study of Human Factors in Housing', '2006-03-12 05:28:05', '2006-03-12 05:28:05');
     INSERT INTO "items" VALUES(8, 1, 'Cybersociety 2.0: Revisiting Computer-Mediated Community and Technology', '2006-03-12 05:28:37', '2006-03-12 05:28:37');
-    INSERT INTO "items" VALUES(9, 2, 'The Computer as a Communication Device', '2006-03-12 05:29:03', '2006-03-12 05:29:03');
-    INSERT INTO "items" VALUES(10, 2, 'What Does Research Say about the Nature of Computer-mediated Communication: Task-Oriented, Social-Emotion-Oriented, or Both?', '2006-03-12 05:29:12', '2006-03-12 05:29:12');
+    INSERT INTO "items" VALUES(9, 3, 'The Computer as a Communication Device', '2006-03-12 05:29:03', '2006-03-12 05:29:03');
+    INSERT INTO "items" VALUES(10, 3, 'What Does Research Say about the Nature of Computer-mediated Communication: Task-Oriented, Social-Emotion-Oriented, or Both?', '2006-03-12 05:29:12', '2006-03-12 05:29:12');
     INSERT INTO "items" VALUES(11, 1, 'The second self: computers and the human spirit', '2006-03-12 05:30:38', '2006-03-12 05:30:38');
     INSERT INTO "items" VALUES(12, 1, 'Life on the screen: identity in the age of the Internet', '2006-03-12 05:30:49', '2006-03-12 05:30:49');
-    INSERT INTO "items" VALUES(13, 2, 'The computer conference: An altered state of communication', '2006-03-12 05:31:00', '2006-03-12 05:31:00');
-    INSERT INTO "items" VALUES(14, 2, 'Computer Networks as Social Networks: Collaborative Work, Telework, and Community', '2006-03-12 05:31:17', '2006-03-12 05:31:17');
+    INSERT INTO "items" VALUES(13, 3, 'The computer conference: An altered state of communication', '2006-03-12 05:31:00', '2006-03-12 05:31:00');
+    INSERT INTO "items" VALUES(14, 3, 'Computer Networks as Social Networks: Collaborative Work, Telework, and Community', '2006-03-12 05:31:17', '2006-03-12 05:31:17');
     INSERT INTO "items" VALUES(15, 1, 'The Internet in everyday life', '2006-03-12 05:31:41', '2006-03-12 05:31:41');
     
     INSERT INTO "itemData" VALUES(1, 9, 2001);
