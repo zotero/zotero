ee943c0d2c9c577ab473def9b52b5063d5e8a086
David Norton <david@nortoncrew.com>
2006-08-21 21:10:40 +0000

    Closes #169, add OpenURL interface hooks  - Added menu, uses OCLC database. (someone should test from GMU or another institution)

--- chrome/chromeFiles/content/scholar/preferences.js
+++ chrome/chromeFiles/content/scholar/preferences.js
@@ -21,6 +21,8 @@
 var autoUpdateBox;
 var positionMenu;
 var parseEndnoteBox;
+var openURLMenu;
+var openURLResolvers;
 var openURLServerField;
 var openURLVersionMenu;
 
@@ -52,6 +54,16 @@ function init()
 	openURLServerField.value = Scholar.Prefs.get('openURL.resolver');
 	openURLVersionMenu = document.getElementById('openURLVersionMenu');
 	openURLVersionMenu.value = Scholar.Prefs.get('openURL.version');
+
+	openURLMenu = document.getElementById('openURLMenu');
+
+	openURLResolvers = Scholar.OpenURL.discoverResolvers();
+	for(var i in openURLResolvers)
+	{
+		openURLMenu.insertItemAt(i,openURLResolvers[i]['name']);
+		if(openURLResolvers[i]['url'] == Scholar.Prefs.get('openURL.resolver') && openURLResolvers[i]['version'] == Scholar.Prefs.get('openURL.version'))
+			openURLMenu.selectedIndex = i;
+	}
 }
 
 function accept()
@@ -64,6 +76,25 @@ function accept()
 		Scholar.Prefs.set('parseEndNoteMIMETypes', parseEndnoteBox.checked);
 		Scholar.Ingester.MIMEHandler.init();
 	}
+	
 	Scholar.Prefs.set('openURL.resolver', openURLServerField.value);
 	Scholar.Prefs.set('openURL.version', openURLVersionMenu.value);
+}
+
+function onOpenURLSelected()
+{
+	if(openURLMenu.value == "custom")
+	{
+		openURLServerField.focus();
+	}
+	else
+	{
+		openURLServerField.value = openURLResolvers[openURLMenu.selectedIndex]['url'];
+		openURLVersionMenu.value = openURLResolvers[openURLMenu.selectedIndex]['version'];
+	}
+}
+
+function onOpenURLCustomized()
+{
+	openURLMenu.value = "custom";
 }
