10efae58229d3eabb1b9a5606cd0ad0236b470ac
Dan Stillman <dstillman@zotero.org>
2006-10-05 17:22:47 +0000

    Don't set (currently unused) downloadAssociatedFiles pref

--- defaults/preferences/zotero.js
+++ defaults/preferences/zotero.js
@@ -8,5 +8,5 @@ pref("extensions.zotero.openURL.resolver","http://athene.gmu.edu:8888/lfp/LinkFi
 pref("extensions.zotero.openURL.version","0.1");
 pref("extensions.zotero.parseEndNoteMIMETypes",true);
 pref("extensions.zotero.automaticSnapshots",true);
-pref("extensions.zotero.downloadAssociatedFiles",false);
+//pref("extensions.zotero.downloadAssociatedFiles",false);
 pref("extensions.zotero.reportTranslationFailure",true);
