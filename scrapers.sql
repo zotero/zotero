df161922c11531749400ff19ec2170299cef9b78
Sean Takats <stakats@takats.org>
2007-06-26 23:14:26 +0000

    Accidentally deleted Simon's WorldCat search translator in last commit.  Restored here without changes.

--- scrapers.sql
+++ scrapers.sql
@@ -1,4 +1,4 @@
--- 246
+-- 247
 
 --  ***** BEGIN LICENSE BLOCK *****
 --  
@@ -9353,6 +9353,87 @@ function doWeb(doc, url){
 		processOWC(doc);}, function() {Zotero.done();});
 	Zotero.wait();
 }');
+
+REPLACE INTO translators VALUES ('e07e9b8c-0e98-4915-bb5a-32a08cb2f365', '1.0.0b3.r1', '', '2007-03-22 18:15:00', 1, 100, 12, 'Open WorldCat (Search)', 'Simon Kornblith', 'http://partneraccess.oclc.org/',
+'function detectSearch(item) {
+	if(item.itemType == "book" || item.itemType == "bookSection") {
+		return true;
+	}
+	return false;
+}',
+'// creates an item from an Open WorldCat document
+function processOWC(doc) {
+	var spanTags = doc.getElementsByTagName("span");
+	for(var i=0; i<spanTags.length; i++) {
+		var spanClass = spanTags[i].getAttribute("class");
+		if(spanClass) {
+			var spanClasses = spanClass.split(" ");
+			if(Zotero.Utilities.inArray("Z3988", spanClasses)) {
+				var spanTitle = spanTags[i].getAttribute("title");
+				var item = new Zotero.Item();
+				if(Zotero.Utilities.parseContextObject(spanTitle, item)) {
+					if(item.title) {
+						item.title = Zotero.Utilities.capitalizeTitle(item.title);
+					} else {
+						item.title = "[Untitled]";
+					}
+					
+					item.complete();
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+	
+	return false;
+}
+
+function doSearch(item) {
+	if(item.contextObject) {
+		var co = item.contextObject;
+	} else {
+		var co = Zotero.Utilities.createContextObject(item);
+	}
+	
+	Zotero.Utilities.loadDocument("http://partneraccess.oclc.org/wcpa/servlet/OpenUrl?"+co, function(doc) {
+		// find new COinS in the Open WorldCat page
+		if(processOWC(doc)) {	// we got a single item page
+			Zotero.done();
+		} else {				// assume we have a search results page
+			var items = new Array();
+			
+			var namespace = doc.documentElement.namespaceURI;
+			var nsResolver = namespace ? function(prefix) {
+				if (prefix == ''x'') return namespace; else return null;
+			} : null;
+			
+			// first try to get only books
+			var elmts = doc.evaluate(''//table[@class="tableLayout"]/tbody/tr/td[@class="content"]/table[@class="tableResults"]/tbody/tr[td/img[@alt="Book"]]/td/div[@class="title"]/a'', doc, nsResolver, Components.interfaces.nsIDOMXPathResult.ANY_TYPE,null);
+			var elmt = elmts.iterateNext();
+			if(!elmt) {	// if that fails, look for other options
+				var elmts = doc.evaluate(''//table[@class="tableLayout"]/tbody/tr/td[@class="content"]/table[@class="tableResults"]/tbody/tr[td/img[@alt="Book"]]/td/div[@class="title"]/a'', doc, nsResolver, Components.interfaces.nsIDOMXPathResult.ANY_TYPE,null);
+				elmt = elmts.iterateNext()
+			}
+			
+			var urlsToProcess = new Array();
+			do {
+				urlsToProcess.push(elmt.href);
+			} while(elmt = elmts.iterateNext());
+			
+			Zotero.Utilities.processDocuments(urlsToProcess, function(doc) {
+				// per URL
+				processOWC(doc);
+			}, function() {	// done
+				Zotero.done();
+			});
+		}
+	}, null);
+	
+	Zotero.wait();
+}');
+
 
 REPLACE INTO translators VALUES ('11645bd1-0420-45c1-badb-53fb41eeb753', '1.0.0b3.r1', '', '2006-11-27 22:45:00', 1, 100, 8, 'CrossRef', 'Simon Kornblith', 'http://partneraccess.oclc.org/',
 'function detectSearch(item) {
