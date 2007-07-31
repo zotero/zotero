56ca32dd963aeb89da70fe9139d53aa13ee2e0eb
Ben Parr <cheeseisgood626@gmail.com>
2007-07-31 17:51:55 +0000

    Fix url's in License block.

--- chrome/skin/default/zotero/timeline/bundle.css
+++ chrome/skin/default/zotero/timeline/bundle.css
@@ -2,7 +2,7 @@
 	***** BEGIN LICENSE BLOCK *****
 
 	© Copyright The SIMILE Project 2003-2005.
-	http://simile.mit.edu/timeline/
+	http://simile.mit.edu/
 	
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions
--- chrome/skin/default/zotero/timeline/timeline-api.js
+++ chrome/skin/default/zotero/timeline/timeline-api.js
@@ -11,7 +11,7 @@
 	***** BEGIN LICENSE BLOCK *****
 	
 	© Copyright The SIMILE Project 2003-2005.
-	http://simile.mit.edu/timeline/
+	http://simile.mit.edu/
 	
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions
