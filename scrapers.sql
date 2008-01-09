30f8a4825e90fc893a75ae55d7a4938aeaf5572b
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-01-09 17:48:39 +0000

    -Updates YouTube regex to handle international URLS -Adds Blackwell Compass translator

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-09 17:00:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-09 19:00:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2007-06-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -1963,7 +1963,7 @@ REPLACE INTO translators VALUES ('5dd22e9a-5124-4942-9b9e-6ee779f1023e', '1.0.0b
 	Zotero.wait();
 }');
 
-REPLACE INTO translators VALUES ('d3b1d34c-f8a1-43bb-9dd6-27aa6403b217', '1.0.0b4.r5', '', '2007-11-21 00:25:00', '0', '100', '4', 'YouTube', 'Sean Takats', '^https?://(?:www\.)?youtube\.com\/', 
+REPLACE INTO translators VALUES ('d3b1d34c-f8a1-43bb-9dd6-27aa6403b217', '1.0.0b4.r5', '', '2008-01-09 19:00:00', '0', '100', '4', 'YouTube', 'Sean Takats', 'https?://[^/]*youtube\.com\/', 
 'function detectWeb(doc, url){
 	var namespace = doc.documentElement.namespaceURI;
 	var nsResolver = namespace ? function(prefix) {
@@ -2083,6 +2083,62 @@ function getData(ids){
 		}
 		newItem.complete();		
 	}, function(){Zotero.done();});	
+	Zotero.wait();
+}');
+
+REPLACE INTO translators VALUES ('60d97c99-47f0-4323-98b6-5699faf827b1', '1.0.0b4.r5', '', '2008-01-09 19:00:00', '0', '100', '4', 'Blackwell Compass', 'Michael Berkowitz', 'http://www.blackwell-compass.com/subject/[^/]+/.+', 
+'function detectWeb(doc, url) {
+	if (url.indexOf("search_results") != -1 || url.indexOf("section_home") != -1) {
+		return "multiple";
+	} else {
+		return "journalArticle";
+	}
+}', 
+'function doWeb(doc, url) {
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
+		if (prefix == ''x'') return namespace; else return null;
+	} : null;
+	
+	var URIs = new Array();
+	var items = new Object();
+	if (detectWeb(doc, url) == "multiple") {
+		
+		var xpath = ''//div[@class="article-holder"]//h4[@class="article"]/a'';
+		var articles = doc.evaluate(xpath, doc, namespace, XPathResult.ANY_TYPE, null);
+		var next_art = articles.iterateNext();
+		while (next_art) {
+			items[next_art.href] = next_art.textContent;
+			next_art = articles.iterateNext();
+		}
+		items = Zotero.selectItems(items);
+		
+		for (var i in items) {
+			URIs.push(i);
+		}
+	} else {
+		URIs.push(url);
+	}
+	
+	Zotero.Utilities.processDocuments(URIs, function(doc, urll) {
+		var doi = doc.evaluate(''//div[@id="content"]/p/span[@class="guide"]/a[substring(@href, 1, 4) = "http"]'', doc, null, XPathResult.ANY_TYPE, null).iterateNext().href.match(/doi\/[^/]*\/([^&]*)/)[1];
+		Zotero.Utilities.HTTP.doGet(''http://www.blackwell-synergy.com/action/downloadCitation?doi='' + doi + ''&include=cit&format=refman&direct=on&submit=Download+references'', function(text) {
+			var translator = Zotero.loadTranslator("import");
+			translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+			translator.setString(text);
+			translator.setHandler("itemDone", function(obj, item) {
+				item.attachments = [
+					{url:item.url, title:"Blackwell Compass Snapshot", mimeType:"text/html"},
+					{url:item.url.replace("/doi/abs", "/doi/pdf"), title:"Blackwell Compass Full Text PDF", mimeType:"application/pdf"}
+				];
+				
+				item.complete();
+		
+			});
+			
+			translator.translate();
+		});
+	}, function() {Zotero.done;});
 	Zotero.wait();
 }');
 
