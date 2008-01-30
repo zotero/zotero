cbc52283a0b6a6eabe0a28e3e33e8ab5b8039c3f
Sean Takats <stakats@takats.org>
2008-01-30 06:32:05 +0000

    Tweaks Voyager translator to support National Library of Australia

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-29 23:00:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-30 07:30:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2007-06-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -930,7 +930,7 @@ function doWeb(doc, url) {
 	}
 }');
 
-REPLACE INTO translators VALUES ('88915634-1af6-c134-0171-56fd198235ed', '1.0.0b3.r1', '', '2007-07-31 16:45:00', '1', '100', '4', 'Library Catalog (Voyager)', 'Simon Kornblith', 'Pwebrecon\.cgi', 
+REPLACE INTO translators VALUES ('88915634-1af6-c134-0171-56fd198235ed', '1.0.0b3.r1', '', '2008-01-30 07:30:00', '1', '100', '4', 'Library Catalog (Voyager)', 'Simon Kornblith', 'Pwebrecon\.cgi', 
 'function detectWeb(doc, url) {
 	var export_options = doc.forms.namedItem(''frm'').elements.namedItem(''RD'').options;
 	for(var i in export_options) {
@@ -941,6 +941,7 @@ REPLACE INTO translators VALUES ('88915634-1af6-c134-0171-56fd198235ed', '1.0.0b
 		|| export_options[i].text == ''MARC (Unicode/UTF-8)''
 		|| export_options[i].text == ''MARC UTF-8''
 		|| export_options[i].text == ''UTF-8 MARC (Unicode)''
+		|| export_options[i].text == ''UTF8-Unicode''
 		|| export_options[i].text == ''MARC (non-Unicode/MARC-8)'') {
 			// We have an exportable single record
 			if(doc.forms.namedItem(''frm'').elements.namedItem(''RC'')) {
@@ -1039,6 +1040,7 @@ REPLACE INTO translators VALUES ('88915634-1af6-c134-0171-56fd198235ed', '1.0.0b
 			latin1 = i;
 		} else if(export_options[i].text == ''UTF-8''
 		|| export_options[i].text == ''UTF-8 MARC (Unicode)''
+		|| export_options[i].text == ''UTF8-Unicode''
 		|| export_options[i].text == ''MARC UTF-8''
 		|| export_options[i].text == ''MARC (Unicode/UTF-8)'') {
 			unicode = i;
