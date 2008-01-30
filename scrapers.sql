405df1ebe2ffb3d7a900b8e82257ded3c848702b
Sean Takats <stakats@takats.org>
2008-01-30 19:43:02 +0000

    Adds CiteULike translator.

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-30 07:30:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-01-30 20:00:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2007-06-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -10475,6 +10475,52 @@ function doWeb(doc, url) {
 	} else {
 		scrape(doc);
 	}
+}');
+
+REPLACE INTO translators VALUES ('8917b41c-8527-4ee7-b2dd-bcbc3fa5eabd', '1.0.0b4.r5', '', '2008-01-30 20:00:00', '0', '100', '4', 'CiteULike', 'Sean Takats', 'https?://(?:www\.)?citeulike.org/(?:journal/|group/[0-9]+/library$|\?page=[0-9]+$|.*article/[0-9]+$|$)', 
+'function detectWeb(doc, url){
+	var articleRe = /\/article\/[0-9]+$/;
+	var m = url.match(articleRe);
+	var newUris = new Array();
+	
+	if (m){
+		return "journalArticle";
+	} else {
+		return "multiple";
+	}
+}', 
+'function doWeb(doc, url){
+	var articleRe = /\/article\/[0-9]+$/;
+	var m = url.match(articleRe);
+	var newUris = new Array();
+	
+	if (m){
+		newUris.push(url.replace(/citeulike\.org\//, "citeulike.org/endnote/"));
+	} else {
+		var namespace = doc.documentElement.namespaceURI;
+		var nsResolver = namespace ? function(prefix) {
+			if (prefix == ''x'') return namespace; else return null;
+		} : null;
+		var elmt;
+		var elmts = doc.evaluate(''//a[@class="title"]'', doc, nsResolver, XPathResult.ANY_TYPE, null);
+		var items = new Object();		
+		while(elmt = elmts.iterateNext()) {
+			items[elmt.href] = Zotero.Utilities.trimInternal(elmt.textContent);
+		} 
+		items = Zotero.selectItems(items);
+		if(!items) return true;
+		for(var uri in items) {
+			newUris.push(uri.replace(/citeulike\.org\//, "citeulike.org/endnote/"));
+		}
+	}
+	Zotero.Utilities.HTTP.doGet(newUris, function(text) {
+			var translator = Zotero.loadTranslator("import");
+			translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+			translator.setString(text);
+			translator.translate();
+			Zotero.done();
+	});
+	Zotero.wait();
 }');
 
 REPLACE INTO translators VALUES ('ecddda2e-4fc6-4aea-9f17-ef3b56d7377a', '1.0.0b3.r1', '', '2007-12-22 06:00:00', '1', '100', '4', 'arXiv.org', 'Sean Takats', '^http://(?:www\.)?(?:(arxiv\.org|xxx.lanl.gov)/(?:find/\w|list/\w|abs/)|eprintweb.org/S/(?:search|archive|article)(?!.*refs$)(?!.*cited$))', 
