5076406483d6ae229d0e783717749ce277297423
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-04-17 21:02:27 +0000

    -Adds AOSIC translator.

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-04-17 19:00:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-04-17 21:00:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2008-03-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats and Michael Berkowitz', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -1144,6 +1144,69 @@ function doWeb(doc, url) {
 			translator.translate();
 		});
 	}
+}');
+
+REPLACE INTO translators VALUES ('dedcae51-073c-48fb-85ce-2425e97f128d', '1.0.0b4.r5', '', '2008-04-17 21:00:00', '0', '100', '4', 'Archive Ouverte en Sciences de l''Information et de la Communication  (AOSIC)', 'Michael Berkowitz', 'http://archivesic.ccsd.cnrs.fr/', 
+'function detectWeb(doc, url) {
+	if (doc.title.toLowerCase().match("fulltext search")) {
+		return "multiple";
+	} else if (url.match(/sic_\d+/)) {
+		return "journalArticle";
+	}
+}', 
+'var metaTags = {
+	"DC.relation":"url",
+	"DC.date":"date",
+	"DC.description":"abstractNote",
+	"DC.creator":"creators",
+	"DC.title":"title"
+}
+
+function doWeb(doc, url) {
+	var articles = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		var items = Zotero.Utilities.getItemArray(doc, doc, /sic_\d+\/fr\//);
+		items = Zotero.selectItems(items) 
+		for (var i in items) {
+			articles.push(i);
+		}
+	} else {
+		articles = [url];
+	}
+	Zotero.Utilities.processDocuments(articles, function(doc) {
+		var xpath = ''//meta[@name]'';
+		var data = new Object();
+		var metas = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+		var meta;
+		while (meta = metas.iterateNext()) {
+			if (data[meta.name]) {
+				data[meta.name] = data[meta.name] + ";" + meta.content;
+			} else {
+				data[meta.name] = meta.content;
+			}
+		}
+		Zotero.debug(data);
+		var item = new Zotero.Item("journalArticle");
+		for (var tag in metaTags) {
+			if (tag == "DC.creator") {
+				var authors = data[''DC.creator''].split(";");
+				for each (var aut in authors) {
+					aut = aut.replace(/^([^,]+),\s+(.*)$/, "$2 $1");
+					item.creators.push(Zotero.Utilities.cleanAuthor(aut, "author"));
+				}
+			} else {
+				item[metaTags[tag]] = data[tag];
+			}
+		}
+		var pdfurl = doc.evaluate(''//a[contains(@href, ".pdf")]'', doc, null, XPathResult.ANY_TYPE, null).iterateNext().href.match(/url=([^&]+)&/)[1];
+		Zotero.debug(pdfurl);
+		item.attachments = [
+			{url:item.url, title:"AOSIC Snapshot", mimeType:"text/html"},
+			{url:pdfurl, title:"AOSIC Full Text PDF", mimeType:"application/pdf"}
+		];
+		item.complete();
+	}, function() {Zotero.done;});
+	Zotero.wait();
 }');
 
 REPLACE INTO translators VALUES ('4afb932d-9211-4c0b-a31c-cfa984d62b66', '1.0.0b4.r5', '', '2008-04-17 17:00:00', '0', '100', '4', 'OAIster', 'Michael Berkowitz', 'http://quod.lib.umich.edu/cgi/b/', 
