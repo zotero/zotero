e101bcc7ec5b736c6e2baa72f770bbb12dcdda3a
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-05-07 15:19:58 +0000

    -Adds Science Links Japan translator.

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-05-06 18:45:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-05-07 16:00:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2008-03-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats and Michael Berkowitz', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -1086,6 +1086,67 @@ REPLACE INTO translators VALUES ('88915634-1af6-c134-0171-56fd198235ed', '1.0.0b
 		
 		Zotero.done();
 	})
+	Zotero.wait();
+}');
+
+REPLACE INTO translators VALUES ('c0d7d260-d795-4782-9446-f6c403a7922c', '1.0.0b4.r5', '', '2008-05-07 16:00:00', '0', '100', '4', 'Science Links Japan', 'Michael Berkowitz', 'http://sciencelinks.jp/', 
+'function detectWeb(doc, url) {
+	if (url.match(/result/) || url.match(/journal/)) {
+		return "multiple";
+	} else if (url.match(/article/)) {
+		return "journalArticle";
+	}
+}', 
+'function doWeb(doc, url) {
+	var ns = doc.documentElement.namespaceURI;
+	nsR = ns ? function(prefix) {
+		if (prefix == ''x'') return ns; else return null;
+	} : null;
+	
+	var arts = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		var items = Zotero.Utilities.getItemArray(doc, doc, "(article|display\.php)");
+		items = Zotero.selectItems(items);
+		for (var i in items) {
+			arts.push(i);
+		}
+	} else {
+		arts = [url];
+	}
+	Zotero.Utilities.processDocuments(arts, function(doc) {
+		var data = new Array();
+		var bits = doc.evaluate(''//div[@id="result_detail"]/table/tbody/tr/td'', doc, nsR, XPathResult.ANY_TYPE, null);
+		var bit;
+		while (bit = bits.iterateNext()) {
+			data.push(Zotero.Utilities.trimInternal(bit.textContent));
+		}
+		var item = new Zotero.Item("journalArticle");
+		for each (var datum in data) {
+			if (datum.match(/^Title;/)) {
+				item.title = Zotero.Utilities.capitalizeTitle(datum.match(/Title;(.*)$/)[1]);
+			} else if (datum.match(/^Author;/)) {
+				var auts = datum.match(/\b[A-Z''\-]+\s+[A-Z''\-]+/g);
+				for each (var aut in auts) {
+					item.creators.push(Zotero.Utilities.cleanAuthor(Zotero.Utilities.capitalizeTitle(aut, true), "author"));
+				}
+			} else if (datum.match(/^Journal Title;/)) {
+				item.publicationTitle = datum.match(/;(.*)$/)[1];
+			} else if (datum.match(/^ISSN/)) {
+				item.ISSN = datum.match(/[\d\-]+/)[0];
+			} else if (datum.match(/^VOL/)) {
+				var voliss = datum.match(/^VOL\.([^;]*);NO\.([^;]*);PAGE\.([^(]*)\((\d+)\)/);
+				item.volume = voliss[1];
+				item.issue = voliss[2];
+				item.pages = voliss[3];
+				item.date = voliss[4];
+			} else if (datum.match(/^Abstract/)) {
+				item.abstractNote = datum.match(/;(.*)/)[1];
+			}
+		}
+		item.url = doc.location.href;
+		item.attachments = [{url:item.url, title:"Science Links Japan Snapshot", mimeType:"text/html"}];
+		item.complete();
+	}, function() {Zotero.done;});
 	Zotero.wait();
 }');
 
