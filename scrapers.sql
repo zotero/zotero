0d0ca58151fa06850d7f52d74473f9a7d9e1b45f
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-05-19 19:47:00 +0000

    -Adds AfroEuropa journal translator.

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-05-19 19:00:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-05-19 20:45:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2008-03-21 20:00:00', '1', '100', '4', 'Amazon.com', 'Sean Takats and Michael Berkowitz', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -1147,6 +1147,81 @@ REPLACE INTO translators VALUES ('83538f48-906f-40ef-bdb3-e94f63676307', '1.0.0b
 		item.complete();
 		
 	}, function() {Zotero.done;});
+}');
+
+REPLACE INTO translators VALUES ('4f62425a-c99f-4ce1-b7c1-5a3ac0d636a3', '1.0.0b4.r5', '', '2008-05-19 20:45:00', '0', '100', '4', 'AfroEuropa', 'Michael Berkowitz', 'http://journal.afroeuropa.eu/', 
+'function detectWeb(doc, url) {
+	if (doc.evaluate(''//tr[td/a[2]]'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		return "multiple";
+	} else if (url.match(/article\/view\//)) {
+		return "journalArticle";
+	}
+}', 
+'function makeExport(str) {
+	var nums = str.match(/\d+(\/\d+)?/)[0];
+	if (!nums.match(/\//)) nums += "/0";
+	return ''http://journal.afroeuropa.eu/index.php/afroeuropa/rt/captureCite/'' + nums + ''/referenceManager'';
+}
+
+function doWeb(doc, url) {
+	var n = doc.documentElement.namespaceURI;
+	var ns = n ? function(prefix) {
+		if (prefix == ''x'') return n; else return null;
+	} : null;
+	
+	var arts = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		var xpath = ''//tr[td/a]'';
+		if (url.match(/search/)) {
+			var titlex = ''./td[2]'';
+			var linkx = ''./td[3]/a[1]'';
+		} else if (url.match(/issue/)) {
+			var titlex = ''./td[1]'';
+			var linkx = ''./td[2]/a[1]'';
+		}
+		var items = new Object();
+		var results = doc.evaluate(xpath, doc, ns, XPathResult.ANY_TYPE, null);
+		var result;
+		while (result = results.iterateNext()) {
+			var title = Zotero.Utilities.trimInternal(doc.evaluate(titlex, result, ns, XPathResult.ANY_TYPE, null).iterateNext().textContent);
+			var link = doc.evaluate(linkx, result, ns, XPathResult.ANY_TYPE, null).iterateNext().href;
+			items[makeExport(link)] = title;
+		}
+		items = Zotero.selectItems(items);
+		for (var i in items) {
+			arts.push(i);
+		}
+	} else {
+		arts = [makeExport(url)];
+	}
+	Zotero.debug(arts);
+	Zotero.Utilities.HTTP.doGet(arts, function(text) {
+		var translator = Zotero.loadTranslator("import");
+		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+		translator.setString(text);
+		translator.setHandler("itemDone", function(obj, item) {
+			item.title = Zotero.Utilities.capitalizeTitle(item.title);
+			var voliss = item.publicationTitle.split(/;\s+/);
+			item.publicationTitle = Zotero.Utilities.trimInternal(voliss[0]);
+			voliss = voliss[1].match(/(\d+),\s+No\s+(\d+)\s+\((\d+)\)/);
+			item.volume = voliss[1];
+			item.issue = voliss[2];
+			item.date = voliss[3];
+			var auts = new Array();
+			for each (var aut in item.creators) {
+				auts.push(aut.lastName);
+			}
+			item.creators = new Array();
+			for each (var aut in auts) {
+				item.creators.push(Zotero.Utilities.cleanAuthor(aut, "author"));
+			}
+			item.attachments[0].mimeType = "text/html";
+			item.attachments[0].title = "AfroEuropa Snapshot";
+			item.complete();
+		});
+		translator.translate();
+	});
+	Zotero.wait();
 }');
 
 REPLACE INTO translators VALUES ('882f70a8-b8ad-403e-bd76-cb160224999d', '1.0.0b4.r5', '', '2008-05-19 17:20:00', '0', '100', '4', 'Vanderbilt eJournals', 'Michael Berkowitz', 'http://ejournals.library.vanderbilt.edu/', 
