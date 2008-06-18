a9a84f758c064405906d5be3de15507069fcc808
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-06-18 15:41:09 +0000

    -Adds Journal of Electronic Publishing translator.

--- scrapers.sql
+++ scrapers.sql
@@ -2238,6 +2238,62 @@ function doImport() {
 	}
 }');
 
+REPLACE INTO translators VALUES ('d93c14fb-d327-4540-b60a-327309ea512b', '1.0.0b4.r5', '', '2008-06-18 11:39:10', '0', '100', '4', 'Journal of Electronic Publishing', 'Michael Berkowitz', 'http://quod.lib.umich.edu/.*c=jep', 
+'function detectWeb(doc, url) {
+	if (doc.evaluate(''//div/span[text() = "Search Results"]'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		return "multiple";
+	} else if (url.match(/\d+\.\d+\.\d+/)) {
+		return "journalArticle";
+	}
+}', 
+'function doWeb(doc, url) {
+	var n = doc.documentElement.namespaceURI;
+	var ns = n ? function(prefix) {
+		if (prefix == ''x'') return n; else return null;
+	} : null;
+	
+	var arts = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		var links = doc.evaluate(''//div[@id="resultslist"]/div[@class="itemcitation"]/div/a'', doc, ns, XPathResult.ANY_TYPE, null);
+		var link;
+		var items = new Object();
+		while (link = links.iterateNext()) {
+			items[link.href] = Zotero.Utilities.trimInternal(link.textContent);
+		}
+		items = Zotero.selectItems(items);
+		for (var i in items) {
+			arts.push(i);
+		}
+	} else {
+		arts = [url];
+	}
+	Zotero.Utilities.processDocuments(arts, function(doc) {
+		var data = new Object();
+		var rows = doc.evaluate(''//table[@id="itemmdataTable"]//tr'', doc, ns, XPathResult.ANY_TYPE, null);
+		var row;
+		while (row = rows.iterateNext()) {
+			var header = doc.evaluate(''./td[1]'', row, ns, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+			var value = doc.evaluate(''./td[2]'', row, ns, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+			data[Zotero.Utilities.trimInternal(header.replace(":", "").replace("Authors", "Author"))] = value;
+		}
+		var item = new Zotero.Item("journalArticle");
+		item.publicationTitle = "Journal of Electronic Publishing";
+		item.title = data["Title"];
+		var authors = data["Author"].split(",");
+		for each (var aut in authors) {
+			item.creators.push(Zotero.Utilities.cleanAuthor(aut, "author"));
+		}
+		item.url = data["URL"];
+		var voliss = data["Source"].match(/vol\.\s+([^,]+),\s+no\.\s+([^,]+),\s+(.*)$/);
+		item.volume = voliss[1];
+		item.issue = voliss[2];
+		item.date = voliss[3];
+		
+		item.complete();
+	}, function() {Zotero.done;});
+	Zotero.wait();
+}');
+
 REPLACE INTO translators VALUES ('58a778cc-25e2-4884-95b3-6b22d7571183', '1.0.0b4.r5', '', '2008-06-17 19:30:00', '1', '100', '4', 'Gmail', 'Michael Berkowitz', 'http://mail.google.com/', 
 'function detectWeb(doc, url) {
 	if (url.match(/#inbox\/[\w\d]+/)) {
