f196cc5253eb02363739b23c11b733b83fbdb292
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-07-14 18:21:01 +0000

    -Adds Revues.org translator.

--- scrapers.sql
+++ scrapers.sql
@@ -1264,6 +1264,64 @@ function doWeb(doc, url) {
 	Zotero.wait();
 }');
 
+REPLACE INTO translators VALUES ('87766765-919e-4d3b-9071-3dd7efe984c8', '1.0.0b4.r5', '', '2008-07-14 14:19:40', '0', '100', '4', 'Revues.org', 'Michael Berkowitz', 'http://.*\.revues\.org', 
+'function detectWeb(doc, url) {
+	if (doc.evaluate(''//div[@id="inside"]/div[@class="sommaire"]/dl[@class="documents"]/dd[@class="titre"]/a'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()
+		|| doc.evaluate(''//ul[@class="summary"]//div[@class="title"]/a'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		return "multiple";
+	} else if (doc.evaluate(''//h1[@id="docTitle"]/span[@class="text"]'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		return "journalArticle";
+	}
+}', 
+'function doWeb(doc, url) {
+	var arts = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		if (doc.evaluate(''//ul[@class="summary"]//div[@class="title"]/a'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+			var xpath = ''//ul[@class="summary"]//div[@class="title"]/a'';
+		} else if (doc.evaluate(''//div[@id="inside"]/div[@class="sommaire"]/dl[@class="documents"]/dd[@class="titre"]/a'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+			var xpath = ''//div[@id="inside"]/div[@class="sommaire"]/dl[@class="documents"]/dd[@class="titre"]/a'';
+		}
+		var titles = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+		var title;
+		var items = new Object();
+		while (title = titles.iterateNext()) {
+			items[title.href] = title.textContent;
+		}
+		items = Zotero.selectItems(items);
+		for (var i in items) {
+			arts.push(i);
+		}
+	} else {
+		arts = [url];
+	}
+  	Zotero.Utilities.processDocuments(arts, function(doc) {
+		var metas = doc.evaluate(''//meta'', doc, null, XPathResult.ANY_TYPE, null);
+		var meta;
+		var data = new Object();
+		while (meta = metas.iterateNext()) {
+			if (data[meta.name]) {
+				data[meta.name] = data[meta.name] + ";" + meta.content;
+			} else {
+				data[meta.name] = meta.content
+			}
+		}
+		var item = new Zotero.Item("journalArticle");
+		item.url = data[''url''];
+		var authors = data[''author''].split('';'');
+		for each (var aut in authors) {
+			if (aut.match(/\w+/)) item.creators.push(Zotero.Utilities.cleanAuthor(aut.replace(/(.*)\s([^\s]+)$/, "$2 $1"), "author"));
+		}
+		item.tags = data[''DC.subject''].split(/,\s+/);
+		item.date = data[''DC.date''];
+		item.title = data[''DC.title''];
+		item.publicationTitle = data[''DC.relation.isPartOf''].match(/^[^,]+/)[0];
+		item.abstractNote = data[''DC.description''];
+		
+		item.complete();
+	}, function() {Zotero.done;});
+	Zotero.wait();
+}');
+
 REPLACE INTO translators VALUES ('daa26181-71d4-48ef-8cac-54c06ff4c20e', '1.0.0b4.r5', '', '2008-07-07 23:40:00', '0', '100', '4', 'Citebase', 'Michael Berkowitz', 'http://(www.)?citebase.org/', 
 'function detectWeb(doc, url) {
 	if (url.match(/\/search/)) {
