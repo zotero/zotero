b2c51800c86e1f2de16edb1df384d4041c42a1e9
Dan Stillman <dstillman@zotero.org>
2008-07-25 17:49:14 +0000

    Fix unwrapped Artefacts Canada translator in scrapers.sql (not yet pushed)

--- scrapers.sql
+++ scrapers.sql
@@ -1538,8 +1538,30 @@ function doWeb(doc, url) {
 	Zotero.wait();
 }');
 
-// FIXME: missing replace line
-function associateData (newItem, dataTags, field, zoteroField) {
+
+REPLACE INTO translators VALUES ('661fc39a-2500-4710-8285-2d67ddc00a69', '1.0', '', '2008-07-24 09:20:44', '0', '100', '4', 'Artefacts Canada', 'Adam Crymble', 'http://daryl.chin.gc.ca', 
+'function detectWeb(doc, url) {
+	var multi1 = '''';
+	var single1 = '''';
+	
+	if (doc.evaluate(''//div[@id="mainContent"]/table/tbody/tr/td[1]/h1'', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		
+		multi1 = doc.evaluate(''//div[@id="mainContent"]/table/tbody/tr/td[1]/h1'', doc, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+	}
+	
+	var xpath = ''//tbody/tr[1]/td[2]/span'';
+	if (doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+		single1 = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+	}
+	
+	if (multi1.match("Search Results") || multi1.match("Résultats de recherche")) {
+		return "multiple";
+	} else if (single1.match("Document") || single1.match("Enregistrement")) {
+		return "artwork";
+	}
+	
+}', 
+'function associateData (newItem, dataTags, field, zoteroField) {
 	if (dataTags[field]) {
 		newItem[zoteroField] = dataTags[field];
 	}
@@ -1549,7 +1571,7 @@ function scrape(doc, url) {
 
 	var namespace = doc.documentElement.namespaceURI;
 	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
+		if (prefix == ''x'') return namespace; else return null;
 	} : null;	
 	
 	var dataTags = new Object();
@@ -1558,21 +1580,37 @@ function scrape(doc, url) {
 	
 	var newItem = new Zotero.Item("artwork");
 
-	var headers = doc.evaluate('//td[1][@class="leftResTitle"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
-	var contents = doc.evaluate('//td[2][@class="pageText"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var headers = doc.evaluate(''//td[1][@class="leftResTitle"]'', doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var contents = doc.evaluate(''//td[2][@class="pageText"]'', doc, nsResolver, XPathResult.ANY_TYPE, null);
 	
 	while (fieldTitle = headers.iterateNext()) {
-		fieldTitle = fieldTitle.textContent.replace(/\s+/g, '');
-		dataTags[fieldTitle] = contents.iterateNext().textContent.replace(/^\s*|\s*$/g, '')
+		fieldTitle = fieldTitle.textContent.replace(/\s+/g, '''');
+		if (fieldTitle == "Titre:") {
+			fieldTitle = "Title:";
+		} else if (fieldTitle == "Nomdel''objet:") {
+			fieldTitle = "NameofObject:";
+		} else if (fieldTitle == "Sujetouimage:") {
+			fieldTitle = "Subject/Image:";
+		} else if (fieldTitle == "Numérod''accession:") {
+			fieldTitle = "AccessionNumber:";
+		} else if (fieldTitle == "Artisteouartisan:") {
+			fieldTitle = "Artist/Maker:";
+		} else if (fieldTitle == "Fabricant:") {
+			fieldTitle = "Manufacturer:";
+		}
+		
+		dataTags[fieldTitle] = contents.iterateNext().textContent.replace(/^\s*|\s*$/g, '''')
 	}
+
+	Zotero.debug(dataTags);
 
 	if (dataTags["Artist/Maker:"]) {
 		if (dataTags["Artist/Maker:"].match(", ")) {
 			var authors = dataTags["Artist/Maker:"].split(", ");
-			authors = authors[0] + ' ' + authors[1];
-			newItem.creators.push(Zotero.Utilities.cleanAuthor(authors, "creator"));	
+			authors = authors[0] + '' '' + authors[1];
+			newItem.creators.push(authors, "creator");	
 		} else {
-			newItem.creators.push(Zotero.Utilities.cleanAuthor(dataTags["Artist/Make:"], "creator"));	
+			newItem.creators.push(dataTags["Artist/Make:"], "creator");	
 		}
 	}
 	
@@ -1605,9 +1643,15 @@ function scrape(doc, url) {
 	}
 	
 	associateData (newItem, dataTags, "LatestProductionDate:", "date");
+	associateData (newItem, dataTags, "Datedefindeproduction:", "date");
+	
 	associateData (newItem, dataTags, "Institution:", "repository");
+	associateData (newItem, dataTags, "Établissement:", "repository");
+	
 	associateData (newItem, dataTags, "Description:", "description");
+	
 	associateData (newItem, dataTags, "Medium:", "medium");
+	associateData (newItem, dataTags, "Médium:", "medium");
 	
 	newItem.url = doc.location.href;
 	newItem.complete();
@@ -1616,7 +1660,7 @@ function scrape(doc, url) {
 function doWeb(doc, url) {
 	var namespace = doc.documentElement.namespaceURI;
 	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
+		if (prefix == ''x'') return namespace; else return null;
 	} : null;
 	
 	var articles = new Array();
@@ -1624,8 +1668,8 @@ function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
 		var items = new Object();
 		
-		var titles = doc.evaluate('//tr[1]/td[2][@class="pageText"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var links = doc.evaluate('//td/a[@class="moreInfoink"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
+		var titles = doc.evaluate(''//tr[1]/td[2][@class="pageText"]'', doc, nsResolver, XPathResult.ANY_TYPE, null);
+		var links = doc.evaluate(''//td/a[@class="moreInfoink"]'', doc, nsResolver, XPathResult.ANY_TYPE, null);
 		
 		var next_title;
 		while (next_title = titles.iterateNext()) {
@@ -1642,6 +1686,9 @@ function doWeb(doc, url) {
 	Zotero.Utilities.processDocuments(articles, scrape, function() {Zotero.done();});
 	Zotero.wait();
 }
+');
+
+
 REPLACE INTO translators VALUES ('39ea814e-8fdb-486c-a88d-59479f341066', '1.0.0b4.r5', '', '2008-07-24 05:15:00', '0', '100', '4', 'Bibliotheque UQAM', 'Adam Crymble', 'http://www.manitou.uqam.ca', 
 'function detectWeb(doc, url) {
 	
