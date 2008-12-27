7bd1f4d51a2ac0b4e11eb3a91dde24816ccfcdd0
Dan Stillman <dstillman@zotero.org>
2008-12-27 20:49:59 +0000

    Pushed new PNAS translator

--- scrapers.sql
+++ scrapers.sql
@@ -22,7 +22,7 @@
 
 
 -- Set the following timestamp to the most recent scraper update date
-REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-12-22 19:50:00'));
+REPLACE INTO version VALUES ('repository', STRFTIME('%s', '2008-12-27 20:50:00'));
 
 REPLACE INTO translators VALUES ('96b9f483-c44d-5784-cdad-ce21b984fe01', '1.0.0b4.r1', '', '2008-08-22 20:30:00', '1', '100', '4', 'Amazon.com', 'Sean Takats and Michael Berkowitz', '^https?://(?:www\.)?amazon', 
 'function detectWeb(doc, url) { 
@@ -28027,6 +28027,84 @@ function doWeb(doc, url) {
 		
 	Zotero.wait();
 }');
+
+
+REPLACE INTO translators VALUES ('8c1f42d5-02fa-437b-b2b2-73afc768eb07', '1.0.0b4.r5', '', '2008-12-27 20:50:00', 1, 100, 4, 'PNAS', 'Matt Burton', 'http://www\.pnas\.org/(content|search|cgi/collection/.+)',
+'// This translator is for HighWire 2.0
+
+function detectWeb(doc, url) {
+	if (url.match("search") || url.match("content/by/section") || doc.title.match("Table of Contents") || doc.title.match("Early Edition") || url.match("cgi/collection/.+")) {
+		return "multiple";
+	} else if (url.match("content/[0-9]")) {
+		return "journalArticle";
+	}
+}',
+'function doWeb(doc, url) {
+	var arts = new Array();
+	if (detectWeb(doc, url) == "multiple") {
+		var items = new Object();
+		if (doc.title.match("Table of Contents") || doc.title.match("Early Edition")) {
+			var searchx = ''//li[@class = "cit toc-cit" and not(ancestor::div/h2/a/text() = "Correction" or ancestor::div/h2/a/text() = "Corrections")]''; 
+			var titlex = ''.//h4'';
+		} else if (url.match("content/by/section") || url.match("cgi/collection/.+")) {
+			var searchx = ''//li[contains(@class, "results-cit cit")]''; 
+			var titlex = ''.//span[@class = "cit-title"]'';
+		}
+		else {
+			var searchx = ''//div[@class = "results-cit cit"]'';
+			var titlex = ''.//span[@class = "cit-title"]'';
+		}	
+		var linkx = ''.//a[1]'';
+		var searchres = doc.evaluate(searchx, doc, null, XPathResult.ANY_TYPE, null);
+		var next_res;
+		while (next_res = searchres.iterateNext()) {
+			var title = doc.evaluate(titlex, next_res, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+			var link = doc.evaluate(linkx, next_res, null, XPathResult.ANY_TYPE, null).iterateNext().href;
+			items[link] = title;
+		}
+		items = Zotero.selectItems(items);
+		for (var i in items) {
+			arts.push(i);
+		} 
+	} else {
+		arts = [url];
+	}
+	var newurls = new Array();
+	for each (var i in arts) {
+		newurls.push(i);
+	}
+	Zotero.Utilities.HTTP.doGet(arts, function(text) {
+		var id = text.match(/=([^=]+)\">\s*Download to citation manager/)[1];
+		var newurl = newurls.shift();		
+		if (newurl.match("cgi/content")) {
+			var pdfurl = newurl.replace(/cgi\/content\/abstract/, "content") + ".full.pdf";
+		} else {
+			// This is not ideal...todo: brew a regex that grabs the correct URL
+			var pdfurl = newurl.slice(0, newurl.lastIndexOf(".")) + ".full.pdf";
+		}
+		var get = ''http://www.pnas.org/citmgr?type=refman&gca='' + id;
+		Zotero.Utilities.HTTP.doGet(get, function(text) {
+			var translator = Zotero.loadTranslator("import");
+			translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+			translator.setString(text);
+			if (text.match(/N1(.*)\n/)) {
+				var doi = text.match(/N1\s+\-\s+(.*)\n/)[1];
+			}
+			translator.setHandler("itemDone", function(obj, item) {
+				item.attachments = [
+					{url:newurl, title:"PNAS Snapshot", mimeType:"text/html"},
+					{url:pdfurl, title:"PNAS Full Text PDF", mimeType:"application/pdf"}
+				];
+				if (doi) item.DOI = doi;
+				if (item.notes) item.notes = [];
+				item.complete();
+			});
+			translator.translate();
+		});
+	});
+	Zotero.wait();
+}');
+
 
 REPLACE INTO translators VALUES ('a354331-981b-43de-a61-bc26dd1be3a9', '1.0.0b3.r1', '', '2007-03-24 22:20:00', '1', '100', '4', 'AMS MathSciNet', 'Simon Kornblith', '^https?://www\.ams\.org[^/]*/mathscinet/search/(?:publications\.html|publdoc\.html)', 
 'function detectWeb(doc, url) {
