8d41d23098f40cab783a1dcb639c84e220cf3935
Simon Kornblith <simon@simonster.com>
2009-04-04 07:10:29 +0000

    shouldn't actually need bibliography-Integration at all

--- chrome/content/zotero/editBibliographyDialog.js
+++ chrome/content/zotero/editBibliographyDialog.js
@@ -138,7 +138,6 @@ var Zotero_Bibliography_Dialog = new function () {
 		
 		if(_lastSelectedItem && editor.value != _originalBibEntry) {
 			Zotero.debug("setting bibliography for "+_lastSelectedItem.getID()+" to "+editor.value);
-			_lastSelectedItem.setProperty("bibliography-Integration", editor.value);
 			_lastSelectedItem.setProperty("bibliography-RTF", editor.value);
 		}
 		
