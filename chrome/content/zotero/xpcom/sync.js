7d936de70403223f12ee022ba5ae4b64ace0b939
Dan Stillman <dstillman@zotero.org>
2009-05-01 23:55:52 +0000

    Update auto-sync expiry on 1.5b2.1

--- chrome/content/zotero/xpcom/sync.js
+++ chrome/content/zotero/xpcom/sync.js
@@ -633,7 +633,7 @@ Zotero.Sync.Server = new function () {
 		}
 		
 		// Set auto-sync expiry
-		var expiry = new Date("May 1, 2009 12:00:00");
+		var expiry = new Date("May 15, 2009 12:00:00");
 		if (new Date() > expiry) {
 			Components.utils.reportError("Build has expired -- syncing disabled");
 			return false;
