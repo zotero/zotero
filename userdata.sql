e414c3624b7289c56f9fd3fcbf2797f97d83d3b6
Dan Stillman <dstillman@zotero.org>
2009-06-10 07:28:30 +0000

    Fix possible "syncDeleteLog not empty and no timestamp in Zotero.Sync.delete()" error after upgrading

--- userdata.sql
+++ userdata.sql
@@ -1,4 +1,4 @@
--- 57
+-- 58
 
 -- This file creates tables containing user-specific data for new users --
 -- any changes made here must be mirrored in transition steps in schema.js::_migrateSchema()
