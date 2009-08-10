19c6e59babd39e67f020a79dd7c1defb86b29c97
Dan Stillman <dstillman@zotero.org>
2009-08-10 00:41:04 +0000

    Add "since starting Firefox" to "The following errors have occurred" in error report window

--- chrome/content/zotero/overlay.js
+++ chrome/content/zotero/overlay.js
@@ -2965,7 +2965,7 @@ var ZoteroPane = new function()
 		var ww = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
 				   .getService(Components.interfaces.nsIWindowWatcher);
 		var data = {
-			msg: Zotero.getString('errorReport.followingErrors'),
+			msg: Zotero.getString('errorReport.followingErrors', Zotero.appName),
 			e: errors.join('\n\n'),
 			askForSteps: true
 		};
