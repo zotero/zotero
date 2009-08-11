47582971f69735d407d84798c1ed26a8527f956d
Dan Stillman <dstillman@zotero.org>
2009-08-11 05:38:19 +0000

    Backports to reflect item pane tab reality as known to 2.0b6.5

--- chrome/content/zotero/itemPane.js
+++ chrome/content/zotero/itemPane.js
@@ -66,12 +66,12 @@ var ZoteroItemPane = new function() {
 			case 0:
 				var box = _itemBox;
 				break;
-				
-			case 1:
+			
+			case 3:
 				var box = _tagsBox;
 				break;
 			
-			case 2:
+			case 4:
 				var box = _relatedBox;
 				break;
 		}
@@ -81,7 +81,7 @@ var ZoteroItemPane = new function() {
 		if (_lastItem && _lastItem != item) {
 			switch (index) {
 				case 0:
-				case 1:
+				case 3:
 					box.blurOpenField();
 					// DEBUG: Currently broken
 					//box.scrollToTop();
@@ -91,6 +91,13 @@ var ZoteroItemPane = new function() {
 		
 		_lastItem = item;
 		
+		switch (index) {
+			case 1:
+			case 2:
+				loadPane(index, mode ? mode : 'edit');
+				return;
+		}
+		
 		if (mode) {
 			box.mode = mode;
 		}
@@ -98,6 +105,112 @@ var ZoteroItemPane = new function() {
 			box.mode = 'edit';
 		}
 		box.item = item;
+	}
+	
+	
+	function loadPane(index, mode) {
+		// Notes pane
+		if(index == 1)
+		{
+			while(_notesList.hasChildNodes())
+				_notesList.removeChild(_notesList.firstChild);
+				
+			var notes = Zotero.Items.get(_lastItem.getNotes());
+			if(notes.length)
+			{
+				for(var i = 0; i < notes.length; i++)
+				{
+					var icon = document.createElement('image');
+					icon.setAttribute('src','chrome://zotero/skin/treeitem-note.png');
+				
+					var label = document.createElement('label');
+					var title = Zotero.Notes.noteToTitle(notes[i].getNote());
+					title = title ? title : Zotero.getString('pane.item.notes.untitled');
+					label.setAttribute('value', title);
+					label.setAttribute('flex','1');	//so that the long names will flex smaller
+					label.setAttribute('crop','end');
+				
+					var box = document.createElement('box');
+					box.setAttribute('onclick',"ZoteroPane.selectItem(" + notes[i].id + ");");
+					box.setAttribute('class','zotero-clicky');
+					box.appendChild(icon);
+					box.appendChild(label);
+				
+					var removeButton = document.createElement('label');
+					removeButton.setAttribute("value","-");
+					removeButton.setAttribute("class","zotero-clicky");
+					removeButton.setAttribute("onclick","ZoteroItemPane.removeNote(" + notes[i].id + ")");
+				
+					var row = document.createElement('row');
+					row.appendChild(box);
+					row.appendChild(removeButton);
+				
+					_notesList.appendChild(row);
+				}
+			}
+		
+			_updateNoteCount();
+		}
+		
+		// Attachments pane
+		else if(index == 2)
+		{
+			while(_attachmentsList.hasChildNodes())
+				_attachmentsList.removeChild(_attachmentsList.firstChild);
+				
+			var attachments = Zotero.Items.get(_lastItem.getAttachments());
+			if(attachments.length)
+			{
+				for(var i = 0; i < attachments.length; i++)
+				{
+					var icon = document.createElement('image');
+					var linkMode = attachments[i].getAttachmentLinkMode();
+					var itemType = '';
+					if(linkMode == Zotero.Attachments.LINK_MODE_IMPORTED_FILE)
+					{
+						itemType = "-file";
+					}
+					else if(linkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE)
+					{
+						itemType = "-link";
+					}
+					else if(linkMode == Zotero.Attachments.LINK_MODE_IMPORTED_URL)
+					{
+						itemType = "-snapshot";
+					}
+					else if(linkMode == Zotero.Attachments.LINK_MODE_LINKED_URL)
+					{
+						itemType = "-web-link";
+					}
+					icon.setAttribute('src','chrome://zotero/skin/treeitem-file'+itemType+'.png');
+				
+					var label = document.createElement('label');
+					label.setAttribute('value',attachments[i].getField('title'));
+					label.setAttribute('flex','1');	//so that the long names will flex smaller
+					label.setAttribute('crop','end');
+				
+					var box = document.createElement('box');
+					box.setAttribute('onclick',"ZoteroPane.selectItem('" + attachments[i].id + "')");
+					box.setAttribute('class','zotero-clicky');
+					box.appendChild(icon);
+					box.appendChild(label);
+				
+					var removeButton = document.createElement('label');
+					removeButton.setAttribute("value","-");
+					removeButton.setAttribute("class","zotero-clicky");
+					removeButton.setAttribute("onclick","ZoteroItemPane.removeAttachment(" + attachments[i].id + ")");
+				
+					var row = document.createElement('row');
+					row.appendChild(box);
+					row.appendChild(removeButton);
+				
+					_attachmentsList.appendChild(row);
+				}
+			}
+		
+			_updateAttachmentCount();
+			
+		}
 	}
 	
 	
@@ -111,7 +224,7 @@ var ZoteroItemPane = new function() {
 	
 	function addNote()
 	{
-		ZoteroPane.openNoteWindow(null, null, _itemBeingEdited.id);
+		ZoteroPane.openNoteWindow(null, null, _lastItem.id);
 	}
 	
 	function _updateNoteCount()
@@ -164,12 +277,12 @@ var ZoteroItemPane = new function() {
 	
 	function addAttachmentFromDialog(link)
 	{
-		ZoteroPane.addAttachmentFromDialog(link, _itemBeingEdited.id);
+		ZoteroPane.addAttachmentFromDialog(link, _lastItem.id);
 	}
 	
 	function addAttachmentFromPage(link)
 	{
-		ZoteroPane.addAttachmentFromPage(link, _itemBeingEdited.id);
+		ZoteroPane.addAttachmentFromPage(link, _lastItem.id);
 	}
 }
 
