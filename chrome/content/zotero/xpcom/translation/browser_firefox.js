b3fc63653f047431e1383b4f615e59a3fe5c35b0
Dan Stillman <dstillman@zotero.org>
2011-03-17 03:49:05 +0000

    Remove stray character

--- chrome/content/zotero/xpcom/translation/browser_firefox.js
+++ chrome/content/zotero/xpcom/translation/browser_firefox.js
@@ -13,7 +13,7 @@
     (at your option) any later version.
     
     Zotero is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty ofc
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     
