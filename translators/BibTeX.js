050a845546162fdbf1c14409bc5c2d3a4201654f
Dan Stillman <dstillman@zotero.org>
2011-04-01 21:19:08 +0000

    Pushed BibTeX again

--- translators/BibTeX.js
+++ translators/BibTeX.js
@@ -10,7 +10,7 @@
 	"configOptions":{"dataMode":"block"},
 	"displayOptions":{"exportCharset":"UTF-8", "exportFileData":false},
 	"inRepository":true,
-	"lastUpdated":"2011-04-01 20:50:00"
+	"lastUpdated":"2011-04-01 21:20:00"
 }
 
 function detectImport() {
