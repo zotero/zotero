c6abc0f77c99365d34003c131461e1ac0a2278af
Dan Stillman <dstillman@zotero.org>
2011-05-18 18:34:22 +0000

    Switch client code license to Affero GPL version 3

--- chrome/content/zotero/webpagedump/common.js
+++ chrome/content/zotero/webpagedump/common.js
@@ -21,7 +21,7 @@
  *   Bernhard Pollak <pollak@dbai.tuwien.ac.at> (WebPageDump Fork)
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * either the GNU Affero General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
