77c66d5612ec7e00fbe5ac1fb10385cee76756ce
Avram Lyon <ajlyon@gmail.com>
2011-05-23 21:54:14 +0000

    Adding new translators using Erik's framework in a minified form; translators by Sebastian Karcher and Avram Lyon; framework by Erik Hetzner

--- translators/Kommersant.js
+++ translators/Kommersant.js
@@ -1,66 +1,101 @@
 {
-        "translatorID": "4c164cc8-be7b-4d02-bfbf-37a5622dfd56",
-        "label": "The New York Review of Books",
-        "creator": "Simon Kornblith, Avram Lyon",
-        "target": "^https?://www\\.nybooks\\.com/",
-        "minVersion": "1.0.0b3.r1",
+        "translatorID": "43bc17ed-e994-4fdb-ac28-594c839658ca",
+        "label": "Kommersant",
+        "creator": "Avram Lyon",
+        "target": "^https?:\\/\\/kommersant\\.ru\\/",
+        "minVersion": "2.1",
         "maxVersion": "",
         "priority": 100,
         "inRepository": true,
         "translatorType": 4,
-        "lastUpdated": "2011-05-24 01:17:10"
+        "lastUpdated": "2011-05-24 01:01:53"
 }
 
 /* FW LINE 43:f6588e951d9d */ function flatten(c){var b=new Array();for(var d in c){var e=c[d];if(e instanceof Array){b=b.concat(flatten(e))}else{b.push(e)}}return b}var FW={_scrapers:new Array()};FW._Base=function(){this.callHook=function(b,c,e,a){if(typeof this["hooks"]==="object"){var d=this["hooks"][b];if(typeof d==="function"){d(c,e,a)}}};this.evaluateThing=function(f,e,c){var b=typeof f;if(b==="string"){return f}else{if(b==="object"){if(f instanceof Array){var d=this.evaluateThing;var a=f.map(function(g){return d(g,e,c)});return flatten(a)}else{return f.evaluate(e,c)}}else{if(b==="function"){return f(e,c)}else{return undefined}}}}};FW.Scraper=function(a){FW._scrapers.push(new FW._Scraper(a))};FW._Scraper=function(a){for(x in a){this[x]=a[x]}this._singleFieldNames=["abstractNote","applicationNumber","archive","archiveLocation","artworkMedium","artworkSize","assignee","audioFileType","audioRecordingType","billNumber","blogTitle","bookTitle","callNumber","caseName","code","codeNumber","codePages","codeVolume","committee","company","conferenceName","country","court","date","dateDecided","dateEnacted","dictionaryTitle","distributor","docketNumber","documentNumber","DOI","edition","encyclopediaTitle","episodeNumber","extra","filingDate","firstPage","forumTitle","genre","history","institution","interviewMedium","ISBN","ISSN","issue","issueDate","issuingAuthority","journalAbbreviation","label","language","legalStatus","legislativeBody","letterType","libraryCatalog","manuscriptType","mapType","medium","meetingName","nameOfAct","network","number","numberOfVolumes","numPages","pages","patentNumber","place","postType","presentationType","priorityNumbers","proceedingsTitle","programTitle","programmingLanguage","publicLawNumber","publicationTitle","publisher","references","reportNumber","reportType","reporter","reporterVolume","rights","runningTime","scale","section","series","seriesNumber","seriesText","seriesTitle","session","shortTitle","studio","subject","system","thesisType","title","type","university","url","version","videoRecordingType","volume","websiteTitle","websiteType"];this._makeAttachments=function(q,b,f,s){if(f instanceof Array){f.forEach(function(k){this._makeAttachments(q,b,k,s)},this)}else{if(typeof f==="object"){var p=f.urls||f.url;var m=f.types||f.type;var e=f.titles||f.title;var h=this.evaluateThing(p,q,b);var o=this.evaluateThing(e,q,b);var r=this.evaluateThing(m,q,b);var l=(r instanceof Array);var n=(o instanceof Array);if(!(h instanceof Array)){h=[h]}for(var j in h){var c=h[j];var g;var d;if(l){g=r[j]}else{g=r}if(n){d=o[j]}else{d=o}s.attachments.push({url:c,title:d,type:g})}}}};this.makeItems=function(o,b,m,c,l){var q=new Zotero.Item(this.itemType);q.url=b;for(var h in this._singleFieldNames){var n=this._singleFieldNames[h];if(this[n]){var g=this.evaluateThing(this[n],o,b);if(g instanceof Array){q[n]=g[0]}else{q[n]=g}}}var r=["creators","tags"];for(var f in r){var p=r[f];var d=this.evaluateThing(this[p],o,b);if(d){for(var e in d){q[p].push(d[e])}}}this._makeAttachments(o,b,this["attachments"],q);c(q);l([q])}};FW._Scraper.prototype=new FW._Base;FW.MultiScraper=function(a){FW._scrapers.push(new FW._MultiScraper(a))};FW._MultiScraper=function(a){for(x in a){this[x]=a[x]}this._mkSelectItems=function(e,d){var b=new Object;for(var c in e){b[d[c]]=e[c]}return b};this._selectItems=function(e,d){var b=new Array();for(var c in Zotero.selectItems(this._mkSelectItems(e,d))){b.push(c)}return b};this._mkAttachments=function(g,d,f){var b=this.evaluateThing(this["attachments"],g,d);var c=new Object();if(b){for(var e in f){c[f[e]]=b[e]}}return c};this._makeChoices=function(f,p,c,d,h){if(f instanceof Array){f.forEach(function(k){this._makeTitlesUrls(k,p,c,d,h)},this)}else{if(typeof f==="object"){var m=f.urls||f.url;var e=f.titles||f.title;var n=this.evaluateThing(m,p,c);var j=this.evaluateThing(e,p,c);var l=(j instanceof Array);if(!(n instanceof Array)){n=[n]}for(var g in n){var b=n[g];var o;if(l){o=j[g]}else{o=j}h.push(b);d.push(o)}}}};this.makeItems=function(m,b,k,c,h){Zotero.debug("Entering MultiScraper.makeItems");if(this.beforeFilter){var n=this.beforeFilter(m,b);if(n!=b){this.makeItems(m,n,k,c,h);return}}var g=[];var l=[];this._makeChoices(this["choices"],m,b,g,l);var f=this._selectItems(g,l);var d=this._mkAttachments(m,b,l);if(!f){h([])}else{var j=[];var e=this.itemTrans;Zotero.Utilities.processDocuments(f,function(q){var p=q.documentURI;var o=e;if(o===undefined){o=FW.getScraper(q,p)}if(o===undefined){}else{o.makeItems(q,p,d[p],function(r){j.push(r);c(r)},function(){})}},function(){h(j)})}}};FW._MultiScraper.prototype=new FW._Base;FW.DelegateTranslator=function(a){return new FW._DelegateTranslator(a)};FW._DelegateTranslator=function(a){for(x in a){this[x]=a[x]}this._translator=Zotero.loadTranslator(this.translatorType);this._translator.setTranslator(this.translatorId);this.makeItems=function(g,d,b,f,c){Zotero.debug("Entering DelegateTranslator.makeItems");var e;Zotero.Utilities.HTTP.doGet(d,function(h){this._translator.setHandler("itemDone",function(k,j){e=j;if(b){j.attachments=b}});this._translator.setString(h);this._translator.translate();f(e)},function(){c([e])})}};FW.DelegateTranslator.prototype=new FW._Scraper;FW._StringMagic=function(){this._filters=new Array();this.addFilter=function(a){this._filters.push(a);return this};this.split=function(a){return this.addFilter(function(b){return b.split(a).filter(function(c){return(c!="")})})};this.replace=function(c,b,a){return this.addFilter(function(d){if(d.match(c)){return d.replace(c,b,a)}else{return d}})};this.prepend=function(a){return this.replace(/^/,a)};this.append=function(a){return this.replace(/$/,a)};this.remove=function(b,a){return this.replace(b,"",a)};this.trim=function(){return this.addFilter(function(a){return Zotero.Utilities.trim(a)})};this.trimInternal=function(){return this.addFilter(function(a){return Zotero.Utilities.trimInternal(a)})};this.match=function(a,b){if(!b){b=0}return this.addFilter(function(d){var c=d.match(a);if(c===undefined||c===null){return undefined}else{return c[b]}})};this.cleanAuthor=function(b,a){return this.addFilter(function(c){return Zotero.Utilities.cleanAuthor(c,b,a)})};this.key=function(a){return this.addFilter(function(b){return b[a]})};this.capitalizeTitle=function(){return this.addFilter(function(a){return Zotero.Utilities.capitalizeTitle(a)})};this.unescapeHTML=function(){return this.addFilter(function(a){return Zotero.Utilities.unescapeHTML(a)})};this.unescape=function(){return this.addFilter(function(a){return unescape(a)})};this._applyFilters=function(c,e){for(i in this._filters){c=flatten(c);c=c.filter(function(a){return((a!==undefined)&&(a!==null))});for(var d=0;d<c.length;d++){try{if((c[d]===undefined)||(c[d]===null)){continue}else{c[d]=this._filters[i](c[d],e)}}catch(b){c[d]=undefined;Zotero.debug("Caught exception "+b+"on filter: "+this._filters[i])}}c=c.filter(function(a){return((a!==undefined)&&(a!==null))})}return c}};FW.PageText=function(){return new FW._PageText()};FW._PageText=function(){this._filters=new Array();this.evaluate=function(c){var b=[c.documentElement.innerHTML];b=this._applyFilters(b,c);if(b.length==0){return false}else{return b}}};FW._PageText.prototype=new FW._StringMagic();FW.Url=function(){return new FW._Url()};FW._Url=function(){this._filters=new Array();this.evaluate=function(d,c){var b=[c];b=this._applyFilters(b,d);if(b.length==0){return false}else{return b}}};FW._Url.prototype=new FW._StringMagic();FW.Xpath=function(a){return new FW._Xpath(a)};FW._Xpath=function(a){this._xpath=a;this._filters=new Array();this.text=function(){var b=function(c){if(typeof c==="object"&&c.textContent){return c.textContent}else{return c}};this.addFilter(b);return this};this.sub=function(b){var c=function(f,e){var d=e.evaluate(b,f,null,XPathResult.ANY_TYPE,null);if(d){return d.iterateNext()}else{return undefined}};this.addFilter(c);return this};this.evaluate=function(e){var d=e.evaluate(this._xpath,e,null,XPathResult.ANY_TYPE,null);var c=new Array();var b;while(b=d.iterateNext()){c.push(b)}c=this._applyFilters(c,e);if(c.length==0){return false}else{return c}}};FW._Xpath.prototype=new FW._StringMagic();FW.detectWeb=function(e,b){for(var c in FW._scrapers){var d=FW._scrapers[c];var f=d.evaluateThing(d.itemType,e,b);if(!d.detect){return f}else{var a=d.evaluateThing(d.detect,e,b);if(a.length>0&&a[0]){return f}}}return undefined};FW.getScraper=function(b,a){var c=FW.detectWeb(b,a);return FW._scrapers.filter(function(d){return(d.evaluateThing(d.itemType,b,a)==c)&&(d.evaluateThing(d.detect,b,a))})[0]};FW.doWeb=function(c,a){Zotero.debug("Entering FW.doWeb");var b=FW.getScraper(c,a);b.makeItems(c,a,[],function(d){b.callHook("scraperDone",d,c,a);if(!d.title){d.title=""}d.complete()},function(){Zotero.done()});Zotero.wait();Zotero.debug("Leaving FW.doWeb")};
-function detectWeb(doc, url) { return FW.detectWeb(doc, url); }
+/*
+   Kommersant Translator
+   Copyright (C) 2011 Avram Lyon, ajlyon@gmail.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+function detectWeb(doc, url) { 
+		return FW.detectWeb(doc, url);
+}
 function doWeb(doc, url) { return FW.doWeb(doc, url); }
- 
+
+
 /** Articles */
-FW.Scraper({
-itemType         : 'newspaperArticle',
-detect           : FW.Url().match(/\/articles\/archives\//),
-title            : FW.Xpath('//div[@id="page-title"]/h2').text().trim(), 
-creators         : FW.Xpath('//div[@id="page-title"]/h3').text().cleanAuthor("author"), 
-url		 : FW.Url().remove(/\?.*/),
-attachments      : [
-		{ url : FW.Url().remove(/\?.*/).append("?pagination=false"),
-		  type : "text/html",
-		  title : "NYRB Snapshot"
-		  }], 
-date             : FW.Xpath('//div[@id="page-title"]/h5').text(), 
-abstractNote     : FW.Xpath('//div[contains(@class, "article-reviewed-items")]').text().trimInternal().prepend("Review of: "),
-publicationTitle : "The New York Review of Books",
-ISSN             : "0028-7504"
+FW.Scraper({ itemType         : 'magazineArticle',
+             detect           : FW.Xpath('//div[@class="issue"]/a[contains(text(),"Журнал")]'),
+             title            : FW.Xpath('//div[@class="document"]/div[@class="title"]').text().trim(),
+             attachments      : [
+             { 
+	             url : FW.Url(),
+	             type : "text/html",
+	             title : "Kommersant Snapshot"
+	       } ],
+             creators         : FW.Xpath('//div[contains(@class, "document_authors")][preceding-sibling::div[@class="document_vvodka"]]').text().split(",").cleanAuthor("author"),
+             date             : FW.Xpath('//div[@class="issue"]/a').text().replace(/.*(\d{2})\.(\d{2})\.(\d{4})\s*$/,"$3-$2-$1"),
+             issue            : FW.Xpath('//div[@class="issue"]/a').text().match(/№\s*(\d+(?: \(\d+\))?)/,1),
+             abstractNote     : FW.Xpath('//div[@class="document_vvodka"]').text().trimInternal(),
+             publicationTitle : FW.Xpath('//div[@class="issue"]/a').text().match(/^\s*Журнал\s+"([^"]+)"/,1)
 });
 
-/** Blog Posts */
-FW.Scraper({
-itemType         : 'blogPost',
-detect           : FW.Url().match(/\/blogs\//),
-title            : FW.Xpath('//div[@id="article-body"]/h2').text().trim(), 
-creators         : FW.Xpath('//div[@id="article-body"]/h3').text().cleanAuthor("author"), 
-url		 : FW.Url().remove(/\?.*/),
-attachments      : [
-		{ url : FW.Url().remove(/\?.*/).append("?pagination=false"),
-		  type : "text/html",
-		  title : "NYRB Snapshot"
-		  }], 
-date             : FW.Xpath('//div[@id="article-body"]//em[last()]').text(), 
-blogTitle        : "NYRblog"
+/** News Articles */
+FW.Scraper({ itemType         : 'newspaperArticle',
+             detect           : FW.Xpath('//div[@class="issue"]/a[contains(text(),"Газета") or contains(text(),"Online")]'),
+             title            : FW.Xpath('//div[@class="document"]/div[@class="title"]').text().trim(),
+             attachments      : [
+             { 
+	             url : FW.Url(),
+	             type : "text/html",
+	             title : "Kommersant Snapshot"
+	       } ],
+	      creators         : FW.Xpath('//div[contains(@class, "document_authors")][preceding-sibling::div[@class="document_vvodka"]]')
+             					.text().replace(/\s?отдел\s[^,]*,?/gi,"")
+             					.split(",").trim().cleanAuthor("author"),
+             date             : FW.Xpath('//div[@class="issue"]/a').text().replace(/.*(\d{2})\.(\d{2})\.(\d{4})\s*(?:\/\/\s\d+:\d+)?$/,"$3-$2-$1"),
+             issue            : FW.Xpath('//div[@class="issue"]/a').text().match(/№\s*(\d+(?: \(\d+\))?)/,1),
+             abstractNote     : FW.Xpath('//div[@class="document_vvodka"]').text().trimInternal(),
+             publicationTitle : FW.Xpath('//div[@class="issue"]/a').text().match(/^\s*"([^"]+)"/,1)
 });
- 
+
+/** News Articles - second format */
+FW.Scraper({ itemType         : 'newspaperArticle',
+             detect           : FW.Url().match(/\/News\//),
+             creators         : FW.Xpath('//div[contains(@class, "document_authors")][preceding-sibling::div[@class="title"]]')
+             					.text().replace(/\s?отдел\s[^,]*,?/gi,"")
+             					.split(",").trim().cleanAuthor("author"),
+             publicationTitle : "Коммерсантъ",
+             title            : FW.Xpath('//table[contains(@class,"news_block")]//div[@class="title"]').text().trim(),
+             attachments      : [
+             { 
+	             url : FW.Url(),
+	             type : "text/html",
+	             title : "Kommersant Snapshot"
+	       } ],
+             date             : FW.Xpath('//div[contains(@class,"other_blockname")]/div[@class="f-h"]').text().replace(/.*(\d{2})\.(\d{2})\.(\d{4}).*$/,"$3-$2-$1")
+ });
+
 /** Search results */
-FW.MultiScraper({
-itemType        : "multiple",
-detect          :  FW.Url().match(/\/search\/\?(.*)q=./),
-choices : { titles          : FW.Xpath('//table[@class="result-table"]/tbody/tr/td[2]/h3/a').text(),
-urls            : FW.Xpath('//table[@class="result-table"]/tbody/tr/td[2]/h3/a').key('href').text() }
-});
-
-/** Issue ToC */
-FW.MultiScraper({
-itemType        : "multiple",
-detect          :  FW.Url().match(/\/issues\//),
-choices : { titles          : FW.Xpath('//ul[@class="issue-article-list"]/li/h3/a').text(),
-urls            : FW.Xpath('//ul[@class="issue-article-list"]/li/h3/a').key('href').text() }
+FW.MultiScraper({ itemType  : "multiple",
+                  detect    : FW.Xpath('//table[contains(@class,"searchresults")]'),
+                  choices : {
+	                titles    : FW.Xpath('//table[contains(@class,"searchresults")]/tbody/tr/td/a').text(),
+                  	urls      : FW.Xpath('//table[contains(@class,"searchresults")]/tbody/tr/td/a').key('href').text()         
+                  }
 });
