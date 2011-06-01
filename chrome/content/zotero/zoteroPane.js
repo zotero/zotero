bb5a12396b8432de0e776c41159301f5f03d4a59
Simon Kornblith <simon@simonster.com>
2011-06-01 16:25:13 +0000

    merge r9398-r9401, r9405 to 2.1 branch

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -349,6 +349,7 @@ var ZoteroPane = new function()
 		}
 		
 		this.unserializePersist();
+		this.updateToolbarPosition();
 		this.updateTagSelectorSize();
 		
 		// restore saved row selection (for tab switching)
@@ -3606,6 +3607,7 @@ var ZoteroPane = new function()
 	 * Moves around the toolbar when the user moves around the pane
 	 */
 	this.updateToolbarPosition = function() {
+		if(document.getElementById("zotero-pane-stack").hidden) return;
 		const PANES = ["collections", "items"];
 		for each(var paneName in PANES) {
 			var pane = document.getElementById("zotero-"+paneName+"-pane");
