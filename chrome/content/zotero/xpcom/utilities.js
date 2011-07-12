347c86b85a4708880e128d83d6bc3c5342549cc8
Dan Stillman <dstillman@zotero.org>
2011-07-12 16:40:23 +0000

    Proper ellipsis in Zotero.Utilities.ellipsize()

--- chrome/content/zotero/xpcom/utilities.js
+++ chrome/content/zotero/xpcom/utilities.js
@@ -462,7 +462,7 @@ Zotero.Utilities = {
 			throw ("Length not specified in Zotero.Utilities.ellipsize()");
 		}
 		if (str.length > len) {
-			return str.substr(0, len) + '...' + (countChars ? ' (' + str.length + ' chars)' : '');
+			return str.substr(0, len) + '…' + (countChars ? ' (' + str.length + ' chars)' : '');
 		}
 		return str;
 	},
