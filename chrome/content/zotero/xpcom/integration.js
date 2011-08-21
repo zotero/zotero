e32da0b5ca422f68da55ed9c7cf1ea789373cfc8
Simon Kornblith <simon@simonster.com>
2011-08-21 21:37:18 +0000

    Remove debug line

--- chrome/content/zotero/xpcom/integration.js
+++ chrome/content/zotero/xpcom/integration.js
@@ -1262,7 +1262,6 @@ Zotero.Integration.Session.prototype.reselectItem = function(exception) {
  * Generates a field from a citation object
  */
 Zotero.Integration.Session.prototype.getCitationField = function(citation) {
-	Zotero.debug(citation);
 	const saveProperties = ["custom", "unsorted", "formattedCitation", "plainCitation", "dontUpdate"];
 	const saveCitationItemKeys = ["locator", "label", "suppress-author", "author-only", "prefix",
 		"suffix"];
