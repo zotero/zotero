2175700312703e3d32052f6c004b81dc97e7e08d
Simon Kornblith <simon@simonster.com>
2011-09-07 04:05:17 +0000

    Fix duplicates merging, broken by r10414

--- chrome/content/zotero/duplicatesMerge.js
+++ chrome/content/zotero/duplicatesMerge.js
@@ -147,6 +147,7 @@ var Zotero_Duplicates_Pane = new function () {
 	
 	this.merge = function () {
 		var itembox = document.getElementById('zotero-duplicates-merge-item-box');
+		Zotero.ItemGroupCache.clear();
 		Zotero.Items.merge(itembox.item, _otherItems);
 	}
 }
