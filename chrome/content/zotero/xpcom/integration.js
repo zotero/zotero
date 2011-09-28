77f27ff3d3fcb93ca3fc631dc18101b30e5cab2f
Simon Kornblith <simon@simonster.com>
2011-09-28 21:48:04 +0000

    Don't break integration if setting document preferences on a document on which set document preferences has already been run, but which has no fields. (Thanks to Frank for spotting this.)

--- chrome/content/zotero/xpcom/integration.js
+++ chrome/content/zotero/xpcom/integration.js
@@ -767,6 +767,8 @@ Zotero.Integration.Document.prototype.setDocPrefs = function() {
 									Zotero.Integration.complete(me._doc);
 								});
 							});
+						} else {
+							Zotero.Integration.complete(me._doc);
 						}
 					});
 				} else {
@@ -1193,6 +1195,7 @@ Zotero.Integration.Fields.prototype._updateDocument = function(forceCitations, f
 					// For ReferenceMarks with formatting, we need to set the text again, because
 					// setting the field code removes formatting from the mark. I don't like this.
 					field.setText(formattedCitation, isRich);
+					Zotero.debug("Setting text again");
 				}
 			}
 			nUpdated++;
