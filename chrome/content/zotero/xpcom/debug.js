zotero/zotero

commit da09a8dfcd53079153aa5bfaa5c8bfda953034d4

Fix debug output of thrown errors
At some point this switched to showing just the line on which the error
occurred.

chrome/content/zotero/xpcom/debug.js
@@ -46,7 +46,23 @@ Zotero.Debug = new function () {
 			return;
 		}
 		
-		if (typeof message != 'string') {
+		// Properly display thrown Error objects
+		if (message && message.constructor) {
+			switch (message.constructor.name) {
+				case 'Error':
+				case 'EvalError':
+				case 'RangeError':
+				case 'ReferenceError':
+				case 'SyntaxError':
+				case 'TypeError':
+				case 'URIError':
+					message = "'message' => \"" + message.message + "\"\n"
+								+ Zotero.Utilities.varDump(message) + "\n"
+								+ message.stack;
+					break;
+			}
+		}
+		else if (typeof message != 'string') {
 			message = Zotero.Utilities.varDump(message);
 		}
 		

commit 39fc50913c2df07b06595e8ab38a934b1dca1ae5

Fix debug output of objects, which I broke in da09a8df

chrome/content/zotero/xpcom/debug.js
@@ -62,7 +62,8 @@ Zotero.Debug = new function () {
 					break;
 			}
 		}
-		else if (typeof message != 'string') {
+		
+		if (typeof message != 'string') {
 			message = Zotero.Utilities.varDump(message);
 		}
 		