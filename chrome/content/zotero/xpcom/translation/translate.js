603e07d21af1f93268f22d0fd0f0e44af3b54ee7
Simon Kornblith <simon@simonster.com>
2012-03-12 17:30:09 -0400

    Translators should always run in browser when called by a parent translator

--- chrome/content/zotero/xpcom/translation/translate.js
+++ chrome/content/zotero/xpcom/translation/translate.js
@@ -1461,7 +1461,8 @@ Zotero.Translate.Web.prototype.translate = function(libraryID, saveAttachments, 
  * Overload _translateTranslatorLoaded to send an RPC call if necessary
  */
 Zotero.Translate.Web.prototype._translateTranslatorLoaded = function() {
-	if(this.translator[0].runMode === Zotero.Translator.RUN_MODE_IN_BROWSER) {
+	if(this.translator[0].runMode === Zotero.Translator.RUN_MODE_IN_BROWSER
+			|| this._parentTranslator) {
 		// begin process to run translator in browser
 		Zotero.Translate.Base.prototype._translateTranslatorLoaded.apply(this);
 	} else {
