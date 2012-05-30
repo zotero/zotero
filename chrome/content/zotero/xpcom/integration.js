zotero/zotero

commit 26f5765c294f5f9cdae2a9e3ad8921ed442691ad

Don't show exceptions multiple times

chrome/content/zotero/xpcom/integration.js
@@ -657,7 +657,7 @@ Zotero.Integration = new function() {
 					
 					if(!message && typeof(e) == "object") message = "\n\n"+e.toString();
 					
-					if(message != "\n\nExceptionAlreadyDisplayed") {
+					if(message.indexOf("ExceptionAlreadyDisplayed") !== -1) {
 						displayError = Zotero.getString("integration.error.generic")+message;
 					}
 					Zotero.debug(e);

commit a549b9a40719e68eedce64f81178a0f729fc20c9

Fix integration error reporting, broken by 26f5765c294f5f9cdae2a9e3ad8921ed442691ad

chrome/content/zotero/xpcom/integration.js
@@ -657,7 +657,7 @@ Zotero.Integration = new function() {
 					
 					if(!message && typeof(e) == "object") message = "\n\n"+e.toString();
 					
-					if(message.indexOf("ExceptionAlreadyDisplayed") !== -1) {
+					if(message.indexOf("ExceptionAlreadyDisplayed") === -1) {
 						displayError = Zotero.getString("integration.error.generic")+message;
 					}
 					Zotero.debug(e);