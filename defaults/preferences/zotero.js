6c65c961e07f9ad843252c93b883c7dbf51f9a6f
Frank Bennett <biercenator@gmail.com>
2012-06-04 21:33:04 +0900

    MLZ changes to preferences (much reorganization, and might well be stale stuff in there)

--- defaults/preferences/zotero.js
+++ defaults/preferences/zotero.js
@@ -3,6 +3,9 @@
 // Add new user-adjustable hidden preferences to
 // http://www.zotero.org/documentation/hidden_prefs
 
+// Hack
+pref("extensions.zotero.hackUseCiteprocJsDateParser", false);
+
 pref("extensions.zotero.firstRun2", true);
 pref("extensions.zotero@chnm.gmu.edu.description", "chrome://zotero/locale/zotero.properties");
 
@@ -10,13 +13,14 @@ pref("extensions.zotero.useDataDir", false);
 pref("extensions.zotero.dataDir", '');
 pref("extensions.zotero.lastDataDir", '');
 pref("extensions.zotero.debug.log",false);
-pref("extensions.zotero.debug.stackTrace", false);
+pref("extensions.zotero.debug.stackTrace",false);
 pref("extensions.zotero.debug.store",false);
 pref("extensions.zotero.debug.store.limit",500000);
 pref("extensions.zotero.debug.store.submitSize",10000000);
 pref("extensions.zotero.debug.store.submitLineLength",10000);
 pref("extensions.zotero.debug.level",5);
 pref("extensions.zotero.debug.time", false);
+pref("extensions.zotero.debugShowDuplicates", true);
 pref("extensions.zotero.automaticScraperUpdates",true);
 pref("extensions.zotero.zoteroDotOrgVersionHeader", true);
 pref("extensions.zotero.cacheTranslatorData",true);
@@ -104,10 +108,21 @@ pref("extensions.zotero.export.quickCopy.quoteBlockquotes.richText", true);
 pref("extensions.zotero.export.quickCopy.compatibility.indentBlockquotes", true);
 pref("extensions.zotero.export.quickCopy.compatibility.word", false);
 
+// Language settings
+pref("extensions.zotero.csl.locale", 'en-US');
+pref("extensions.zotero.csl.citationPersons", "orig");
+pref("extensions.zotero.csl.citationInstitutions", "orig");
+pref("extensions.zotero.csl.citationTitles", "orig");
+pref("extensions.zotero.csl.citationPublishers", "orig");
+pref("extensions.zotero.csl.citationPlaces", "orig");
+pref("extensions.zotero.csl.autoVietnameseNames", false);
+pref("extensions.zotero.csl.enableInstitutionFormatting", true);
+pref("extensions.zotero.csl.trigraphFormat", "Aaaa00:AaAa00:AaAA00:AAAA00");
+
 // Integration settings
 pref("extensions.zotero.integration.port", 50001);
 pref("extensions.zotero.integration.autoRegenerate", -1);	// -1 = ask; 0 = no; 1 = yes
-pref("extensions.zotero.integration.useClassicAddCitationDialog", false);
+pref("extensions.zotero.integration.useClassicAddCitationDialog", true);
 pref("extensions.zotero.integration.keepAddCitationDialogRaised", false);
 
 // Connector settings
