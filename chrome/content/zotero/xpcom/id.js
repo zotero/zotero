89863e95a5efe22f94121fd5e4c5caf94a043629
Frank Bennett <biercenator@gmail.com>
2012-06-04 22:43:09 +0900

    MLZ changes to id.js (legacy conversion support, no longer needed really)

--- chrome/content/zotero/xpcom/id.js
+++ chrome/content/zotero/xpcom/id.js
@@ -58,6 +58,7 @@ Zotero.ID_Tracker = function () {
 			case 'items':
 			case 'creators':
 			case 'creatorData':
+			case 'creatorDataAlt':
 			case 'collections':
 			case 'savedSearches':
 			case 'tags':
@@ -141,10 +142,15 @@ Zotero.ID_Tracker = function () {
 			table = 'savedSearches';
 		}
 		
+		// Note: creatorDataAlt is used for
+		// legacy data conversions on the multilingual
+		// branch. When that is no longer needed, it can
+		// be dropped.
 		switch (table) {
 			case 'collections':
 			case 'creators':
 			case 'creatorData':
+			case 'creatorDataAlt':
 			case 'itemDataValues':
 			case 'items':
 			case 'savedSearches':
@@ -245,6 +251,7 @@ Zotero.ID_Tracker = function () {
 		switch (table) {
 			case 'creators':
 			case 'creatorData':
+			case 'creatorDataAlt':
 			case 'items':
 			case 'itemDataValues':
 			case 'tags':
@@ -373,6 +380,9 @@ Zotero.ID_Tracker = function () {
 			
 			case 'creatorData':
 				return 'creatorDataID';
+			
+			case 'creatorDataAlt':
+				return 'creatorDataAltID';
 			
 			default:
 				return table.substr(0, table.length - 1) + 'ID';
