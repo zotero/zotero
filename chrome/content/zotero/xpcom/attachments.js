e23e1c92e3366ddc5e78243019c29c6d3f8a1b73
Frank Bennett <biercenator@gmail.com>
2012-06-04 19:24:09 +0900

    MLZ changes to attachments.js (allow webpagedump to snip out portions of DOM)

--- chrome/content/zotero/xpcom/attachments.js
+++ chrome/content/zotero/xpcom/attachments.js
@@ -480,6 +480,7 @@ Zotero.Attachments = new function(){
 	 * Save a snapshot -- uses synchronous WebPageDump or asynchronous saveURI()
 	 */
 	function importFromDocument(document, sourceItemID, forceTitle, parentCollectionIDs, callback, libraryID) {
+		var url, bases, base, pos, len;
 		Zotero.debug('Importing attachment from document');
 		
 		if (sourceItemID && parentCollectionIDs) {
@@ -489,7 +490,34 @@ Zotero.Attachments = new function(){
 			parentCollectionIDs = undefined;
 		}
 		
-		var url = document.location.href;
+		if (document.location) {
+			url = document.location.href;
+		} else {
+			//
+			// For documents generated internally with createDocument(),
+			// attempts to query the document URL produce the following
+			// results:
+			//
+			//   document.baseURIObject.spec:  about:blank
+			//   document.location:            null
+			//
+			// To process XHTML documents of this kind as attachments,
+			// set a base element inside the HEAD node of the document.
+			// If no base element is present, a generated attachment
+			// document will fail.
+			//
+			url = "about:blank";
+			var html = document.getElementsByTagName("html")[0];
+			bases = html.getElementsByTagName("base");
+			len = bases.length;
+			for (pos = 0; pos < len; pos += 1) {
+				base = bases.item(pos);
+				if (base.hasAttribute("href")) {
+					url = base.getAttribute("href");
+					break;
+				}
+			}
+		}
 		var title = forceTitle ? forceTitle : document.title;
 		var mimeType = document.contentType;
 		if(Zotero.Attachments.isPDFJS(document)) {
@@ -559,6 +587,7 @@ Zotero.Attachments = new function(){
 			}
 			
 			if (mimeType === 'text/html' || mimeType === 'application/xhtml+xml') {
+				Zotero.debug('Saving with wpdDOMSaver.saveHTMLDocument()');
 				var sync = true;
 				
 				// Load WebPageDump code
