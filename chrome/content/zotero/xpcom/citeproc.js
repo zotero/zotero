45034a151e73946685c8800aec73a91f16272126
Frank Bennett <biercenator@gmail.com>
2012-08-11 17:52:42 +0900

    Upgrade citeproc-js to version 1.0.377

--- chrome/content/zotero/xpcom/citeproc.js
+++ chrome/content/zotero/xpcom/citeproc.js
@@ -57,7 +57,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.0.376",
+    PROCESSOR_VERSION: "1.0.377",
     STATUTE_SUBDIV_GROUPED_REGEX: /((?:^| )(?:art|ch|Ch|subch|p|pp|para|subpara|pt|r|sec|subsec|Sec|sch|tit)\.)/g,
     STATUTE_SUBDIV_PLAIN_REGEX: /(?:(?:^| )(?:art|ch|Ch|subch|p|pp|para|subpara|pt|r|sec|subsec|Sec|sch|tit)\.)/,
     STATUTE_SUBDIV_STRINGS: {
@@ -679,6 +679,9 @@ CSL_CHROME.prototype.getAttributeName = function (attr) {
 }
 CSL_CHROME.prototype.getAttributeValue = function (myxml,name,namespace) {
     var ret = "";
+    if (namespace) {
+        name = namespace+":"+name;
+    }
     if (myxml && this.hasAttributes(myxml) && myxml.getAttribute(name)) {
         ret = myxml.getAttribute(name);
     }
