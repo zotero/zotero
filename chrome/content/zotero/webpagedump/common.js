b6fee9a3577de8e56e80e107cd20e9b21a5d95bb
Simon Kornblith <simon@simonster.com>
2012-11-12 00:45:35 -0500

    Fix for Firefox 18+

--- chrome/content/zotero/webpagedump/common.js
+++ chrome/content/zotero/webpagedump/common.js
@@ -675,7 +675,13 @@ var wpdCommon = {
 
 			// has the url the same filetype like the file extension?
 			//save file to target
-			obj_Persist.saveURI(obj_URI, null, null, null, null, obj_TargetFile);
+			try {
+				obj_Persist.saveURI(obj_URI, null, null, null, null, obj_TargetFile);
+			} catch(e if e.name === "NS_ERROR_XPC_NOT_ENOUGH_ARGS") {
+				// https://bugzilla.mozilla.org/show_bug.cgi?id=794602
+				// XXX Always use when we no longer support Firefox < 18
+				obj_Persist.saveURI(obj_URI, null, null, null, null, obj_TargetFile, null);
+			}
 
 			return true;
 
