5764aa23a88492be587a1f89d9044dc671e2d922
Frank Bennett <biercenator@gmail.com>
2013-01-04 18:46:16 +0900

    Provide for passing of subjurisdiction nicknames, and fetching of label from jurisdiction key

--- chrome/content/zotero/xpcom/multilingual/jurisdiction.js
+++ chrome/content/zotero/xpcom/multilingual/jurisdiction.js
@@ -7,6 +7,7 @@ Zotero.Jurisdiction = new function () {
 	var _ios;
 
 	this.getListData = getListData;
+	this.getLabelFromKey = getLabelFromKey;
 
 	this.init = function() {
 		_ios = Components.classes["@mozilla.org/network/io-service;1"].
@@ -24,7 +25,7 @@ Zotero.Jurisdiction = new function () {
 		}
 		var ret = [];
 		for (var key in lst) {
-			ret.push([key, lst[key].name, !!lst[key].children]);
+			ret.push([key, lst[key].name, lst[key].nickname]);
 		}
 		ret.sort(function(a,b){
 			if (a[1] > b[1]) {
@@ -35,6 +36,29 @@ Zotero.Jurisdiction = new function () {
 				return 0;
 			}
 		});
+		return ret;
+	};
+	function getLabelFromKey(key) {
+		ret = false;
+		lst = key.split(";");
+		pos = lst.length;
+		var trykey;
+		while (pos > 0) {
+			var trykey = lst.slice(0,pos).join(";");
+			if (_jurisdictionObject[trykey]) {
+				if (pos < lst.length) {
+					if (pos == 1) {
+						trykey = trykey + ";subunit";
+					}
+					if (_jurisdictionObject[trykey] && _jurisdictionObject[trykey].children[key]) {
+						ret = _jurisdictionObject[trykey].children[key].name;
+					}
+				} else {
+					ret = _jurisdictionObject[trykey].name;
+				}
+			}
+			pos += -1;
+		}
 		return ret;
 	};
 	function _getJurisdictionFile() {
