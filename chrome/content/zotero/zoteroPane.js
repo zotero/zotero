56bb5b17ad386d67a1ef52987a4ead38a862f47f
Dan Stillman <dstillman@zotero.org>
2013-01-28 22:44:02 -0500

    Better MIME type detection of Office files
    For at least one Windows user, a .docx file was being interpreted as
    text/plain. Instead of relying entirely on the system, hard-code some
    extensions we know. (More can be added.)
    
    Also:
    
    - Determine MIME type when opening files instead of using stored type,
      since we might have gotten smarter

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -3443,14 +3443,10 @@ var ZoteroPane = new function()
 				if(forceExternalViewer !== undefined) {
 					var externalViewer = forceExternalViewer;
 				} else {
-					var mimeType = attachment.attachmentMIMEType;
-					// If no MIME type specified, try to detect again (I guess in case
-					// we've gotten smarter since the file was imported?)
-					if (!mimeType) {
-						mimeType = Zotero.MIME.getMIMETypeFromFile(file);
-						
-						// TODO: update DB with new info
-					}
+					var mimeType = Zotero.MIME.getMIMETypeFromFile(file);
+					
+					//var mimeType = attachment.attachmentMIMEType;
+					// TODO: update DB with new info if changed?
 					
 					var ext = Zotero.File.getExtension(file);
 					var externalViewer = Zotero.isStandalone || (!Zotero.MIME.hasNativeHandler(mimeType, ext) &&
