zotero/zotero

commit d2f028d797c5c0ae3d77d54a5bbabaa5cd4a961b

Tag colors, synced settings, and (stopgap) silent DB upgrades
- New tag colors support, with the ability to assign colors to up to 6
  tags per library. Tags with colors assigned will show up at the top of
  the tag selector and can be added to (and removed from) selected items
  by pressing the 1-6 keys on the keyboard. The tags will show up as
  color swatches before an item's title in the items list.
- Synced settings, with Notifier triggers when they change and
  accessible via the API (currently restricted on the server to
  'tagColors', but available for other things upon request)
- Silent DB upgrades for backwards-compatible changes. We'll do
  something fancier with async DB queries in 4.0, but this will work for
  changes that can be made without breaking compatibility with older
  clients, like the creation of new tables. The 'userdata' value is
  capped at 76, while further increments go to 'userdata2'.

TODO:

- Try to avoid jitter when redrawing swatches
- Optimize tag color images for retina displays
- Redo attachment dots in flat style?
- Clear all colors from an item with 0 (as in Thunderbird), but I don't
  think we can do this without undo

chrome/content/zotero/xpcom/itemTreeView.js
@@ -37,8 +37,10 @@
 Zotero.ItemTreeView = function(itemGroup, sourcesOnly)
 {
 	this.wrappedJSObject = this;
+	this.rowCount = 0;
 	
 	this._initialized = false;
+	this._skipKeypress = false;
 	
 	this._itemGroup = itemGroup;
 	this._sourcesOnly = sourcesOnly;
@@ -50,7 +52,7 @@ Zotero.ItemTreeView = function(itemGroup, sourcesOnly)
 	this._needsSort = false;
 	
 	this._dataItems = [];
-	this.rowCount = 0;
+	this._itemImages = {};
 	
 	this._unregisterID = Zotero.Notifier.registerObserver(this, ['item', 'collection-item', 'share-items', 'bucket']);
 }
@@ -136,44 +138,118 @@ Zotero.ItemTreeView.prototype._setTreeGenerator = function(treebox)
 		
 		// Add a keypress listener for expand/collapse
 		var tree = this._treebox.treeBody.parentNode;
-		var me = this;
+		var self = this;
+		var coloredTagsRE = new RegExp("^[1-" + Zotero.Tags.MAX_COLORED_TAGS + "]{1}$");
 		var listener = function(event) {
+			if (self._skipKeyPress) {
+				self._skipKeyPress = false;
+				return;
+			}
+			
 			// Handle arrow keys specially on multiple selection, since
 			// otherwise the tree just applies it to the last-selected row
 			if (event.keyCode == 39 || event.keyCode == 37) {
-				if (me._treebox.view.selection.count > 1) {
+				if (self._treebox.view.selection.count > 1) {
 					switch (event.keyCode) {
 						case 39:
-							me.expandSelectedRows();
+							self.expandSelectedRows();
 							break;
 							
 						case 37:
-							me.collapseSelectedRows();
+							self.collapseSelectedRows();
 							break;
 					}
 					
 					event.preventDefault();
 				}
-				
 				return;
 			}
 			
-			var key = String.fromCharCode(event.which);
-			if (key == '+' && !(event.ctrlKey || event.altKey || event.metaKey)) {
-				me.expandAllRows();
-				event.preventDefault();
+			// Ignore other non-character keypresses
+			if (!event.charCode) {
 				return;
 			}
-			else if (key == '-' && !(event.shiftKey || event.ctrlKey || event.altKey || event.metaKey)) {
-				me.collapseAllRows();
-				event.preventDefault();
-				return;
-			}
+			
+			event.preventDefault();
+			
+			Q.fcall(function () {
+				var key = String.fromCharCode(event.which);
+				if (key == '+' && !(event.ctrlKey || event.altKey || event.metaKey)) {
+					self.expandAllRows();
+					return false;
+				}
+				else if (key == '-' && !(event.shiftKey || event.ctrlKey || event.altKey || event.metaKey)) {
+					self.collapseAllRows();
+					return false;
+				}
+				else if (coloredTagsRE.test(key)) {
+					let libraryID = self._itemGroup.libraryID;
+					libraryID = libraryID ? parseInt(libraryID) : 0;
+					let position = parseInt(key) - 1;
+					return Zotero.Tags.getColorByPosition(libraryID, position)
+					.then(function (colorData) {
+						// If a color isn't assigned to this number, allow key navigation,
+						// though I'm not sure this is a good idea.
+						if (!colorData) {
+							return true;
+						}
+						
+						var items = self.getSelectedItems();
+						return Zotero.Tags.toggleItemsListTags(libraryID, items, colorData.name)
+						.then(function () {
+							return false;
+						});
+					});
+				}
+				return true;
+			})
+			// We have to disable key navigation on the tree in order to
+			// keep it from acting on the 1-6 keys used for colored tags.
+			// To allow navigation with other keys, we temporarily enable
+			// key navigation and recreate the keyboard event. Since
+			// that will trigger this listener again, we set a flag to
+			// ignore the event, and then clear the flag above when the
+			// event comes in. I see no way this could go wrong...
+			.then(function (resend) {
+				if (!resend) {
+					return;
+				}
+				
+				tree.disableKeyNavigation = false;
+				self._skipKeyPress = true;
+				var nsIDWU = Components.interfaces.nsIDOMWindowUtils
+				var domWindowUtils = event.originalTarget.ownerDocument.defaultView
+					.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+					.getInterface(nsIDWU);
+				var modifiers = 0;
+				if (event.ctrlKey) {
+					modifiers |= nsIDWU.MODIFIER_CTRL;
+				}
+				if (event.shiftKey) {
+					modifiers |= nsIDWU.MODIFIER_SHIFT;
+				}
+				if (event.metaKey) {
+					modifiers |= nsIDWU.MODIFIER_META;
+				}
+				domWindowUtils.sendKeyEvent(
+					'keypress',
+					event.keyCode,
+					event.charCode,
+					modifiers
+				);
+				tree.disableKeyNavigation = true;
+			})
+			.catch(function (e) {
+				Zotero.debug(e, 1);
+				Components.utils.reportError(e);
+			})
+			.done();
 		};
 		// Store listener so we can call removeEventListener()
 		// in overlay.js::onCollectionSelected()
 		this.listener = listener;
-		tree.addEventListener('keypress', listener, false);
+		tree.addEventListener('keypress', listener);
+		
 		// This seems to be the only way to prevent Enter/Return
 		// from toggle row open/close. The event is handled by
 		// handleKeyPress() in zoteroPane.js.
@@ -371,17 +447,22 @@ Zotero.ItemTreeView.prototype.notify = function(action, type, ids, extraData)
 					'zotero-items-column-' + extraData.column
 				);
 				for each(var id in ids) {
+					if (extraData.column == 'title') {
+						delete this._itemImages[id];
+					}
 					this._treebox.invalidateCell(this._itemRowMap[id], col);
 				}
 			}
 			else {
 				for each(var id in ids) {
+					delete this._itemImages[id];
 					this._treebox.invalidateRow(this._itemRowMap[id]);
 				}
 			}
 		}
 		// Redraw the whole tree
 		else {
+			this._itemImages = {};
 			this._treebox.invalidate();
 		}
 		return;
@@ -513,6 +594,8 @@ Zotero.ItemTreeView.prototype.notify = function(action, type, ids, extraData)
 					this._refreshHashMap();
 				}
 				var row = this._itemRowMap[id];
+				// Clear item type icon and tag colors
+				delete this._itemImages[id];
 				
 				// Deleted items get a modify that we have to ignore when
 				// not viewing the trash
@@ -898,7 +981,51 @@ Zotero.ItemTreeView.prototype.getImageSrc = function(row, col)
 {
 	if(col.id == 'zotero-items-column-title')
 	{
-		return this._getItemAtRow(row).ref.getImageSrc();
+		// Get item type icon and tag swatches
+		var item = this._getItemAtRow(row).ref;
+		var itemID = item.id;
+		if (this._itemImages[itemID]) {
+			return this._itemImages[itemID];
+		}
+		var uri = item.getImageSrc();
+		var tags = item.getTags();
+		if (!tags.length) {
+			this._itemImages[itemID] = uri;
+			return uri;
+		}
+		
+		//Zotero.debug("Generating tree image for item " + itemID);
+		
+		var colorData = [];
+		for (let i=0, len=tags.length; i<len; i++) {
+			let libraryIDInt = item.libraryIDInt; // TEMP
+			colorData.push(Zotero.Tags.getColor(libraryIDInt, tags[i].name));
+		}
+		var self = this;
+		Q.all(colorData)
+		.then(function (colorData) {
+			colorData = colorData.filter(function (val) val !== false);
+			if (!colorData.length) {
+				return false;
+			}
+			colorData.sort(function (a, b) {
+				return a.position - b.position;
+			});
+			var colors = colorData.map(function (val) val.color);
+			return Zotero.Tags.generateItemsListImage(colors, uri);
+		})
+		// When the promise is fulfilled, the data URL is ready, so invalidate
+		// the cell to force requesting it again
+		.then(function (dataURL) {
+			self._itemImages[itemID] = dataURL ? dataURL : uri;
+			if (dataURL) {
+				self._treebox.invalidateCell(row, col);
+			}
+		})
+		.done();
+		
+		this._itemImages[itemID] = uri;
+		return uri;
 	}
 	else if (col.id == 'zotero-items-column-hasAttachment') {
 		if (this._itemGroup.isTrash()) return false;
@@ -2830,36 +2957,11 @@ Zotero.ItemTreeView.prototype.onDragExit = function (event) {
 ////////////////////////////////////////////////////////////////////////////////
 
 Zotero.ItemTreeView.prototype.isSeparator = function(row) 						{ return false; }
-Zotero.ItemTreeView.prototype.getRowProperties = function(row, prop) {
-	var treeRow = this._getItemAtRow(row);
-	var itemID = treeRow.ref.id;
-	
-	// Set background color for selected items with colored tags
-	if (this.selection.isSelected(row)) {
-		if (color = Zotero.Tags.getItemColor(itemID)) {
-			var aServ = Components.classes["@mozilla.org/atom-service;1"].
-				getService(Components.interfaces.nsIAtomService);
-			prop.AppendElement(aServ.getAtom("color" + color.substr(1)));
-		}
-	}
-}
-Zotero.ItemTreeView.prototype.getColumnProperties = function(col, prop) { }
+Zotero.ItemTreeView.prototype.getRowProperties = function(row, prop) {}
+Zotero.ItemTreeView.prototype.getColumnProperties = function(col, prop) {}
 Zotero.ItemTreeView.prototype.getCellProperties = function(row, col, prop) {
 	var treeRow = this._getItemAtRow(row);
 	var itemID = treeRow.ref.id;
-	
-	// Set tag colors
-	//
-	// Don't set the text color if the row is selected, in which case the background
-	// color is set in getRowProperties() instead, unless the tree isn't focused,
-	// in which case it's not
-	if (!this.selection.isSelected(row) || !this._treebox.focused) {
-		if (color = Zotero.Tags.getItemColor(itemID)) {
-			var aServ = Components.classes["@mozilla.org/atom-service;1"].
-				getService(Components.interfaces.nsIAtomService);
-			prop.AppendElement(aServ.getAtom("color" + color.substr(1)));
-		}
-	}
 	
 	// Mark items not matching search as context rows, displayed in gray
 	if (this._searchMode && !this._searchItemIDs[itemID]) {

chrome/content/zotero/bindings/customcolorpicker.xml
@@ -1,0 +1,136 @@
+<?xml version="1.0"?>
+<!--
+  An extension of the Mozilla colorpicker that allows for a custom set of colors
+-->
+<bindings id="colorpickerBindings"
+		xmlns="http://www.mozilla.org/xbl"
+		xmlns:xbl="http://www.mozilla.org/xbl"
+		xmlns:xul="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul">
+	
+	<binding id="custom-colorpicker" extends="chrome://global/content/bindings/colorpicker.xml#colorpicker">
+		<resources>
+			<stylesheet src="chrome://zotero/skin/bindings/customcolorpicker.css"/>
+		</resources>
+		
+		<content>
+			<vbox anonid="tiles" flex="1" xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul">
+				<hbox>
+					<spacer class="colorpickertile" style="background-color: #000000" color="#000000"/>
+				</hbox>
+			</vbox>
+		</content>
+		
+		<implementation implements="nsIDOMEventListener">
+			<constructor><![CDATA[
+				this.initialize();
+				
+				this._colors = this.getAttribute('colors');
+				if (this._colors) {
+					this._cols = this.getAttribute('cols');
+				}
+				this.redraw();
+			]]></constructor>
+			
+			<!-- Defaults from the Mozilla colorpicker -->
+			<field name="_defaultColors">
+			[
+				'L#FFFFFF','L#FFCCCC','L#FFCC99','L#FFFF99','L#FFFFCC','L#99FF99','L#99FFFF','L#CCFFFF','L#CCCCFF','L#FFCCFF',
+				'#CCCCCC','#FF6666','#FF9966','L#FFFF66','L#FFFF33','L#66FF99','L#33FFFF','L#66FFFF','#9999FF','#FF99FF',
+				'#C0C0C0','#FF0000','#FF9900','#FFCC66','L#FFFF00','L#33FF33','#66CCCC','#33CCFF','#6666CC','#CC66CC',
+				'#999999','#CC0000','#FF6600','#FFCC33','#FFCC00','#33CC00','#00CCCC','#3366FF','#6633FF','#CC33CC',
+				'#666666','#990000','#CC6600','#CC9933','#999900','#009900','#339999','#3333FF','#6600CC','#993399',
+				'#333333','#660000','#993300','#996633','#666600','#006600','#336666','#000099','#333399','#663366',
+				'#000000','#330000','#663300','#663333','#333300','#003300','#003333','#000066','#330099','#330033'
+			]
+			</field>
+			<field name="_defaultCols">10</field>
+			
+			<property name="colors" onget="return this._colors ? this._colors : []">
+				<setter><![CDATA[
+					if (typeof val == 'string') {
+						val = val ? val.split(',') : null;
+					}
+					this._colors = val;
+					this.redraw();
+				]]></setter>
+			</property>
+			
+			<property name="cols" onget="return this.getAttribute('cols')">
+				<setter><![CDATA[
+					this.setAttribute('cols', val);
+					this.redraw();
+				]]></setter>
+			</property>
+			
+			<method name="redraw">
+				<body><![CDATA[
+					//Zotero.debug("Redrawing color picker");
+					
+					var tiles = document.getAnonymousNodes(this)[0];
+					
+					var cols = this.getAttribute('cols') || this._defaultCols;
+					var colors = this._colors.concat() || this._defaultColors.concat();
+					
+					while (tiles.hasChildNodes()) {
+						tiles.removeChild(tiles.firstChild);
+					}
+					
+					var rows = Math.ceil(colors.length / cols);
+					
+					var tileWidth = this.getAttribute('tileWidth');
+					var tileHeight = this.getAttribute('tileHeight');
+					
+					for (let i=0; i<rows; i++) {
+						var hbox = document.createElement('hbox');
+						for (let j=0; j<cols; j++) {
+							let color = colors.shift();
+							if (!color) {
+								break;
+							}
+							let light = color.charAt(0) == 'L';
+							color = light ? color.substr(1) : color;
+							
+							var spacer = document.createElement('spacer');
+							spacer.className = 'colorpickertile' + (light ? ' cp-light' : '');
+							spacer.setAttribute('style', 'background-color: ' + color);
+							spacer.setAttribute('color', color);
+							if (tileWidth) {
+								spacer.width = tileWidth;
+							}
+							if (tileHeight) {
+								spacer.height = tileHeight;
+							}
+							hbox.appendChild(spacer);
+						}
+						tiles.appendChild(hbox);
+					}
+				]]></body>
+			</method>
+		</implementation>
+	</binding>
+	
+	<!-- The content of the Mozilla colorpicker-button, but with a customcolorpicker
+	     with some extra inherited attributes instead -->
+	<binding id="custom-colorpicker-button" display="xul:menu"
+			extends="chrome://global/content/bindings/colorpicker.xml#colorpicker-button">
+		<resources>
+			<stylesheet src="chrome://zotero/skin/bindings/customcolorpicker.css"/>
+		</resources>
+		<content>
+			<xul:hbox class="colorpicker-button-colorbox" anonid="colorbox" flex="1" xbl:inherits="disabled"/>
+			
+			<xul:panel class="colorpicker-button-menupopup"
+					anonid="colorpopup" noautofocus="true" level="top"
+					onmousedown="event.stopPropagation()"
+					onpopupshowing="this._colorPicker.onPopupShowing()"
+					onpopuphiding="this._colorPicker.onPopupHiding()"
+					onselect="this._colorPicker.pickerChange()">
+				<xul:customcolorpicker xbl:inherits="palettename,disabled,cols,columns,tileWidth,tileHeight" allowevents="true" anonid="colorpicker"/>
+			</xul:panel>
+		</content>
+		
+		<implementation>
+			<property name="colors" onget="return this.mPicker.colors" onset="this.mPicker.colors = val"/>
+		</implementation>
+	</binding>
+</bindings>

chrome/content/zotero/bindings/tagsbox.xml
@@ -37,6 +37,7 @@
 		<implementation>
 			<field name="clickHandler"/>
 			<field name="_tagColors"/>
+			<field name="_notifierID"/>
 			
 			<!-- Modes are predefined settings groups for particular tasks -->
 			<field name="_mode">"view"</field>
@@ -109,38 +110,64 @@
 				if (this.hasAttribute('mode')) {
 					this.mode = this.getAttribute('mode');
 				}
+				
+				this._notifierID = Zotero.Notifier.registerObserver(this, ['setting']);
 			]]>
 			</constructor>
+			
+			
+			<destructor>
+			<![CDATA[
+				Zotero.Notifier.unregisterObserver(this._notifierID);
+			]]>
+			</destructor>
+			
+			
+			<method name="notify">
+				<parameter name="event"/>
+				<parameter name="type"/>
+				<parameter name="ids"/>
+				<body>
+				<![CDATA[
+					if (type == 'setting') {
+						if (ids.some(function (val) val.split("/")[1] == 'tagColors') && this.item) {
+							this.reload();
+						}
+						return;
+					}
+				]]>
+				</body>
+			</method>
+			
 			
 			<method name="reload">
 				<body>
 					<![CDATA[
+						var self = this;
 						Zotero.debug('Reloading tags');
+						var addButton = self.id('addButton');
+						addButton.hidden = !self.editable;
 						
-						var addButton = this.id('addButton');
-						addButton.hidden = !this.editable;
-						
-						var rows = this.id('tagRows');
-						while(rows.hasChildNodes())
-							rows.removeChild(rows.firstChild);
-						
-						var tags = this.item.getTags();
-						this._tagColors = Zotero.Tags.getColors();
-						
-						if(tags)
-						{
-							for (var i=0; i<tags.length; i++) {
-								this.addDynamicRow(tags[i], i+1);
+						return Zotero.Tags.getColors(self.item.libraryIDInt)
+						.then(function (colors) {
+							self._tagColors = colors;
+							
+							var rows = self.id('tagRows');
+							while(rows.hasChildNodes()) {
+								rows.removeChild(rows.firstChild);
 							}
-							this.updateCount(tags.length);
+							var tags = self.item.getTags();
+							if (tags) {
+								for (var i=0; i<tags.length; i++) {
+									self.addDynamicRow(tags[i], i+1);
+								}
+								
+								//self.fixPopup();
+							}
 							
-							//this.fixPopup();
-							
-							return tags.length;
-						}
-						
-						this.updateCount(0);
-						return 0;
+							self.updateCount(0);
+						})
+						.done();
 					]]>
 				</body>
 			</method>
@@ -264,8 +291,13 @@
 					}
 					
 					// Tag color
-					if (color = this._tagColors[valueText]) {
-						valueElement.setAttribute('style', 'color:' + this._tagColors[valueText]);
+					let color = this._tagColors[valueText];
+					if (color) {
+						valueElement.setAttribute(
+							'style',
+							'color:' + this._tagColors[valueText].color + '; '
+								+ 'font-weight: bold'
+						);
 					}
 					
 					return valueElement;

chrome/content/zotero/tagColorChooser.js
@@ -1,0 +1,154 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2013 Center for History and New Media
+                     George Mason University, Fairfax, Virginia, USA
+                     http://zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+"use strict";
+var _io;
+
+var Zotero_Tag_Color_Chooser = new function() {
+	this.init = function () {
+		// Set font size from pref
+		Zotero.setFontSize(document.getElementById("tag-color-chooser-container"));
+		
+		if (window.arguments && window.arguments.length) {
+			_io = window.arguments[0];
+			if (_io.wrappedJSObject) _io = _io.wrappedJSObject;
+		}
+		if (typeof _io.libraryID == 'undefined') throw new Error("libraryID not set");
+		if (typeof _io.name == 'undefined' || _io.name === "") throw new Error("name not set");
+		
+		window.sizeToContent();
+		
+		var dialog = document.getElementById('tag-color-chooser');
+		var colorPicker = document.getElementById('color-picker');
+		var tagPosition = document.getElementById('tag-position');
+		
+		colorPicker.setAttribute('cols', 3);
+		colorPicker.setAttribute('tileWidth', 24);
+		colorPicker.setAttribute('tileHeight', 24);
+		colorPicker.colors = [
+			'#990000', '#CC9933', '#FF9900',
+			'#FFCC00', '#007439', '#1049A9',
+			'#9999FF', '#CC66CC', '#993399'
+		];
+		
+		var maxTags = document.getElementById('max-tags');
+		maxTags.value = Zotero.getString('tagColorChooser.maxTags', Zotero.Tags.MAX_COLORED_TAGS);
+		
+		var self = this;
+		Zotero.Tags.getColors(_io.libraryID)
+		.then(function (tagColors) {
+			var colorData = tagColors[_io.name];
+			
+			// Color
+			if (colorData) {
+				colorPicker.color = colorData.color;
+				dialog.buttons = "extra1,cancel,accept";
+			}
+			else {
+				// Get unused color at random
+				var usedColors = [];
+				for (var i in tagColors) {
+					usedColors.push(tagColors[i].color);
+				}
+				var unusedColors = Zotero.Utilities.arrayDiff(
+					colorPicker.colors, usedColors
+				);
+				var color = unusedColors[Zotero.Utilities.rand(0, unusedColors.length - 1)];
+				colorPicker.color = color;
+				dialog.buttons = "cancel,accept";
+			}
+			colorPicker.setAttribute('disabled', 'false');
+			
+			var numColors = Object.keys(tagColors).length;
+			var max = colorData ? numColors : numColors + 1;
+			
+			// Position
+			for (let i=1; i<=max; i++) {
+				tagPosition.appendItem(i, i-1);
+			}
+			if (numColors) {
+				tagPosition.setAttribute('disabled', 'false');
+				if (colorData) {
+					tagPosition.selectedIndex = colorData.position;
+				}
+				// If no color currently, default to end
+				else {
+					tagPosition.selectedIndex = numColors;
+				}
+			}
+			// If no colors currently, only position "1" is available
+			else {
+				tagPosition.selectedIndex = 0;
+			}
+			
+			self.onPositionChange();
+			window.sizeToContent();
+		})
+		.catch(function (e) {
+			Zotero.debug(e, 1);
+			Components.utils.reportError(e);
+			dialog.cancelDialog();
+		})
+		.done();
+	};
+	
+	
+	this.onPositionChange = function () {
+		var tagPosition = document.getElementById('tag-position');
+		var instructions = document.getElementById('number-key-instructions');
+		
+		while (instructions.hasChildNodes()) {
+			instructions.removeChild(instructions.firstChild);
+		}
+		
+		var msg = Zotero.getString('tagColorChooser.numberKeyInstructions');
+		var matches = msg.match(/(.+)\$NUMBER(.+)/);
+		
+		var num = document.createElement('label');
+		num.id = 'number-key';
+		num.setAttribute('value', parseInt(tagPosition.value) + 1);
+		
+		instructions.appendChild(document.createTextNode(matches[1]));
+		instructions.appendChild(num);
+		instructions.appendChild(document.createTextNode(matches[2]));
+	};
+	
+	
+	this.onDialogAccept = function () {
+		var colorPicker = document.getElementById('color-picker');
+		var tagPosition = document.getElementById('tag-position');
+		_io.color = colorPicker.color;
+		_io.position = tagPosition.value;
+	};
+	
+	
+	this.onDialogCancel = function () {};
+	
+	
+	this.onDialogRemoveColor = function () {
+		_io.color = false;
+		window.close();
+	};
+};

chrome/content/zotero/tagColorChooser.xul
@@ -1,0 +1,63 @@
+<?xml version="1.0"?>
+<!--
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2013 Center for History and New Media
+                     George Mason University, Fairfax, Virginia, USA
+                     http://zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+-->
+<?xml-stylesheet href="chrome://global/skin/" type="text/css"?>
+<?xml-stylesheet href="chrome://zotero/skin/zotero.css" type="text/css"?>
+<?xml-stylesheet href="chrome://zotero/skin/tagColorChooser.css" type="text/css"?>
+<!DOCTYPE window SYSTEM "chrome://zotero/locale/zotero.dtd">
+
+<dialog xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
+	title="&zotero.tagColorChooser.title;" buttons="cancel,accept"
+	id="tag-color-chooser"
+	buttonlabelaccept="&zotero.tagColorChooser.setColor;"
+	buttonlabelextra1="&zotero.tagColorChooser.removeColor;"
+	ondialogaccept="Zotero_Tag_Color_Chooser.onDialogAccept()"
+	ondialogcancel="Zotero_Tag_Color_Chooser.onDialogCancel()"
+	ondialogextra1="Zotero_Tag_Color_Chooser.onDialogRemoveColor()"
+	onload="Zotero_Tag_Color_Chooser.init()"
+	width="300"
+	height="140">
+	
+	<script src="include.js"/>
+	<script src="tagColorChooser.js" type="text/javascript;version=1.8"/>
+	
+	<vbox id="tag-color-chooser-container">
+		<hbox align="center">
+			<label value="&zotero.tagColorChooser.color;" control="color-picker"/>
+			<customcolorpicker id="color-picker" type="button" disabled="true"/>
+			<separator width="20"/>
+			<label value="&zotero.tagColorChooser.position;" control="tag-position" disabled="true"/>
+			<menulist id="tag-position" disabled="true" sizetopopup="always"
+					oncommand="Zotero_Tag_Color_Chooser.onPositionChange()">
+				<menupopup/>
+			</menulist>
+		</hbox>
+		<separator/>
+		<description id="number-key-instructions"/>
+		<separator class="thin"/>
+		<description id="max-tags"/>
+		<separator class="thin"/>
+	</vbox>
+</dialog>

chrome/content/zotero/xpcom/data/group.js
@@ -355,6 +355,10 @@ Zotero.Group.prototype.erase = function() {
 	var prefix = "groups/" + this.id;
 	Zotero.Relations.eraseByURIPrefix(Zotero.URI.defaultPrefix + prefix);
 	
+	// Delete settings
+	sql = "DELETE FROM syncedSettings WHERE libraryID=?";
+	Zotero.DB.query(sql, this.libraryID ? parseInt(this.libraryID) : 0);
+	
 	// Delete group
 	sql = "DELETE FROM groups WHERE groupID=?";
 	Zotero.DB.query(sql, this.id)

chrome/content/zotero/xpcom/data/item.js
@@ -113,6 +113,8 @@ Zotero.Item.prototype.__defineGetter__('itemID', function () {
 });
 Zotero.Item.prototype.__defineSetter__('id', function (val) { this.setField('id', val); });
 Zotero.Item.prototype.__defineGetter__('libraryID', function () { return this.getField('libraryID'); });
+// Temporary until everything expects an integer
+Zotero.Item.prototype.__defineGetter__('libraryIDInt', function () { var libraryID = this.getField('libraryID'); return libraryID ? parseInt(libraryID) : 0; });
 Zotero.Item.prototype.__defineSetter__('libraryID', function (val) { this.setField('libraryID', val); });
 Zotero.Item.prototype.__defineGetter__('key', function () { return this.getField('key'); });
 Zotero.Item.prototype.__defineSetter__('key', function (val) { this.setField('key', val) });

commit 0a7cc4448123154dd015da886d5994d7e78a7d06

Fix Select-All on Windows (broken by d2f028d)

chrome/content/zotero/xpcom/itemTreeView.js
@@ -170,8 +170,6 @@ Zotero.ItemTreeView.prototype._setTreeGenerator = function(treebox)
 				return;
 			}
 			
-			event.preventDefault();
-			
 			Q.fcall(function () {
 				var key = String.fromCharCode(event.which);
 				if (key == '+' && !(event.ctrlKey || event.altKey || event.metaKey)) {