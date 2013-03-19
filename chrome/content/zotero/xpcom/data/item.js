zotero/zotero

commit dbf6bc0ee1fb216953be43176011f93627ca2208

Don't attempt to mark changes when creating new item.

chrome/content/zotero/xpcom/data/item.js
@@ -590,7 +590,7 @@ Zotero.Item.prototype.setType = function(itemTypeID, loadIn) {
 	if (loadIn) {
 		this._itemDataLoaded = false;
 	}
-	else {
+	else if(oldItemTypeID) {
 		this._markFieldChange('itemType', Zotero.ItemTypes.getName(oldItemTypeID));
 		if (!this._changedPrimaryData) {
 			this._changedPrimaryData = {};

commit cc93a0c7b884660d413c37d186122f54ef6f26bf

Unbreak adding new items. Broken by dbf6bc0ee1fb216953be43176011f93627ca2208.

chrome/content/zotero/xpcom/data/item.js
@@ -590,8 +590,8 @@ Zotero.Item.prototype.setType = function(itemTypeID, loadIn) {
 	if (loadIn) {
 		this._itemDataLoaded = false;
 	}
-	else if(oldItemTypeID) {
-		this._markFieldChange('itemType', Zotero.ItemTypes.getName(oldItemTypeID));
+	else {
+		if(oldItemTypeID) this._markFieldChange('itemType', Zotero.ItemTypes.getName(oldItemTypeID));
 		if (!this._changedPrimaryData) {
 			this._changedPrimaryData = {};
 		}