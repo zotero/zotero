zotero/zotero

commit 2f8f18c9571010b5f9d9035f62b590e63ffc1e4f

Ensure that arrays are not strings

chrome/content/zotero/xpcom/translation/translate.js
@@ -93,17 +93,24 @@ Zotero.Translate.Sandbox = {
 			delete item.complete;
 			for(var i in item) {
 				var val = item[i];
-				var type = typeof val;
 				if(!val && val !== 0) {
 					// remove null, undefined, and false properties, and convert objects to strings
 					delete item[i];
-				} else if(type === "string") {
-					// trim strings
-					item[i] = val.trim();
-				} else if((type === "object" || type === "xml" || type === "function") && allowedObjects.indexOf(i) === -1) {
-					// convert things that shouldn't be objecst to objects
+					continue;
+				}
+
+				var isObject = typeof val === "object" || typeof val === "xml" || typeof val === "function",
+					shouldBeObject = allowedObjects.indexOf(i) !== -1;
+				if(isObject && !shouldBeObject) {
+					// Convert things that shouldn't be objects to objects
 					translate._debug("Translate: WARNING: typeof "+i+" is "+type+"; converting to string");
 					item[i] = val.toString();
+				} else if(shouldBeObject && !isObject) {
+					translate._debug("Translate: WARNING: typeof "+i+" is "+type+"; converting to array");
+					item[i] = [val];
+				} else if(typeof val === "string") {
+					// trim strings
+					item[i] = val.trim();
 				}
 			}
 			

commit 6a0351c9405354c3722cb4f317653ab3bf07b045

Don't break when stringifying item fields. Broken by 2f8f18c9571010b5f9d9035f62b590e63ffc1e4f

chrome/content/zotero/xpcom/translation/translate.js
@@ -98,8 +98,9 @@ Zotero.Translate.Sandbox = {
 					delete item[i];
 					continue;
 				}
-
-				var isObject = typeof val === "object" || typeof val === "xml" || typeof val === "function",
+				
+				var type = typeof val;
+				var isObject = type === "object" || type === "xml" || type === "function",
 					shouldBeObject = allowedObjects.indexOf(i) !== -1;
 				if(isObject && !shouldBeObject) {
 					// Convert things that shouldn't be objects to objects
@@ -108,7 +109,7 @@ Zotero.Translate.Sandbox = {
 				} else if(shouldBeObject && !isObject) {
 					translate._debug("Translate: WARNING: typeof "+i+" is "+type+"; converting to array");
 					item[i] = [val];
-				} else if(typeof val === "string") {
+				} else if(type === "string") {
 					// trim strings
 					item[i] = val.trim();
 				}