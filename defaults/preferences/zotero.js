db73b5d639f5c09c8db9874c032eaca78a3a69c5
Frank Bennett <biercenator@gmail.com>
2013-04-02 08:12:51 +0900

    Disable automatic abbreviations from 4.0 for the time being.

--- defaults/preferences/zotero.js
+++ defaults/preferences/zotero.js
@@ -103,7 +103,7 @@ pref("extensions.zotero.export.bibliographySettings", 'save-as-rtf');
 pref("extensions.zotero.export.bibliographyLocale", '');
 pref("extensions.zotero.export.displayCharsetOption", false);
 pref("extensions.zotero.export.citePaperJournalArticleURL", false);
-pref("extensions.zotero.cite.automaticJournalAbbreviations", true);
+pref("extensions.zotero.cite.automaticJournalAbbreviations", false);
 pref("extensions.zotero.import.charset", "auto");
 pref("extensions.zotero.import.createNewCollection.fromFileOpenHandler", true);
 pref("extensions.zotero.rtfScan.lastInputFile", "");
