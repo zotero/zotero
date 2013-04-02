bbaed83ce6bf0d143647e7b8e00b12632e897c77
Frank Bennett <biercenator@gmail.com>
2013-04-03 00:14:27 +0900

    Refresh 1.0.444

--- chrome/content/zotero/xpcom/citeproc.js
+++ chrome/content/zotero/xpcom/citeproc.js
@@ -13227,6 +13227,9 @@ CSL.Disambiguation.prototype.disExtraText = function () {
         this.base.disambiguate = true;
         this.betterbase.disambiguate = true;
         this.initGivens = true;
+        for (var i = 0, ilen = this.lists[this.listpos][1].length; i < ilen; i += 1) {
+            this.state.tmp.taintedItemIDs[this.lists[this.listpos][1][i].id] = true;
+        }
     } else if (this.lists[this.listpos][1].length > 1) {
         if (this.modeindex === this.modes.length - 1) {
             var base = this.lists[this.listpos][0];
