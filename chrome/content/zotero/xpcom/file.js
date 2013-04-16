zotero/zotero

commit 82448c4a4d367e4e17382ae0c458bdaef3e59d45

Escape special characters in file paths when attaching files

chrome/content/zotero/xpcom/file.js
@@ -43,6 +43,21 @@ Zotero.File = new function(){
 	this.getCharsetFromFile = getCharsetFromFile;
 	this.addCharsetListener = addCharsetListener;
 	
+	/**
+	 * Encode special characters in file paths that might cause problems,
+	 *  like # (but preserve slashes or colons)
+	 *
+	 * @param {String} path File path
+	 * @return {String} Encoded file path
+	 */
+	this.encodeFilePath = function(path) {
+		var parts = path.split(/([\\\/:]+)/);
+		// Every other item is the separator
+		for (var i=0, n=parts.length; i<n; i+=2) {
+			parts[i] = encodeURIComponent(parts[i]);
+		}
+		return parts.join('');
+	}
 	
 	function getExtension(file){
 		var pos = file.leafName.lastIndexOf('.');

chrome/content/zotero/xpcom/translation/translate_item.js
@@ -291,7 +291,7 @@ Zotero.Translate.ItemSaver.prototype = {
 		var IOService = Components.classes["@mozilla.org/network/io-service;1"].
 						getService(Components.interfaces.nsIIOService);
 		try {
-			var uri = IOService.newURI(path, "", this._baseURI);
+			var uri = IOService.newURI(Zotero.File.encodeFilePath(path), "", this._baseURI);
 		}
 		catch (e) {
 			var msg = "Error parsing attachment path: " + path;

commit 38ac75540d433c44133fb6b98fe2191b6a7841a5

Don't double-encode file paths. Broken by 82448c4

chrome/content/zotero/xpcom/file.js
@@ -54,7 +54,8 @@ Zotero.File = new function(){
 		var parts = path.split(/([\\\/:]+)/);
 		// Every other item is the separator
 		for (var i=0, n=parts.length; i<n; i+=2) {
-			parts[i] = encodeURIComponent(parts[i]);
+			//decode before re-encoding so we don't double-encode
+			parts[i] = encodeURIComponent(decodeURIComponent(parts[i]));
 		}
 		return parts.join('');
 	}