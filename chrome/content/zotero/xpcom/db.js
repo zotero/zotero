zotero/zotero

commit 9d3f55be5193763d4c6d2afc3ddecea0bf5a9f39

Use async DB for autocomplete
This was probably hanging previously because I was passing a JS function for
onProgress, which apparently isn't allowed.

chrome/content/zotero/xpcom/db.js
@@ -794,7 +794,7 @@ Zotero.DBConnection.prototype.checkException = function (e) {
 Zotero.DBConnection.prototype.closeDatabase = function () {
 	if(this._connection) {
 		this.stopDummyStatement();
-		this._connection.close();
+		this._connection.asyncClose();
 		return true;
 	}
 }

components/zotero-autocomplete.js
@@ -223,36 +223,7 @@ ZoteroAutoComplete.prototype.startSearch = function(searchString, searchParams, 
 			statement = this._zotero.DB.getStatement(sql, sqlParams);
 	}
 	
-	// Disable asynchronous until we figure out the hangs
-	if (true) {
-		var rows = this._zotero.DB.query(sql, sqlParams);
-		
-		if (resultsCallback) {
-			resultsCallback(rows);
-		}
-		
-		var results = [];
-		var comments = [];
-		for each(var row in rows) {
-			results.push(row.val);
-			let comment = row.comment;
-			if (comment) {
-				comments.push(comment);
-			}
-		}
-		this.updateResults(results, comments);
-		return;
-	}
-	
 	var self = this;
-	
-	this._zotero.DB._connection.setProgressHandler(5000, {
-		onProgress: function (connection) {
-			if (self._cancelled) {
-				return true;
-			}
-		}
-	});
 	
 	this.pendingStatement = statement.executeAsync({
 		handleResult: function (storageResultSet) {
@@ -282,7 +253,7 @@ ZoteroAutoComplete.prototype.startSearch = function(searchString, searchParams, 
 		},
 		
 		handleError: function (e) {
-			//Components.utils.reportError(e.message);
+			Components.utils.reportError(e.message);
 		},
 		
 		handleCompletion: function (reason) {
@@ -352,10 +323,7 @@ ZoteroAutoComplete.prototype.updateResults = function (results, comments, ongoin
 ZoteroAutoComplete.prototype.stopSearch = function(){
 	if (this.pendingStatement) {
 		this._zotero.debug('Stopping autocomplete search');
-		// This appears to take as long as letting the query complete,
-		// so we flag instead and abort from the progress handler
-		//this.pendingStatement.cancel();
-		this._cancelled = true;
+		this.pendingStatement.cancel();
 	}
 }
 

commit 2069b5b396c0ec61717f4f66e4c96e57b8c08607

Fix switching in and out of connector mode
Broken by 9d3f55be5193763d4c6d2afc3ddecea0bf5a9f39

chrome/content/zotero/xpcom/db.js
@@ -794,8 +794,11 @@ Zotero.DBConnection.prototype.checkException = function (e) {
 Zotero.DBConnection.prototype.closeDatabase = function () {
 	if(this._connection) {
 		this.stopDummyStatement();
-		this._connection.asyncClose();
-		return true;
+		var deferred = Q.defer();
+		this._connection.asyncClose(deferred.resolve);
+		return deferred.promise;
+	} else {
+		return Q();
 	}
 }
 

chrome/content/zotero/xpcom/zotero.js
@@ -423,7 +423,7 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 		}
 		
 		// Register shutdown handler to call Zotero.shutdown()
-		var _shutdownObserver = {observe:Zotero.shutdown};
+		var _shutdownObserver = {observe:function() { Zotero.shutdown() }};
 		Services.obs.addObserver(_shutdownObserver, "quit-application", false);
 		
 		try {
@@ -783,7 +783,7 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 	}
 	
 	
-	this.shutdown = function (subject, topic, data) {
+	this.shutdown = function(callback) {
 		Zotero.debug("Shutting down Zotero");
 		
 		try {
@@ -811,10 +811,13 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 				Components.utils.forceGC();
 				
 				// unlock DB
-				Zotero.DB.closeDatabase();
-				
-				// broadcast that DB lock has been released
-				Zotero.IPC.broadcast("lockReleased");
+				Zotero.DB.closeDatabase().then(function() {				
+					// broadcast that DB lock has been released
+					Zotero.IPC.broadcast("lockReleased");
+					callback();
+				});
+			} else {
+				callback();
 			}
 		} catch(e) {
 			Zotero.debug(e);

components/zotero-service.js
@@ -164,11 +164,16 @@ ZoteroContext.prototype = {
 	 */
 	"switchConnectorMode":function(isConnector) {
 		if(isConnector !== this.isConnector) {
-			zContext.Zotero.shutdown();
-			
-			// create a new zContext
-			makeZoteroContext(isConnector);
-			zContext.Zotero.init();
+			zContext.Zotero.shutdown(function() {
+				try {
+					// create a new zContext
+					makeZoteroContext(isConnector);
+					zContext.Zotero.init();
+				} catch(e) {
+					dump(e.toSource());
+					throw e;
+				}
+			});
 		}
 		
 		return zContext;
@@ -294,9 +299,16 @@ function ZoteroService() {
 			} catch(e) {
 				if(e === "ZOTERO_SHOULD_START_AS_CONNECTOR") {
 					// if Zotero should start as a connector, reload it
-					zContext.Zotero.shutdown();
-					makeZoteroContext(true);
-					zContext.Zotero.init();
+					zContext.Zotero.shutdown(function() {
+						try {
+							makeZoteroContext(true);
+							zContext.Zotero.init();
+						} catch(e) {
+							dump(e.toSource());
+							Components.utils.reportError(e);
+							throw e;
+						}
+					});
 				} else {
 					dump(e.toSource());
 					Components.utils.reportError(e);