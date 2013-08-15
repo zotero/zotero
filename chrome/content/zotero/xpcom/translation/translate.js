zotero/zotero

commit dde09c54a201a4298815135cdd0eb76903992234

Fix handling of errors when saving items to data store asynchronously

chrome/content/zotero/xpcom/translation/translate.js
@@ -1304,8 +1304,6 @@ Zotero.Translate.Base.prototype = {
 				if(!this._waitingForRPC) this._detectTranslatorsCollected();
 			}
 		} else {
-			this._currentState = null;
-			
 			// unset return value is equivalent to true
 			if(returnValue === undefined) returnValue = true;
 			
@@ -1327,6 +1325,8 @@ Zotero.Translate.Base.prototype = {
 				
 				this._runHandler("error", error);
 			}
+			
+			this._currentState = null;
 			
 			// call handlers
 			this._runHandler("itemsDone", returnValue);

commit ff0f649a4d8a7f3ebcd4aec0932667bac2a40f8a

Call done handler after saving
Broken by dde09c54a201a4298815135cdd0eb76903992234

chrome/content/zotero/xpcom/translation/translate.js
@@ -1413,7 +1413,7 @@ Zotero.Translate.Base.prototype = {
 	 * Checks if saving done, and if so, fires done event
 	 */
 	"_checkIfDone":function() {
-		if(!this._savingItems && !this._savingAttachments.length && !this._currentState) {
+		if(!this._savingItems && !this._savingAttachments.length) {
 			this._runHandler("done", true);
 		}
 	},