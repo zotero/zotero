83db8c7e0623cf8eca1901a02df58021651b9c78
Frank Bennett <biercenator@gmail.com>
2013-09-06 12:39:37 +0900

    Add conferenceDate to date kludge code

--- chrome/content/zotero/xpcom/data/item.js
+++ chrome/content/zotero/xpcom/data/item.js
@@ -256,7 +256,7 @@ Zotero.Item.prototype.getField = function(field, unformatted, includeBaseMapped,
 	if (!unformatted) {
 		// Multipart date fields
 		// TEMP - filingDate
-		if (Zotero.ItemFields.isFieldOfBase(fieldID, 'date') || ['filingDate','priorityDate','publicationDate','originalDate','signingDate','openingDate','adoptionDate','newsCaseDate'].indexOf(field) > -1) {
+		if (Zotero.ItemFields.isFieldOfBase(fieldID, 'date') || ['filingDate','priorityDate','publicationDate','originalDate','signingDate','openingDate','adoptionDate','newsCaseDate','conferenceDate'].indexOf(field) > -1) {
 			var value = Zotero.Date.multipartToStr(value);
 		}
 	}
@@ -945,8 +945,8 @@ Zotero.Item.prototype.setField = function(field, value, loadIn, lang, force_top)
 	
 	if (!loadIn) {
 		// Save date field etc as multipart dates
-		// fieldIDs represent: ['filingDate','newsCaseDate','priorityDate','publicationDate','originalDate','signingDate','openingDate','adoptionDate']
-		if ((Zotero.ItemFields.isFieldOfBase(fieldID, 'date') || [121,1265,266,1268,1272,1277,1278,1279].indexOf(field) > -1)
+		// fieldIDs represent: ['filingDate','newsCaseDate','priorityDate','publicationDate','originalDate','signingDate','openingDate','adoptionDate','conferenceDate']
+		if ((Zotero.ItemFields.isFieldOfBase(fieldID, 'date') || [121,1265,266,1268,1272,1277,1278,1279,1283].indexOf(fieldID) > -1)
 			&& !Zotero.Date.isMultipart(value)) {
 			value = Zotero.Date.strToMultipart(value);
 		}
