4cb938443f4e259c794c0e308f10c2988012f51c
Frank Bennett <biercenator@gmail.com>
2013-11-12 21:37:45 +0900

    Add commenter to case type

--- resource/schema/system.sql
+++ resource/schema/system.sql
@@ -1,4 +1,4 @@
--- 10058
+-- 10059
 
 -- Copyright (c) 2009 Center for History and New Media
 --                    George Mason University, Fairfax, Virginia, USA
@@ -1341,6 +1341,7 @@ INSERT INTO itemTypeCreatorTypes VALUES(16,28,0);
 INSERT INTO itemTypeCreatorTypes VALUES(16,2,0);
 INSERT INTO itemTypeCreatorTypes VALUES(17,1,1);
 INSERT INTO itemTypeCreatorTypes VALUES(17,13,0);
+INSERT INTO itemTypeCreatorTypes VALUES(17,23,0); -- commenter [ADDED]
 INSERT INTO itemTypeCreatorTypes VALUES(17,2,0);
 INSERT INTO itemTypeCreatorTypes VALUES(18,1261,1);
 INSERT INTO itemTypeCreatorTypes VALUES(18,2,0);
