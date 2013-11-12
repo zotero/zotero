0d7e14bb6bbb92fded98780e012f09e30e183a91
Dan Stillman <dstillman@zotero.org>
2013-11-12 00:33:19 -0500

    Update versions, submodules, and repotime

--- chrome/content/zotero/xpcom/zotero.js
+++ chrome/content/zotero/xpcom/zotero.js
@@ -39,7 +39,7 @@ const ZOTERO_CONFIG = {
 	BOOKMARKLET_ORIGIN : 'https://www.zotero.org',
 	HTTP_BOOKMARKLET_ORIGIN : 'http://www.zotero.org',
 	BOOKMARKLET_URL: 'https://www.zotero.org/bookmarklet/',
-	VERSION: "4.0.14.SOURCE"
+	VERSION: "4.0.15.SOURCE"
 };
 
 // Commonly used imports accessible anywhere
