2f9c7fd8393088735b91d060f6082f44b271ab06
Joscha Legewie <jpl2136@columbia.edu>
2014-02-03 14:03:36 +0100

    TinyMCE Improvements: Open links from note window with popup and auto-create links

--- resource/tinymce/plugins/linksmenu/editor_plugin.js
+++ resource/tinymce/plugins/linksmenu/editor_plugin.js
@@ -1,22 +1,13 @@
-/**
- * editor_plugin_src.js
- *
- * Copyright 2009, Moxiecode Systems AB
- * Released under LGPL License.
- *
- * License: http://tinymce.moxiecode.com/license
- * Contributing: http://tinymce.moxiecode.com/contributing
- */
-
 (function() {
 	var Event = tinymce.dom.Event, each = tinymce.each, DOM = tinymce.DOM;
 
 	/**
-	 * This plugin a context menu to TinyMCE editor instances.
+	 * This plugin adds a left-click context menu to links in the TinyMCE editor for Zotero.
+	 * Code adopted and modified from TinyMCE contextmenu plugin.
 	 *
-	 * @class tinymce.plugins.ContextMenu
+	 * @class tinymce.plugins.LinksMenu
 	 */
-	tinymce.create('tinymce.plugins.ContextMenu', {
+	tinymce.create('tinymce.plugins.LinksMenu', {
 		/**
 		 * Initializes the plugin, this will be executed after the plugin has been created.
 		 * This call is done before the editor instance has finished it's initialization so use the onInit event
@@ -33,36 +24,40 @@
 
 			contextmenuNeverUseNative = ed.settings.contextmenu_never_use_native;
 
+			// add editor command to open links through zoteroHandleEvent
+			ed.addCommand('openlink', function(command) {
+				var node = tinyMCE.activeEditor.selection.getNode();
+				if (node.nodeName == 'A') {
+					zoteroHandleEvent({ type: 'openlink', target: node });
+				}
+			});
+
 			/**
 			 * This event gets fired when the context menu is shown.
 			 *
-			 * @event onContextMenu
-			 * @param {tinymce.plugins.ContextMenu} sender Plugin instance sending the event.
+			 * @event onClick
+			 * @param {tinymce.plugins.LinksMenu} sender Plugin instance sending the event.
 			 * @param {tinymce.ui.DropMenu} menu Drop down menu to fill with more items if needed.
 			 */
-			t.onContextMenu = new tinymce.util.Dispatcher(this);
+			t.onClick = new tinymce.util.Dispatcher(this);
 
 			hideMenu = function(e) {
 				hide(ed, e);
 			};
 
-			showMenu = ed.onContextMenu.add(function(ed, e) {
-				// Block TinyMCE menu on ctrlKey and work around Safari issue
-				if ((realCtrlKey !== 0 ? realCtrlKey : e.ctrlKey) && !contextmenuNeverUseNative)
+			showMenu = ed.onClick.add(function(ed, e) {
+				// only show when <a> node + Block TinyMCE menu on ctrlKey and work around Safari issue
+				if (e.target.nodeName != 'A' || ((realCtrlKey !== 0 ? realCtrlKey : e.ctrlKey) && !contextmenuNeverUseNative))
 					return;
 
 				Event.cancel(e);
-
-				// Select the image if it's clicked. WebKit would other wise expand the selection
-				if (e.target.nodeName == 'IMG')
-					ed.selection.select(e.target);
 
 				t._getMenu(ed).showMenu(e.clientX || e.pageX, e.clientY || e.pageY);
 				Event.add(ed.getDoc(), 'click', hideMenu);
 
 				ed.nodeChanged();
 			});
-			
+
 			ed.onRemove.add(function() {
 				if (t._menu)
 					t._menu.removeAll();
@@ -105,10 +100,10 @@
 		 */
 		getInfo : function() {
 			return {
-				longname : 'Contextmenu',
-				author : 'Moxiecode Systems AB',
-				authorurl : 'http://tinymce.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/contextmenu',
+				longname : 'Linksmenu',
+				author : '',
+				authorurl : '',
+				infourl : '',
 				version : tinymce.majorVersion + "." + tinymce.minorVersion
 			};
 		},
@@ -123,7 +118,7 @@
 
 			p = DOM.getPos(ed.getContentAreaContainer());
 
-			m = ed.controlManager.createDropMenu('contextmenu', {
+			m = ed.controlManager.createDropMenu('linksmenu', {
 				offset_x : p.x + ed.getParam('contextmenu_offset_x', 0),
 				offset_y : p.y + ed.getParam('contextmenu_offset_y', 0),
 				constrain : 1,
@@ -132,34 +127,30 @@
 
 			t._menu = m;
 
-			m.add({title : 'advanced.cut_desc', icon : 'cut', cmd : 'Cut'}).setDisabled(col);
-			m.add({title : 'advanced.copy_desc', icon : 'copy', cmd : 'Copy'}).setDisabled(col);
-			m.add({title : 'advanced.paste_desc', icon : 'paste', cmd : 'Paste'});
+			m.add({
+				title : 'Open Link',
+				icon : 'link',
+				cmd : 'openlink',
+				ui : true
+			});
+			m.add({
+				title : 'Edit Link',
+				icon : 'link',
+				cmd : ed.plugins.advlink ? 'mceAdvLink' : 'mceLink',
+				ui : true
+			});
+			m.add({
+				title : 'advanced.unlink_desc',
+				icon : 'unlink',
+				cmd : 'UnLink'
+			});
 
-			if ((el.nodeName == 'A' && !ed.dom.getAttrib(el, 'name')) || !col) {
-				m.addSeparator();
-				// Added by Zotero
-				if(el.nodeName == 'A') m.add({title : 'Open Link', icon : 'link', cmd : 'openlink', ui : true });
-				m.add({title : 'advanced.link_desc', icon : 'link', cmd : ed.plugins.advlink ? 'mceAdvLink' : 'mceLink', ui : true});
-				m.add({title : 'advanced.unlink_desc', icon : 'unlink', cmd : 'UnLink'});
-			}
-
-			m.addSeparator();
-			m.add({title : 'advanced.image_desc', icon : 'image', cmd : ed.plugins.advimage ? 'mceAdvImage' : 'mceImage', ui : true});
-
-			m.addSeparator();
-			am = m.addMenu({title : 'contextmenu.align'});
-			am.add({title : 'contextmenu.left', icon : 'justifyleft', cmd : 'JustifyLeft'});
-			am.add({title : 'contextmenu.center', icon : 'justifycenter', cmd : 'JustifyCenter'});
-			am.add({title : 'contextmenu.right', icon : 'justifyright', cmd : 'JustifyRight'});
-			am.add({title : 'contextmenu.full', icon : 'justifyfull', cmd : 'JustifyFull'});
-
-			t.onContextMenu.dispatch(t, m, el, col);
+			t.onClick.dispatch(t, m, el, col);
 
 			return m;
 		}
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('contextmenu', tinymce.plugins.ContextMenu);
+	tinymce.PluginManager.add('linksmenu', tinymce.plugins.LinksMenu);
 })();
