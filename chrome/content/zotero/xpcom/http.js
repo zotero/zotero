zotero/zotero

commit 62e779beecca3f1e126dc607c98b741416bceaf6

Show redirect URL in the sync stream listener (for file downloads)
Also consolidate the display-URL-password-hiding logic in http.js

chrome/content/zotero/xpcom/http.js
@@ -68,12 +68,8 @@ Zotero.HTTP = new function() {
 	this.promise = function promise(method, url, options) {
 		if (url instanceof Components.interfaces.nsIURI) {
 			// Don't display password in console
-			var dispURL = url.clone();
-			if (dispURL.password) {
-				dispURL.password = "********";
-			}
+			var dispURL = this.getDisplayURI(url).spec;
 			url = url.spec;
-			dispURL = dispURL.spec;
 		}
 		else {
 			var dispURL = url;
@@ -212,10 +208,7 @@ Zotero.HTTP = new function() {
 	this.doGet = function(url, onDone, responseCharset, cookieSandbox) {
 		if (url instanceof Components.interfaces.nsIURI) {
 			// Don't display password in console
-			var disp = url.clone();
-			if (disp.password) {
-				disp.password = "********";
-			}
+			var disp = this.getDisplayURI(url);
 			Zotero.debug("HTTP GET " + disp.spec);
 			url = url.spec;
 		}
@@ -276,10 +269,7 @@ Zotero.HTTP = new function() {
 	this.doPost = function(url, body, onDone, headers, responseCharset, cookieSandbox) {
 		if (url instanceof Components.interfaces.nsIURI) {
 			// Don't display password in console
-			var disp = url.clone();
-			if (disp.password) {
-				disp.password = "********";
-			}
+			var disp = this.getDisplayURI(url);
 			url = url.spec;
 		}
 		
@@ -363,10 +353,7 @@ Zotero.HTTP = new function() {
 	this.doHead = function(url, onDone, requestHeaders, cookieSandbox) {
 		if (url instanceof Components.interfaces.nsIURI) {
 			// Don't display password in console
-			var disp = url.clone();
-			if (disp.password) {
-				disp.password = "********";
-			}
+			var disp = this.getDisplayURI(url);
 			Zotero.debug("HTTP HEAD " + disp.spec);
 			url = url.spec;
 		}
@@ -424,10 +411,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.doOptions = function (uri, callback) {
 		// Don't display password in console
-		var disp = uri.clone();
-		if (disp.password) {
-			disp.password = "********";
-		}
+		var disp = this.getDisplayURI(uri);
 		Zotero.debug("HTTP OPTIONS for " + disp.spec);
 		
 		if (Zotero.HTTP.browserIsOffline()){
@@ -619,10 +603,7 @@ Zotero.HTTP = new function() {
 		}
 		
 		// Don't display password in console
-		var disp = uri.clone();
-		if (disp.password) {
-			disp.password = "********";
-		}
+		var disp = this.getDisplayURI(uri);
 		
 		var bodyStart = body.substr(0, 1024);
 		Zotero.debug("HTTP " + method + " "
@@ -672,10 +653,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doMkCol = function (uri, callback) {
 		// Don't display password in console
-		var disp = uri.clone();
-		if (disp.password) {
-			disp.password = "********";
-		}
+		var disp = this.getDisplayURI(uri);
 		Zotero.debug("HTTP MKCOL " + disp.spec);
 		
 		if (Zotero.HTTP.browserIsOffline()) {
@@ -709,10 +687,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doPut = function (uri, body, callback) {
 		// Don't display password in console
-		var disp = uri.clone();
-		if (disp.password) {
-			disp.password = "********";
-		}
+		var disp = this.getDisplayURI(uri);
 		
 		var bodyStart = "'" + body.substr(0, 1024) + "'";
 		Zotero.debug("HTTP PUT "
@@ -754,10 +729,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doDelete = function (uri, callback) {
 		// Don't display password in console
-		var disp = uri.clone();
-		if (disp.password) {
-			disp.password = "********";
-		}
+		var disp = this.getDisplayURI(uri);
 		
 		Zotero.debug("WebDAV DELETE to " + disp.spec);
 		
@@ -782,6 +754,15 @@ Zotero.HTTP = new function() {
 		};
 		xmlhttp.send(null);
 		return xmlhttp;
+	}
+	
+	
+	this.getDisplayURI = function (uri) {
+		var disp = uri.clone();
+		if (disp.password) {
+			disp.password = "********";
+		}
+		return disp;
 	}
 	
 	

chrome/content/zotero/xpcom/storage/streamListener.js
@@ -141,6 +141,9 @@ Zotero.Sync.Storage.StreamListener.prototype = {
 	// nsIHttpEventSink
 	onRedirect: function (oldChannel, newChannel) {
 		Zotero.debug('onRedirect');
+		
+		var newURL = Zotero.HTTP.getDisplayURI(newChannel.URI).spec;
+		Zotero.debug("Redirecting to " + newURL);
 	},
 	
 	

commit 47c466163542818f23aa499388d048ce150cb304

Fix WebDAV syncing (broken in 62e779bee)

chrome/content/zotero/xpcom/http.js
@@ -603,7 +603,7 @@ Zotero.HTTP = new function() {
 		}
 		
 		// Don't display password in console
-		var disp = this.getDisplayURI(uri);
+		var disp = Zotero.HTTP.getDisplayURI(uri);
 		
 		var bodyStart = body.substr(0, 1024);
 		Zotero.debug("HTTP " + method + " "
@@ -653,7 +653,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doMkCol = function (uri, callback) {
 		// Don't display password in console
-		var disp = this.getDisplayURI(uri);
+		var disp = Zotero.HTTP.getDisplayURI(uri);
 		Zotero.debug("HTTP MKCOL " + disp.spec);
 		
 		if (Zotero.HTTP.browserIsOffline()) {
@@ -687,7 +687,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doPut = function (uri, body, callback) {
 		// Don't display password in console
-		var disp = this.getDisplayURI(uri);
+		var disp = Zotero.HTTP.getDisplayURI(uri);
 		
 		var bodyStart = "'" + body.substr(0, 1024) + "'";
 		Zotero.debug("HTTP PUT "
@@ -729,7 +729,7 @@ Zotero.HTTP = new function() {
 	 */
 	this.WebDAV.doDelete = function (uri, callback) {
 		// Don't display password in console
-		var disp = this.getDisplayURI(uri);
+		var disp = Zotero.HTTP.getDisplayURI(uri);
 		
 		Zotero.debug("WebDAV DELETE to " + disp.spec);
 		