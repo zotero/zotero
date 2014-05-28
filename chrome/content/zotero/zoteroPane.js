db6ec2b160b6f80bfb70d3c06a53a98be85d9405
Dan Stillman <dstillman@zotero.org>
2014-05-28 14:31:58 -0400

    Persist single position/size for all note windows
    Previously, position/size was persisted for each item's note
    individually, but that meant that there was no default position/size for
    the note window and an entry was created in localstore.rdf for
    every note opened in a new window. There's also a good chance people had
    no idea what was going on.

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -3012,7 +3012,7 @@ var ZoteroPane = new function()
 			
 			var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
 					.getService(Components.interfaces.nsIWindowMediator);
-			var e = wm.getEnumerator('');
+			var e = wm.getEnumerator('zotero:note');
 			while (e.hasMoreElements()) {
 				var w = e.getNext();
 				if (w.name == name) {
@@ -3022,10 +3022,8 @@ var ZoteroPane = new function()
 			}
 		}
 		
-		window.open('chrome://zotero/content/note.xul?v=1'
-			+ (itemID ? '&id=' + itemID : '') + (col ? '&coll=' + col : '')
-			+ (parentItemID ? '&p=' + parentItemID : ''),
-			name, 'chrome,resizable,centerscreen');
+		var io = { itemID: itemID, collectionID: col, parentItemID: parentItemID };
+		window.openDialog('chrome://zotero/content/note.xul', name, 'chrome,resizable,centerscreen', io);
 	}
 	
 	
