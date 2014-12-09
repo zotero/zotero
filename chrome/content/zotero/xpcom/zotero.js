c28ef6f2d61031e517004fc9730d7d103a2673a8
Dan Stillman <dstillman@zotero.org>
2014-12-09 18:36:06 -0500

    Use HTTPS for proxy auth check

--- chrome/content/zotero/xpcom/zotero.js
+++ chrome/content/zotero/xpcom/zotero.js
@@ -30,7 +30,7 @@ const ZOTERO_CONFIG = {
 	REPOSITORY_RETRY_INTERVAL: 3600, // 1 hour
 	BASE_URI: 'http://zotero.org/',
 	WWW_BASE_URL: 'http://www.zotero.org/',
-	PROXY_AUTH_URL: 'http://zotero.org.s3.amazonaws.com/proxy-auth',
+	PROXY_AUTH_URL: 'https://s3.amazonaws.com/zotero.org/proxy-auth',
 	SYNC_URL: 'https://sync.zotero.org/',
 	API_URL: 'https://api.zotero.org/',
 	API_VERSION: 2,
