2d0df16509c03d74e8c8ed1e7dea8a49212fd3db
Dan Stillman <dstillman@zotero.org>
2014-12-09 20:43:48 -0500

    Switch WWW_BASE_URL to HTTPS

--- resource/config.js
+++ resource/config.js
@@ -4,7 +4,7 @@ var ZOTERO_CONFIG = {
 	REPOSITORY_CHECK_INTERVAL: 86400, // 24 hours
 	REPOSITORY_RETRY_INTERVAL: 3600, // 1 hour
 	BASE_URI: 'http://zotero.org/',
-	WWW_BASE_URL: 'http://www.zotero.org/',
+	WWW_BASE_URL: 'https://www.zotero.org/',
 	PROXY_AUTH_URL: 'https://s3.amazonaws.com/zotero.org/proxy-auth',
 	SYNC_URL: 'https://sync.zotero.org/',
 	API_URL: 'https://api.zotero.org/',
