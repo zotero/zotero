fcdb4fb4cb212ff9af399c14d2b7b4471a4dc8a8
Dan Stillman <dstillman@zotero.org>
2015-03-18 19:04:57 -0400

    Additional safety check when checking for persisted item pane state
    I don't think there's any way for this to happen short of manual
    preference editing, and the pane width would have to be exactly 250px,
    and it's pretty harmless anyway, but might as well be safe.

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -4067,6 +4067,7 @@ var ZoteroPane = new function()
 				if (el.id == 'zotero-items-splitter') continue;
 				// And don't restore to min-width if splitter was collapsed
 				if (el.id == 'zotero-item-pane' && attr == 'width' && elValues[attr] == 250
+						&& 'zotero-items-splitter' in serializedValues
 						&& serializedValues['zotero-items-splitter'].state == 'collapsed') {
 					continue;
 				}
