0eea3eaf9961adcd1374499f73c8e8debf779a0e
Dan Stillman <dstillman@zotero.org>
2015-06-02 03:48:17 -0400

    Reenable emptyTrash() assertion to see if it still fails on Travis

--- test/tests/itemsTest.js
+++ test/tests/itemsTest.js
@@ -110,8 +110,7 @@ describe("Zotero.Items", function () {
 			assert.isFalse(yield Zotero.Items.getAsync(id2));
 			assert.isFalse(yield Zotero.Items.getAsync(id3));
 			
-			// TEMP: This is failing on Travis due to a race condition
-			//assert.equal(zp.itemsView.rowCount, 0)
+			assert.equal(zp.itemsView.rowCount, 0)
 		})
 	})
 });
