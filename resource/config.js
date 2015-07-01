4c751740b4ea1ac188ebbd13c916b3e5d4f22399
Dan Stillman <dstillman@zotero.org>
2015-06-30 23:41:06 -0400

    Update versions and translators

--- resource/config.js
+++ resource/config.js
@@ -14,7 +14,7 @@ var ZOTERO_CONFIG = {
 	BOOKMARKLET_ORIGIN: 'https://www.zotero.org',
 	HTTP_BOOKMARKLET_ORIGIN: 'http://www.zotero.org',
 	BOOKMARKLET_URL: 'https://www.zotero.org/bookmarklet/',
-	VERSION: '4.0.27.SOURCE'
+	VERSION: '4.0.27.1.SOURCE'
 };
 
 EXPORTED_SYMBOLS = ["ZOTERO_CONFIG"];
