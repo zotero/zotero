zotero/zotero

commit f5ce2d238eb20dcf39f1d3ef8712a45a343c65d0

Fixes #546, Advanced search : match "any" is broken

chrome/content/zotero/xpcom/collectionTreeView.js
@@ -2148,21 +2148,6 @@ Zotero.ItemGroup.prototype.getSearchResults = function(asTempTable) {
 	
 	if(!Zotero.ItemGroupCache.lastResults) {
 		var s = this.getSearchObject();
-	
-		// FIXME: Hack to exclude group libraries for now
-		if (this.isSearch()) {
-			var currentLibraryID = this.ref.libraryID;
-			if (currentLibraryID) {
-				s.addCondition('libraryID', 'is', currentLibraryID);
-			}
-			else {
-				var groups = Zotero.Groups.getAll();
-				for each(var group in groups) {
-					s.addCondition('libraryID', 'isNot', group.libraryID);
-				}
-			}
-		}
-		
 		Zotero.ItemGroupCache.lastResults = s.search();
 		Zotero.ItemGroupCache.lastItemGroup = this;
 	}

chrome/content/zotero/advancedSearch.js
@@ -62,25 +62,7 @@ var ZoteroAdvancedSearch = new function() {
 			isSearchMode: function() { return true; },
 			getItems: function () {
 				var search = _searchBox.search.clone();
-				
-				// Hack to create a condition for the search's library --
-				// this logic should really go in the search itself instead of here
-				// and in collectionTreeView.js
-				var conditions = search.getSearchConditions();
-				if (!conditions.some(function (condition) condition.condition == 'libraryID')) {
-					let libraryID = _searchBox.search.libraryID;
-					// TEMP: libraryIDInt
-					if (libraryID) {
-						search.addCondition('libraryID', 'is', libraryID);
-					}
-					else {
-						let groups = Zotero.Groups.getAll();
-						for (let i=0; i<groups.length; i++) {
-							search.addCondition('libraryID', 'isNot', groups[i].libraryID);
-						}
-					}
-				}
-				
+				search.libraryID = _libraryID;
 				return Zotero.Items.get(search.search());
 			},
 			isLibrary: function () { return false; },

chrome/content/zotero/xpcom/search.js
@@ -36,7 +36,7 @@ Zotero.Search = function() {
 Zotero.Search.prototype._init = function () {
 	// Public members for access by public methods -- do not access directly
 	this._id = null;
-	this._libraryID = null;
+	this._libraryID; // TEMP: libraryIDInt
 	this._key = null;
 	this._name = null;
 	this._dateAdded = null;
@@ -290,7 +290,7 @@ Zotero.Search.prototype.save = function(fixGaps) {
 			this._changed.dateModified ?
 				this.dateModified : Zotero.DB.transactionDateTime,
 			Zotero.DB.transactionDateTime,
-			this.libraryID ? this.libraryID : this.libraryID,
+			this.libraryID ? this.libraryID : null,
 			key
 		];
 		
@@ -1139,6 +1139,23 @@ Zotero.Search.prototype._buildQuery = function(){
 			+ ")";
 	}
 	
+	// Limit to library search belongs to
+	//
+	// This is equivalent to adding libraryID as a search condition,
+	// but it works with ANY
+	if (this.libraryID !== undefined) {
+		sql += " AND (itemID IN (SELECT itemID FROM items WHERE libraryID";
+		// TEMP: libraryIDInt
+		if (this.libraryID) {
+			sql += "=?";
+			sqlParams.push(this.libraryID);
+		}
+		else {
+			sql += " IS NULL";
+		}
+		sql += "))";
+	}
+	
 	if (this._hasPrimaryConditions) {
 		sql += " AND ";
 		
@@ -1755,6 +1772,7 @@ Zotero.Searches = new function(){
 		for each(var row in rows) {
 			var search = new Zotero.Search;
 			search.id = row.id;
+			search.libraryID = libraryID;
 			searches.push(search);
 		}
 		return searches;

commit 238183a3fa14bda0de8249a23460754f5f908465

Fix collapse library ("-") for collections
Broken in f5ce2d238e

chrome/content/zotero/xpcom/collectionTreeView.js
@@ -653,7 +653,12 @@ Zotero.CollectionTreeView.prototype.collapseLibrary = function(self) {
 	
 	var found = false;
 	for (var i=self.rowCount-1; i>=0; i--) {
-		if (self._getItemAtRow(i).ref.libraryID !== selectedLibraryID) {
+		let rowLibraryID = self._getItemAtRow(i).ref.libraryID;
+		// TEMP: Remove in 5.0 when we can just compare libraryIDs without worrying about null
+		if (rowLibraryID === 0) {
+			rowLibraryID = null;
+		}
+		if (rowLibraryID !== selectedLibraryID) {
 			// Once we've moved beyond the original library, stop looking
 			if (found) {
 				break;
@@ -1013,9 +1018,7 @@ Zotero.CollectionTreeView.prototype._expandRow = function (row, forceOpen) {
 	// Unfiled items
 	if (showUnfiled) {
 		var s = new Zotero.Search;
-		if (isGroup) {
-			s.libraryID = libraryID;
-		}
+		s.libraryID = intLibraryID;
 		s.name = Zotero.getString('pane.collections.unfiled');
 		s.addCondition('libraryID', 'is', libraryID);
 		s.addCondition('unfiled', 'true');