zotero/zotero

commit f3daf6b4bc727eadd0b6951534255ee7d26cb793

Always capture up/down when QuickFormat reference panel is open
It was annoying if you pressed the up key too many times and the cursor
moved.

chrome/content/zotero/integration/quickFormat.js
@@ -1084,7 +1084,7 @@ var Zotero_QuickFormat = new function () {
 				selection.addRange(nodeRange);
 			}
 
-		} else if(keyCode === event.DOM_VK_UP) {
+		} else if(keyCode === event.DOM_VK_UP && referencePanel.state === "open") {
 			var selectedItem = referenceBox.selectedItem;
 
 			var previousSibling;
@@ -1104,9 +1104,9 @@ var Zotero_QuickFormat = new function () {
 					visibleItem = visibleItem.previousSibling;
 				}
 				referenceBox.ensureElementIsVisible(visibleItem);
-				event.preventDefault();
 			};
-		} else if(keyCode === event.DOM_VK_DOWN) {
+			event.preventDefault();
+		} else if(keyCode === event.DOM_VK_DOWN && referencePanel.state === "open") {
 			if((Zotero.isMac ? event.metaKey : event.ctrlKey)) {
 				// If meta key is held down, show the citation properties panel
 				var bubble = _getSelectedBubble();
@@ -1126,8 +1126,8 @@ var Zotero_QuickFormat = new function () {
 				if(nextSibling){
 					referenceBox.selectedItem = nextSibling;
 					referenceBox.ensureElementIsVisible(nextSibling);
-					event.preventDefault();
 				};
+				event.preventDefault();
 			}
 		} else {
 			_resetSearchTimer();

commit 4d354027b55fa413a3e8545aa2c08bb3879d4d4f

Fix Cmd/Ctrl-Down in Quick Format
Broken in f3daf6b4bc

chrome/content/zotero/integration/quickFormat.js
@@ -1106,14 +1106,14 @@ var Zotero_QuickFormat = new function () {
 				referenceBox.ensureElementIsVisible(visibleItem);
 			};
 			event.preventDefault();
-		} else if(keyCode === event.DOM_VK_DOWN && referencePanel.state === "open") {
+		} else if(keyCode === event.DOM_VK_DOWN) {
 			if((Zotero.isMac ? event.metaKey : event.ctrlKey)) {
 				// If meta key is held down, show the citation properties panel
 				var bubble = _getSelectedBubble();
 
 				if(bubble) _showCitationProperties(bubble);
 				event.preventDefault();
-			} else {
+			} else if (referencePanel.state === "open") {
 				var selectedItem = referenceBox.selectedItem;
 				var nextSibling;
 				