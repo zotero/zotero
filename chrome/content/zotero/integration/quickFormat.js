zotero/zotero

commit 7d404e8d4ad636987acfe33d0b8620263004d6d0

Closes #896, Legacy array/generator comprehension is removed in Firefox 46
Possible there are some bugs here (and it also may fix a couple existing bugs).

chrome/content/zotero/integration/quickFormat.js
@@ -303,9 +303,13 @@ var Zotero_QuickFormat = new function () {
 						citedItemsMatchingSearch = [];
 					for(var i=0, iCount=citedItems.length; i<iCount; i++) {
 						// Generate a string to search for each item
-						var item = citedItems[i],
-							itemStr = [creator.ref.firstName+" "+creator.ref.lastName for (creator of item.getCreators())];
-						itemStr = itemStr.concat([item.getField("title"), item.getField("date", true, true).substr(0, 4)]).join(" ");
+						let item = citedItems[i];
+						let itemStr = item.getCreators()
+							.map(creator => {
+								creator.ref.firstName + " " + creator.ref.lastName
+							})
+							.concat([item.getField("title"), item.getField("date", true, true).substr(0, 4)])
+							.join(" ");
 						
 						// See if words match
 						for(var j=0, jCount=splits.length; j<jCount; j++) {

chrome/content/zotero/bindings/itembox.xml
@@ -1729,7 +1729,7 @@
 							this._addCreatorRow = false;
 							
 							//Filter out bad names
-							var nameArray = [tempName for each(tempName in rawNameArray) if(tempName)];
+							var nameArray = rawNameArray.filter(name => name);
 							
 							//If not adding names at the end of the creator list, make new creator 
 							//entries and then shift down existing creators.

chrome/content/zotero/fileInterface.js
@@ -328,7 +328,7 @@ var Zotero_File_Interface = new function() {
 			translation.setHandler("done", function(obj, worked) {
 				// add items to import collection
 				if(importCollection) {
-					importCollection.addItems([item.id for each(item in obj.newItems)]);
+					importCollection.addItems(obj.newItems.map(item => item.id));
 				}
 				
 				Zotero.DB.commitTransaction();
@@ -467,7 +467,10 @@ var Zotero_File_Interface = new function() {
 							   getService(Components.interfaces.nsIClipboard);
 		
 		var style = Zotero.Styles.get(style).getCiteProc(locale);
-		var citation = {"citationItems":[{id:item.id} for each(item in items)], properties:{}};
+		var citation = {
+			citationItems: items.map(item => ({ id: item.id })),
+			properties: {}
+		};
 		
 		// add HTML
 		var bibliography = style.previewCitationCluster(citation, [], [], "html");

chrome/content/zotero/integration/editBibliographyDialog.js
@@ -99,7 +99,7 @@ var Zotero_Bibliography_Dialog = new function () {
 			_addButton.disabled = true;
 			_removeButton.disabled = false;
 			_updateRevertButtonStatus();
-			[_itemList.toggleItemSelection(item) for each(item in itemsToSelect)];
+			itemsToSelect.forEach(item => _itemList.toggleItemSelection(item));
 			_itemList.ensureIndexIsVisible(itemsToSelect[0]);
 		}
 		_suppressAllSelectEvents = false;

chrome/content/zotero/locateManager.xul
@@ -231,7 +231,7 @@ To add a new preference:
 				hidden = false;
 			}
 			
-			[engine.hidden = hidden for each(engine in Zotero.LocateManager.getEngines())];
+			Zotero.LocateManager.getEngines().forEach(engine => engine.hidden = hidden);
 
 			refreshLocateEnginesList();
 		}

chrome/content/zotero/rtfScan.js
@@ -528,7 +528,7 @@ var Zotero_RTFScan = new function() {
 		}
 		Zotero.debug(cslCitations);
 		
-		itemIDs = [itemID for(itemID in itemIDs)];
+		itemIDs = Object.keys(itemIDs);
 		Zotero.debug(itemIDs);
 		
 		// prepare the list of rendered citations

chrome/content/zotero/tools/cslpreview.js
@@ -95,13 +95,17 @@ var Zotero_CSL_Preview = new function() {
 		
 		// Generate multiple citations
 		var citations = styleEngine.previewCitationCluster(
-		{"citationItems":[{"id":item.id} for each(item in items)], "properties":{}},
-		[], [], "html");
+			{
+				citationItems: items.map(item => ({ id: item.id })),
+				properties: {}
+			},
+			[], [], "html"
+		);
 		
 		// Generate bibliography
 		var bibliography = '';
 		if(style.hasBibliography) {
-			styleEngine.updateItems([item.id for each(item in items)]);
+			styleEngine.updateItems(items.map(item => item.id));
 			bibliography = Zotero.Cite.makeFormattedBibliography(styleEngine, "html");
 		}
 		

chrome/content/zotero/xpcom/cite.js
@@ -73,7 +73,7 @@ Zotero.Cite = {
 	 */
 	"makeFormattedBibliographyOrCitationList":function(cslEngine, items, format, asCitationList) {
 		cslEngine.setOutputFormat(format);
-		cslEngine.updateItems([item.id for each(item in items)]);
+		cslEngine.updateItems(items.map(item => item.id));
 		 		
 		if(!asCitationList) {
 			var bibliography = Zotero.Cite.makeFormattedBibliography(cslEngine, format);
@@ -297,7 +297,7 @@ Zotero.Cite = {
 		var slashIndex;
 		
 		if(id instanceof Array) {
-			return [Zotero.Cite.getItem(anId) for each(anId in id)];
+			return id.map(anId => Zotero.Cite.getItem(anId));
 		} else if(typeof id === "string" && (slashIndex = id.indexOf("/")) !== -1) {		
 			var sessionID = id.substr(0, slashIndex),
 				session = Zotero.Integration.sessions[sessionID],

commit 11e3603a736ea2c795bf504abfcb6ea84d6facbe

Fix creator search in Quick Format dialog
Broken by 7d404e8d4a

chrome/content/zotero/integration/quickFormat.js
@@ -305,9 +305,7 @@ var Zotero_QuickFormat = new function () {
 						// Generate a string to search for each item
 						let item = citedItems[i];
 						let itemStr = item.getCreators()
-							.map(creator => {
-								creator.ref.firstName + " " + creator.ref.lastName
-							})
+							.map(creator => creator.ref.firstName + " " + creator.ref.lastName)
 							.concat([item.getField("title"), item.getField("date", true, true).substr(0, 4)])
 							.join(" ");
 						