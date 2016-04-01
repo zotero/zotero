4f54214f11435e9788f1ac8fad5389574ac3f378
Dan Stillman <dstillman@zotero.org>
2016-04-01 02:49:50 -0400

    Disable auto-sync pref for tests
    We don't want auto-sync to try to run during unrelated tests.
    Additionally, even though the sync process wouldn't actually start due
    to the absence of an API key (which is injected directly into the sync
    functions for sync tests), since auto-sync kicks off after a one-second
    delay on pane open, during tests the Zotero object has often been torn
    down by the time the check code runs, producing spurious errors in the
    console.

--- test/runtests.sh
+++ test/runtests.sh
@@ -118,6 +118,7 @@ user_pref("extensions.zotero.firstRun2", false);
 user_pref("extensions.zotero.reportTranslationFailure", false);
 user_pref("extensions.zotero.httpServer.enabled", true);
 user_pref("extensions.zotero.backup.numBackups", 0);
+user_pref("extensions.zotero.sync.autoSync", false);
 user_pref("xpinstall.signatures.required", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("datareporting.healthreport.service.enabled", false);
