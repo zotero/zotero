9b41c8dd9708ab5396bbb591a8f3bd0af1c21bb7
Dan Stillman <dstillman@zotero.org>
2016-05-07 13:19:27 -0400

    Update version

--- resource/config.js
+++ resource/config.js
@@ -15,7 +15,7 @@ var ZOTERO_CONFIG = {
 	BOOKMARKLET_ORIGIN: 'https://www.zotero.org',
 	HTTP_BOOKMARKLET_ORIGIN: 'http://www.zotero.org',
 	BOOKMARKLET_URL: 'https://www.zotero.org/bookmarklet/',
-	VERSION: '4.0.29.9.SOURCE'
+	VERSION: '4.0.29.10.SOURCE'
 };
 
 EXPORTED_SYMBOLS = ["ZOTERO_CONFIG"];
