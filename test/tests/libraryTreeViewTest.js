91b0acac1e495755dc29683d1e37ce9340e3a426
Dan Stillman <dstillman@zotero.org>
2016-07-19 20:31:06 -0400

    Fix potential error in libraryTreeView::#_removeRow() test

--- test/tests/libraryTreeViewTest.js
+++ test/tests/libraryTreeViewTest.js
@@ -31,15 +31,15 @@ describe("Zotero.LibraryTreeView", function() {
 		it("should remove the last row", function* () {
 			var collection = yield createDataObject('collection');
 			yield waitForItemsLoad(win);
-			var item1 = yield createDataObject('item', { collections: [collection.id] });
-			var item2 = yield createDataObject('item', { collections: [collection.id] });
+			yield createDataObject('item', { collections: [collection.id] });
+			yield createDataObject('item', { collections: [collection.id] });
 			
 			var view = zp.itemsView;
-			assert.equal(view.getRowIndexByID(item2.id), 1);
+			var treeViewID = view.getRow(1).id;
 			zp.itemsView._removeRow(1);
 			
 			assert.equal(view.rowCount, 1);
-			assert.isFalse(view.getRowIndexByID(item2.id));
+			assert.isFalse(view.getRowIndexByID(treeViewID));
 		});
 	});
 })
