11fb0cdd10d3c6a286a918dfa6f93b3389bf74c0
Adomas Venčkauskas <adomas.ven@gmail.com>
2016-08-09 15:28:23 +0300

    Fixes a bug when zotero dir exists elsewhere and you want to select a custom one

--- chrome/content/zotero/xpcom/zotero.js
+++ chrome/content/zotero/xpcom/zotero.js
@@ -1116,7 +1116,13 @@ Components.utils.import("resource://gre/modules/osfile.jsm");
 				var fp = Components.classes["@mozilla.org/filepicker;1"]
 							.createInstance(nsIFilePicker);
 				fp.init(win, Zotero.getString('dataDir.selectDir'), nsIFilePicker.modeGetFolder);
-				fp.displayDirectory = Zotero.getZoteroDirectory();
+				try {
+					fp.displayDirectory = Zotero.getZoteroDirectory();
+				} catch (e) {
+					if(e.name == "ZOTERO_DIR_MAY_EXIST") {
+						fp.displayDirectory = e.dir;
+					}
+				}
 				fp.appendFilters(nsIFilePicker.filterAll);
 				if (fp.show() == nsIFilePicker.returnOK) {
 					var file = fp.file;
