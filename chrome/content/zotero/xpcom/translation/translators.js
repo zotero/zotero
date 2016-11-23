cb300005cd5223e5b5f286ebb565db7f0583114b
Dan Stillman <dstillman@zotero.org>
2016-11-22 20:55:27 -0500

    More Travis debugging

--- chrome/content/zotero/xpcom/translation/translators.js
+++ chrome/content/zotero/xpcom/translation/translators.js
@@ -43,6 +43,11 @@ Zotero.Translators = new function() {
 	 *     available (e.g., in updateBundledFiles()), to avoid unnecesary file reads
 	 */
 	this.reinit = Zotero.Promise.coroutine(function* (options = {}) {
+		// Travis debugging
+		Zotero.Debug.init(true);
+		Zotero.debug(new Error().stack);
+		Zotero.Debug.init();
+		
 		// Wait until bundled files have been updated, except when this is called by the schema update
 		// code itself
 		if (!options.fromSchemaUpdate) {
