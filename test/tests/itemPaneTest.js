zotero/zotero

commit 3a0e0cb0886bdcd105ec03d8bac12fff5079e2f7

Drastically speed up moving items to the trash
E.g., moving 3,600 items to the trash now takes 4 seconds instead of 62

Instead of saving each item, update internal state and database directly
(which is more brittle but worth it). Also avoid unnecessary sorting
after removing an item from the items tree.

chrome/content/zotero/xpcom/data/item.js
@@ -3603,6 +3603,28 @@ Zotero.Item.prototype.inCollection = function (collectionID) {
 };
 
 
+/**
+ * Update item deleted (i.e., trash) state without marking as changed or modifying DB
+ *
+ * This is used by Zotero.Items.trash().
+ *
+ * Database state must be set separately!
+ *
+ * @param {Boolean} deleted
+ */
+Zotero.DataObject.prototype.setDeleted = Zotero.Promise.coroutine(function* (deleted) {
+	if (!this.id) {
+		throw new Error("Cannot update deleted state of unsaved item");
+	}
+	
+	this._deleted = !!deleted;
+	
+	if (this._changed.deleted) {
+		delete this._changed.deleted;
+	}
+});
+
+
 Zotero.Item.prototype.getImageSrc = function() {
 	var itemType = Zotero.ItemTypes.getName(this.itemTypeID);
 	if (itemType == 'attachment') {

chrome/content/zotero/xpcom/data/items.js
@@ -103,12 +103,23 @@ Zotero.Items = function() {
 	
 	this._relationsTable = "itemRelations";
 	
+	
+	/**
+	 * @param {Integer} libraryID
+	 * @return {Promise<Boolean>} - True if library has items in trash, false otherwise
+	 */
+	this.hasDeleted = Zotero.Promise.coroutine(function* (libraryID) {
+		var sql = "SELECT COUNT(*) > 0 FROM items JOIN deletedItems USING (itemID) WHERE libraryID=?";
+		return !!(yield Zotero.DB.valueQueryAsync(sql, [libraryID]));
+	});
+	
+	
 	/**
 	 * Return items marked as deleted
 	 *
 	 * @param {Integer} libraryID - Library to search
 	 * @param {Boolean} [asIDs] - Return itemIDs instead of Zotero.Item objects
-	 * @return {Zotero.Item[]|Integer[]}
+	 * @return {Promise<Zotero.Item[]|Integer[]>}
 	 */
 	this.getDeleted = Zotero.Promise.coroutine(function* (libraryID, asIDs, days) {
 		var sql = "SELECT itemID FROM items JOIN deletedItems USING (itemID) "
@@ -818,14 +829,14 @@ Zotero.Items = function() {
 	this.trash = Zotero.Promise.coroutine(function* (ids) {
 		Zotero.DB.requireTransaction();
 		
+		var libraryIDs = new Set();
 		ids = Zotero.flattenArguments(ids);
-		
-		for (let i=0; i<ids.length; i++) {
-			let id = ids[i];
-			let item = yield this.getAsync(id);
+		var items = [];
+		for (let id of ids) {
+			let item = this.get(id);
 			if (!item) {
 				Zotero.debug('Item ' + id + ' does not exist in Items.trash()!', 1);
-				Zotero.Notifier.queue('delete', 'item', id);
+				Zotero.Notifier.queue('trash', 'item', id);
 				continue;
 			}
 			
@@ -833,15 +844,32 @@ Zotero.Items = function() {
 				throw new Error(item._ObjectType + " " + item.libraryKey + " is not editable");
 			}
 			
-			if (!Zotero.Libraries.hasTrash(item.libraryID)) {
-				throw new Error(Zotero.Libraries.getName(item.libraryID) + " does not have Trash");
+			if (!Zotero.Libraries.get(item.libraryID).hasTrash) {
+				throw new Error(Zotero.Libraries.getName(item.libraryID) + " does not have a trash");
 			}
 			
-			item.deleted = true;
-			yield item.save({
-				skipDateModifiedUpdate: true
-			});
+			items.push(item);
+			libraryIDs.add(item.libraryID);
 		}
+		
+		items.forEach(item => {
+			item.setDeleted(true);
+		});
+		yield Zotero.Utilities.Internal.forEachChunkAsync(ids, 250, Zotero.Promise.coroutine(function* (chunk) {
+			yield Zotero.DB.queryAsync(
+				"UPDATE items SET synced=1, clientDateModified=CURRENT_TIMESTAMP "
+					+ `WHERE itemID IN (${chunk.map(id => parseInt(id)).join(", ")})`
+			);
+			yield Zotero.DB.queryAsync(
+				"INSERT OR IGNORE INTO deletedItems (itemID) VALUES "
+					+ chunk.map(id => "(" + id + ")").join(", ")
+			);
+		}.bind(this)));
+		
+		Zotero.Notifier.queue('trash', 'item', ids);
+		Array.from(libraryIDs).forEach(libraryID => {
+			Zotero.Notifier.queue('refresh', 'trash', libraryID);
+		});
 	});
 	
 	

test/tests/itemsTest.js
@@ -87,6 +87,28 @@ describe("Zotero.Items", function () {
 		})
 	})
 	
+	
+	describe("#trash()", function () {
+		it("should send items to the trash", function* () {
+			var items = [];
+			items.push(
+				(yield createDataObject('item')),
+				(yield createDataObject('item')),
+				(yield createDataObject('item'))
+			);
+			var ids = items.map(item => item.id);
+			yield Zotero.Items.trashTx(ids);
+			items.forEach(item => {
+				assert.isTrue(item.deleted);
+				assert.isFalse(item.hasChanged());
+			});
+			assert.equal((yield Zotero.DB.valueQueryAsync(
+				`SELECT COUNT(*) FROM deletedItems WHERE itemID IN (${ids})`
+			)), 3);
+		});
+	});
+	
+	
 	describe("#emptyTrash()", function () {
 		it("should delete items in the trash", function* () {
 			var item1 = createUnsavedDataObject('item');

chrome/content/zotero/xpcom/collectionTreeView.js
@@ -338,8 +338,7 @@ Zotero.CollectionTreeView.prototype.notify = Zotero.Promise.coroutine(function* 
 		// If trash is refreshed, we probably need to update the icon from full to empty
 		if (type == 'trash') {
 			// libraryID is passed as parameter to 'refresh'
-			let deleted = yield Zotero.Items.getDeleted(ids[0], true);
-			this._trashNotEmpty[ids[0]] = !!deleted.length;
+			this._trashNotEmpty[ids[0]] = yield Zotero.Items.hasDeleted(ids[0]);
 			let row = this.getRowIndexByID("T" + ids[0]);
 			this._treebox.invalidateRow(row);
 		}

chrome/content/zotero/xpcom/itemTreeView.js
@@ -641,7 +641,6 @@ Zotero.ItemTreeView.prototype.notify = Zotero.Promise.coroutine(function* (actio
 			}
 			
 			madeChanges = true;
-			sort = true;
 		}
 	}
 	else if (type == 'item' && action == 'modify')

commit 3830aa11252562dd42e5722241cb5fb805193bfa

Mark trashed items as unsynced and update parents (including note list)
Regression from 3a0e0cb0886

chrome/content/zotero/xpcom/data/item.js
@@ -1775,6 +1775,7 @@ Zotero.Item.prototype._saveData = Zotero.Promise.coroutine(function* (env) {
 	// Update child item counts and contents
 	if (reloadParentChildItems) {
 		for (let parentItemID in reloadParentChildItems) {
+			// Keep in sync with Zotero.Items.trash()
 			let parentItem = yield this.ObjectsClass.getAsync(parentItemID);
 			yield parentItem.reload(['primaryData', 'childItems'], true);
 			parentItem.clearBestAttachmentState();

chrome/content/zotero/xpcom/data/items.js
@@ -852,12 +852,17 @@ Zotero.Items = function() {
 			libraryIDs.add(item.libraryID);
 		}
 		
+		var parentItemIDs = new Set();
 		items.forEach(item => {
 			item.setDeleted(true);
+			item.synced = false;
+			if (item.parentItemID) {
+				parentItemIDs.add(item.parentItemID);
+			}
 		});
 		yield Zotero.Utilities.Internal.forEachChunkAsync(ids, 250, Zotero.Promise.coroutine(function* (chunk) {
 			yield Zotero.DB.queryAsync(
-				"UPDATE items SET synced=1, clientDateModified=CURRENT_TIMESTAMP "
+				"UPDATE items SET synced=0, clientDateModified=CURRENT_TIMESTAMP "
 					+ `WHERE itemID IN (${chunk.map(id => parseInt(id)).join(", ")})`
 			);
 			yield Zotero.DB.queryAsync(
@@ -866,6 +871,11 @@ Zotero.Items = function() {
 			);
 		}.bind(this)));
 		
+		// Keep in sync with Zotero.Item::saveData()
+		for (let parentItemID of parentItemIDs) {
+			let parentItem = yield Zotero.Items.getAsync(parentItemID);
+			yield parentItem.reload(['primaryData', 'childItems'], true);
+		}
 		Zotero.Notifier.queue('trash', 'item', ids);
 		Array.from(libraryIDs).forEach(libraryID => {
 			Zotero.Notifier.queue('refresh', 'trash', libraryID);

test/tests/itemPaneTest.js
@@ -151,6 +151,55 @@ describe("Item pane", function () {
 			
 			assert.equal(noteBox.noteField.value, '<p>Test</p>');
 		})
+		
+		it("should refresh on note trash", function* () {
+			var item = yield createDataObject('item');
+			
+			var note = new Zotero.Item('note');
+			note.parentItemID = item.id;
+			yield note.saveTx();
+			yield itemsView.selectItem(note.id);
+			
+			// Wait for the note editor, just to be polite
+			var noteBox = doc.getElementById('zotero-note-editor');
+			var val = false;
+			do {
+				try {
+					val = noteBox.noteField.value;
+				}
+				catch (e) {}
+				yield Zotero.Promise.delay(1);
+			}
+			while (val === false)
+			
+			// Select parent and make sure there's 1 note
+			yield itemsView.selectItem(item.id);
+			var tabs = doc.getElementById('zotero-editpane-tabs');
+			var infoTab = doc.getElementById('zotero-editpane-info-tab');
+			var notesTab = doc.getElementById('zotero-editpane-notes-tab');
+			var notesList = doc.getElementById('zotero-editpane-dynamic-notes');
+			tabs.selectedItem = notesTab;
+			// Wait for note list to update
+			do {
+				yield Zotero.Promise.delay(1);
+			}
+			while (notesList.childNodes.length !== 1);
+			tabs.selectedItem = infoTab;
+			
+			// Select child and trash it
+			yield itemsView.selectItem(note.id);
+			yield Zotero.Items.trashTx([note.id]);
+			
+			// Select parent and select notes pane
+			yield itemsView.selectItem(item.id);
+			tabs.selectedItem = notesTab;
+			// Wait for note list to update
+			var len = false;
+			do {
+				yield Zotero.Promise.delay(1);
+			}
+			while (notesList.childNodes.length !== 0);
+		})
 	})
 	
 	describe("Feed buttons", function() {

test/tests/itemTest.js
@@ -301,6 +301,8 @@ describe("Zotero.Item", function () {
 		it("should be set to true after save", function* () {
 			var item = yield createDataObject('item');
 			item.deleted = true;
+			// Sanity check for itemsTest#trash()
+			assert.isTrue(item._changed.deleted);
 			yield item.saveTx();
 			assert.ok(item.deleted);
 		})

test/tests/itemsTest.js
@@ -92,19 +92,38 @@ describe("Zotero.Items", function () {
 		it("should send items to the trash", function* () {
 			var items = [];
 			items.push(
-				(yield createDataObject('item')),
-				(yield createDataObject('item')),
-				(yield createDataObject('item'))
+				(yield createDataObject('item', { synced: true })),
+				(yield createDataObject('item', { synced: true })),
+				(yield createDataObject('item', { synced: true }))
 			);
+			items.forEach(item => {
+				// Sanity-checked as true in itemTest#deleted
+				assert.isUndefined(item._changed.deleted);
+			});
 			var ids = items.map(item => item.id);
 			yield Zotero.Items.trashTx(ids);
 			items.forEach(item => {
 				assert.isTrue(item.deleted);
-				assert.isFalse(item.hasChanged());
+				// Item should be saved (can't use hasChanged() because that includes .synced)
+				assert.isUndefined(item._changed.deleted);
+				assert.isFalse(item.synced);
 			});
 			assert.equal((yield Zotero.DB.valueQueryAsync(
 				`SELECT COUNT(*) FROM deletedItems WHERE itemID IN (${ids})`
 			)), 3);
+			for (let item of items) {
+				assert.equal((yield Zotero.DB.valueQueryAsync(
+					`SELECT synced FROM items WHERE itemID=${item.id}`
+				)), 0);
+			}
+		});
+		
+		it("should update parent item when trashing child item", function* () {
+			var item = yield createDataObject('item');
+			var note = yield createDataObject('item', { itemType: 'note', parentID: item.id });
+			assert.lengthOf(item.getNotes(), 1);
+			yield Zotero.Items.trashTx([note.id]);
+			assert.lengthOf(item.getNotes(), 0);
 		});
 	});
 	