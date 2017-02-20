zotero/zotero

commit 80f888f37426059875b25e45641939d8e45d84c7

Fix replacement of existing item storage directories
- Make Zotero.Attachments.createDirectoryForItem() delete existing
  directory instead of moving it to orphaned-files; also now returns a
  string path instead of an nsIFile
- Use above function during file sync instead of
  _deleteExistingAttachmentFiles(), which was partly broken
- Fix throwing on errors when saving some attachment types

chrome/content/zotero/xpcom/fulltext.js
@@ -659,7 +659,15 @@ Zotero.Fulltext = Zotero.FullText = new function(){
 		// If file is stored outside of Zotero, create a directory for the item
 		// in the storage directory and save the cache file there
 		if (linkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE) {
-			var parentDirPath = (yield Zotero.Attachments.createDirectoryForItem(item)).path;
+			let path = item.getFilePath();
+			if (!path) {
+				Zotero.debug("Invalid path for item " + itemID);
+				return false;
+			}
+			var parentDirPath = OS.Path.dirname(path);
+			if (!(yield OS.File.exists(parentDirPath))) {
+				yield Zotero.Attachments.createDirectoryForItem(item);
+			}
 		}
 		else {
 			var parentDirPath = OS.Path.dirname(filePath);

chrome/content/zotero/xpcom/attachments.js
@@ -61,62 +61,61 @@ Zotero.Attachments = new function(){
 		}
 		
 		var attachmentItem, itemID, newFile, contentType;
-		return Zotero.DB.executeTransaction(function* () {
-			// Create a new attachment
-			attachmentItem = new Zotero.Item('attachment');
-			if (parentItemID) {
-				let {libraryID: parentLibraryID, key: parentKey} =
-					Zotero.Items.getLibraryAndKeyFromID(parentItemID);
-				attachmentItem.libraryID = parentLibraryID;
-			}
-			else if (libraryID) {
-				attachmentItem.libraryID = libraryID;
-			}
-			attachmentItem.setField('title', newName);
-			attachmentItem.parentID = parentItemID;
-			attachmentItem.attachmentLinkMode = this.LINK_MODE_IMPORTED_FILE;
-			if (collections) {
-				attachmentItem.setCollections(collections);
-			}
-			yield attachmentItem.save(saveOptions);
-			
-			// Create directory for attachment files within storage directory
-			destDir = yield this.createDirectoryForItem(attachmentItem);
-			
-			// Point to copied file
-			newFile = destDir.clone();
-			newFile.append(newName);
-			
-			// Copy file to unique filename, which automatically shortens long filenames
-			newFile = Zotero.File.copyToUnique(file, newFile);
-			
-			contentType = yield Zotero.MIME.getMIMETypeFromFile(newFile);
-			
-			attachmentItem.attachmentContentType = contentType;
-			attachmentItem.attachmentPath = newFile.path;
-			yield attachmentItem.save(saveOptions);
-		}.bind(this))
-		.then(function () {
-			return _postProcessFile(attachmentItem, newFile, contentType);
-		})
-		.catch(function (e) {
+		try {
+			yield Zotero.DB.executeTransaction(function* () {
+				// Create a new attachment
+				attachmentItem = new Zotero.Item('attachment');
+				if (parentItemID) {
+					let {libraryID: parentLibraryID, key: parentKey} =
+						Zotero.Items.getLibraryAndKeyFromID(parentItemID);
+					attachmentItem.libraryID = parentLibraryID;
+				}
+				else if (libraryID) {
+					attachmentItem.libraryID = libraryID;
+				}
+				attachmentItem.setField('title', newName);
+				attachmentItem.parentID = parentItemID;
+				attachmentItem.attachmentLinkMode = this.LINK_MODE_IMPORTED_FILE;
+				if (collections) {
+					attachmentItem.setCollections(collections);
+				}
+				yield attachmentItem.save(saveOptions);
+				
+				// Create directory for attachment files within storage directory
+				destDir = yield this.createDirectoryForItem(attachmentItem);
+				
+				// Point to copied file
+				newFile = OS.Path.join(destDir, newName);
+				
+				// Copy file to unique filename, which automatically shortens long filenames
+				newFile = Zotero.File.copyToUnique(file, newFile);
+				
+				contentType = yield Zotero.MIME.getMIMETypeFromFile(newFile);
+				
+				attachmentItem.attachmentContentType = contentType;
+				attachmentItem.attachmentPath = newFile.path;
+				yield attachmentItem.save(saveOptions);
+			}.bind(this));
+			yield _postProcessFile(attachmentItem, newFile, contentType);
+		}
+		catch (e) {
 			Zotero.logError(e);
-			var msg = "Failed importing file " + file.path;
-			Zotero.logError(msg);
+			Zotero.logError("Failed importing file " + file.path);
 			
 			// Clean up
 			try {
-				if (destDir && destDir.exists()) {
-					destDir.remove(true);
+				if (destDir && (yield OS.File.exists(destDir))) {
+					yield OS.File.removeDir(destDir);
 				}
 			}
 			catch (e) {
 				Zotero.logError(e);
 			}
-		}.bind(this))
-		.then(function () {
-			return attachmentItem;
-		});
+			
+			throw e;
+		}
+		
+		return attachmentItem;
 	});
 	
 	
@@ -174,39 +173,39 @@ Zotero.Attachments = new function(){
 		}
 		
 		var attachmentItem, itemID, destDir, newFile;
-		return Zotero.DB.executeTransaction(function* () {
-			// Create a new attachment
-			attachmentItem = new Zotero.Item('attachment');
-			let {libraryID, key: parentKey} = Zotero.Items.getLibraryAndKeyFromID(parentItemID);
-			attachmentItem.libraryID = libraryID;
-			attachmentItem.setField('title', title);
-			attachmentItem.setField('url', url);
-			attachmentItem.parentID = parentItemID;
-			attachmentItem.attachmentLinkMode = this.LINK_MODE_IMPORTED_URL;
-			attachmentItem.attachmentContentType = contentType;
-			attachmentItem.attachmentCharset = charset;
-			
-			// DEBUG: this should probably insert access date too so as to
-			// create a proper item, but at the moment this is only called by
-			// translate.js, which sets the metadata fields itself
-			itemID = yield attachmentItem.save();
-			
-			var storageDir = Zotero.getStorageDirectory();
-			destDir = this.getStorageDirectory(attachmentItem);
-			yield _moveOrphanedDirectory(destDir);
-			file.parent.copyTo(storageDir, destDir.leafName);
-			
-			// Point to copied file
-			newFile = destDir.clone();
-			newFile.append(file.leafName);
-			
-			attachmentItem.attachmentPath = newFile.path;
-			yield attachmentItem.save();
-		}.bind(this))
-		.then(function () {
-			return _postProcessFile(attachmentItem, newFile, contentType, charset);
-		})
-		.catch(function (e) {
+		try {
+			yield Zotero.DB.executeTransaction(function* () {
+				// Create a new attachment
+				attachmentItem = new Zotero.Item('attachment');
+				let {libraryID, key: parentKey} = Zotero.Items.getLibraryAndKeyFromID(parentItemID);
+				attachmentItem.libraryID = libraryID;
+				attachmentItem.setField('title', title);
+				attachmentItem.setField('url', url);
+				attachmentItem.parentID = parentItemID;
+				attachmentItem.attachmentLinkMode = this.LINK_MODE_IMPORTED_URL;
+				attachmentItem.attachmentContentType = contentType;
+				attachmentItem.attachmentCharset = charset;
+				
+				// DEBUG: this should probably insert access date too so as to
+				// create a proper item, but at the moment this is only called by
+				// translate.js, which sets the metadata fields itself
+				itemID = yield attachmentItem.save();
+				
+				var storageDir = Zotero.getStorageDirectory();
+				destDir = this.getStorageDirectory(attachmentItem);
+				yield OS.File.removeDir(destDir.path);
+				file.parent.copyTo(storageDir, destDir.leafName);
+				
+				// Point to copied file
+				newFile = destDir.clone();
+				newFile.append(file.leafName);
+				
+				attachmentItem.attachmentPath = newFile.path;
+				yield attachmentItem.save();
+			}.bind(this));
+			yield _postProcessFile(attachmentItem, newFile, contentType, charset);
+		}
+		catch (e) {
 			Zotero.logError(e);
 			
 			// Clean up
@@ -218,10 +217,10 @@ Zotero.Attachments = new function(){
 			catch (e) {
 				Zotero.logError(e, 1);
 			}
-		}.bind(this))
-		.then(function () {
-			return attachmentItem;
-		});
+			
+			throw e;
+		}
+		return attachmentItem;
 	});
 	
 	
@@ -787,21 +786,18 @@ Zotero.Attachments = new function(){
 	/**
 	 * Create directory for attachment files within storage directory
 	 *
-	 * If a directory exists with the same name, move it to orphaned-files
+	 * If a directory exists, delete and recreate
 	 *
 	 * @param {Number} itemID - Item id
-	 * @return {Promise<nsIFile>}
+	 * @return {Promise<String>} - Path of new directory
 	 */
 	this.createDirectoryForItem = Zotero.Promise.coroutine(function* (item) {
 		if (!(item instanceof Zotero.Item)) {
 			throw new Error("'item' must be a Zotero.Item");
 		}
-		var dir = this.getStorageDirectory(item);
-		yield _moveOrphanedDirectory(dir);
-		if (!dir.exists()) {
-			Zotero.debug("Creating directory " + dir.path);
-			dir.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
-		}
+		var dir = this.getStorageDirectory(item).path;
+		yield OS.File.removeDir(dir, { ignoreAbsent: true });
+		yield Zotero.File.createDirectoryIfMissingAsync(dir);
 		return dir;
 	});
 	
@@ -1158,55 +1154,6 @@ Zotero.Attachments = new function(){
 		nsIURL.spec = url;
 		return Zotero.MIME.getPrimaryExtension(contentType, nsIURL.fileExtension);
 	}
-	
-	
-	/**
-	 * If directory exists and is non-empty, move it to orphaned-files directory
-	 *
-	 * If empty, just remove it
-	 */
-	var _moveOrphanedDirectory = Zotero.Promise.coroutine(function* (dir) {
-		if (!dir.exists()) {
-			return;
-		}
-		
-		dir = dir.clone();
-		
-		// If directory is empty or has only hidden files, delete it
-		var files = dir.directoryEntries;
-		files.QueryInterface(Components.interfaces.nsIDirectoryEnumerator);
-		var empty = true;
-		while (files.hasMoreElements()) {
-			var file = files.getNext();
-			file.QueryInterface(Components.interfaces.nsIFile);
-			if (file.leafName[0] == '.') {
-				continue;
-			}
-			empty = false;
-			break;
-		}
-		files.close();
-		if (empty) {
-			dir.remove(true);
-			return;
-		}
-		
-		// Create orphaned-files directory if it doesn't exist
-		var orphaned = OS.Path.join(Zotero.DataDirectory.dir, 'orphaned-files');
-		yield Zotero.File.createDirectoryIfMissingAsync(orphaned);
-		
-		// Find unique filename for orphaned file
-		var orphanTarget = OS.Path.join(orphaned, dir.leafName);
-		var newName = null;
-		if (yield OS.File.exists(orphanTarget)) {
-			let newFile = yield OS.File.openUnique(orphanTarget, { humanReadable: true })
-			newName = OS.Path.basename(newFile.path);
-			newFile.file.close();
-		}
-		
-		// Move target to orphaned files directory
-		dir.moveTo(Zotero.File.pathToFile(orphaned), newName);
-	});
 	
 	
 	/**

chrome/content/zotero/xpcom/data/item.js
@@ -2536,7 +2536,7 @@ Zotero.Item.prototype.relinkAttachmentFile = Zotero.Promise.coroutine(function* 
 		}
 		// Create storage directory if necessary
 		else if (!(yield OS.File.exists(storageDir))) {
-			Zotero.Attachments.createDirectoryForItem(this);
+			yield Zotero.Attachments.createDirectoryForItem(this);
 		}
 		
 		let newFile;

chrome/content/zotero/xpcom/storage/storageLocal.js
@@ -626,12 +626,7 @@ Zotero.Sync.Storage.Local = {
 			throw new Error("Downloaded file not found");
 		}
 		
-		var parentDirPath = Zotero.Attachments.getStorageDirectory(item).path;
-		if (!(yield OS.File.exists(parentDirPath))) {
-			yield Zotero.Attachments.createDirectoryForItem(item);
-		}
-		
-		yield this._deleteExistingAttachmentFiles(item);
+		yield Zotero.Attachments.createDirectoryForItem(item);
 		
 		var path = item.getFilePath();
 		if (!path) {
@@ -741,16 +736,12 @@ Zotero.Sync.Storage.Local = {
 		}
 		
 		var parentDir = Zotero.Attachments.getStorageDirectory(item).path;
-		if (!(yield OS.File.exists(parentDir))) {
-			yield Zotero.Attachments.createDirectoryForItem(item);
-		}
-		
 		try {
-			yield this._deleteExistingAttachmentFiles(item);
+			yield Zotero.Attachments.createDirectoryForItem(item);
 		}
 		catch (e) {
 			zipReader.close();
-			throw (e);
+			throw e;
 		}
 		
 		var returnFile = null;
@@ -903,17 +894,6 @@ Zotero.Sync.Storage.Local = {
 		zipFile.remove(false);
 		
 		return returnFile;
-	}),
-	
-	
-	_deleteExistingAttachmentFiles: Zotero.Promise.method(function (item) {
-		var parentDir = Zotero.Attachments.getStorageDirectory(item).path;
-		// OS.File.DirectoryIterator, used by OS.File.removeDir(), isn't reliable on Travis,
-		// returning entry.isDir == false for subdirectories, so use nsIFile instead
-		if (Zotero.automatedTest) {
-			Zotero.File.pathToFile(parentDir).remove(true);
-		}
-		return OS.File.removeDir(parentDir);
 	}),
 	
 	

test/tests/attachmentsTest.js
@@ -274,5 +274,36 @@ describe("Zotero.Attachments", function() {
 			assert.isTrue(yield Zotero.Attachments.hasMultipleFiles(item));
 			assert.equal((yield Zotero.Attachments.getNumFiles(item)), 2);
 		})
-	})
+	});
+	
+	describe("#createDirectoryForItem()", function () {
+		it("should create missing directory", function* () {
+			var item = yield importFileAttachment('test.png');
+			var path = OS.Path.dirname(item.getFilePath());
+			yield OS.File.removeDir(path);
+			yield Zotero.Attachments.createDirectoryForItem(item);
+			assert.isTrue(yield OS.File.exists(path));
+		});
+		
+		it("should delete all existing files", function* () {
+			var item = yield importFileAttachment('test.html');
+			var path = OS.Path.dirname(item.getFilePath());
+			var files = ['a', 'b', 'c', 'd'];
+			for (let file of files) {
+				yield Zotero.File.putContentsAsync(OS.Path.join(path, file), file);
+			}
+			yield Zotero.Attachments.createDirectoryForItem(item);
+			assert.isTrue(yield Zotero.File.directoryIsEmpty(path));
+			assert.isTrue(yield OS.File.exists(path));
+		});
+		
+		it("should handle empty directory", function* () {
+			var item = yield importFileAttachment('test.png');
+			var file = item.getFilePath();
+			var dir = OS.Path.dirname(item.getFilePath());
+			yield OS.File.remove(file);
+			yield Zotero.Attachments.createDirectoryForItem(item);
+			assert.isTrue(yield OS.File.exists(dir));
+		});
+	});
 })

test/tests/storageLocalTest.js
@@ -218,24 +218,6 @@ describe("Zotero.Sync.Storage.Local", function () {
 		})
 	})
 	
-	describe("#_deleteExistingAttachmentFiles()", function () {
-		it("should delete all files", function* () {
-			var item = yield importFileAttachment('test.html');
-			var path = OS.Path.dirname(item.getFilePath());
-			var files = ['a', 'b', 'c', 'd'];
-			for (let file of files) {
-				yield Zotero.File.putContentsAsync(OS.Path.join(path, file), file);
-			}
-			yield Zotero.Sync.Storage.Local._deleteExistingAttachmentFiles(item);
-			for (let file of files) {
-				assert.isFalse(
-					(yield OS.File.exists(OS.Path.join(path, file))),
-					`File '${file}' doesn't exist`
-				);
-			}
-		})
-	})
-	
 	describe("#getConflicts()", function () {
 		it("should return an array of objects for attachments in conflict", function* () {
 			var libraryID = Zotero.Libraries.userLibraryID;

commit 5c50bb00cfc936013ebd907c09d1437124f9c111

Don't save full-text cache files for linked files to linked directory
Regression from 80f888f374. Not entirely sure what I was trying to fix there.

chrome/content/zotero/xpcom/fulltext.js
@@ -659,15 +659,7 @@ Zotero.Fulltext = Zotero.FullText = new function(){
 		// If file is stored outside of Zotero, create a directory for the item
 		// in the storage directory and save the cache file there
 		if (linkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE) {
-			let path = item.getFilePath();
-			if (!path) {
-				Zotero.debug("Invalid path for item " + itemID);
-				return false;
-			}
-			var parentDirPath = OS.Path.dirname(path);
-			if (!(yield OS.File.exists(parentDirPath))) {
-				yield Zotero.Attachments.createDirectoryForItem(item);
-			}
+			var parentDirPath = yield Zotero.Attachments.createDirectoryForItem(item);
 		}
 		else {
 			var parentDirPath = OS.Path.dirname(filePath);

test/tests/fulltextTest.js
@@ -14,7 +14,7 @@ describe("Zotero.Fulltext", function () {
 		}
 	});
 	
-	describe("#indexItems()", function () {
+	describe("Indexing", function () {
 		before(function* () {
 			yield Zotero.Fulltext.downloadPDFTool('info', pdfToolsVersion);
 			yield Zotero.Fulltext.downloadPDFTool('converter', pdfToolsVersion);
@@ -29,49 +29,68 @@ describe("Zotero.Fulltext", function () {
 			Zotero.Prefs.clear('fulltext.pdfMaxPages');
 		});
 		
-		it("should index a text file by default", function* () {
-			var item = yield importFileAttachment('test.txt');
-			assert.equal(
-				(yield Zotero.Fulltext.getIndexedState(item)),
-				Zotero.Fulltext.INDEX_STATE_INDEXED
-			);
-		})
+		describe("#indexItems()", function () {
+			it("should index a text file by default", function* () {
+				var item = yield importFileAttachment('test.txt');
+				assert.equal(
+					(yield Zotero.Fulltext.getIndexedState(item)),
+					Zotero.Fulltext.INDEX_STATE_INDEXED
+				);
+			})
+			
+			it("should skip indexing of a text file if fulltext.textMaxLength is 0", function* () {
+				Zotero.Prefs.set('fulltext.textMaxLength', 0);
+				var item = yield importFileAttachment('test.txt');
+				assert.equal(
+					(yield Zotero.Fulltext.getIndexedState(item)),
+					Zotero.Fulltext.INDEX_STATE_UNINDEXED
+				);
+			})
+			
+			it("should index a PDF by default", function* () {
+				var item = yield importFileAttachment('test.pdf');
+				assert.equal(
+					(yield Zotero.Fulltext.getIndexedState(item)),
+					Zotero.Fulltext.INDEX_STATE_INDEXED
+				);
+			})
+			
+			it("should skip indexing of a PDF if fulltext.textMaxLength is 0", function* () {
+				Zotero.Prefs.set('fulltext.textMaxLength', 0);
+				var item = yield importFileAttachment('test.pdf');
+				assert.equal(
+					(yield Zotero.Fulltext.getIndexedState(item)),
+					Zotero.Fulltext.INDEX_STATE_UNINDEXED
+				);
+			})
+			
+			it("should skip indexing of a PDF if fulltext.pdfMaxPages is 0", function* () {
+				Zotero.Prefs.set('fulltext.pdfMaxPages', 0);
+				var item = yield importFileAttachment('test.pdf');
+				assert.equal(
+					(yield Zotero.Fulltext.getIndexedState(item)),
+					Zotero.Fulltext.INDEX_STATE_UNINDEXED
+				);
+			})
+		});
 		
-		it("should skip indexing of a text file if fulltext.textMaxLength is 0", function* () {
-			Zotero.Prefs.set('fulltext.textMaxLength', 0);
-			var item = yield importFileAttachment('test.txt');
-			assert.equal(
-				(yield Zotero.Fulltext.getIndexedState(item)),
-				Zotero.Fulltext.INDEX_STATE_UNINDEXED
-			);
-		})
-		
-		it("should index a PDF by default", function* () {
-			var item = yield importFileAttachment('test.pdf');
-			assert.equal(
-				(yield Zotero.Fulltext.getIndexedState(item)),
-				Zotero.Fulltext.INDEX_STATE_INDEXED
-			);
-		})
-		
-		it("should skip indexing of a PDF if fulltext.textMaxLength is 0", function* () {
-			Zotero.Prefs.set('fulltext.textMaxLength', 0);
-			var item = yield importFileAttachment('test.pdf');
-			assert.equal(
-				(yield Zotero.Fulltext.getIndexedState(item)),
-				Zotero.Fulltext.INDEX_STATE_UNINDEXED
-			);
-		})
-		
-		it("should skip indexing of a PDF if fulltext.pdfMaxPages is 0", function* () {
-			Zotero.Prefs.set('fulltext.pdfMaxPages', 0);
-			var item = yield importFileAttachment('test.pdf');
-			assert.equal(
-				(yield Zotero.Fulltext.getIndexedState(item)),
-				Zotero.Fulltext.INDEX_STATE_UNINDEXED
-			);
-		})
-	})
+		describe("#indexPDF()", function () {
+			it("should create cache files for linked attachments in storage directory", function* () {
+				var filename = 'test.pdf';
+				var file = OS.Path.join(getTestDataDirectory().path, filename);
+				var tempDir = yield getTempDirectory();
+				var linkedFile = OS.Path.join(tempDir, filename);
+				yield OS.File.copy(file, linkedFile);
+				
+				var item = yield Zotero.Attachments.linkFromFile({ file: linkedFile });
+				var storageDir = Zotero.Attachments.getStorageDirectory(item).path;
+				assert.isTrue(yield OS.File.exists(storageDir));
+				assert.isTrue(yield OS.File.exists(OS.Path.join(storageDir, '.zotero-ft-info')));
+				assert.isTrue(yield OS.File.exists(OS.Path.join(storageDir, '.zotero-ft-cache')));
+				assert.isFalse(yield OS.File.exists(OS.Path.join(storageDir, filename)));
+			});
+		});
+	});
 	
 	describe("#downloadPDFTool()", function () {
 		it("should install the PDF tools", function* () {