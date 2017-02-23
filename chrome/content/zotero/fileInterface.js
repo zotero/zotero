zotero/zotero

commit 32bebeb1cd0739becf470798b952fff8adc76cb0

Fix importing from clipboard
Since there's no filename to show in the progress popup, show the
translator label (e.g., "BibTeX") instead.

chrome/content/zotero/fileInterface.js
@@ -338,7 +338,9 @@ var Zotero_File_Interface = new function() {
 		});
 		progressWin.changeHeadline(Zotero.getString('fileInterface.importing'));
 		var icon = 'chrome://zotero/skin/treesource-unfiled' + (Zotero.hiDPI ? "@2x" : "") + '.png';
-		let progress = new progressWin.ItemProgress(icon, OS.Path.basename(translation.path));
+		let progress = new progressWin.ItemProgress(
+			icon, translation.path ? OS.Path.basename(translation.label) : translators[0].label
+		);
 		progressWin.show();
 		
 		translation.setHandler("itemDone",  function () {

commit bb38c974bf6410f7e253ad5047239b74c8d8bd2b

Fix error importing from file (regression from 32bebeb1cd0)

chrome/content/zotero/fileInterface.js
@@ -339,7 +339,7 @@ var Zotero_File_Interface = new function() {
 		progressWin.changeHeadline(Zotero.getString('fileInterface.importing'));
 		var icon = 'chrome://zotero/skin/treesource-unfiled' + (Zotero.hiDPI ? "@2x" : "") + '.png';
 		let progress = new progressWin.ItemProgress(
-			icon, translation.path ? OS.Path.basename(translation.label) : translators[0].label
+			icon, translation.path ? OS.Path.basename(translation.path) : translators[0].label
 		);
 		progressWin.show();
 		