zotero/zotero

commit b2b751e0a099fcb030d0ff25f37acf320fbf3b60

Fix error when DB backup can't be opened

chrome/content/zotero/xpcom/db.js
@@ -1057,22 +1057,12 @@ Zotero.DBConnection.prototype.backupDatabase = Zotero.Promise.coroutine(function
 			var connection = storageService.openDatabase(Zotero.File.pathToFile(tmpFile));
 		}
 		catch (e) {
-			this._debug("Database file '" + OS.Path.basename(tmpFile) + "' is corrupt -- skipping backup");
+			Zotero.logError(e);
+			this._debug("Database file '" + OS.Path.basename(tmpFile) + "' can't be opened -- skipping backup");
 			if (yield OS.File.exists(tmpFile)) {
 				yield OS.File.remove(tmpFile);
 			}
 			return false;
-		}
-		finally {
-			let resolve;
-			connection.asyncClose({
-				complete: function () {
-					resolve();
-				}
-			});
-			yield new Zotero.Promise(function () {
-				resolve = arguments[0];
-			});
 		}
 		
 		// Special backup

commit fa680bbbf5efa5e11941a81ef1b07dd15e034f59

Fix startup error on Windows DB upgrades caused by b2b751e0a0

chrome/content/zotero/xpcom/db.js
@@ -1064,6 +1064,17 @@ Zotero.DBConnection.prototype.backupDatabase = Zotero.Promise.coroutine(function
 			}
 			return false;
 		}
+		finally {
+			if (connection) {
+				let deferred = Zotero.Promise.defer();
+				connection.asyncClose({
+					complete: function () {
+						deferred.resolve();
+					}
+				});
+				yield deferred.promise;
+			}
+        }
 		
 		// Special backup
 		if (!suffix && numBackups > 1) {