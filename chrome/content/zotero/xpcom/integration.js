746171ab7844e66eed1f3e1f81addd8ab7832a35
Adomas Venčkauskas <adomas.ven@gmail.com>
2017-05-08 09:23:53 +0300

    Fix doc.prefs.noteType handling with NaN values

--- chrome/content/zotero/xpcom/integration.js
+++ chrome/content/zotero/xpcom/integration.js
@@ -3137,11 +3137,8 @@ Zotero.Integration.DocumentData.prototype.unserializeXML = function(xmlData) {
 		
 		this.prefs[name] = value;
 	}
-	try {
-		this.prefs.noteType = parseInt(this.prefs.noteType);
-	} catch (e) {
-		this.prefs.noteType = 0;
-	}
+	
+	this.prefs.noteType = parseInt(this.prefs.noteType) || 0;
 	if (this.prefs["automaticJournalAbbreviations"] === undefined) this.prefs["automaticJournalAbbreviations"] = false;
 	this.zoteroVersion = doc.documentElement.getAttribute("zotero-version");
 	if (!this.zoteroVersion) this.zoteroVersion = "2.0";
