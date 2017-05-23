c0f7f6070a9929ec942ebea49f345ef48add0327
Dan Stillman <dstillman@zotero.org>
2017-05-23 02:11:09 -0400

    Avoid a warning from a storageRequest test from the item not existing

--- test/tests/storageRequestTest.js
+++ test/tests/storageRequestTest.js
@@ -5,10 +5,11 @@ describe("Zotero.Sync.Storage.Request", function () {
 		it("should run a request and wait for it to complete", function* () {
 			var libraryID = Zotero.Libraries.userLibraryID;
 			var count = 0;
+			var item = yield importFileAttachment('test.png');
 			var request = new Zotero.Sync.Storage.Request({
 				type: 'download',
 				libraryID,
-				name: "1/AAAAAAAA",
+				name: `${item.libraryID}/${item.key}`,
 				onStart: Zotero.Promise.coroutine(function* () {
 					yield Zotero.Promise.delay(25);
 					count++;
