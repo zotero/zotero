b0c0cd6e0ca1c92e062d2a5facf354e1c485f026
Frank Bennett <biercenator@gmail.com>
2017-08-24 23:26:03 +0900

    Expose styleEngine in csledit.js, for Abbreviation Filter

--- chrome/content/zotero/tools/csledit.js
+++ chrome/content/zotero/tools/csledit.js
@@ -157,6 +157,7 @@ var Zotero_CSL_Editor = new function() {
 	
 	this.generateBibliography = function(style) {
 		var iframe = document.getElementById('zotero-csl-preview-box');
+		var editor = document.getElementById('zotero-csl-editor');
 		
 		var items = Zotero.getActiveZoteroPane().getSelectedItems();
 		if (items.length == 0) {
@@ -228,6 +229,7 @@ var Zotero_CSL_Editor = new function() {
 				iframe.contentDocument.documentElement.innerHTML = '<div>' + Zotero.getString('styles.editor.warning.renderError') + '</div><div>'+e+'</div>';
 				throw e;
 		}
+		editor.styleEngine = styleEngine;
 	}
 	
 	
