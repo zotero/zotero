zotero/zotero

commit 921b21c3e161a0c60946bd7c47b5113ee109ba54

Remove the additional setText() call for libreoffice plugin
Possible since zotero/zotero-libreoffice-integration@2deea6bd46b50f72cdc937a468e88ab6cd4461ad

chrome/content/zotero/xpcom/integration.js
@@ -1714,13 +1714,6 @@ Zotero.Integration.Fields.prototype._updateDocument = function* (forceCitations,
 		var fieldCode = this._session.getCitationField(citation);
 		if(fieldCode != citation.properties.field) {
 			field.setCode(`ITEM CSL_CITATION ${fieldCode}`);
-			
-			if(this._session.data.prefs.fieldType === "ReferenceMark" && isRich
-					&& !citation.properties.dontUpdate) {
-				// For ReferenceMarks with formatting, we need to set the text again, because
-				// setting the field code removes formatting from the mark. I don't like this.
-				field.setText(formattedCitation, isRich);
-			}
 		}
 		nUpdated++;
 	}

commit da387874a2bc5b7639b12286e8f9af8b7e9a6782

Temporarily restore double text setting in LibreOffice for footnotes
Regressions from 921b21c3. Footnotes in LibreOffice lose styling

chrome/content/zotero/xpcom/integration.js
@@ -1714,6 +1714,13 @@ Zotero.Integration.Fields.prototype._updateDocument = function* (forceCitations,
 		var fieldCode = this._session.getCitationField(citation);
 		if(fieldCode != citation.properties.field) {
 			field.setCode(`ITEM CSL_CITATION ${fieldCode}`);
+			if(this._session.data.prefs.fieldType === "ReferenceMark"
+					&& this._session.data.prefs.noteType != 0 && isRich
+					&& !citation.properties.dontUpdate) {
+				// For ReferenceMarks with formatting, we need to set the text again, because
+				// setting the field code removes formatting from the mark. I don't like this.
+				field.setText(formattedCitation, isRich);
+			}
 		}
 		nUpdated++;
 	}