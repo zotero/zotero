zotero/zotero

commit f8b41c971c89ffb474df2103c4bf1a49389a5048

Automatic attachment file renaming changes
Automatic renaming is now done for dragging of an external file onto an
item (as added in 7cb95f41) as well as dragging as a linked file,
dragging a non-native (not text or HTML) URL, "Attach Link to File…",
"Attach Stored Copy of File…", and "Retrieve Metadata for PDF". It only
applies if a single file is being added and if the parent item has no
non-HTML attachments. By default, the renaming only applies to PDFs, but
this can be changed with the renameAttachmentFiles.automatic.fileTypes
hidden pref.

A new General pref, "Automatically rename attachment files using parent
metadata", controls whether the renaming happens.

Files saved via web translators are renamed regardless of this pref,
because they would often be gibberish otherwise.

Closes #113

chrome/content/zotero/xpcom/recognizePDF.js
@@ -241,30 +241,48 @@ Zotero.RecognizePDF = new function () {
 	 * @return {Promise}
 	 */
 	async function _processItem(itemID) {
-		let item = await Zotero.Items.getAsync(itemID);
+		let attachment = await Zotero.Items.getAsync(itemID);
 		
-		if (!item || item.parentItemID) throw new Zotero.Exception.Alert('recognizePDF.fileNotFound');
-		
-		let newItem = await _recognize(item);
-		
-		if (newItem) {
-			// put new item in same collections as the old one
-			let itemCollections = item.getCollections();
-			await Zotero.DB.executeTransaction(async function () {
-				for (let itemCollection of itemCollections) {
-					let collection = Zotero.Collections.get(itemCollection);
-					await collection.addItem(newItem.id);
-				}
-				
-				// put old item as a child of the new item
-				item.parentID = newItem.id;
-				await item.save();
-			});
-			
-			return newItem
+		if (!attachment || attachment.parentItemID) {
+			throw new Zotero.Exception.Alert('recognizePDF.error');
 		}
 		
-		return null;
+		let parentItem = await _recognize(attachment);
+		if (!parentItem) {
+			return null;
+		}
+		
+		// Put new item in same collections as the old one
+		let collections = attachment.getCollections();
+		await Zotero.DB.executeTransaction(async function () {
+			if (collections.length) {
+				for (let collection of collections) {
+					parentItem.addToCollection(collection.id);
+				}
+				await parentItem.save();
+			}
+			
+			// Put old item as a child of the new item
+			attachment.parentID = parentItem.id;
+			await attachment.save();
+		});
+		
+		// Rename attachment file to match new metadata
+		if (Zotero.Prefs.get('renameAttachmentFiles')) {
+			let path = attachment.getFilePath();
+			let ext = Zotero.File.getExtension(path);
+			let fileBaseName = Zotero.Attachments.getFileBaseNameFromItem(parentItem);
+			let newName = fileBaseName + (ext ? '.' + ext : '');
+			let result = await attachment.renameAttachmentFile(newName, false, true);
+			if (result !== true) {
+				throw new Error("Error renaming " + path);
+			}
+			// Rename attachment title
+			attachment.setField('title', newName);
+			await attachment.saveTx();
+		}
+		
+		return parentItem;
 	}
 	
 	/**

test/tests/recognizePDFTest.js
@@ -27,31 +27,36 @@ describe("PDF Recognition", function() {
 		}
 	});
 
-	it("should recognize a PDF", function* () {
+	it("should recognize a PDF", async function () {
 		this.timeout(30000);
 		// Import the PDF
 		var testdir = getTestDataDirectory();
 		testdir.append("recognizePDF_test_GS.pdf");
-		var item = yield Zotero.Attachments.importFromFile({
+		var attachment = await Zotero.Attachments.importFromFile({
 			file: testdir
 		});
 		
 		// Recognize the PDF
 		win.ZoteroPane.recognizeSelected();
 		
-		var addedIDs = yield waitForItemEvent("add");
-		var modifiedIDs = yield waitForItemEvent("modify");
+		var addedIDs = await waitForItemEvent("add");
+		var modifiedIDs = await waitForItemEvent("modify");
 		assert.lengthOf(addedIDs, 1);
 		var item = Zotero.Items.get(addedIDs[0]);
 		assert.equal(item.getField("title"), "Scaling study of an improved fermion action on quenched lattices");
 		assert.lengthOf(modifiedIDs, 2);
 		
-		yield Zotero.Promise.delay(0);
-		
+		// Wait for status to show as complete
 		var progressWindow = getWindows("chrome://zotero/content/recognizePDFDialog.xul")[0];
+		var completeStr = Zotero.getString("recognizePDF.complete.label");
+		while (progressWindow.document.getElementById("label").value != completeStr) {
+			await Zotero.Promise.delay(20);
+		}
+		
+		// The file should have been renamed
 		assert.equal(
-			progressWindow.document.getElementById("label").value,
-			Zotero.getString("recognizePDF.complete.label")
+			attachment.attachmentFilename,
+			Zotero.Attachments.getFileBaseNameFromItem(item) + '.pdf'
 		);
 	});
 });

chrome/content/zotero/preferences/preferences_general.xul
@@ -37,6 +37,7 @@
 			<preference id="pref-reportTranslationFailure" name="extensions.zotero.reportTranslationFailure" type="bool"/>
 			<preference id="pref-automaticSnapshots" name="extensions.zotero.automaticSnapshots" type="bool"/>
 			<preference id="pref-downloadAssociatedFiles" name="extensions.zotero.downloadAssociatedFiles" type="bool"/>
+			<preference id="pref-renameAttachmentFiles" name="extensions.zotero.renameAttachmentFiles.automatic" type="bool"/>
 			<preference id="pref-automaticTags" name="extensions.zotero.automaticTags" type="bool"/>
 			<preference id="pref-trashAutoEmptyDays" name="extensions.zotero.trashAutoEmptyDays" type="int"/>
 			
@@ -122,6 +123,7 @@
 				label="&zotero.preferences.automaticSnapshots;"
 				preference="pref-automaticSnapshots"/>
 			<checkbox label="&zotero.preferences.downloadAssociatedFiles;" preference="pref-downloadAssociatedFiles"/>
+			<checkbox label="&zotero.preferences.renameAttachmentFiles;" preference="pref-renameAttachmentFiles"/>
 			<checkbox label="&zotero.preferences.automaticTags;" preference="pref-automaticTags"/>
 			<hbox align="center">
 				<label value="&zotero.preferences.trashAutoEmptyDaysPre;"/>

chrome/content/zotero/xpcom/attachments.js
@@ -245,9 +245,18 @@ Zotero.Attachments = new function(){
 	
 	
 	/**
-	 * @param {Object} options - 'libraryID', 'url', 'parentItemID', 'collections', 'title',
-	 *                           'fileBaseName', 'contentType', 'referrer', 'cookieSandbox',
-	 *                           'saveOptions'
+	 * @param {Object} options
+	 * @param {Integer} options.libraryID
+	 * @param {String} options.url
+	 * @param {Integer} [options.parentItemID]
+	 * @param {Integer[]} [options.collections]
+	 * @param {String} [options.title]
+	 * @param {String} [options.fileBaseName]
+	 * @param {Boolean} [options.renameIfAllowedType=false]
+	 * @param {String} [options.contentType]
+	 * @param {String} [options.referrer]
+	 * @param {CookieSandbox} [options.cookieSandbox]
+	 * @param {Object} [options.saveOptions]
 	 * @return {Promise<Zotero.Item>} - A promise for the created attachment item
 	 */
 	this.importFromURL = Zotero.Promise.coroutine(function* (options) {
@@ -257,6 +266,7 @@ Zotero.Attachments = new function(){
 		var collections = options.collections;
 		var title = options.title;
 		var fileBaseName = options.fileBaseName;
+		var renameIfAllowedType = options.renameIfAllowedType;
 		var contentType = options.contentType;
 		var referrer = options.referrer;
 		var cookieSandbox = options.cookieSandbox;
@@ -320,6 +330,11 @@ Zotero.Attachments = new function(){
 		
 		// Save using remote web browser persist
 		var externalHandlerImport = Zotero.Promise.coroutine(function* (contentType) {
+			// Rename attachment
+			if (renameIfAllowedType && !fileBaseName && this.getRenamedFileTypes().includes(contentType)) {
+				let parentItem = Zotero.Items.get(parentItemID);
+				fileBaseName = this.getFileBaseNameFromItem(parentItem);
+			}
 			if (fileBaseName) {
 				let ext = _getExtensionFromURL(url, contentType);
 				var fileName = fileBaseName + (ext != '' ? '.' + ext : '');
@@ -801,6 +816,30 @@ Zotero.Attachments = new function(){
 		
 		formatString = Zotero.File.getValidFileName(formatString);
 		return formatString;
+	}
+	
+	
+	this.getRenamedFileTypes = function () {
+		try {
+			var types = Zotero.Prefs.get('renameAttachmentFiles.automatic.fileTypes');
+			return types ? types.split(',') : [];
+		}
+		catch (e) {
+			return [];
+		}
+	};
+	
+	
+	this.getRenamedFileBaseNameIfAllowedType = async function (parentItem, file) {
+		var types = this.getRenamedFileTypes();
+		var contentType = file.endsWith('.pdf')
+			// Don't bother reading file if there's a .pdf extension
+			? 'application/pdf'
+			: await Zotero.MIME.getMIMETypeFromFile(file);
+		if (!types.includes(contentType)) {
+			return false;
+		}
+		return this.getFileBaseNameFromItem(parentItem);
 	}
 	
 	

chrome/content/zotero/xpcom/data/item.js
@@ -2120,6 +2120,15 @@ Zotero.Item.prototype.numAttachments = function (includeTrashed) {
 }
 
 
+Zotero.Item.prototype.numNonHTMLFileAttachments = function () {
+	this._requireData('childItems');
+	return this.getAttachments()
+		.map(itemID => Zotero.Items.get(itemID))
+		.filter(item => item.isFileAttachment() && item.attachmentContentType != 'text/html')
+		.length;
+};
+
+
 Zotero.Item.prototype.getFile = function () {
 	Zotero.debug("Zotero.Item.prototype.getFile() is deprecated -- use getFilePath[Async]()", 2);
 	

chrome/content/zotero/xpcom/itemTreeView.js
@@ -3231,25 +3231,21 @@ Zotero.ItemTreeView.prototype.drop = Zotero.Promise.coroutine(function* (row, or
 		
 		var notifierQueue = new Zotero.Notifier.Queue;
 		try {
+			// If there's a single file being added to a parent, automatic renaming is enabled,
+			// and there are no other non-HTML attachments, we'll rename the file as long as it's
+			// an allowed type. The dragged data could be a URL, so we don't yet know the file type.
+			// This should be kept in sync with ZoteroPane.addAttachmentFromDialog().
+			let renameIfAllowedType = false;
 			let parentItem;
-			let numExistingFileAttachments;
-			if (parentItemID) {
+			if (parentItemID && data.length == 1 && Zotero.Prefs.get('renameAttachmentFiles.automatic')) {
 				parentItem = Zotero.Items.get(parentItemID);
-				numExistingFileAttachments = parentItem.getAttachments()
-					.map(itemID => Zotero.Items.get(itemID))
-					.filter(item => item.isFileAttachment())
-					.length;
+				if (!parentItem.numNonHTMLFileAttachments()) {
+					renameIfAllowedType = true;
+				}
 			}
 			
 			for (var i=0; i<data.length; i++) {
 				var file = data[i];
-				
-				let fileBaseName;
-				// If only one item is being dragged and it's the only attachment, run
-				// "Rename File from Parent Metadata" automatically
-				if (data.length == 1 && parentItem && !numExistingFileAttachments) {
-					fileBaseName = Zotero.Attachments.getFileBaseNameFromItem(parentItem);
-				}
 				
 				if (dataType == 'text/x-moz-url') {
 					var url = data[i];
@@ -3281,7 +3277,7 @@ Zotero.ItemTreeView.prototype.drop = Zotero.Promise.coroutine(function* (row, or
 							yield Zotero.Attachments.importFromURL({
 								libraryID: targetLibraryID,
 								url,
-								fileBaseName,
+								renameIfAllowedType,
 								parentItemID,
 								saveOptions: {
 									notifierQueue
@@ -3298,7 +3294,36 @@ Zotero.ItemTreeView.prototype.drop = Zotero.Promise.coroutine(function* (row, or
 					// Otherwise file, so fall through
 				}
 				
+				file = file.path;
+				
+				// Rename file if it's an allowed type
+				let fileBaseName = false;
+				if (renameIfAllowedType) {
+					 fileBaseName = yield Zotero.Attachments.getRenamedFileBaseNameIfAllowedType(
+						parentItem, file
+					);
+				}
+				
 				if (dropEffect == 'link') {
+					// Rename linked file, with unique suffix if necessary
+					try {
+						if (fileBaseName) {
+							let ext = Zotero.File.getExtension(file);
+							let newName = yield Zotero.File.rename(
+								file,
+								fileBaseName + (ext ? '.' + ext : ''),
+								{
+									unique: true
+								}
+							);
+							// Update path in case the name was changed to be unique
+							file = OS.Path.join(OS.Path.dirname(file), newName);
+						}
+					}
+					catch (e) {
+						Zotero.logError(e);
+					}
+					
 					yield Zotero.Attachments.linkFromFile({
 						file,
 						parentItemID,
@@ -3309,9 +3334,9 @@ Zotero.ItemTreeView.prototype.drop = Zotero.Promise.coroutine(function* (row, or
 					});
 				}
 				else {
-					if (file.leafName.endsWith(".lnk")) {
+					if (file.endsWith(".lnk")) {
 						let win = Services.wm.getMostRecentWindow("navigator:browser");
-						win.ZoteroPane.displayCannotAddShortcutMessage(file.path);
+						win.ZoteroPane.displayCannotAddShortcutMessage(file);
 						continue;
 					}
 					
@@ -3328,10 +3353,10 @@ Zotero.ItemTreeView.prototype.drop = Zotero.Promise.coroutine(function* (row, or
 					// If moving, delete original file
 					if (dragData.dropEffect == 'move') {
 						try {
-							file.remove(false);
+							yield OS.File.remove(file);
 						}
 						catch (e) {
-							Components.utils.reportError("Error deleting original file " + file.path + " after drag");
+							Zotero.logError("Error deleting original file " + file + " after drag");
 						}
 					}
 				}

chrome/content/zotero/zoteroPane.js
@@ -3678,41 +3678,81 @@ var ZoteroPane = new function()
 		var fp = Components.classes["@mozilla.org/filepicker;1"]
         					.createInstance(nsIFilePicker);
 		fp.init(window, Zotero.getString('pane.item.attachments.select'), nsIFilePicker.modeOpenMultiple);
-		fp.appendFilters(Components.interfaces.nsIFilePicker.filterAll);
+		fp.appendFilters(nsIFilePicker.filterAll);
 		
-		if(fp.show() == nsIFilePicker.returnOK)
-		{
-			if (!parentItemID) {
-				var collection = this.getSelectedCollection(true);
+		if (fp.show() != nsIFilePicker.returnOK) {
+			return;
+		}
+		
+		var enumerator = fp.files;
+		var files = [];
+		while (enumerator.hasMoreElements()) {
+			let file = enumerator.getNext();
+			file.QueryInterface(Components.interfaces.nsIFile);
+			files.push(file.path);
+		}
+		
+		var collection;
+		var fileBaseName;
+		if (parentItemID) {
+			// If only one item is being added, automatic renaming is enabled, and the parent item
+			// doesn't have any other non-HTML file attachments, rename the file.
+			// This should be kept in sync with itemTreeView::drop().
+			if (files.length == 1 && Zotero.Prefs.get('renameAttachmentFiles.automatic')) {
+				let parentItem = Zotero.Items.get(parentItemID);
+				if (!parentItem.numNonHTMLFileAttachments()) {
+					fileBaseName = await Zotero.Attachments.getRenamedFileBaseNameIfAllowedType(
+						parentItem, files[0]
+					);
+				}
 			}
-			
-			var files = fp.files;
-			while (files.hasMoreElements()){
-				var file = files.getNext();
-				file.QueryInterface(Components.interfaces.nsILocalFile);
-				var attachmentID;
-				if (link) {
-					yield Zotero.Attachments.linkFromFile({
-						file: file,
-						parentItemID: parentItemID,
-						collections: collection ? [collection] : undefined
-					});
-				}
-				else {
-					if (file.leafName.endsWith(".lnk")) {
-						let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-						.getService(Components.interfaces.nsIWindowMediator);
-						let win = wm.getMostRecentWindow("navigator:browser");
-						win.ZoteroPane.displayCannotAddShortcutMessage(file.path);
-						continue;
+		}
+		// If not adding to an item, add to the current collection
+		else {
+			collection = this.getSelectedCollection(true);
+		}
+		
+		for (let file of files) {
+			if (link) {
+				// Rename linked file, with unique suffix if necessary
+				try {
+					if (fileBaseName) {
+						let ext = Zotero.File.getExtension(file);
+						let newName = yield Zotero.File.rename(
+							file,
+							fileBaseName + (ext ? '.' + ext : ''),
+							{
+								unique: true
+							}
+						);
+						// Update path in case the name was changed to be unique
+						file = OS.Path.join(OS.Path.dirname(file), newName);
 					}
-					yield Zotero.Attachments.importFromFile({
-						file: file,
-						libraryID: libraryID,
-						parentItemID: parentItemID,
-						collections: collection ? [collection] : undefined
-					});
 				}
+				catch (e) {
+					Zotero.logError(e);
+				}
+				
+				let item = yield Zotero.Attachments.linkFromFile({
+					file,
+					parentItemID,
+					collections: collection ? [collection] : undefined
+				});
+			}
+			else {
+				if (file.endsWith(".lnk")) {
+					let win = Services.wm.getMostRecentWindow("navigator:browser");
+					win.ZoteroPane.displayCannotAddShortcutMessage(file);
+					continue;
+				}
+				
+				yield Zotero.Attachments.importFromFile({
+					file,
+					libraryID,
+					fileBaseName,
+					parentItemID,
+					collections: collection ? [collection] : undefined
+				});
 			}
 		}
 	});

chrome/locale/en-US/zotero/preferences.dtd
@@ -27,6 +27,7 @@
 <!ENTITY zotero.preferences.parseRISRefer				"Use Zotero for downloaded BibTeX/RIS/Refer files">
 <!ENTITY zotero.preferences.automaticSnapshots			"Automatically take snapshots when creating items from web pages">
 <!ENTITY zotero.preferences.downloadAssociatedFiles		"Automatically attach associated PDFs and other files when saving items">
+<!ENTITY zotero.preferences.renameAttachmentFiles     "Automatically rename attachment files using parent metadata">
 <!ENTITY zotero.preferences.automaticTags				"Automatically tag items with keywords and subject headings">
 <!ENTITY zotero.preferences.trashAutoEmptyDaysPre		"Automatically remove items in the trash deleted more than">
 <!ENTITY zotero.preferences.trashAutoEmptyDaysPost		"days ago">

commit 3cc92fe1e9c7ed8acdac3d4b824e0eccb5da21ab

Fix PDF recognition within collection (broken by f8b41c971c)

chrome/content/zotero/xpcom/recognizePDF.js
@@ -256,8 +256,8 @@ Zotero.RecognizePDF = new function () {
 		let collections = attachment.getCollections();
 		await Zotero.DB.executeTransaction(async function () {
 			if (collections.length) {
-				for (let collection of collections) {
-					parentItem.addToCollection(collection.id);
+				for (let collectionID of collections) {
+					parentItem.addToCollection(collectionID);
 				}
 				await parentItem.save();
 			}

test/tests/recognizePDFTest.js
@@ -60,4 +60,33 @@ describe("PDF Recognition", function() {
 			Zotero.Attachments.getFileBaseNameFromItem(item) + '.pdf'
 		);
 	});
+	
+	it("should put new item in same collection", async function () {
+		this.timeout(30000);
+		// Import the PDF
+		var testdir = getTestDataDirectory();
+		testdir.append("recognizePDF_test_GS.pdf");
+		var collection = await createDataObject('collection');
+		var attachment = await Zotero.Attachments.importFromFile({
+			file: testdir,
+			collections: [collection.id]
+		});
+		
+		win.ZoteroPane.recognizeSelected();
+		
+		var addedIDs = await waitForItemEvent("add");
+		var modifiedIDs = await waitForItemEvent("modify");
+		assert.lengthOf(addedIDs, 1);
+		var item = Zotero.Items.get(addedIDs[0]);
+		assert.lengthOf(modifiedIDs, 2);
+		
+		// Wait for status to show as complete
+		var progressWindow = getWindows("chrome://zotero/content/recognizePDFDialog.xul")[0];
+		var completeStr = Zotero.getString("recognizePDF.complete.label");
+		while (progressWindow.document.getElementById("label").value != completeStr) {
+			await Zotero.Promise.delay(20);
+		}
+		
+		assert.isTrue(collection.hasItem(item.id));
+	});
 });