zotero/zotero

commit 78e87a351cfc9c845eace9b2dee9cdfaa261ebf7

Handle relations property in collection fromJSON()

chrome/content/zotero/xpcom/data/collection.js
@@ -698,8 +698,7 @@ Zotero.Collection.prototype.fromJSON = function (json) {
 	this.name = json.name;
 	this.parentKey = json.parentCollection ? json.parentCollection : false;
 	
-	// TODO
-	//this.setRelations(json.relations);
+	this.setRelations(json.relations);
 }
 
 

commit 2b49e94a617e31a5f3c98884f1da552d7aa073f0

Fix sync breakage from 78e87a351

chrome/content/zotero/xpcom/data/collection.js
@@ -698,7 +698,7 @@ Zotero.Collection.prototype.fromJSON = function (json) {
 	this.name = json.name;
 	this.parentKey = json.parentCollection ? json.parentCollection : false;
 	
-	this.setRelations(json.relations);
+	this.setRelations(json.relations || {});
 }
 
 