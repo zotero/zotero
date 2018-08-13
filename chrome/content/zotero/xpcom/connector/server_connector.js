zotero/zotero

commit 0f1687f1c7b50635e2908e5cce4bb88d8a359a18

Fix invalid document citation style recovery. Closes #1543

chrome/content/zotero/xpcom/connector/server_connector.js
@@ -1030,7 +1030,7 @@ Zotero.Server.Connector.InstallStyle.prototype = {
 	
 	init: Zotero.Promise.coroutine(function* (requestData) {
 		try {
-			var styleName = yield Zotero.Styles.install(
+			var { styleName, styleID } = yield Zotero.Styles.install(
 				requestData.data, requestData.query.origin || null, true
 			);
 		} catch (e) {

chrome/content/zotero/xpcom/integration.js
@@ -448,9 +448,10 @@ Zotero.Integration = new function() {
 
 							let installed = false;
 							try {
-								await Zotero.Styles.install(
+								let { styleTitle, styleID } = await Zotero.Styles.install(
 									{url: data.style.styleID}, data.style.styleID, true
 								);
+								data.style.styleID = styleID;
 								installed = true;
 							}
 							catch (e) {
@@ -464,10 +465,15 @@ Zotero.Integration = new function() {
 							}
 							if (installed) {
 								await session.setData(data, true);
+							} else {
+								await session.setDocPrefs();
 							}
+						} else {
+							await session.setDocPrefs();
 						}
+					} else {
+						await session.setDocPrefs();
 					}
-					await session.setDocPrefs();
 				} else {
 					throw e;
 				}

chrome/content/zotero/xpcom/style.js
@@ -279,7 +279,6 @@ Zotero.Styles = new function() {
 	 * @param {Boolean} [silent=false] Skip prompts
 	 */
 	this.install = Zotero.Promise.coroutine(function* (style, origin, silent=false) {
-		var styleTitle;
 		var warnDeprecated;
 		if (style instanceof Components.interfaces.nsIFile) {
 			warnDeprecated = true;
@@ -299,7 +298,7 @@ Zotero.Styles = new function() {
 			else if (style.url) {
 				style.string = yield Zotero.File.getContentsFromURLAsync(style.url);
 			}
-			styleTitle = yield _install(style.string, origin, false, silent);
+			var { styleTitle, styleID } = yield _install(style.string, origin, false, silent);
 		}
 		catch (error) {
 			// Unless user cancelled, show an alert with the error
@@ -321,7 +320,7 @@ Zotero.Styles = new function() {
 				}
 			}
 		}
-		return styleTitle;
+		return { styleTitle, styleID };
 	});
 	
 	/**
@@ -503,7 +502,10 @@ Zotero.Styles = new function() {
 				yield win.Zotero_Preferences.Cite.refreshStylesList(styleID);
 			}
 		}
-		return existingTitle || title;
+		return {
+			styleTitle: existingTitle || title,
+			styleID: styleID
+		};
 	});
 	
 	/**

commit 11f8bd20d6efb0818b211f8d79eabc453d5a5e08

Fix title display when installing style via connector (broken in 0f1687f1c7)

chrome/content/zotero/xpcom/connector/server_connector.js
@@ -1030,13 +1030,13 @@ Zotero.Server.Connector.InstallStyle.prototype = {
 	
 	init: Zotero.Promise.coroutine(function* (requestData) {
 		try {
-			var { styleName, styleID } = yield Zotero.Styles.install(
+			var { styleTitle, styleID } = yield Zotero.Styles.install(
 				requestData.data, requestData.query.origin || null, true
 			);
 		} catch (e) {
 			return [400, "text/plain", e.message];
 		}
-		return [201, "application/json", JSON.stringify({name: styleName})];
+		return [201, "application/json", JSON.stringify({name: styleTitle})];
 	})
 };
 

test/tests/server_connectorTest.js
@@ -674,10 +674,14 @@ describe("Connector Server", function () {
 					.createInstance(Components.interfaces.nsIDOMParser),
 				doc = parser.parseFromString(style, "application/xml");
 				
-				return Zotero.Promise.resolve(
-					Zotero.Utilities.xpathText(doc, '/csl:style/csl:info[1]/csl:title[1]',
-						Zotero.Styles.ns)
-				);
+				return Zotero.Promise.resolve({
+					styleTitle: Zotero.Utilities.xpathText(
+						doc, '/csl:style/csl:info[1]/csl:title[1]', Zotero.Styles.ns
+					),
+					styleID: Zotero.Utilities.xpathText(
+						doc, '/csl:style/csl:info[1]/csl:id[1]', Zotero.Styles.ns
+					)
+				});
 			});
 			
 			var style = `<?xml version="1.0" encoding="utf-8"?>