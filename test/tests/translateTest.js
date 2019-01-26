zotero/zotero

commit 5a6a772ca2aefd72c90430f590b25a19ee5d9ba3

Fix linked-URL attachments not being saved to groups without files
It looks like this may have been broken for years.

chrome/content/zotero/xpcom/translation/translate_item.js
@@ -445,7 +445,11 @@ Zotero.Translate.ItemSaver.prototype = {
 	
 	
 	_canSaveAttachment: function (attachment) {
-		if (this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_DOWNLOAD) {
+		// Always save link attachments
+		var isLink = Zotero.MIME.isWebPageType(attachment.mimeType)
+			// .snapshot coming from most translators, .linkMode coming from RDF
+			&& (attachment.snapshot === false || attachment.linkMode == Zotero.Attachments.LINK_MODE_LINKED_URL);
+		if (isLink || this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_DOWNLOAD) {
 			if (!attachment.url && !attachment.document) {
 				Zotero.debug("Translate: Not adding attachment: no URL specified");
 				return false;
@@ -489,9 +493,12 @@ Zotero.Translate.ItemSaver.prototype = {
 	_saveAttachment: Zotero.Promise.coroutine(function* (attachment, parentItemID, attachmentCallback) {
 		try {
 			let newAttachment;
-
+			
 			// determine whether to save files and attachments
-			if (this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_DOWNLOAD) {
+			let isLink = Zotero.MIME.isWebPageType(attachment.mimeType)
+				// .snapshot coming from most translators, .linkMode coming from RDF
+				&& (attachment.snapshot === false || attachment.linkMode == Zotero.Attachments.LINK_MODE_LINKED_URL);
+			if (isLink || this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_DOWNLOAD) {
 				newAttachment = yield this._saveAttachmentDownload.apply(this, arguments);
 			} else if (this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_FILE) {
 				newAttachment = yield this._saveAttachmentFile.apply(this, arguments);

test/tests/translateTest.js
@@ -6,7 +6,7 @@ Components.utils.import("resource://gre/modules/osfile.jsm");
  * @param {String} translatorType - "import" or "web"
  * @param {Object} items - items as translator JSON
  */
-function saveItemsThroughTranslator(translatorType, items) {
+function saveItemsThroughTranslator(translatorType, items, translateOptions = {}) {
 	let tyname;
 	if (translatorType == "web") {
 		tyname = "Web";
@@ -35,7 +35,7 @@ function saveItemsThroughTranslator(translatorType, items) {
 		"		item.complete();\n"+
 		"	}\n"+
 		"}"));
-	return translate.translate().then(function(items) {
+	return translate.translate(translateOptions).then(function(items) {
 		if (browser) Zotero.Browser.deleteHiddenBrowser(browser);
 		return items;
 	});
@@ -404,7 +404,47 @@ describe("Zotero.Translate", function() {
 			assert.equal(newItems[0].getField("title"), "Container Item");
 			assert.equal(newItems[0].getAttachments().length, 0);
 		});
-
+		
+		it("import translators should save linked-URL attachments with savingAttachments: false", async function () {
+			var json = [
+				{
+					itemType: "journalArticle",
+					title: "Parent Item",
+					attachments: [
+						// snapshot: false
+						{
+							title: "Link",
+							mimeType: "text/html",
+							url: "http://example.com",
+							snapshot: false
+						},
+						// linkMode (used by RDF import)
+						{
+							title: "Link",
+							mimeType: "text/html",
+							url: "http://example.com",
+							linkMode: Zotero.Attachments.LINK_MODE_LINKED_URL
+						}
+					]
+				}
+			];
+			
+			var newItems = itemsArrayToObject(
+				await saveItemsThroughTranslator(
+					"import",
+					json,
+					{
+						saveAttachments: false
+					}
+				)
+			);
+			var attachmentIDs = newItems["Parent Item"].getAttachments();
+			assert.lengthOf(attachmentIDs, 2);
+			var attachments = await Zotero.Items.getAsync(attachmentIDs);
+			assert.equal(attachments[0].attachmentLinkMode, Zotero.Attachments.LINK_MODE_LINKED_URL);
+			assert.equal(attachments[1].attachmentLinkMode, Zotero.Attachments.LINK_MODE_LINKED_URL);
+		});
+		
 		it('web translators should set accessDate to current date', function* () {
 			let myItem = {
 				"itemType":"webpage",

commit 8b4bb62efcbe21261b1ed32891ab6c0f043aace4

Don't load linked URLs during import
Regression from 5a6a772ca2ae, I think

.eslintrc
@@ -8,6 +8,7 @@
     "Zotero": false,
     "ZOTERO_CONFIG": false,
     "AddonManager": false,
+    "assert": false,
     "Cc": false,
     "Ci": false,
     "Components": false,

chrome/content/zotero/xpcom/translation/translate_item.js
@@ -495,7 +495,7 @@ Zotero.Translate.ItemSaver.prototype = {
 			let newAttachment;
 			
 			// determine whether to save files and attachments
-			let isLink = Zotero.MIME.isWebPageType(attachment.mimeType)
+			var isLink = Zotero.MIME.isWebPageType(attachment.mimeType)
 				// .snapshot coming from most translators, .linkMode coming from RDF
 				&& (attachment.snapshot === false || attachment.linkMode == Zotero.Attachments.LINK_MODE_LINKED_URL);
 			if (isLink || this.attachmentMode == Zotero.Translate.ItemSaver.ATTACHMENT_MODE_DOWNLOAD) {
@@ -597,7 +597,7 @@ Zotero.Translate.ItemSaver.prototype = {
 			}
 
 			// At this point, must be a valid HTTP/HTTPS url
-			attachment.linkMode = "linked_file";
+			attachment.linkMode = "linked_url";
 			newItem = yield Zotero.Attachments.linkFromURL({
 				url: attachment.url,
 				parentItemID,
@@ -775,7 +775,9 @@ Zotero.Translate.ItemSaver.prototype = {
 		// Commit to saving
 		attachmentCallback(attachment, 0);
 		
-		if(attachment.snapshot === false || this.attachmentMode === Zotero.Translate.ItemSaver.ATTACHMENT_MODE_IGNORE) {
+		var isLink = attachment.snapshot === false
+			|| attachment.linkMode == Zotero.Attachments.LINK_MODE_LINKED_URL;
+		if (isLink || this.attachmentMode === Zotero.Translate.ItemSaver.ATTACHMENT_MODE_IGNORE) {
 			// if snapshot is explicitly set to false, attach as link
 			attachment.linkMode = "linked_url";
 			let url, mimeType;

test/tests/translateTest.js
@@ -1,5 +1,6 @@
 new function() {
 Components.utils.import("resource://gre/modules/osfile.jsm");
+Components.utils.import("resource://zotero-unit/httpd.js");
 
 /**
  * Create a new translator that saves the specified items
@@ -403,6 +404,65 @@ describe("Zotero.Translate", function() {
 			assert.equal(newItems.length, 1);
 			assert.equal(newItems[0].getField("title"), "Container Item");
 			assert.equal(newItems[0].getAttachments().length, 0);
+		});
+		
+		it('import translators should save link attachments', async function () {
+			// Start a local server so we can make sure a web request isn't made for the URL
+			var port = 16213;
+			var baseURL = `http://127.0.0.1:${port}/`;
+			var httpd = new HttpServer();
+			httpd.start(port);
+			var callCount = 0;
+			var handler = function (_request, response) {
+				callCount++;
+				response.setStatusLine(null, 200, "OK");
+				response.write("<html><head><title>Title</title><body>Body</body></html>");
+			};
+			httpd.registerPathHandler("/1", { handle: handler });
+			httpd.registerPathHandler("/2", { handle: handler });
+			
+			var items = [{
+				itemType: "book",
+				title: "Item",
+				attachments: [
+					// With mimeType
+					{
+						itemType: "attachment",
+						linkMode: Zotero.Attachments.LINK_MODE_LINKED_URL,
+						title: "Link 1",
+						url: baseURL + "1",
+						mimeType: 'text/html'
+					},
+					// Without mimeType
+					{
+						itemType: "attachment",
+						linkMode: Zotero.Attachments.LINK_MODE_LINKED_URL,
+						title: "Link 2",
+						url: baseURL + "2"
+					}
+				]
+			}];
+			
+			var newItems = itemsArrayToObject(await saveItemsThroughTranslator("import", items));
+			
+			assert.equal(callCount, 0);
+			
+			var attachments = await Zotero.Items.getAsync(newItems.Item.getAttachments());
+			assert.equal(attachments.length, 2);
+			
+			assert.equal(attachments[0].getField("title"), "Link 1");
+			assert.equal(attachments[0].getField("url"), baseURL + "1");
+			assert.equal(attachments[0].attachmentContentType, "text/html");
+			assert.equal(attachments[0].attachmentLinkMode, Zotero.Attachments.LINK_MODE_LINKED_URL);
+			
+			assert.equal(attachments[1].getField("title"), "Link 2");
+			assert.equal(attachments[1].getField("url"), baseURL + "2");
+			assert.equal(attachments[1].attachmentLinkMode, Zotero.Attachments.LINK_MODE_LINKED_URL);
+			assert.equal(attachments[1].attachmentContentType, '');
+			
+			await new Promise(function (resolve) {
+				httpd.stop(resolve);
+			});
 		});
 		
 		it("import translators should save linked-URL attachments with savingAttachments: false", async function () {