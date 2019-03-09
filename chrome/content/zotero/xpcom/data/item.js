zotero/zotero

commit 8cd5b09053bcddd3258596b50867766ad4ae7f99

Add icon for linked PDF files

chrome/content/zotero/xpcom/data/item.js
@@ -3686,12 +3686,13 @@ Zotero.Item.prototype.getImageSrc = function() {
 	if (itemType == 'attachment') {
 		var linkMode = this.attachmentLinkMode;
 		
-		// Quick hack to use PDF icon for imported files and URLs --
-		// extend to support other document types later
-		if ((linkMode == Zotero.Attachments.LINK_MODE_IMPORTED_FILE ||
-				linkMode == Zotero.Attachments.LINK_MODE_IMPORTED_URL) &&
-				this.attachmentContentType == 'application/pdf') {
-			itemType += '-pdf';
+		if (this.attachmentContentType == 'application/pdf') {
+			if (linkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE) {
+				itemType += '-pdf-link';
+			}
+			else {
+				itemType += '-pdf';
+			}
 		}
 		else if (linkMode == Zotero.Attachments.LINK_MODE_IMPORTED_FILE) {
 			itemType += "-file";

chrome/content/zotero/xpcom/data/cachedTypes.js
@@ -460,6 +460,7 @@ Zotero.ItemTypes = new function() {
 			// HiDPI images available
 			case 'attachment-link':
 			case 'attachment-pdf':
+			case 'attachment-pdf-link':
 			case 'attachment-snapshot':
 			case 'attachment-web-link':
 			case 'artwork':

commit c735423996bdbba202b362233c8b6c22e3ca31fd

Don't show PDF icon for linked-URL PDFs (regression from 8cd5b09053b)

chrome/content/zotero/xpcom/data/item.js
@@ -3686,7 +3686,7 @@ Zotero.Item.prototype.getImageSrc = function() {
 	if (itemType == 'attachment') {
 		var linkMode = this.attachmentLinkMode;
 		
-		if (this.attachmentContentType == 'application/pdf') {
+		if (this.attachmentContentType == 'application/pdf' && this.isFileAttachment()) {
 			if (linkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE) {
 				itemType += '-pdf-link';
 			}