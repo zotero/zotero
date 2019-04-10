a57d38aa29448bc0f1bf812e19bd205d5e4a764d
Adomas Venčkauskas <adomas.ven@gmail.com>
2019-04-10 10:35:30 +0300

    Fix numbers in strings addition caused bug in providing citeproc citation data

--- chrome/content/zotero/xpcom/integration.js
+++ chrome/content/zotero/xpcom/integration.js
@@ -1660,7 +1660,7 @@ Zotero.Integration.Session.prototype.getCiteprocLists = function() {
 			continue;
 		}
 		citations.push([this.citationsByIndex[idx].citationID, this.citationsByIndex[idx].properties.noteIndex]);
-		fieldToCitationIdxMapping[i] = idx;
+		fieldToCitationIdxMapping[i] = parseInt(idx);
 		citationToFieldIdxMapping[idx] = i++;
 	}
 	return [citations, fieldToCitationIdxMapping, citationToFieldIdxMapping];
