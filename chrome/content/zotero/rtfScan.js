zotero/zotero

commit 269e2f8bff3eaf6d2257b06bdc80f7b2949766f6

Replace legacy syntax for each...in with for...of

chrome/content/zotero/rtfScan.js
@@ -460,7 +460,7 @@ var Zotero_RTFScan = new function() {
 	 */
 	function _refreshCanAdvance() {
 		var canAdvance = true;
-		for each(var itemList in citationItemIDs) {
+		for (let itemList of citationItemIDs) {
 			if(itemList.length != 1) {
 				canAdvance = false;
 				break;

chrome/content/zotero/bindings/itembox.xml
@@ -475,7 +475,7 @@
 							
 							var popup = button.appendChild(document.createElement("menupopup"));
 							
-							for each(var v in this._fieldAlternatives[fieldName]) {
+							for (let v of this._fieldAlternatives[fieldName]) {
 								var menuitem = document.createElement("menuitem");
 								var sv = Zotero.Utilities.ellipsize(v, 60);
 								menuitem.setAttribute('label', sv);

chrome/content/zotero/bindings/tagselector.xml
@@ -528,7 +528,7 @@
 							this._tags = yield Zotero.Tags.getAll(this.libraryID, this._types);
 							
 							for (let tag of this.selection) {
-								for each(var tag2 in this._tags) {
+								for (let tag2 of this._tags) {
 									if (tag == tag2) {
 										var found = true;
 										break;

chrome/content/zotero/fileInterface.js
@@ -474,7 +474,7 @@ var Zotero_File_Interface = new function() {
 	function _doBibliographyOptions(name, items) {
 		// make sure at least one item is not a standalone note or attachment
 		var haveRegularItem = false;
-		for each(var item in items) {
+		for (let item of items) {
 			if (item.isRegularItem()) {
 				haveRegularItem = true;
 				break;

chrome/content/zotero/tools/csledit.js
@@ -37,7 +37,7 @@ var Zotero_CSL_Editor = new function() {
 		
 		var styles = Zotero.Styles.getAll();
 		var currentStyle = null;
-		for each(var style in styles) {
+		for (let style of styles) {
 			if (style.source) {
 				continue;
 			}

chrome/content/zotero/tools/cslpreview.js
@@ -56,7 +56,7 @@ var Zotero_CSL_Preview = new function() {
 			var styles = Zotero.Styles.getAll();
 			// XXX needs its own string really for the title!
 			var str = '<html><head><title></title></head><body>';
-			for each(var style in styles) {
+			for (let style of styles) {
 				if (style.source) {
 					continue;
 				}

chrome/content/zotero/xpcom/annotate.js
@@ -141,7 +141,7 @@ Zotero.Annotate = new function() {
 			} else {
 				var browsers = win.document.getElementsByTagNameNS(XUL_NAMESPACE, "browser");
 			}
-			for each(var browser in browsers) {
+			for (let browser of browsers) {
 				if(browser.currentURI) {
 					if(browser.currentURI.spec == annotationURL) {
 						if(haveBrowser) {
@@ -364,7 +364,7 @@ Zotero.Annotate.Path.prototype.fromNode = function(node, offset) {
 					// is still part of the first text node
 					if(sibling.getAttribute) {
 						// get offset of all child nodes
-						for each(var child in sibling.childNodes) {
+						for (let child of sibling.childNodes) {
 							if(child && child.nodeType == TEXT_TYPE) {
 								this.offset += child.nodeValue.length;
 							}
@@ -754,14 +754,14 @@ Zotero.Annotations.prototype.save = function() {
 Zotero.Annotations.prototype.load = Zotero.Promise.coroutine(function* () {
 	// load annotations
 	var rows = yield Zotero.DB.queryAsync("SELECT * FROM annotations WHERE itemID = ?", [this.itemID]);
-	for each(var row in rows) {
+	for (let row of rows) {
 		var annotation = this.createAnnotation();
 		annotation.initWithDBRow(row);
 	}
 	
 	// load highlights
 	var rows = yield Zotero.DB.queryAsync("SELECT * FROM highlights WHERE itemID = ?", [this.itemID]);
-	for each(var row in rows) {
+	for (let row of rows) {
 		try {
 			var highlight = new Zotero.Highlight(this);
 			highlight.initWithDBRow(row);

chrome/content/zotero/xpcom/api.js
@@ -76,7 +76,7 @@ Zotero.API = {
 					var s2 = new Zotero.Search();
 					s2.setScope(s);
 					var groups = Zotero.Groups.getAll();
-					for each(var group in groups) {
+					for (let group of groups) {
 						s2.addCondition('libraryID', 'isNot', group.libraryID);
 					}
 					var ids = yield s2.search();

commit 7306fe910d609613bb5c6bc0bd82c6a4e39a7bb9

Fix updating of Next button in RTFScan (regression from 269e2f8bff)
And fix a logged warning

chrome/content/zotero/rtfScan.js
@@ -428,7 +428,7 @@ var Zotero_RTFScan = new function() {
 				var io = {singleSelection:true};
 				if(citationItemIDs[citation] && citationItemIDs[citation].length == 1) {	// mapped citation
 					// specify that item should be selected in window
-					io.select = citationItemIDs[citation];
+					io.select = citationItemIDs[citation][0];
 				}
 				
 				window.openDialog('chrome://zotero/content/selectItemsDialog.xul', '', 'chrome,modal', io);
@@ -460,7 +460,8 @@ var Zotero_RTFScan = new function() {
 	 */
 	function _refreshCanAdvance() {
 		var canAdvance = true;
-		for (let itemList of citationItemIDs) {
+		for (let i in citationItemIDs) {
+			let itemList = citationItemIDs[i];
 			if(itemList.length != 1) {
 				canAdvance = false;
 				break;