zotero/zotero

commit a0be2da42c2901483858a19439ccb983f70027ec

Automatically capitalize subtitle for APA styles
Applied to all styles with ids matching 'apa($|-)'

Currently limited to colons, but Juris-M/citeproc-js#74 may extend this
to other punctuation

Closes #1681

chrome/content/zotero/xpcom/style.js
@@ -648,6 +648,8 @@ Zotero.Style = function (style, path) {
 		Zotero.Styles.ns).replace(/(.+)T([^\+]+)\+?.*/, "$1 $2");
 	this.locale = Zotero.Utilities.xpathText(doc, '/csl:style/@default-locale',
 		Zotero.Styles.ns) || null;
+	var shortID = this.styleID.match(/\/?([^/]+)$/)[1];
+	this._isAPA = /^apa($|-)/.test(shortID);
 	this._class = doc.documentElement.getAttribute("class");
 	this._usesAbbreviation = !!Zotero.Utilities.xpath(doc,
 		'//csl:text[(@variable="container-title" and @form="short") or (@variable="container-title-short")][1]',
@@ -745,7 +747,10 @@ Zotero.Style.prototype.getCiteProc = function(locale, automaticJournalAbbreviati
 	
 	try {
 		var citeproc = new Zotero.CiteProc.CSL.Engine(
-			new Zotero.Cite.System(automaticJournalAbbreviations),
+			new Zotero.Cite.System({
+				automaticJournalAbbreviations,
+				uppercaseSubtitles: this._isAPA
+			}),
 			xml,
 			locale,
 			overrideLocale

chrome/content/zotero/xpcom/cite.js
@@ -584,13 +584,20 @@ Zotero.Cite.getAbbreviation = new function() {
 
 /**
  * citeproc-js system object
+ *
  * @class
+ * @param {Object} options
+ * @param {Boolean} [options.automaticJournalAbbreviations]
+ * @param {Boolean} [options.uppercaseSubtitles]
  */
-Zotero.Cite.System = function(automaticJournalAbbreviations) {
-	if(automaticJournalAbbreviations) {
+Zotero.Cite.System = function ({ automaticJournalAbbreviations, uppercaseSubtitles }) {
+	if (automaticJournalAbbreviations) {
 		this.getAbbreviation = Zotero.Cite.getAbbreviation;
 	}
-}
+	if (uppercaseSubtitles) {
+		this.uppercase_subtitles = true; // eslint-disable-line camelcase
+	}
+};
 
 Zotero.Cite.System.prototype = {
 	/**

commit f49d5805cda37feea79526c5a694b51c7efd06a6

Apply subtitle capitalization to additional styles
This also fixes a style initialization error when the style id is
invalid, which was a regression in a0be2da42c.

chrome/content/zotero/xpcom/style.js
@@ -648,8 +648,10 @@ Zotero.Style = function (style, path) {
 		Zotero.Styles.ns).replace(/(.+)T([^\+]+)\+?.*/, "$1 $2");
 	this.locale = Zotero.Utilities.xpathText(doc, '/csl:style/@default-locale',
 		Zotero.Styles.ns) || null;
-	var shortID = this.styleID.match(/\/?([^/]+)$/)[1];
-	this._isAPA = /^apa($|-)/.test(shortID);
+	this._uppercaseSubtitles = false;
+	var uppercaseSubtitlesRE = /^apa($|-)|^(academy-of-management|american-medical-association)/;
+	var shortIDMatches = this.styleID.match(/\/?([^/]+)$/);
+	this._uppercaseSubtitles = !!shortIDMatches && uppercaseSubtitlesRE.test(shortIDMatches[1]);
 	this._class = doc.documentElement.getAttribute("class");
 	this._usesAbbreviation = !!Zotero.Utilities.xpath(doc,
 		'//csl:text[(@variable="container-title" and @form="short") or (@variable="container-title-short")][1]',
@@ -749,7 +751,7 @@ Zotero.Style.prototype.getCiteProc = function(locale, automaticJournalAbbreviati
 		var citeproc = new Zotero.CiteProc.CSL.Engine(
 			new Zotero.Cite.System({
 				automaticJournalAbbreviations,
-				uppercaseSubtitles: this._isAPA
+				uppercaseSubtitles: this._uppercaseSubtitles
 			}),
 			xml,
 			locale,

test/tests/styleTest.js
@@ -42,4 +42,45 @@ describe("Zotero.Styles", function() {
 			yield Zotero.Styles.install({file: stylePath}, styleID, true);
 		})
 	});
+	
+	describe("subtitle capitalization", function () {
+		var item;
+		
+		before(async function () {
+			item = createUnsavedDataObject(
+				'item',
+				{
+					itemType: 'journalArticle',
+					title: 'Foo bar: baz qux'
+				}
+			);
+			item.setField('shortTitle', 'Foo bar');
+			item.setField('date', '2019');
+			await item.saveTx();
+		});
+		
+		it("should capitalize subtitles in APA", async function () {
+			var o = Zotero.QuickCopy.getContentFromItems(
+				[item],
+				'bibliography=http://www.zotero.org/styles/apa'
+			);
+			assert.equal(o.text, 'Foo bar: Baz qux. (2019).\n');
+		});
+		
+		it("should capitalize subtitles in AMA", async function () {
+			var o = Zotero.QuickCopy.getContentFromItems(
+				[item],
+				'bibliography=http://www.zotero.org/styles/american-medical-association'
+			);
+			assert.equal(o.text, '1. Foo bar: Baz qux. 2019.\n');
+		});
+		
+		it("shouldn't capitalize subtitles in Vancouver", async function () {
+			var o = Zotero.QuickCopy.getContentFromItems(
+				[item],
+				'bibliography=http://www.zotero.org/styles/vancouver'
+			);
+			assert.equal(o.text, '1. Foo bar: baz qux. 2019; \n');
+		});
+	});
 });