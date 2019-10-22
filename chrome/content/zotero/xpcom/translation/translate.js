1756ed181e90fb1b3d8093e4a58ed540b90a691f
Lars Willighagen <lars.willighagen@gmail.com>
2019-10-22 22:39:40 +0200

    Mark function as asynchronous
    Also let it use the try-catch block instead of returning the error to 
    the parent immediately.

--- chrome/content/zotero/xpcom/translation/translate.js
+++ chrome/content/zotero/xpcom/translation/translate.js
@@ -1344,7 +1344,7 @@ Zotero.Translate.Base.prototype = {
 			return loadPromise.then(() => rest.apply(this, arguments))
 		}
 		
-		function rest() {
+		async function rest() {
 			Zotero.debug("Translate: Beginning translation with " + this.translator[0].label);
 
 			this.incrementAsyncProcesses("Zotero.Translate#translate()");
@@ -1359,8 +1359,9 @@ Zotero.Translate.Base.prototype = {
 				// doImport can return a promise to allow for incremental saves (via promise-returning
 				// item.complete() calls)
 				if (maybePromise) {
-					return maybePromise
+					await maybePromise
 						.then(() => this.decrementAsyncProcesses("Zotero.Translate#translate()"));
+          return;
 				}
 			} catch (e) {
 				this.complete(false, e);
