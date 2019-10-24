zotero/zotero

commit 4b60c6ca275ec69249b3ea81a669ae4823678d03

Type/field handling overhaul
This changes the way item types, item fields, creator types, and CSL
mappings are defined and handled, in preparation for updated types and
fields.

Instead of being predefined in SQL files or code, type/field info is
read from a bundled JSON file shared with other parts of the Zotero
ecosystem [1], referred to as the "global schema". Updates to the
bundled schema file are automatically applied to the database at first
run, allowing changes to be made consistently across apps.

When syncing, invalid JSON properties are now rejected instead of being
ignored and processed later, which will allow for schema changes to be
made without causing problems in existing clients. We considered many
alternative approaches, but this approach is by far the simplest,
safest, and most transparent to the user.

For now, there are no actual changes to types and fields, since we'll
first need to do a sync cut-off for earlier versions that don't reject
invalid properties.

For third-party code, the main change is that type and field IDs should
no longer be hard-coded, since they may not be consistent in new
installs. For example, code should use `Zotero.ItemTypes.getID('note')`
instead of hard-coding `1`.

[1] https://github.com/zotero/zotero-schema

chrome/content/zotero/xpcom/data/dataObjectUtilities.js
@@ -129,7 +129,7 @@ Zotero.DataObjectUtilities = {
 					delete target[i];
 				}
 			}
-			// If field from base doesn't exist in new version, clear it
+			// Field from base doesn't exist in new version
 			else {
 				switch (i) {
 				// When changing an item from top-level to child, the collections property is
@@ -137,14 +137,17 @@ Zotero.DataObjectUtilities = {
 				case 'collections':
 					break;
 				
+				// Set known boolean fields to false
 				case 'deleted':
 				case 'parentItem':
 				case 'inPublications':
 					target[i] = false;
 					break;
 				
+				// Skip other fields. This prevents us from clearing fields in the pristine JSON
+				// that aren't handled here.
 				default:
-					target[i] = '';
+					delete target[i];
 				}
 			}
 		}

test/tests/dataObjectUtilitiesTest.js
@@ -47,6 +47,15 @@ describe("Zotero.DataObjectUtilities", function() {
 			assert.equal(obj.conditions[0].value, 'B');
 			assert.equal(obj.conditions[1].value, 'en');
 		})
+		
+		it("should omit unknown base properties", function () {
+			var patchBase = {
+				unknownField: 'Foo'
+			};
+			var obj = {};
+			obj = Zotero.DataObjectUtilities.patch(patchBase, obj);
+			assert.notProperty(obj, 'unknownField');
+		});
 	})
 	
 	describe("#diff()", function () {

.gitmodules
@@ -22,3 +22,6 @@
 	path = test/resource/chai-as-promised
 	url = https://github.com/domenic/chai-as-promised.git
 	branch = master
+[submodule "resource/schema/global"]
+	path = resource/schema/global
+	url = git://github.com/zotero/zotero-schema.git

chrome/content/zotero/bindings/itembox.xml
@@ -387,9 +387,7 @@
 							
 							if (fieldIsClickable
 									&& !Zotero.Items.isPrimaryField(fieldName)
-									&& (Zotero.ItemFields.isFieldOfBase(Zotero.ItemFields.getID(fieldName), 'date')
-										// TEMP - filingDate
-										|| fieldName == 'filingDate')
+									&& Zotero.ItemFields.isDate(fieldName)
 									// TEMP - NSF
 									&& fieldName != 'dateSent') {
 								this.addDateRow(fieldNames[i], this.item.getField(fieldName, true), tabindex);
@@ -414,7 +412,7 @@
 						
 						if (fieldName) {
 							label.setAttribute("value", prefix +
-								Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, fieldName));
+								Zotero.ItemFields.getLocalizedString(fieldName));
 						}
 						
 						// TEMP - NSF (homepage)
@@ -897,7 +895,7 @@
 				<body>
 				<![CDATA[
 					var label = document.createElement("label");
-					label.setAttribute("value", Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, field));
+					label.setAttribute("value", Zotero.ItemFields.getLocalizedString(field));
 					label.setAttribute("fieldname", field);
 					label.setAttribute("onclick", "this.nextSibling.firstChild.blur()");
 					
@@ -1158,7 +1156,7 @@
 						var fieldNames = "";
 						for (var i=0; i<fieldsToDelete.length; i++) {
 							fieldNames += "\n - " +
-								Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, fieldsToDelete[i]);
+								Zotero.ItemFields.getLocalizedString(fieldsToDelete[i]);
 						}
 						
 						var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
@@ -1215,7 +1213,7 @@
 					);
 					valueElement.parentNode.replaceChild(newValueElement, valueElement);
 					
-					var text = Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, 'abstractNote');
+					var text = Zotero.ItemFields.getLocalizedString('abstractNote');
 					// Add '(...)' before "Abstract" for collapsed abstracts
 					if (valueText && cur) {
 						text = '(\u2026) ' + text;

chrome/content/zotero/bindings/zoterosearch.xml
@@ -333,7 +333,7 @@
 							if (!this.id(condition.name + '-tooltip')) {
 								var fieldName = null;
 								try {
-									fieldName = Zotero.ItemFields.getLocalizedString(null, condition.name);
+									fieldName = Zotero.ItemFields.getLocalizedString(condition.name);
 								}
 								catch (e) {}
 								

chrome/content/zotero/xpcom/data/cachedTypes.js
@@ -239,6 +239,7 @@ Zotero.CreatorTypes = new function() {
 		var sql = "SELECT itemTypeID, creatorTypeID AS id, creatorType AS name, primaryField "
 			+ "FROM itemTypeCreatorTypes NATURAL JOIN creatorTypes";
 		var rows = yield Zotero.DB.queryAsync(sql);
+		_creatorTypesByItemType = {};
 		for (let i=0; i<rows.length; i++) {
 			let row = rows[i];
 			let itemTypeID = row.itemTypeID;
@@ -307,7 +308,8 @@ Zotero.CreatorTypes = new function() {
 	
 	
 	this.getLocalizedString = function(idOrName) {
-		return Zotero.getString("creatorTypes."+this.getName(idOrName));
+		var name = this.getName(idOrName);
+		return Zotero.Schema.globalSchemaLocale.creatorTypes[name];
 	}
 	
 	
@@ -437,7 +439,7 @@ Zotero.ItemTypes = new function() {
 			return _customLabels[id];
 		}
 		
-		return Zotero.getString("itemTypes." + typeName);
+		return Zotero.Schema.globalSchemaLocale.itemTypes[typeName];
 	}
 	
 	this.getImageSrc = function (itemType) {

chrome/content/zotero/xpcom/data/collection.js
@@ -699,7 +699,25 @@ Zotero.Collection.prototype.serialize = function(nested) {
  *
  * If this object is identified (has an id or library/key), loadAllData() must have been called.
  */
-Zotero.Collection.prototype.fromJSON = function (json) {
+Zotero.Collection.prototype.fromJSON = function (json, options = {}) {
+	if (options.strict) {
+		for (let prop in json) {
+			switch (prop) {
+			case 'key':
+			case 'version':
+			case 'name':
+			case 'parentCollection':
+			case 'relations':
+				break;
+			
+			default:
+				let e = new Error(`Unknown collection property '${prop}'`);
+				e.name = "ZoteroInvalidDataError";
+				throw e;
+			}
+		}
+	}
+	
 	if (!json.name) {
 		throw new Error("'name' property not provided for collection");
 	}

chrome/content/zotero/xpcom/data/creators.js
@@ -171,7 +171,7 @@ Zotero.Creators = new function() {
 	},
 	
 	
-	this.cleanData = function (data) {
+	this.cleanData = function (data, options = {}) {
 		// Validate data
 		if (data.name === undefined && data.lastName === undefined) {
 			throw new Error("Creator data must contain either 'name' or 'firstName'/'lastName' properties");
@@ -227,9 +227,12 @@ Zotero.Creators = new function() {
 		if (creatorType) {
 			cleanedData.creatorTypeID = Zotero.CreatorTypes.getID(creatorType);
 			if (!cleanedData.creatorTypeID) {
-				let msg = "'" + creatorType + "' isn't a valid creator type";
-				Zotero.debug(msg, 2);
-				Components.utils.reportError(msg);
+				if (options.strict) {
+					let e = new Error(`Unknown creator type '${creatorType}'`);
+					e.name = "ZoteroInvalidDataError";
+					throw e;
+				}
+				Zotero.warn(`'${creatorType}' isn't a valid creator type`);
 			}
 		}
 		

commit 52fd91950d1d717d89da05c4d861d3b55e554b4e

Fix deleted item fields reappearing on sync
This was a regression from 4b60c6ca27. The original plan for introducing
new fields was to have them save to the sync cache even if they weren't
supported by the current Zotero version and process them on upgrade, and
so I changed `DataObjectUtilities.patch()` to omit fields that didn't
exist locally when patching the sync cache JSON so they wouldn't be
wiped on the server. That caused this bug where locally deleted fields
were restored on every sync. It's also no longer necessary now that
we decided to just reject unknown fields from saving, so we can just
revert to the previous behavior of blanking out locally missing fields
(with the tweak that fields that are already false or empty in the base
version can be omitted).

chrome/content/zotero/xpcom/data/dataObjectUtilities.js
@@ -133,21 +133,24 @@ Zotero.DataObjectUtilities = {
 			else {
 				switch (i) {
 				// When changing an item from top-level to child, the collections property is
-				// no valid, so it doesn't need to be cleared
+				// no longer valid, so it doesn't need to be cleared
 				case 'collections':
 					break;
 				
-				// Set known boolean fields to false
+				// Set known boolean fields to false if not already
 				case 'deleted':
 				case 'parentItem':
 				case 'inPublications':
-					target[i] = false;
+					if (base[i]) {
+						target[i] = false;
+					}
 					break;
 				
-				// Skip other fields. This prevents us from clearing fields in the pristine JSON
-				// that aren't handled here.
 				default:
-					delete target[i];
+					// If base field isn't already empty, blank it out
+					if (base[i] !== '') {
+						target[i] = '';
+					}
 				}
 			}
 		}

test/tests/dataObjectUtilitiesTest.js
@@ -48,13 +48,16 @@ describe("Zotero.DataObjectUtilities", function() {
 			assert.equal(obj.conditions[1].value, 'en');
 		})
 		
-		it("should omit unknown base properties", function () {
+		it("should blank out deleted properties", function () {
 			var patchBase = {
-				unknownField: 'Foo'
+				title: 'Test',
+				place: ''
 			};
 			var obj = {};
 			obj = Zotero.DataObjectUtilities.patch(patchBase, obj);
-			assert.notProperty(obj, 'unknownField');
+			assert.propertyVal(obj, 'title', '');
+			// place was already empty, so it shouldn't be included
+			assert.notProperty(obj, 'place');
 		});
 	})
 	