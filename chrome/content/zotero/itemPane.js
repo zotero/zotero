zotero/zotero

commit 5791ffeb16f5b15748ba908422b6cf4bd6c0e7e6

Reactify item tags box
Improvements:

- Fixes autocomplete text remaining in field after selection in Fx60
- No more text or icon shifting on select (tested on macOS)

Changes:

- Tags are now selected on mousedown with no active state, as in web
  library

Regressions:

- Tooltip with tag type doesn't appear when hovering over icon
- Pressing Tab after modifying a tag loses focus
- Right-click in textbox shows custom menu instead of default text
  editing context menu (Cut/Copy/Paste)

To-do:

- Switch to this version for note tags box
- Style colored tags in autocomplete drop-down? Sort to top?
- Only show delete button on row hover, as in web library?

chrome/content/zotero/itemPane.js
@@ -23,6 +23,10 @@
     ***** END LICENSE BLOCK *****
 */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TagsBoxContainer from 'containers/tagsBoxContainer';
+
 var ZoteroItemPane = new function() {
 	var _lastItem, _itemBox, _notesLabel, _notesButton, _notesList, _tagsBox, _relatedBox;
 	var _selectedNoteID;
@@ -45,7 +49,10 @@ var ZoteroItemPane = new function() {
 		_notesLabel = document.getElementById('zotero-editpane-notes-label');
 		_notesButton = document.getElementById('zotero-editpane-notes-add');
 		_notesList = document.getElementById('zotero-editpane-dynamic-notes');
-		_tagsBox = document.getElementById('zotero-editpane-tags');
+		// Fake a ref
+		_tagsBox = {
+			current: null
+		};
 		_relatedBox = document.getElementById('zotero-editpane-related');
 		
 		this._unregisterID = Zotero.Notifier.registerObserver(this, ['item'], 'itemPane');
@@ -72,10 +79,6 @@ var ZoteroItemPane = new function() {
 				var box = _itemBox;
 				break;
 			
-			case 2:
-				var box = _tagsBox;
-				break;
-			
 			case 3:
 				var box = _relatedBox;
 				break;
@@ -86,10 +89,13 @@ var ZoteroItemPane = new function() {
 		if (_lastItem && _lastItem != item) {
 			switch (index) {
 				case 0:
-				case 2:
 					yield box.blurOpenField();
 					// DEBUG: Currently broken
 					//box.scrollToTop();
+					break;
+				
+				case 2:
+					_tagsBox.current.blurOpenField();
 					break;
 			}
 		}
@@ -175,19 +181,33 @@ var ZoteroItemPane = new function() {
 			_updateNoteCount();
 			return;
 		}
+		else if (index == 2) {
+			ReactDOM.render(
+				<TagsBoxContainer
+					key={"tagsBox-" + item.id}
+					item={item}
+					editable={mode != 'view'}
+					ref={_tagsBox}
+					onResetSelection={focusItemsList}
+				/>,
+				document.getElementById('tags-box-container')
+			);
+		}
 		
-		if (mode) {
-			box.mode = mode;
-			
-			if (box.mode == 'view') {
-				box.hideEmptyFields = true;
+		if (box) {
+			if (mode) {
+				box.mode = mode;
+				
+				if (box.mode == 'view') {
+					box.hideEmptyFields = true;
+				}
 			}
+			else {
+				box.mode = 'edit';
+			}
+			
+			box.item = item;
 		}
-		else {
-			box.mode = 'edit';
-		}
-		
-		box.item = item;
 	});
 	
 	
@@ -214,13 +234,21 @@ var ZoteroItemPane = new function() {
 			break;
 			
 		case 2:
-			var box = _tagsBox;
+			var box = _tagsBox.current;
 			break;
 		}
 		if (box) {
 			yield box.blurOpenField();
 		}
 	});
+	
+	
+	function focusItemsList() {
+		var tree = document.getElementById('zotero-items-tree');
+		if (tree) {
+			tree.focus();
+		}
+	}
 	
 	
 	this.onNoteSelected = function (item, editable) {
@@ -288,6 +316,14 @@ var ZoteroItemPane = new function() {
 								.getService(Components.interfaces.nsIPromptService);
 		if (ps.confirm(null, '', Zotero.getString('pane.item.notes.delete.confirm'))) {
 			Zotero.Items.trashTx(id);
+		}
+	}
+	
+	
+	this.removeAllTags = async function () {
+		if (Services.prompt.confirm(null, "", Zotero.getString('pane.item.tags.removeAll'))) {
+			_lastItem.setTags([]);
+			await _lastItem.saveTx();
 		}
 	}
 	

chrome/content/zotero-platform/mac/overlay.css
@@ -1,3 +1,8 @@
+/* Force use of Lucida for HTML input elements (e.g., Editable) */
+input {
+	font-family: Lucida Grande, Lucida Sans Unicode, Lucida Sans, Geneva, -apple-system, sans-serif !important;
+}
+
 #zotero-items-toolbar[state=collapsed]
 {
 	margin-left: -8px !important;

chrome/content/zotero/components/editable.jsx
@@ -1,0 +1,117 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2019 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+                     https://digitalscholar.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import EditableContent from './editable/content';
+import Input from './form/input';
+import TextAreaInput from './form/textArea';
+import SelectInput from './form/select';
+import { noop } from './utils';
+
+class Editable extends React.PureComponent {
+	get isActive() {
+		return (this.props.isActive || this.props.isBusy) && !this.props.isDisabled;
+	}
+
+	get isReadOnly() {
+		return this.props.isReadOnly || this.props.isBusy;
+	}
+
+	get className() {
+		const { input, inputComponent } = this.props;
+		return {
+			'editable': true,
+			'editing': this.isActive,
+			'textarea': inputComponent === TextAreaInput || input && input.type === TextAreaInput,
+			'select': inputComponent === SelectInput || input && input.type === SelectInput,
+		};
+	}
+
+	renderContent() {
+		const hasChildren = typeof this.props.children !== 'undefined';
+		return (
+			<React.Fragment>
+				{
+				hasChildren ?
+					this.props.children :
+					<EditableContent { ...this.props } />
+				}
+			</React.Fragment>
+		);
+	}
+
+	renderControls() {
+		const { input: InputElement, inputComponent: InputComponent } = this.props;
+		if(InputElement) {
+			return InputElement;
+		} else {
+			const { className, innerRef, ...props } = this.props;
+			props.ref = innerRef;
+			
+			return <InputComponent
+				className={ cx(className, "editable-control") }
+				{ ...props }
+			/>
+		}
+	}
+
+	render() {
+		const { isDisabled } = this.props;
+		return (
+			<div
+				tabIndex={ isDisabled ? null : this.isActive ? null : 0 }
+				onClick={ event => this.props.onClick(event) }
+				onFocus={ event => this.props.onFocus(event) }
+				onMouseDown={ event => this.props.onMouseDown(event) }
+				className={ cx(this.className) }
+			>
+				{ this.isActive ? this.renderControls() : this.renderContent() }
+			</div>
+		);
+	}
+	static defaultProps = {
+		inputComponent: Input,
+		onClick: noop,
+		onFocus: noop,
+		onMouseDown: noop,
+	};
+
+	static propTypes = {
+		children: PropTypes.oneOfType([PropTypes.element, PropTypes.array]),
+		input: PropTypes.element,
+		inputComponent: PropTypes.elementType,
+		isActive: PropTypes.bool,
+		isBusy: PropTypes.bool,
+		isDisabled: PropTypes.bool,
+		isReadOnly: PropTypes.bool,
+	};
+}
+
+
+export default React.forwardRef((props, ref) => <Editable
+	innerRef={ref} {...props}
+/>);

chrome/content/zotero/components/editable/content.jsx
@@ -1,0 +1,91 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2019 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+                     https://digitalscholar.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import TextAreaInput from '../form/textArea';
+import Select from '../form/select';
+
+class EditableContent extends React.PureComponent {
+	get hasValue() {
+		const { input, value } = this.props;
+		return !!(value || input && input.props.value);
+	}
+
+	get isSelect() {
+		const { input, inputComponent } = this.props;
+		return inputComponent === Select || input && input.type == Select;
+	}
+
+	get isTextarea() {
+		const { input, inputComponent } = this.props;
+		return inputComponent === TextAreaInput || input && input.type === TextAreaInput;
+	}
+
+	get displayValue() {
+		const { options, display, input } = this.props;
+		const value = this.props.value || input && input.props.value;
+		const placeholder = this.props.placeholder || input && input.props.placeholder;
+
+		if(!this.hasValue) { return placeholder; }
+		if(display) { return display; }
+
+		if(this.isSelect && options) {
+			const displayValue = options.find(e => e.value == value);
+			return displayValue ? displayValue.label : value;
+		}
+
+		return value;
+	}
+
+	render() {
+		const className = {
+			'editable-content': true,
+			'placeholder': !this.hasValue
+		};
+
+		return <div className={ cx(this.props.className, className) }>{ this.displayValue }</div>;
+	}
+
+	static defaultProps = {
+		value: '',
+		placeholder: ''
+	};
+
+	static propTypes = {
+		display: PropTypes.string,
+		input: PropTypes.element,
+		inputComponent: PropTypes.elementType,
+		options: PropTypes.array,
+		placeholder: PropTypes.string,
+		value: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.number
+		])
+	};
+}
+
+export default EditableContent;

chrome/content/zotero/components/form/input.jsx
@@ -1,114 +1,207 @@
-/* eslint-disable react/no-deprecated */
-'use strict';
-
-const React = require('react');
-const PropTypes = require('prop-types');
-const cx = require('classnames');
-const { noop } = () => {};
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { noop } from '../utils';
+import { pickKeys } from '@zotero/immutable';
+//import AutoResizer from './auto-resizer';
+import Autosuggest from 'react-autosuggest';
 
 class Input extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
+			suggestions: [],
 			value: props.value
 		};
+		this.suggestions = React.createRef();
+		this.showSuggestions = React.createRef(false);
+		this.preSuggestionValue = React.createRef();
+		this.selectedSuggestion = React.createRef();
 	}
 
 	cancel(event = null) {
-		this.props.onCancel && this.props.onCancel(this.hasChanged, event);
+		this.props.onCancel(this.hasChanged, event);
 		this.hasBeenCancelled = true;
-		this.input.blur();
+		this.props.innerRef.current && this.props.innerRef.current.blur();
 	}
 
 	commit(event = null) {
-		this.props.onCommit && this.props.onCommit(this.state.value, this.hasChanged, event);
+		this.props.onCommit(this.state.value, this.hasChanged, event);
 		this.hasBeenCommitted = true;
 	}
 
 	focus() {
-		if(this.input != null) {
-			this.input.focus();
-			this.props.selectOnFocus && this.input.select();
+		if (this.props.innerRef.current != null) {
+			this.props.innerRef.current.focus();
+			this.props.selectOnFocus && this.props.innerRef.current.select();
 		}
 	}
 
-	componentWillReceiveProps({ value }) {
+	UNSAFE_componentWillReceiveProps({ value }) {
 		if (value !== this.props.value) {
 			this.setState({ value });
 		}
 	}
 
-	handleChange({ target }) {
-		this.setState({ value: target.value });
-		this.props.onChange && this.props.onChange(target.value);
+	handleChange({ target }, options) {
+		var newValue = options.newValue || target.value;
+		this.setState({
+			value: newValue,
+		});
+		this.props.onChange(newValue);
 	}
 
 	handleBlur(event) {
-		const shouldCancel = this.props.onBlur && this.props.onBlur(event);
+		if (this.selectedSuggestion.current) {
+			this.selectedSuggestion.current = null;
+			return;
+		}
 		if (this.hasBeenCancelled || this.hasBeenCommitted) { return; }
+		const shouldCancel = this.props.onBlur(event);
 		shouldCancel ? this.cancel(event) : this.commit(event);
 	}
 
 	handleFocus(event) {
-		this.props.selectOnFocus && event.target.select();
-		this.props.onFocus && this.props.onFocus(event);
+		!this.focused && this.props.selectOnFocus && event.target.select();
+		// Only focus the input once so that the entered text doesn't get selected when it matches
+		// a suggestion and the input gets rerendered with the suggestions drop-down
+		this.focused = true;
+		this.showSuggestions.current = false;
+		this.props.onFocus(event);
 	}
 
 	handleKeyDown(event) {
+		this.showSuggestions.current = true;
 		switch (event.key) {
 			case 'Escape':
 				this.cancel(event);
 			break;
+			
 			case 'Enter':
-				this.commit(event);
+				if (this.selectedSuggestion.current) {
+					let value = this.selectedSuggestion.current;
+					this.selectedSuggestion.current = null;
+					this.setState({ value });
+				}
+				else {
+					this.commit(event);
+				}
 			break;
-		default:
-			return;
 		}
+		this.props.onKeyDown(event);
+	}
+
+	handlePaste(event) {
+		this.props.onPaste && this.props.onPaste(event);
+	}
+
+	// Autosuggest will call this function every time you need to update suggestions.
+	// You already implemented this logic above, so just use it.
+	async handleSuggestionsFetchRequested({ value }) {
+		this.setState({
+			suggestions: await this.props.getSuggestions(value)
+		});
+	}
+	
+	// Autosuggest will call this function every time you need to clear suggestions.
+	handleSuggestionsClearRequested() {
+		this.setState({
+			suggestions: []
+		});
+	}
+	
+	getSuggestionValue(suggestion) {
+		return suggestion;
+	}
+	
+	shouldRenderSuggestions(value) {
+		return value.length && this.showSuggestions.current;
+	}
+	
+	renderSuggestion(suggestion) {
+		return <span>
+			{suggestion}
+		</span>;
+	}
+	
+	handleSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+		this.selectedSuggestion.current = suggestionValue;
+		// focusInputOnSuggestionClick in Autosuggest doesn't work with a custom renderInputComponent,
+		// so refocus the textbox manually
+		setTimeout(() => this.props.innerRef.current.focus());
+	}
+
+	get value() {
+		return this.state.value;
 	}
 
 	get hasChanged() {
 		return this.state.value !== this.props.value;
 	}
 
-	render() {
+	renderInput() {
 		this.hasBeenCancelled = false;
 		this.hasBeenCommitted = false;
-		const extraProps = Object.keys(this.props).reduce((aggr, key) => {
-			if(key.match(/^(aria-|data-).*/)) {
-				aggr[key] = this.props[key];
-			}
-			return aggr;
-		}, {});
-		const input = <input
-			autoFocus={ this.props.autoFocus }
-			className={ this.props.className }
-			disabled={ this.props.isDisabled }
-			form={ this.props.form }
-			id={ this.props.id }
-			inputMode={ this.props.inputMode }
-			max={ this.props.max }
-			maxLength={ this.props.maxLength }
-			min={ this.props.min }
-			minLength={ this.props.minLength }
-			name={ this.props.name }
-			onBlur={ this.handleBlur.bind(this) }
-			onChange={ this.handleChange.bind(this) }
-			onFocus={ this.handleFocus.bind(this) }
-			onKeyDown={ this.handleKeyDown.bind(this) }
-			placeholder={ this.props.placeholder }
-			readOnly={ this.props.isReadOnly }
-			ref={ input => this.input = input }
-			required={ this.props.isRequired }
-			size={ this.props.size }
-			spellCheck={ this.props.spellCheck }
-			step={ this.props.step }
-			tabIndex={ this.props.tabIndex }
-			type={ this.props.type }
-			value={ this.state.value }
-			{ ...extraProps }
-		/>;
+		
+		const inputProps = {
+			disabled: this.props.isDisabled,
+			onBlur: this.handleBlur.bind(this),
+			onChange: this.handleChange.bind(this),
+			onFocus: this.handleFocus.bind(this),
+			onKeyDown: this.handleKeyDown.bind(this),
+			onPaste: this.handlePaste.bind(this),
+			readOnly: this.props.isReadOnly,
+			required: this.props.isRequired,
+			value: this.state.value,
+			...pickKeys(this.props, ['autoFocus', 'className', 'form', 'id', 'inputMode', 'max',
+				'maxLength', 'min', 'minLength', 'name', 'placeholder', 'type', 'spellCheck',
+				'step', 'tabIndex']),
+			...pickKeys(this.props, key => key.match(/^(aria-|data-).*/))
+		};
+		
+		var input = this.props.autoComplete ? (
+			<Autosuggest
+				suggestions={this.state.suggestions}
+				onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested.bind(this)}
+				onSuggestionsClearRequested={this.handleSuggestionsClearRequested.bind(this)}
+				onSuggestionSelected={this.handleSuggestionSelected}
+				getSuggestionValue={this.getSuggestionValue.bind(this)}
+				renderSuggestion={this.renderSuggestion.bind(this)}
+				// https://github.com/moroshko/react-autosuggest/issues/474
+				renderInputComponent={(inputProps) => <input {...inputProps} ref={this.props.innerRef} />}
+				focusInputOnSuggestionClick={false}
+				shouldRenderSuggestions={this.shouldRenderSuggestions.bind(this)}
+				inputProps={inputProps}
+			/>
+		) : (
+			<input { ...inputProps } />
+		);
+
+		if(this.props.resize) {
+			/*input = (
+				<AutoResizer
+					content={ this.state.value }
+					vertical={ this.props.resize === 'vertical' }
+				>
+					{ input }
+				</AutoResizer>
+			);*/
+		}
+
 		return input;
+	}
+
+	render() {
+		const className = cx({
+			'input-group': true,
+			'input': true,
+			'busy': this.props.isBusy
+		}, this.props.inputGroupClassName);
+		return (
+			<div className={ className }>
+				{ this.renderInput() }
+			</div>
+		);
 	}
 
 	static defaultProps = {
@@ -118,17 +211,23 @@ class Input extends React.PureComponent {
 		onChange: noop,
 		onCommit: noop,
 		onFocus: noop,
+		onKeyDown: noop,
+		onPaste: noop,
 		tabIndex: -1,
 		type: 'text',
 		value: '',
 	};
 
 	static propTypes = {
+		autoComplete: PropTypes.bool,
 		autoFocus: PropTypes.bool,
 		className: PropTypes.string,
 		form: PropTypes.string,
+		getSuggestions: PropTypes.func,
 		id: PropTypes.string,
+		inputGroupClassName: PropTypes.string,
 		inputMode: PropTypes.string,
+		isBusy: PropTypes.bool,
 		isDisabled: PropTypes.bool,
 		isReadOnly: PropTypes.bool,
 		isRequired: PropTypes.bool,
@@ -137,12 +236,15 @@ class Input extends React.PureComponent {
 		min: PropTypes.number,
 		minLength: PropTypes.number,
 		name: PropTypes.string,
-		onBlur: PropTypes.func,
-		onCancel: PropTypes.func,
-		onChange: PropTypes.func,
-		onCommit: PropTypes.func,
-		onFocus: PropTypes.func,
+		onBlur: PropTypes.func.isRequired,
+		onCancel: PropTypes.func.isRequired,
+		onChange: PropTypes.func.isRequired,
+		onCommit: PropTypes.func.isRequired,
+		onFocus: PropTypes.func.isRequired,
+		onKeyDown: PropTypes.func,
+		onPaste: PropTypes.func,
 		placeholder: PropTypes.string,
+		resize: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
 		selectOnFocus: PropTypes.bool,
 		spellCheck: PropTypes.bool,
 		step: PropTypes.number,
@@ -152,4 +254,6 @@ class Input extends React.PureComponent {
 	};
 }
 
-module.exports = Input;
+export default React.forwardRef((props, ref) => <Input
+	innerRef={ref} {...props}
+/>);

chrome/content/zotero/components/form/select.jsx
@@ -1,0 +1,221 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2019 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+                     https://digitalscholar.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { noop } from '../utils';
+//import Spinner from '../ui/spinner';
+import Select from 'react-select';
+
+class SelectInput extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: props.value
+		};
+	}
+
+	cancel(event = null) {
+		this.props.onCancel(this.hasChanged, event);
+	}
+
+	commit(event = null, value = null, force = false) {
+		this.props.onCommit(value || this.state.value, force ? true : this.hasChanged, event);
+	}
+
+	focus() {
+		if(this.input != null) {
+			this.input.focus();
+		}
+	}
+
+	UNSAFE_componentWillReceiveProps({ value }) {
+		if (value !== this.props.value) {
+			this.setState({ value });
+		}
+	}
+
+	handleChange(value, ev) {
+		value = value !== null || (value === null && this.props.clearable) ?
+			value : this.props.value;
+		this.setState({ value });
+
+		if(this.props.onChange(value) || this.forceCommitOnNextChange) {
+			if(!ev) {
+				//@NOTE: this is using undocumeneted feature of react-selct v1, but see #131
+				const source = typeof this.input.input.getInput === 'function' ?
+					this.input.input.getInput() : this.input.input;
+				ev = {
+					type: 'change',
+					currentTarget: source,
+					target: source
+				}
+			}
+			this.commit(ev, value, value !== this.props.value);
+		}
+		this.forceCommitOnNextChange = false;
+	}
+
+	handleBlur(event) {
+		this.props.onBlur(event);
+		this.cancel(event);
+		if(this.props.autoBlur) {
+			this.forceCommitOnNextChange = true;
+		}
+	}
+
+	handleFocus(event) {
+		this.props.onFocus(event);
+	}
+
+	handleKeyDown(event) {
+		switch (event.key) {
+			case 'Escape':
+				this.cancel(event);
+			break;
+		default:
+			return;
+		}
+	}
+
+	get hasChanged() {
+		return this.state.value !== this.props.value;
+	}
+
+	get defaultSelectProps() {
+		return {
+			simpleValue: true,
+			clearable: false,
+		};
+	}
+
+	renderInput(userType, viewport) {
+		const {
+			options,
+			autoFocus,
+			className,
+			id,
+			placeholder,
+			tabIndex,
+			value,
+		} = this.props;
+
+		const commonProps = {
+			disabled: this.props.isDisabled,
+			onBlur: this.handleBlur.bind(this),
+			onFocus: this.handleFocus.bind(this),
+			readOnly: this.props.isReadOnly,
+			ref: input => this.input = input,
+			required: this.props.isRequired,
+		};
+
+		if(userType === 'touch' || viewport.xxs || viewport.xs || viewport.sm) {
+			const props = {
+				...commonProps,
+				onKeyDown: this.handleKeyDown.bind(this),
+				onChange: ev => this.handleChange(ev.target.value, ev),
+				autoFocus, id, placeholder, tabIndex, value
+			};
+			return (
+				<div className="native-select-wrap" >
+					<select { ...props }>
+						{ options.map(({ value, label }) => (
+							<option key={ value } value={ value }>{ label }</option>)
+						)}
+					</select>
+					<div className={ className }>
+						{ (options.find(o => o.value === value) || options[0] || {}).label }
+					</div>
+				</div>
+
+			);
+		} else {
+			const props = {
+				...this.defaultSelectProps,
+				...this.props,
+				...commonProps,
+				onInputKeyDown: this.handleKeyDown.bind(this),
+				onChange: this.handleChange.bind(this),
+			};
+
+			return <Select { ...props } />;
+		}
+	}
+
+	renderSpinner() {
+		return null;
+		//return this.props.isBusy ? <Spinner className="small" /> : null;
+	}
+
+	render() {
+		const { userType, viewport } = this.props.device;
+		const className = cx({
+			'input-group': true,
+			'select': true,
+			'busy': this.props.isBusy
+		}, this.props.inputGroupClassName);
+		return (
+			<div className={ className }>
+				{ this.renderInput(userType, viewport) }
+				{ this.renderSpinner() }
+			</div>
+		);
+	}
+
+	static defaultProps = {
+		className: 'form-control',
+		onBlur: noop,
+		onCancel: noop,
+		onChange: noop,
+		onCommit: noop,
+		onFocus: noop,
+		options: [],
+		tabIndex: -1,
+		value: '',
+	};
+
+	static propTypes = {
+		autoFocus: PropTypes.bool,
+		className: PropTypes.string,
+		id: PropTypes.string,
+		inputGroupClassName: PropTypes.string,
+		isBusy: PropTypes.bool,
+		isDisabled: PropTypes.bool,
+		isReadOnly: PropTypes.bool,
+		isRequired: PropTypes.bool,
+		onBlur: PropTypes.func.isRequired,
+		onCancel: PropTypes.func.isRequired,
+		onChange: PropTypes.func.isRequired,
+		onCommit: PropTypes.func.isRequired,
+		onFocus: PropTypes.func.isRequired,
+		options: PropTypes.array.isRequired,
+		placeholder: PropTypes.string,
+		tabIndex: PropTypes.number,
+		value: PropTypes.string.isRequired,
+	};
+}
+
+export default SelectInput;

commit b0eb048bce6e290b6db7eeb9f5ab6e3e8b5368ed

Fix New Item menu when tag pane is focused
Regression from 5791ffeb1

chrome/content/zotero/itemPane.js
@@ -231,14 +231,17 @@ var ZoteroItemPane = new function() {
 		switch (tabBox.selectedIndex) {
 		case 0:
 			var box = _itemBox;
+			if (box) {
+				yield box.blurOpenField();
+			}
 			break;
 			
 		case 2:
 			var box = _tagsBox.current;
+			if (box) {
+				box.blurOpenField();
+			}
 			break;
-		}
-		if (box) {
-			yield box.blurOpenField();
 		}
 	});
 	