zotero/zotero

commit a549a64de937c3045a504f7b5745988650083c8b

Remove 'yesterday'/'today'/'tomorrow' parsing from strToDate()
I don't think this ever should've been here. It was added in 2010 but
then added to itembox.xml a couple weeks later in d500b50e2, since
that's where the conversion should happen.

Closes #1749

chrome/content/zotero/xpcom/date.js
@@ -263,27 +263,14 @@ Zotero.Date = new function(){
 			order: ''
 		};
 		
+		string = Zotero.Utilities.trimInternal(string.toString());
+		
 		// skip empty things
 		if(!string) {
 			return date;
 		}
 		
 		var parts = [];
-		
-		// Parse 'yesterday'/'today'/'tomorrow'
-		var lc = (string + '').toLowerCase();
-		if (lc == 'yesterday' || (Zotero.isClient && lc === Zotero.getString('date.yesterday'))) {
-			string = Zotero.Date.dateToSQL(new Date(Date.now() - 1000*60*60*24)).substr(0, 10); // no 'this' for translator sandbox
-		}
-		else if (lc == 'today' || (Zotero.isClient && lc == Zotero.getString('date.today'))) {
-			string = Zotero.Date.dateToSQL(new Date()).substr(0, 10);
-		}
-		else if (lc == 'tomorrow' || (Zotero.isClient && lc == Zotero.getString('date.tomorrow'))) {
-			string = Zotero.Date.dateToSQL(new Date(Date.now() + 1000*60*60*24)).substr(0, 10);
-		}
-		else {
-			string = string.toString().replace(/^\s+|\s+$/g, "").replace(/\s+/, " ");
-		}
 		
 		// first, directly inspect the string
 		var m = _slashRe.exec(string);

commit fbb54a762188b71dd7257460d46d05512b5d5bc8

Fix "string is undefined" export error
Regression from a549a64de93

chrome/content/zotero/xpcom/date.js
@@ -263,7 +263,9 @@ Zotero.Date = new function(){
 			order: ''
 		};
 		
-		string = Zotero.Utilities.trimInternal(string.toString());
+		if (typeof string == 'string' || typeof string == 'number') {
+			string = Zotero.Utilities.trimInternal(string.toString());
+		}
 		
 		// skip empty things
 		if(!string) {

test/tests/dateTest.js
@@ -157,6 +157,31 @@ describe("Zotero.Date", function() {
 	})
 	
 	describe("#strToDate()", function () {
+		it("should return object without date parts for null", function () {
+			var o = Zotero.Date.strToDate(null);
+			assert.notProperty(o, 'year');
+		});
+		
+		it("should return object without date parts for undefined", function () {
+			var o = Zotero.Date.strToDate();
+			assert.notProperty(o, 'year');
+		});
+		
+		it("should return object without date parts for false", function () {
+			var o = Zotero.Date.strToDate(false);
+			assert.notProperty(o, 'year');
+		});
+		
+		it("should return object without date parts for empty string", function () {
+			var o = Zotero.Date.strToDate('');
+			assert.notProperty(o, 'year');
+		});
+		
+		it("should return object without date parts for blank string", function () {
+			var o = Zotero.Date.strToDate(' ');
+			assert.notProperty(o, 'year');
+		});
+		
 		it("should work in translator sandbox", function* () {
 			var item = createUnsavedDataObject('item');
 			item.libraryID = Zotero.Libraries.userLibraryID;