5c95d4e0ba52ca2a32c100a425c5f35ba8ef9c24
Dan Stillman <dstillman@zotero.org>
2020-02-16 12:43:21 -0500

    Additional logging for "Item collection not found" error in toJSON()

--- chrome/content/zotero/xpcom/data/item.js
+++ chrome/content/zotero/xpcom/data/item.js
@@ -4506,7 +4506,7 @@ Zotero.Item.prototype.toJSON = function (options = {}) {
 		obj.collections = this.getCollections().map(function (id) {
 			var { libraryID, key } = this.ContainerObjectsClass.getLibraryAndKeyFromID(id);
 			if (!key) {
-				throw new Error("Item collection " + id + " not found");
+				throw new Error("Collection " + id + " not found for item " + this.libraryKey);
 			}
 			return key;
 		}.bind(this));
