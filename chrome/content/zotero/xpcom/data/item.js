2645f0de12578cfcf35f039ee34c78f428afb21e
Dan Stillman <dstillman@zotero.org>
2020-03-03 00:23:32 -0500

    Fix "extra is null" error (also fixed via API)

--- chrome/content/zotero/xpcom/data/item.js
+++ chrome/content/zotero/xpcom/data/item.js
@@ -4209,7 +4209,7 @@ Zotero.Item.prototype.fromJSON = function (json, options = {}) {
 	var isValidForType = {};
 	var setFields = new Set();
 	var { fields: extraFields, creators: extraCreators, extra } = Zotero.Utilities.Internal.extractExtraFields(
-		json.extra !== undefined ? json.extra : '',
+		json.extra || '',
 		this,
 		Object.keys(json)
 			// TEMP until we move creator lines to real creators
