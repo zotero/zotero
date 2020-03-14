440b65935eb4873e10187d703f8a9407bef5f0c7
Dan Stillman <dstillman@zotero.org>
2020-03-13 22:05:53 -0400

    Fix breakage in 067980973

--- chrome/content/zotero/xpcom/schema.js
+++ chrome/content/zotero/xpcom/schema.js
@@ -1910,7 +1910,7 @@ Zotero.Schema = new function(){
 					Zotero.logError(e);
 					// Clear flag on failure, to avoid showing an error on every startup if someone
 					// doesn't know how to deal with it
-					await setIntegrityCheckRequired(false);
+					yield setIntegrityCheckRequired(false);
 				}
 			}
 			
@@ -1919,7 +1919,7 @@ Zotero.Schema = new function(){
 		
 		// Clear flag on success
 		if (fix) {
-			await setIntegrityCheckRequired(false);
+			yield setIntegrityCheckRequired(false);
 		}
 		
 		return true;
