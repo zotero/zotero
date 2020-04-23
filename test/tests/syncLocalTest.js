zotero/zotero

commit 4b60c6ca275ec69249b3ea81a669ae4823678d03

Type/field handling overhaul
This changes the way item types, item fields, creator types, and CSL
mappings are defined and handled, in preparation for updated types and
fields.

Instead of being predefined in SQL files or code, type/field info is
read from a bundled JSON file shared with other parts of the Zotero
ecosystem [1], referred to as the "global schema". Updates to the
bundled schema file are automatically applied to the database at first
run, allowing changes to be made consistently across apps.

When syncing, invalid JSON properties are now rejected instead of being
ignored and processed later, which will allow for schema changes to be
made without causing problems in existing clients. We considered many
alternative approaches, but this approach is by far the simplest,
safest, and most transparent to the user.

For now, there are no actual changes to types and fields, since we'll
first need to do a sync cut-off for earlier versions that don't reject
invalid properties.

For third-party code, the main change is that type and field IDs should
no longer be hard-coded, since they may not be consistent in new
installs. For example, code should use `Zotero.ItemTypes.getID('note')`
instead of hard-coding `1`.

[1] https://github.com/zotero/zotero-schema

chrome/content/zotero/xpcom/sync/syncLocal.js
@@ -721,11 +721,10 @@ Zotero.Sync.Data.Local = {
 		var ObjectType = Zotero.Utilities.capitalize(objectType);
 		var libraryName = Zotero.Libraries.get(libraryID).name;
 		
-		var knownErrors = [
-			'ZoteroUnknownTypeError',
-			'ZoteroUnknownFieldError',
+		var knownErrors = new Set([
+			'ZoteroInvalidDataError',
 			'ZoteroMissingObjectError'
-		];
+		]);
 		
 		Zotero.debug("Processing " + json.length + " downloaded "
 			+ (json.length == 1 ? objectType : objectTypePlural)
@@ -1027,8 +1026,11 @@ Zotero.Sync.Data.Local = {
 					}
 				}
 				catch (e) {
+					// This allows errors handled by syncRunner to know the library in question
+					e.libraryID = libraryID;
+					
 					// Display nicer debug line for known errors
-					if (knownErrors.indexOf(e.name) != -1) {
+					if (knownErrors.has(e.name)) {
 						let desc = e.name
 							.replace(/^Zotero/, "")
 							// Convert "MissingObjectError" to "missing object error"
@@ -1407,7 +1409,7 @@ Zotero.Sync.Data.Local = {
 			json = this._checkCacheJSON(json);
 			
 			if (!options.skipData) {
-				obj.fromJSON(json.data);
+				obj.fromJSON(json.data, { strict: true });
 			}
 			if (obj.objectType == 'item' && obj.isImportedAttachment()) {
 				yield this._checkAttachmentForDownload(obj, json.data.mtime, options.isNewObject);

.gitmodules
@@ -22,3 +22,6 @@
 	path = test/resource/chai-as-promised
 	url = https://github.com/domenic/chai-as-promised.git
 	branch = master
+[submodule "resource/schema/global"]
+	path = resource/schema/global
+	url = git://github.com/zotero/zotero-schema.git

chrome/content/zotero/bindings/itembox.xml
@@ -387,9 +387,7 @@
 							
 							if (fieldIsClickable
 									&& !Zotero.Items.isPrimaryField(fieldName)
-									&& (Zotero.ItemFields.isFieldOfBase(Zotero.ItemFields.getID(fieldName), 'date')
-										// TEMP - filingDate
-										|| fieldName == 'filingDate')
+									&& Zotero.ItemFields.isDate(fieldName)
 									// TEMP - NSF
 									&& fieldName != 'dateSent') {
 								this.addDateRow(fieldNames[i], this.item.getField(fieldName, true), tabindex);
@@ -414,7 +412,7 @@
 						
 						if (fieldName) {
 							label.setAttribute("value", prefix +
-								Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, fieldName));
+								Zotero.ItemFields.getLocalizedString(fieldName));
 						}
 						
 						// TEMP - NSF (homepage)
@@ -897,7 +895,7 @@
 				<body>
 				<![CDATA[
 					var label = document.createElement("label");
-					label.setAttribute("value", Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, field));
+					label.setAttribute("value", Zotero.ItemFields.getLocalizedString(field));
 					label.setAttribute("fieldname", field);
 					label.setAttribute("onclick", "this.nextSibling.firstChild.blur()");
 					
@@ -1158,7 +1156,7 @@
 						var fieldNames = "";
 						for (var i=0; i<fieldsToDelete.length; i++) {
 							fieldNames += "\n - " +
-								Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, fieldsToDelete[i]);
+								Zotero.ItemFields.getLocalizedString(fieldsToDelete[i]);
 						}
 						
 						var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
@@ -1215,7 +1213,7 @@
 					);
 					valueElement.parentNode.replaceChild(newValueElement, valueElement);
 					
-					var text = Zotero.ItemFields.getLocalizedString(this.item.itemTypeID, 'abstractNote');
+					var text = Zotero.ItemFields.getLocalizedString('abstractNote');
 					// Add '(...)' before "Abstract" for collapsed abstracts
 					if (valueText && cur) {
 						text = '(\u2026) ' + text;

chrome/content/zotero/bindings/zoterosearch.xml
@@ -333,7 +333,7 @@
 							if (!this.id(condition.name + '-tooltip')) {
 								var fieldName = null;
 								try {
-									fieldName = Zotero.ItemFields.getLocalizedString(null, condition.name);
+									fieldName = Zotero.ItemFields.getLocalizedString(condition.name);
 								}
 								catch (e) {}
 								

chrome/content/zotero/xpcom/data/cachedTypes.js
@@ -239,6 +239,7 @@ Zotero.CreatorTypes = new function() {
 		var sql = "SELECT itemTypeID, creatorTypeID AS id, creatorType AS name, primaryField "
 			+ "FROM itemTypeCreatorTypes NATURAL JOIN creatorTypes";
 		var rows = yield Zotero.DB.queryAsync(sql);
+		_creatorTypesByItemType = {};
 		for (let i=0; i<rows.length; i++) {
 			let row = rows[i];
 			let itemTypeID = row.itemTypeID;
@@ -307,7 +308,8 @@ Zotero.CreatorTypes = new function() {
 	
 	
 	this.getLocalizedString = function(idOrName) {
-		return Zotero.getString("creatorTypes."+this.getName(idOrName));
+		var name = this.getName(idOrName);
+		return Zotero.Schema.globalSchemaLocale.creatorTypes[name];
 	}
 	
 	
@@ -437,7 +439,7 @@ Zotero.ItemTypes = new function() {
 			return _customLabels[id];
 		}
 		
-		return Zotero.getString("itemTypes." + typeName);
+		return Zotero.Schema.globalSchemaLocale.itemTypes[typeName];
 	}
 	
 	this.getImageSrc = function (itemType) {

chrome/content/zotero/xpcom/data/collection.js
@@ -699,7 +699,25 @@ Zotero.Collection.prototype.serialize = function(nested) {
  *
  * If this object is identified (has an id or library/key), loadAllData() must have been called.
  */
-Zotero.Collection.prototype.fromJSON = function (json) {
+Zotero.Collection.prototype.fromJSON = function (json, options = {}) {
+	if (options.strict) {
+		for (let prop in json) {
+			switch (prop) {
+			case 'key':
+			case 'version':
+			case 'name':
+			case 'parentCollection':
+			case 'relations':
+				break;
+			
+			default:
+				let e = new Error(`Unknown collection property '${prop}'`);
+				e.name = "ZoteroInvalidDataError";
+				throw e;
+			}
+		}
+	}
+	
 	if (!json.name) {
 		throw new Error("'name' property not provided for collection");
 	}

chrome/content/zotero/xpcom/data/creators.js
@@ -171,7 +171,7 @@ Zotero.Creators = new function() {
 	},
 	
 	
-	this.cleanData = function (data) {
+	this.cleanData = function (data, options = {}) {
 		// Validate data
 		if (data.name === undefined && data.lastName === undefined) {
 			throw new Error("Creator data must contain either 'name' or 'firstName'/'lastName' properties");
@@ -227,9 +227,12 @@ Zotero.Creators = new function() {
 		if (creatorType) {
 			cleanedData.creatorTypeID = Zotero.CreatorTypes.getID(creatorType);
 			if (!cleanedData.creatorTypeID) {
-				let msg = "'" + creatorType + "' isn't a valid creator type";
-				Zotero.debug(msg, 2);
-				Components.utils.reportError(msg);
+				if (options.strict) {
+					let e = new Error(`Unknown creator type '${creatorType}'`);
+					e.name = "ZoteroInvalidDataError";
+					throw e;
+				}
+				Zotero.warn(`'${creatorType}' isn't a valid creator type`);
 			}
 		}
 		

chrome/content/zotero/xpcom/data/dataObjectUtilities.js
@@ -129,7 +129,7 @@ Zotero.DataObjectUtilities = {
 					delete target[i];
 				}
 			}
-			// If field from base doesn't exist in new version, clear it
+			// Field from base doesn't exist in new version
 			else {
 				switch (i) {
 				// When changing an item from top-level to child, the collections property is
@@ -137,14 +137,17 @@ Zotero.DataObjectUtilities = {
 				case 'collections':
 					break;
 				
+				// Set known boolean fields to false
 				case 'deleted':
 				case 'parentItem':
 				case 'inPublications':
 					target[i] = false;
 					break;
 				
+				// Skip other fields. This prevents us from clearing fields in the pristine JSON
+				// that aren't handled here.
 				default:
-					target[i] = '';
+					delete target[i];
 				}
 			}
 		}

commit 2c046a227b69f2a2660cf297e9d5925030eee20b

Fix sync error on collection/search conflict
> Invalid data error for collection [key] in [library]: Unknown
> collection property 'dateAdded'"

Regression from 4b60c6ca27, probably

chrome/content/zotero/xpcom/sync/syncLocal.js
@@ -956,6 +956,7 @@ Zotero.Sync.Data.Local = {
 								);
 								// Transfer properties that aren't in the changeset
 								['version', 'dateAdded', 'dateModified'].forEach(x => {
+									if (jsonData[x] === undefined) return;
 									if (jsonDataLocal[x] !== jsonData[x]) {
 										Zotero.debug(`Applying remote '${x}' value`);
 									}
@@ -1499,6 +1500,7 @@ Zotero.Sync.Data.Local = {
 		Zotero.debug("CHANGESET2");
 		Zotero.debug(changeset2);
 		
+		const isAutoMergeType = objectType != 'item';
 		var conflicts = [];
 		var matchedLocalChanges = new Set();
 		
@@ -1602,7 +1604,7 @@ Zotero.Sync.Data.Local = {
 				// Automatically apply remote changes if both items are in trash and for non-items,
 				// even if in conflict
 				if ((objectType == 'item' && currentJSON.deleted && newJSON.deleted)
-						|| objectType != 'item') {
+						|| isAutoMergeType) {
 					continue;
 				}
 				
@@ -1613,12 +1615,23 @@ Zotero.Sync.Data.Local = {
 			}
 		}
 		
+		// If there were local changes that weren't made remotely as well, the object needs to be
+		// kept as unsynced
+		var localChanged = changeset1.length > matchedLocalChanges.size;
+		
+		// If we're applying remote changes automatically, only consider the local object as changed
+		// if fields were changed that weren't changed remotely
+		if (isAutoMergeType && localChanged) {
+			let remoteFields = new Set(changeset2.map(x => x.field));
+			if (changeset1.every(x => remoteFields.has(x.field))) {
+				localChanged = false;
+			}
+		}
+		
 		return {
 			changes: changeset2,
 			conflicts,
-			// If there were local changes that weren't made remotely as well, the item needs to be
-			// kept as unsynced
-			localChanged: changeset1.length > matchedLocalChanges.size
+			localChanged
 		};
 	},
 	

test/tests/syncLocalTest.js
@@ -674,6 +674,37 @@ describe("Zotero.Sync.Data.Local", function() {
 			}
 		});
 		
+		it("should automatically resolve collection name conflict", async function () {
+			var libraryID = Zotero.Libraries.userLibraryID;
+			
+			var type = 'collection';
+			let obj = await createDataObject(type, { version: 5 });
+			let data = obj.toJSON();
+			await Zotero.Sync.Data.Local.saveCacheObjects(type, libraryID, [data]);
+			
+			// Change local name
+			await modifyDataObject(obj);
+			var changedName = Zotero.Utilities.randomString();
+			
+			// Create remote version with changed name
+			data.version = 10;
+			data.name = changedName;
+			let json = {
+				key: obj.key,
+				version: 10,
+				data
+			};
+			await Zotero.Sync.Data.Local.processObjectsFromJSON(
+				type, libraryID, [json], { stopOnError: true }
+			);
+			assert.equal(obj.version, 10);
+			assert.equal(obj.name, changedName);
+			assert.isTrue(obj.synced);
+			// Sync cache should match remote
+			var cacheJSON = await Zotero.Sync.Data.Local.getCacheObject(type, libraryID, data.key, data.version);
+			assert.propertyVal(cacheJSON.data, "name", changedName);
+		});
+		
 		it("should delete older versions in sync cache after processing", function* () {
 			var libraryID = Zotero.Libraries.userLibraryID;
 			