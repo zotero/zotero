zotero/zotero

commit 617564982c81962a9ddf819f7e09385ddd91ec51

Include only related-item relations when duplicating items
Don't include linked-object or replaced-item relations. Previously, if
you duplicated an item, modified it to represent a different source, and
dragged it to another library where you had already copied the original
item, the new item wouldn't be transferred.

https://forums.zotero.org/discussion/comment/353246/#Comment_353246

chrome/content/zotero/xpcom/data/item.js
@@ -4006,7 +4006,23 @@ Zotero.Item.prototype.clone = function (libraryID, options = {}) {
 	
 	if (sameLibrary) {
 		// DEBUG: this will add reverse-only relateds too
-		newItem.setRelations(this.getRelations());
+		let relations = this.getRelations();
+		
+		// Only include certain relations
+		let predicates = [
+			Zotero.Relations.relatedItemPredicate,
+		];
+		let any = false;
+		let newRelations = {};
+		for (let predicate of predicates) {
+			if (relations[predicate]) {
+				newRelations[predicate] = relations[predicate];
+				any = true;
+			}
+		}
+		if (any) {
+			newItem.setRelations(newRelations);
+		}
 	}
 	
 	return newItem;

test/tests/itemTest.js
@@ -1336,6 +1336,16 @@ describe("Zotero.Item", function () {
 			var newItem = item.clone();
 			assert.sameDeepMembers(item.getCreators(), newItem.getCreators());
 		})
+		
+		it("shouldn't copy linked-item relation", async function () {
+			var group = await getGroup();
+			var groupItem = await createDataObject('item', { libraryID: group.libraryID });
+			var item = await createDataObject('item');
+			await item.addLinkedItem(groupItem);
+			assert.equal(await item.getLinkedItem(group.libraryID), groupItem);
+			var newItem = item.clone();
+			assert.isEmpty(Object.keys(newItem.toJSON().relations));
+		});
 	})
 	
 	describe("#moveToLibrary()", function () {

commit a8c682bf4beb003d6210f413fa5ba86ad621b8c4

Preserve linked-object and replaced-item relations when merging items
https://groups.google.com/d/msgid/zotero-dev/6f764822-ed7c-46eb-8068-ce9ed1a1538c%40googlegroups.com

Regression from 617564982c

chrome/content/zotero/duplicatesMerge.js
@@ -141,7 +141,7 @@ var Zotero_Duplicates_Pane = new function () {
 		}
 		
 		_masterItem = item;
-		itembox.item = item.clone(null, { includeCollections: true });
+		itembox.item = item.clone();
 	}
 	
 	
@@ -149,7 +149,12 @@ var Zotero_Duplicates_Pane = new function () {
 		var itembox = document.getElementById('zotero-duplicates-merge-item-box');
 		Zotero.CollectionTreeCache.clear();
 		// Update master item with any field alternatives from the item box
-		_masterItem.fromJSON(itembox.item.toJSON());
+		var json = _masterItem.toJSON();
+		// Exclude certain properties that are empty in the cloned object, so we don't clobber them
+		const { relations, collections, tags, ...keep } = itembox.item.toJSON();
+		Object.assign(json, keep);
+		
+		_masterItem.fromJSON(json);
 		Zotero.Items.merge(_masterItem, _otherItems);
 	});
 }

chrome/content/zotero/xpcom/data/item.js
@@ -4624,7 +4624,7 @@ Zotero.Item.prototype.toJSON = function (options = {}) {
 	}
 	
 	// Relations
-	obj.relations = this.getRelations()
+	obj.relations = this.getRelations();
 	
 	if (obj.accessDate) obj.accessDate = Zotero.Date.sqlToISO8601(obj.accessDate);
 	

test/tests/itemPaneTest.js
@@ -316,4 +316,53 @@ describe("Item pane", function () {
 			});
 		});
 	});
+	
+	describe("Duplicates Merge pane", function () {
+		// Same as test in itemsTest, but via UI, which makes a copy via toJSON()/fromJSON()
+		it("should transfer merge-tracking relations when merging two pairs into one item", async function () {
+			var item1 = await createDataObject('item', { title: 'A' });
+			var item2 = await createDataObject('item', { title: 'B' });
+			var item3 = await createDataObject('item', { title: 'C' });
+			var item4 = await createDataObject('item', { title: 'D' });
+			
+			var uris = [item2, item3, item4].map(item => Zotero.URI.getItemURI(item));
+			
+			var p;
+			
+			var zp = win.ZoteroPane;
+			await zp.selectItems([item1.id, item2.id]);
+			zp.mergeSelectedItems();
+			p = waitForItemEvent('modify');
+			doc.getElementById('zotero-duplicates-merge-button').click();
+			await p;
+			
+			assert.sameMembers(
+				item1.getRelations()[Zotero.Relations.replacedItemPredicate],
+				[uris[0]]
+			);
+			
+			await zp.selectItems([item3.id, item4.id]);
+			zp.mergeSelectedItems();
+			p = waitForItemEvent('modify');
+			doc.getElementById('zotero-duplicates-merge-button').click();
+			await p;
+			
+			assert.sameMembers(
+				item3.getRelations()[Zotero.Relations.replacedItemPredicate],
+				[uris[2]]
+			);
+			
+			await zp.selectItems([item1.id, item3.id]);
+			zp.mergeSelectedItems();
+			p = waitForItemEvent('modify');
+			doc.getElementById('zotero-duplicates-merge-button').click();
+			await p;
+			
+			// Remaining item should include all other URIs
+			assert.sameMembers(
+				item1.getRelations()[Zotero.Relations.replacedItemPredicate],
+				uris
+			);
+		});
+	});
 })

test/tests/itemsTest.js
@@ -291,6 +291,27 @@ describe("Zotero.Items", function () {
 			assert.sameMembers(rels, [item2URI, item3URI]);
 		})
 		
+		// Same as test in itemPaneTest, but without the UI
+		it("should transfer merge-tracking relations when merging two pairs into one item", async function () {
+			var item1 = await createDataObject('item', { title: 'A' });
+			var item2 = await createDataObject('item', { title: 'B' });
+			var item3 = await createDataObject('item', { title: 'C' });
+			var item4 = await createDataObject('item', { title: 'D' });
+			
+			var uris = [item2, item3, item4].map(item => Zotero.URI.getItemURI(item));
+			
+			await Zotero.Items.merge(item1, [item2]);
+			await Zotero.Items.merge(item3, [item4]);
+			
+			await Zotero.Items.merge(item1, [item3]);
+			
+			// Remaining item should include all other URIs
+			assert.sameMembers(
+				item1.getRelations()[Zotero.Relations.replacedItemPredicate],
+				uris
+			);
+		});
+		
 		it("should update relations pointing to replaced item to point to master", function* () {
 			var item1 = yield createDataObject('item');
 			var item1URI = Zotero.URI.getItemURI(item1);