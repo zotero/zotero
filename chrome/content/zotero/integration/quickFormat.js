6bbb7ddc094fe104eca034db0e7266d2c7f52f6a
Adomas Venčkauskas <adomas.ven@gmail.com>
2020-06-22 10:00:18 +0300

    Ignore leading comma in citation dialog search. Closes #1839

--- chrome/content/zotero/integration/quickFormat.js
+++ chrome/content/zotero/integration/quickFormat.js
@@ -301,6 +301,7 @@ var Zotero_QuickFormat = new function () {
 			
 			var s = new Zotero.Search();
 			str = str.replace(/ (?:&|and) /g, " ", "g");
+			str = str.replace(/^,/, '');
 			if(charRe.test(str)) {
 				Zotero.debug("QuickFormat: QuickSearch: "+str);
 				// Exclude feeds
