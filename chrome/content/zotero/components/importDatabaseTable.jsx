c70917193dafcc020b505870b513fe74c6bdd75c
Tom Najdek <tom@doppnet.com>
2020-07-20 17:01:27 +0200

    Add license blocks

--- chrome/content/zotero/components/importDatabaseTable.jsx
+++ chrome/content/zotero/components/importDatabaseTable.jsx
@@ -1,3 +1,27 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2020 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+                     https://digitalscholar.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
 import React, { forwardRef, memo, useCallback, useImperativeHandle, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
