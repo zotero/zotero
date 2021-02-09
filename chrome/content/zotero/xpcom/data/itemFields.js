31478d1fbac372d46ccfe09e08f7f38a7d52bf4a
Dan Stillman <dstillman@zotero.org>
2021-02-09 17:05:50 -0500

    Use multi-line editor for all title fields (e.g., Case Name)
    https://forums.zotero.org/discussion/87286/suggestion-multi-line-case-name-display-in-info-panel

--- chrome/content/zotero/xpcom/data/itemFields.js
+++ chrome/content/zotero/xpcom/data/itemFields.js
@@ -382,13 +382,13 @@ Zotero.ItemFields = new function() {
 	
 	
 	/**
-	 * A long field expands into a multiline textbox while editing but displays
-	 * as a single line in non-editing mode; newlines are not allowed
+	 * A long field expands into a multiline textbox while editing; newlines are not allowed
 	 */
 	this.isLong = function (field) {
 		field = this.getName(field);
 		var fields = [
 			'title',
+			...this.getTypeFieldsFromBase('title', true),
 			'bookTitle'
 		];
 		return fields.indexOf(field) != -1;
@@ -396,8 +396,7 @@ Zotero.ItemFields = new function() {
 	
 	
 	/**
-	 * A multiline field displays as a multiline text box in editing mode
-	 * and non-editing mode; newlines are allowed
+	 * A multiline field displays as a multiline text box in editing mode; newlines are allowed
 	 */
 	this.isMultiline = function (field) {
 		field = this.getName(field);
