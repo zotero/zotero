zotero/zotero

commit d5334dc483689abb5d0937f729147a8cc09e3227

Fix item tree breakage if annotation is added while search is active

chrome/content/zotero/xpcom/itemTreeView.js
@@ -1892,7 +1892,15 @@ Zotero.ItemTreeView.prototype.selectItems = async function (ids, noRecurse) {
 	var rowsToSelect = [];
 	for (let id of idsToSelect) {
 		let row = this._rowMap[id];
+		if (!row) {
+			Zotero.debug(`Item ${id} not in row map -- skipping`);
+			continue;
+		}
 		rowsToSelect.push(row);
+	}
+	
+	if (!rowsToSelect.length) {
+		return 0;
 	}
 	
 	// If items are already selected, just scroll to the top-most one

commit ae98488d2bcbd133acadcdde9aa0ba1c868cf8d0

Fix selectItem() on first items row (regression from d5334dc483)

chrome/content/zotero/xpcom/itemTreeView.js
@@ -1892,7 +1892,7 @@ Zotero.ItemTreeView.prototype.selectItems = async function (ids, noRecurse) {
 	var rowsToSelect = [];
 	for (let id of idsToSelect) {
 		let row = this._rowMap[id];
-		if (!row) {
+		if (row === undefined) {
 			Zotero.debug(`Item ${id} not in row map -- skipping`);
 			continue;
 		}