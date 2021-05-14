zotero/zotero

commit 52932b6eb03f72b5fb5591ba52d8e0f4c2ef825f

Reduce auto-sync timeout to 3 seconds and optimize auto-sync process
Reduce timeout after an object change from 15 seconds to 3 seconds,
ecxept for individual notes. We should probably kick off an auto-sync
3 seconds after a note is blurred, but that's not implemented.

We also now skip file syncing and full-text content syncing altogether
unless an attachment has changed.

Reindexing an item now kicks off auto-sync, whereas before it didn't.

chrome/content/zotero/xpcom/notifier.js
@@ -93,7 +93,7 @@ Zotero.Notifier = new function(){
 	* Possible values:
 	*
 	* 	event: 'add', 'modify', 'delete', 'move' ('c', for changing parent),
-	*		'remove' (ci, it), 'refresh', 'redraw', 'trash', 'unreadCountUpdated'
+	*		'remove' (ci, it), 'refresh', 'redraw', 'trash', 'unreadCountUpdated', 'index'
 	* 	type - 'collection', 'search', 'item', 'collection-item', 'item-tag', 'tag',
 	*		'group', 'relation', 'feed', 'feedItem'
 	* 	ids - single id or array of ids

chrome/content/zotero/xpcom/sync/syncEventListeners.js
@@ -99,7 +99,8 @@ Zotero.Sync.EventListeners.ChangeListener = new function () {
 
 
 Zotero.Sync.EventListeners.AutoSyncListener = {
-	_editTimeout: 15,
+	_editTimeout: 3,
+	_noteEditTimeout: 15,
 	_observerID: null,
 	
 	init: function () {
@@ -114,13 +115,15 @@ Zotero.Sync.EventListeners.AutoSyncListener = {
 	},
 	
 	notify: function (event, type, ids, extraData) {
-		// TODO: skip others
-		if (event == 'refresh' || event == 'redraw') {
-			return;
-		}
-		
-		if (Zotero.Sync.Runner.syncInProgress) {
-			return;
+		switch (event) {
+			case 'add':
+			case 'modify':
+			case 'delete':
+			case 'index':
+				break;
+			
+			default:
+				return;
 		}
 		
 		// Only trigger sync for certain types
@@ -131,6 +134,8 @@ Zotero.Sync.EventListeners.AutoSyncListener = {
 		
 		// Determine affected libraries so only those can be synced
 		let libraries = [];
+		var fileLibraries = new Set();
+		var fullTextLibraries = new Set();
 		
 		if (type == 'setting') {
 			for (let id of ids) {
@@ -163,11 +168,33 @@ Zotero.Sync.EventListeners.AutoSyncListener = {
 			return;
 		}
 		
+		var noteEdit = false;
+		if (type == 'item' && (event == 'add' || event == 'modify' || event == 'index')) {
+			// Use a longer timeout for a single note edit, to avoid repeating syncing during typing
+			if (ids.length == 1 && (Zotero.Items.get(ids[0]) || {}).itemType == 'note') {
+				noteEdit = true;
+			}
+			else {
+				for (let id of ids) {
+					let item = Zotero.Items.get(id);
+					if (!item) continue;
+					if (item.isStoredFileAttachment()) {
+						fileLibraries.add(item.libraryID);
+					}
+					if (item.isFileAttachment()) {
+						fullTextLibraries.add(item.libraryID);
+					}
+				}
+			}
+		}
+		
 		Zotero.Sync.Runner.setSyncTimeout(
-			this._editTimeout,
+			noteEdit ? this._noteEditTimeout : this._editTimeout,
 			false,
 			{
-				libraries: libraries.map(library => library.libraryID)
+				libraries: libraries.map(library => library.libraryID),
+				fileLibraries: [...fileLibraries],
+				fullTextLibraries: [...fullTextLibraries]
 			}
 		);
 	},

chrome/content/zotero/xpcom/fulltext.js
@@ -290,6 +290,7 @@ Zotero.Fulltext = Zotero.FullText = new function(){
 			Zotero.DB.query(sql, [itemID, {string:text}]);
 			*/
 			
+			Zotero.Notifier.queue('index', 'item', itemID);
 			Zotero.Notifier.queue('refresh', 'item', itemID);
 		}.bind(this));
 		

chrome/content/zotero/xpcom/sync/syncRunner.js
@@ -75,6 +75,7 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 	var _delayPromises = [];
 	var _firstInSession = true;
 	var _syncInProgress = false;
+	var _queuedSyncOptions = [];
 	var _stopping = false;
 	var _canceller;
 	var _manualSyncRequired = false; // TODO: make public?
@@ -238,6 +239,19 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 				options.libraries ? Array.from(options.libraries) : []
 			);
 			
+			// If file and full-text libraries are specified, limit to libraries we're already
+			// syncing
+			var fileLibrariesToSync = new Set(
+				options.fileLibraries
+					? options.fileLibraries.filter(id => librariesToSync.includes(id))
+					: librariesToSync
+			);
+			var fullTextLibrariesToSync = new Set(
+				options.fullTextLibraries
+					? options.fullTextLibraries.filter(id => librariesToSync.includes(id))
+					: librariesToSync
+			);
+			
 			_stopCheck();
 			
 			// If items not yet loaded for libraries we need, load them now
@@ -268,8 +282,11 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 				
 				_stopCheck();
 				
-				// Run file sync on all libraries that passed the last data sync
-				librariesToSync = yield _doFileSync(nextLibraries, engineOptions);
+				// Run file sync on all allowed libraries that passed the last data sync
+				librariesToSync = yield _doFileSync(
+					nextLibraries.filter(libraryID => fileLibrariesToSync.has(libraryID)),
+					engineOptions
+				);
 				if (librariesToSync.length) {
 					attempt++;
 					continue;
@@ -277,8 +294,11 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 				
 				_stopCheck();
 				
-				// Run full-text sync on all libraries that haven't failed a data sync
-				librariesToSync = yield _doFullTextSync([...successfulLibraries], engineOptions);
+				// Run full-text sync on all allowed libraries that haven't failed a data sync
+				librariesToSync = yield _doFullTextSync(
+					[...successfulLibraries].filter(libraryID => fullTextLibrariesToSync.has(libraryID)),
+					engineOptions
+				);
 				if (librariesToSync.length) {
 					attempt++;
 					continue;
@@ -312,6 +332,12 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 				delete options.restartSync;
 				Zotero.debug("Restarting sync");
 				yield this._sync(options);
+				return;
+			}
+			// If an auto-sync was queued while a sync was ongoing, start again with its options
+			else if (_queuedSyncOptions.length) {
+				Zotero.debug("Restarting sync");
+				yield this._sync(_queuedSyncOptions.shift());
 				return;
 			}
 			
@@ -942,11 +968,13 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 				
 				if (Zotero.locked) {
 					Zotero.debug('Zotero is locked -- skipping auto-sync', 4);
+					_queuedSyncOptions.push(mergedOpts);
 					return;
 				}
 				
 				if (_syncInProgress) {
 					Zotero.debug('Sync already in progress -- skipping auto-sync', 4);
+					_queuedSyncOptions.push(mergedOpts);
 					return;
 				}
 				
@@ -968,6 +996,7 @@ Zotero.Sync.Runner_Module = function (options = {}) {
 		else {
 			if (_syncInProgress) {
 				Zotero.debug('Sync in progress -- not setting auto-sync timeout', 4);
+				_queuedSyncOptions.push(mergedOpts);
 				return;
 			}
 			

test/tests/syncEventListenersTest.js
@@ -96,5 +96,23 @@ describe("Zotero.Sync.EventListeners", function () {
 			mock.verify();
 			assert.sameMembers(expectation.getCall(0).args[2].libraries, [Zotero.Libraries.userLibraryID]);
 		});
+		
+		it("should auto-sync after attachment reindex", async function () {
+			Zotero.Prefs.set('sync.autoSync', false);
+			var attachment = await importFileAttachment('test.pdf');
+			Zotero.Prefs.set('sync.autoSync', true);
+			
+			var mock = sinon.mock(Zotero.Sync.Runner);
+			var expectation = mock.expects("setSyncTimeout").once();
+			
+			await Zotero.Fulltext.indexItems(attachment.id);
+			
+			await Zotero.Promise.delay(10);
+			mock.verify();
+			assert.sameMembers(
+				expectation.getCall(0).args[2].fullTextLibraries,
+				[Zotero.Libraries.userLibraryID]
+			);
+		});
 	});
 });

commit 7ace5ea29e1ebb2382b51ea741bb29fbbf775de1

Fix unnecessary sync looping after downloading items
An extra sync loop would be performed for every object downloaded, so a
download to an empty database could result in a huge number of
unnecessary loops. This was a regression from 52932b6eb, which started
queuing auto-syncs while a sync was in progress. The fix here is to skip
auto-sync for all objects saved from a sync download.

There are two new mechanisms involved:

- Event-level notifier options that get passed to passed to notify() at
  the top level of extraData rather than being included with every
  object (e.g., because `skipAutoSync` should apply to an entire save
  transaction)
- The ability to pass event-level notifier options when initializing
  a Zotero.Notifier.Queue, such as the one used for sync downloads

chrome/content/zotero/xpcom/data/dataObject.js
@@ -918,12 +918,18 @@ Zotero.DataObject.prototype.save = Zotero.Promise.coroutine(function* (options =
 		}
 		
 		env.notifierData = {};
-		// Pass along any 'notifierData' values
+		// Pass along any 'notifierData' values, which become 'extraData' in notifier events
 		if (env.options.notifierData) {
 			Object.assign(env.notifierData, env.options.notifierData);
 		}
 		if (env.options.skipSelect) {
 			env.notifierData.skipSelect = true;
+		}
+		// Pass along event-level notifier options, which become top-level extraData properties
+		for (let option of Zotero.Notifier.EVENT_LEVEL_OPTIONS) {
+			if (env.options[option]) {
+				env.notifierData[option] = true;
+			}
 		}
 		if (!env.isNew) {
 			env.changed = this._previousData;

chrome/content/zotero/xpcom/notifier.js
@@ -26,6 +26,9 @@
 "use strict";
 
 Zotero.Notifier = new function(){
+	// Options that apply to an entire event, not a specific object
+	this.EVENT_LEVEL_OPTIONS = ['autoSyncDelay', 'skipAutoSync'];
+	
 	var _observers = {};
 	var _types = [
 		'collection', 'search', 'share', 'share-items', 'item', 'file',
@@ -201,6 +204,14 @@ Zotero.Notifier = new function(){
 		
 		// Merge extraData keys
 		if (extraData) {
+			// Set event-level options as top-level properties in extraData
+			for (let option of Zotero.Notifier.EVENT_LEVEL_OPTIONS) {
+				if (extraData[option]) {
+					queue[type][event].data[option] = true;
+					delete extraData[option];
+				}
+			}
+			
 			// If just a single id, extra data can be keyed by id or passed directly
 			if (ids.length == 1) {
 				let id = ids[0];
@@ -300,11 +311,21 @@ Zotero.Notifier = new function(){
 			}
 			
 			var queue = {};
-			for (let q of queues) {
-				q = q._queue;
+			for (let { _queue: q, options } of queues) {
 				for (let type in q) {
 					for (let event in q[type]) {
-						_mergeEvent(queue, event, type, q[type][event].ids, q[type][event].data);
+						let extraData = Object.assign({}, q[type][event].data);
+						// Add options from queue as top-level options in extraData
+						if (options) {
+							for (let option in options) {
+								if (!this.EVENT_LEVEL_OPTIONS.includes(option)) {
+									throw new Error(`Queue option '${option} must be an event-level option`);
+								}
+								extraData[option] = options[option];
+							}
+						}
+						
+						_mergeEvent(queue, event, type, q[type][event].ids, extraData);
 					}
 				}
 			}
@@ -312,6 +333,7 @@ Zotero.Notifier = new function(){
 		else if (!_transactionID) {
 			throw new Error("Can't commit outside of transaction");
 		}
+		// Use default queue
 		else {
 			var queue = _queue;
 		}
@@ -419,9 +441,10 @@ Zotero.Notifier = new function(){
 }
 
 
-Zotero.Notifier.Queue = function () {
+Zotero.Notifier.Queue = function (options = {}) {
 	this.id = Zotero.Utilities.randomString();
 	Zotero.debug("Creating notifier queue " + this.id);
 	this._queue = {};
 	this.size = 0;
+	this.options = options;
 };

chrome/content/zotero/xpcom/sync/syncEventListeners.js
@@ -131,6 +131,19 @@ Zotero.Sync.EventListeners.AutoSyncListener = {
 			return;
 		}
 		
+		var autoSyncDelay = 0;
+		if (extraData) {
+			// Some events (e.g., writes from the server) skip auto-syncing altogether
+			if (extraData.skipAutoSync) {
+				return;
+			}
+			
+			// Use a different timeout if specified (e.g., for note editing)
+			if (extraData.autoSyncDelay) {
+				autoSyncDelay = extraData.autoSyncDelay;
+			}
+		}
+		
 		// Determine affected libraries so only those can be synced
 		let libraries = [];
 		var fileLibraries = new Set();
@@ -175,13 +188,7 @@ Zotero.Sync.EventListeners.AutoSyncListener = {
 			return;
 		}
 		
-		var autoSyncDelay = 0;
 		if (type == 'item') {
-			// Use a different timeout if specified (e.g., for note editing)
-			if (extraData[ids[0]] && extraData[ids[0]].autoSyncDelay) {
-				autoSyncDelay = Math.max(autoSyncDelay, extraData[ids[0]].autoSyncDelay);
-			}
-			
 			// Check whether file syncing or full-text syncing are necessary
 			if (event == 'add' || event == 'modify' || event == 'index') {
 				for (let id of ids) {

chrome/content/zotero/xpcom/sync/syncLocal.js
@@ -807,7 +807,9 @@ Zotero.Sync.Data.Local = {
 						batchSize = options.getNotifierBatchSize()
 					}
 				}
-				let notifierQueue = new Zotero.Notifier.Queue;
+				let notifierQueue = new Zotero.Notifier.Queue({
+					skipAutoSync: true
+				});
 				
 				let jsonObject = json[i];
 				let jsonData = jsonObject.data;

test/tests/notifierTest.js
@@ -78,4 +78,38 @@ describe("Zotero.Notifier", function () {
 			assert.isTrue(promise2.isRejected());
 		});
 	});
+	
+	describe("Queue", function () {
+		describe("#commit()", function () {
+			it("should add options from queue to extraData", async function () {
+				var called = false;
+				var data;
+				
+				var notifierID = Zotero.Notifier.registerObserver({
+					notify: (event, type, ids, extraData) => {
+						called = true;
+						data = extraData;
+					}
+				});
+				
+				var notifierQueue = new Zotero.Notifier.Queue({
+					skipAutoSync: true
+				});
+				
+				var item = createUnsavedDataObject('item');
+				await item.saveTx({
+					notifierQueue
+				});
+				
+				assert.isFalse(called);
+				
+				await Zotero.Notifier.commit(notifierQueue);
+				
+				assert.isTrue(called);
+				assert.propertyVal(data, 'skipAutoSync', true);
+				
+				Zotero.Notifier.unregisterObserver(notifierID);
+			});
+		});
+	});
 });

test/tests/syncLocalTest.js
@@ -515,6 +515,35 @@ describe("Zotero.Sync.Data.Local", function() {
 			});
 		})
 		
+		it("shouldn't trigger an auto-sync", async function () {
+			var libraryID = Zotero.Libraries.userLibraryID;
+			
+			var item = createUnsavedDataObject('item');
+			let data = item.toJSON();
+			data.key = Zotero.DataObjectUtilities.generateKey();
+			data.version = 10;
+			let json = {
+				key: data.key,
+				version: 10,
+				data
+			};
+			
+			// Make sure the pref in question is still disabled by default during tests
+			assert.isFalse(Zotero.Prefs.get('sync.autoSync'));
+			Zotero.Prefs.set('sync.autoSync', true);
+			var stub = sinon.stub(Zotero.Sync.Runner, 'setSyncTimeout');
+			
+			await Zotero.Sync.Data.Local.processObjectsFromJSON(
+				'item', libraryID, [json], { stopOnError: true }
+			);
+			
+			// setSyncTimeout() shouldn't have been called at all
+			assert.isFalse(stub.called);
+			
+			stub.restore();
+			Zotero.Prefs.set('sync.autoSync', false);
+		});
+		
 		it("should update local version number and mark as synced if remote version is identical", function* () {
 			var libraryID = Zotero.Libraries.userLibraryID;
 			