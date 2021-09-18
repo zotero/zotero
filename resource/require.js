4cfa230508e05a241cfeda05f68d137b8b8d3031
Dan Stillman <dstillman@zotero.org>
2021-09-18 06:54:09 -0400

    Add module import shim for 'zotero/filePicker' for plugins
    Changed to 'zotero/modules/filePicker' in af597d943b, but a lot of
    plugins haven't yet been updated

--- resource/require.js
+++ resource/require.js
@@ -100,6 +100,9 @@ var require = (function() {
 			'containers/': 'chrome://zotero/content/containers/',
 			'components/': 'chrome://zotero/content/components/',
 			'zotero/': 'chrome://zotero/content/',
+			// TEMP until plugins updated
+			// TODO: Possible to show a deprecation warning?
+			'zotero/filePicker': 'chrome://zotero/content/modules/filePicker',
 		},
 		globals
 	});
