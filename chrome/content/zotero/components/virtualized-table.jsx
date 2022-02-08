zotero/zotero

commit 54ad6da1af26849f56633dbac0a0dea8cea1b115

HTML Tree: make tooltip setting code use any parent xul node

chrome/content/zotero/components/virtualized-table.jsx
@@ -997,7 +997,11 @@ class VirtualizedTable extends React.Component {
 	 */
 	_setXulTooltip() {
 		// Make sure container xul element has a tooltip set
-		let xulElem = this._topDiv.closest('.virtualized-table-container');
+		let xulElem = this._topDiv;
+		while (xulElem && xulElem.namespaceURI !== "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul") {
+			xulElem = xulElem.parentElement;
+		}
+		if (!xulElem) return;
 		if (xulElem.getAttribute('tooltip') != 'html-tooltip') {
 			xulElem.setAttribute('tooltip', 'html-tooltip');
 		}
@@ -1008,6 +1012,7 @@ class VirtualizedTable extends React.Component {
 			let tooltipTitleNode = document.tooltipNode.closest('div *[title], iframe *[title], browser *[title]');
 			if (document.tooltipNode && tooltipTitleNode) {
 				this.setAttribute('label', tooltipTitleNode.getAttribute('title'));
+				return;
 			}
 			e.preventDefault();
 		});

commit 0d2cfef160d920d2dbb4036ff2ea0e5e16b7b207

HTML Tree: fix errors being thrown on hover over empty cells
Regression in 54ad6da1

chrome/content/zotero/components/virtualized-table.jsx
@@ -865,7 +865,7 @@ class VirtualizedTable extends React.Component {
 		// otherwise the .cell element if its immediate child is a text node
 		// should be used.
 		if (!textElem) {
-			if (elem.childNodes[0].nodeType != window.Node.TEXT_NODE) return;
+			if (!elem.childNodes.length || elem.childNodes[0].nodeType != window.Node.TEXT_NODE) return;
 			textElem = elem;
 		}
 		// We need to set the [title] attribute on the .label element in the header