zotero/zotero

commit 8e31bf5b6dee3fc94e6efe80ac3b64b2d5baf64b

Possible fix for missing More Columns submenu. Addresses #2443

chrome/content/zotero/itemTree.jsx
@@ -3593,7 +3593,7 @@ var ItemTree = class ItemTree extends LibraryTree {
 			}
 		});
 		
-		const columns = this._getColumns();
+		const columns = this._getColumns().filter(col => !col.ignoreInColumnPicker);
 		for (let i = 0; i < columns.length; i++) {
 			const column = columns[i];
 			if (column.ignoreInColumnPicker === true) continue;

commit c87b40e5cace1638f4a493b9bfdcc4be4e35b07b

Fix secondary sort submenu breakage in 8e31bf5b

chrome/content/zotero/itemTree.jsx
@@ -3593,7 +3593,8 @@ var ItemTree = class ItemTree extends LibraryTree {
 			}
 		});
 		
-		const columns = this._getColumns().filter(col => !col.ignoreInColumnPicker);
+		// Filter out ignored columns
+		let columns = this._getColumns().filter(col => !col.ignoreInColumnPicker);
 		for (let i = 0; i < columns.length; i++) {
 			const column = columns[i];
 			if (column.ignoreInColumnPicker === true) continue;
@@ -3649,7 +3650,9 @@ var ItemTree = class ItemTree extends LibraryTree {
 			Zotero.logError(e);
 			Zotero.debug(e, 1);
 		}
-		
+
+		// Secondary sort submenu can include columns that cannot have their visibility toggled
+		columns = this._getColumns();
 		//
 		// Secondary Sort menu
 		//