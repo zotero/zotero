62f07a7356165c91a09afa7c7f4f0f6c63b06e77
Martynas Bagdonas <git.martynas@gmail.com>
2022-03-24 10:21:04 +0200

    Upgrade note-editor schema to v8: - Allow highlight and citation nodes in headers
    Fixes #2467
    Related #2390

--- chrome/content/zotero/xpcom/editorInstance.js
+++ chrome/content/zotero/xpcom/editorInstance.js
@@ -44,7 +44,7 @@ const DOWNLOADED_IMAGE_TYPE = [
 ];
 
 // Schema version here has to be the same as in note-editor!
-const SCHEMA_VERSION = 7;
+const SCHEMA_VERSION = 8;
 
 class EditorInstance {
 	constructor() {
