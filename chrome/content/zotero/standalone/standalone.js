837c6f2e515779d0803397f4c19ccd4d5c23540c
Dan Stillman <dstillman@zotero.org>
2022-03-30 13:05:44 -0400

    Remove "Store Annotations in File" option
    This is a confusing, bad option, which was already disabled in groups,
    and this removes it from personal libraries as well. People seem to be
    using it mainly because they think annotations are locked into Zotero if
    they don't, which is causing them to ask for this to be done
    automatically on every edit, which we don't do because it would result
    in constant file syncing and file conflicts [1].
    
    We provide multiple export options for exporting PDFs with annotations,
    and people can use those when sharing a file with people who don't use
    Zotero. Otherwise, if storing annotations in the file is a priority,
    they can use an external reader that's not designed to be tightly
    integrated into Zotero.
    
    (As a rare hack, it's of course also possible to export the PDF with
    annotations, replace the original file, and import the annotations.)
    
    [1] https://www.zotero.org/support/kb/annotations_in_database

--- chrome/content/zotero/standalone/standalone.js
+++ chrome/content/zotero/standalone/standalone.js
@@ -166,13 +166,10 @@ const ZoteroStandalone = new function() {
 					&& !(item.deleted || item.parentItem && item.parentItem.deleted)) {
 				let annotations = item.getAnnotations();
 				let canTransferFromPDF = annotations.find(x => x.annotationIsExternal);
-				let canTransferToPDF = annotations.find(x => !x.annotationIsExternal);
 				this.updateMenuItemEnabled('menu_transferFromPDF', canTransferFromPDF);
-				this.updateMenuItemEnabled('menu_transferToPDF', canTransferToPDF);
 			}
 			else {
 				this.updateMenuItemEnabled('menu_transferFromPDF', false);
-				this.updateMenuItemEnabled('menu_transferToPDF', false);
 			}
 		}
 		
