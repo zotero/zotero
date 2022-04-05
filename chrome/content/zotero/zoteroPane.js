zotero/zotero

commit d7b1a5324804283a582a1e02d6f640dd877fec3a

HTML Tree: Improve item tree column header alignment.
Addresses #2153

chrome/content/zotero/zoteroPane.js
@@ -106,6 +106,7 @@ var ZoteroPane = new function()
 			this.updateWindow();
 			this.updateToolbarPosition();
 			this.updateTagsBoxSize();
+			this.updateItemTreeColumnWidths();
 		});
 		window.setTimeout(this.updateToolbarPosition.bind(this), 0);
 		
@@ -5457,7 +5458,12 @@ var ZoteroPane = new function()
 			list.style.height = height + 'px';
 		}
 	};
-	
+
+	this.updateItemTreeColumnWidths = function() {
+		if (!ZoteroPane.itemsView) return;
+		ZoteroPane.itemsView.tree.updateColumnWidths();
+	};
+
 	/**
 	 * Opens the about dialog
 	 */

chrome/content/zotero/components/virtualized-table.jsx
@@ -760,6 +760,21 @@ class VirtualizedTable extends React.Component {
 	}
 	
 	// ------------------------ Column Methods ------------------------- //
+
+	/**
+	 * A public function to update the column CSS flex widths. To be used
+	 * upon window resize and similar. Especially important on macOS where
+	 * column borders are used and get out of sync with column headers.
+	 */
+	updateColumnWidths = Zotero.Utilities.debounce(() => {
+		let resizeData = {};
+		const columns = this._getVisibleColumns();
+		for (const column of columns) {
+			const elem = document.querySelector(`#${this.props.id} .virtualized-table-header .cell.${column.dataKey}`)
+			resizeData[column.dataKey] = elem.getBoundingClientRect().width;
+		}
+		this._columns.onResize(resizeData, true);
+	}, 200)
 	
 	_handleResizerDragStart = (index, event) => {
 		if (event.button !== 0) return false;

commit 5b9ac2ca53f23c67e1f123c2feaf8dc460205ea6

Fix a non-terminal startup error introduced in d7b1a53

chrome/content/zotero/zoteroPane.js
@@ -5460,7 +5460,7 @@ var ZoteroPane = new function()
 	};
 
 	this.updateItemTreeColumnWidths = function() {
-		if (!ZoteroPane.itemsView) return;
+		if (!ZoteroPane.itemsView || !ZoteroPane.itemsView.tree) return;
 		ZoteroPane.itemsView.tree.updateColumnWidths();
 	};
 