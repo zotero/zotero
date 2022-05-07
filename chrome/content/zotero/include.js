4f5b7bbccbcf34a0ebfb19a78fe8a171cd5f7e40
Dan Stillman <dstillman@zotero.org>
2022-05-07 01:21:33 -0400

    Remove old commented-out lines in include.js

--- chrome/content/zotero/include.js
+++ chrome/content/zotero/include.js
@@ -2,8 +2,6 @@
 /* eslint-disable no-unused-vars */
 
 var Zotero = Components.classes['@zotero.org/Zotero;1']
-	// Currently uses only nsISupports
-	//.getService(Components.interfaces.chnmIZoteroService).
 	.getService(Components.interfaces.nsISupports)
 	.wrappedJSObject;
 
