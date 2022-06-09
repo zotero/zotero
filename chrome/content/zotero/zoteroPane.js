57ab92b5b9cddf1d45d6ae95bb8ecefd3df8be26
Abe Jellinek <jellinek@berkeley.edu>
2022-06-09 18:32:14 -0500

    Fix error if New Collection prompt is canceled

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -3507,6 +3507,9 @@ var ZoteroPane = new function()
 				throw new Error('collection must be null if createNew is true');
 			}
 			let id = await this.newCollection();
+			if (!id) {
+				return;
+			}
 			collection = Zotero.Collections.get(id);
 		}
 
