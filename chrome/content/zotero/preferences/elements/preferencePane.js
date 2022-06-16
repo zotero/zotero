86f157b6486cdb839e4469bd5f5639ef053be451
Abe Jellinek <jellinek@berkeley.edu>
2022-06-16 13:54:24 -0500

    Remove unused CEs
    Left in by mistake.

--- chrome/content/zotero/preferences/elements/preferencePane.js
+++
@@ -1,9 +0,0 @@
-{
-	class PreferencePane extends XULElement {
-		connectedCallback() {
-			this.attachShadow({ mode: 'open' });
-		}
-	}
-
-	customElements.define('preference-pane', PreferencePane);
-}
