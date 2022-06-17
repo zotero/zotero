14e62ee34f2516181379e033f3b1b2aa07832a93
Martynas Bagdonas <git.martynas@gmail.com>
2022-06-17 16:47:53 +0300

    Update pdf-reader and note-editor, use specific prebuilds, tweak code

--- scripts/pdf-worker.js
+++ scripts/pdf-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs-extra');
+const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const { getSignatures, writeSignatures, onSuccess, onError } = require('./utils');
@@ -9,27 +10,32 @@ const { buildsURL } = require('./config');
 async function getPDFWorker(signatures) {
 	const t1 = Date.now();
 
-	const { stdout } = await exec('git rev-parse HEAD', { cwd: 'pdf-worker' });
+	const modulePath = path.join(__dirname, '..', 'pdf-worker');
+
+	const { stdout } = await exec('git rev-parse HEAD', { cwd: modulePath });
 	const hash = stdout.trim();
 
 	if (!('pdf-worker' in signatures) || signatures['pdf-worker'].hash !== hash) {
-		const targetDir = 'build/chrome/content/zotero/xpcom/pdfWorker/';
+		const targetDir = path.join(__dirname, '..', 'build', 'chrome', 'content', 'zotero', 'xpcom', 'pdfWorker');
 		try {
 			const filename = hash + '.zip';
-			const tmpDir = 'tmp/builds/pdf-worker';
+			const tmpDir = path.join(__dirname, '..', 'tmp', 'builds', 'pdf-worker');
 			const url = buildsURL + 'client-pdf-worker/' + filename;
+
+			await fs.remove(targetDir);
+			await fs.ensureDir(targetDir);
+			await fs.ensureDir(tmpDir);
+
 			await exec(
-				`mkdir -p ${tmpDir}`
-				+ `&& cd ${tmpDir}`
-				+ `&& (test -f ${filename} || curl -f ${url} -o ${filename})`
-				+ `&& mkdir -p ../../../${targetDir}`
-				+ `&& unzip -o ${filename} -d ../../../${targetDir}`
+				`cd ${tmpDir}`
+				+ ` && (test -f ${filename} || curl -f ${url} -o ${filename})`
+				+ ` && unzip -o ${filename} -d ${targetDir}`
 			);
 		}
 		catch (e) {
-			await exec('npm ci', { cwd: 'pdf-worker' });
-			await exec('npm run build', { cwd: 'pdf-worker' });
-			await fs.copy('pdf-worker/build/worker.js', targetDir + 'worker.js');
+			await exec('npm ci', { cwd: modulePath });
+			await exec('npm run build', { cwd: modulePath });
+			await fs.copy(path.join(modulePath, 'build', 'worker.js'), path.join(targetDir, 'worker.js'));
 		}
 		signatures['pdf-worker'] = { hash };
 	}
