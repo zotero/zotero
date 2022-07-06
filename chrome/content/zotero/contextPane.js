f650f9508161c7c4680cd558df9e628c2d3b589c
Martynas Bagdonas <git.martynas@gmail.com>
2022-07-06 11:30:18 +0300

    Fix disabled "Add to Note" when a child note is open Fixes #2687

--- chrome/content/zotero/contextPane.js
+++ chrome/content/zotero/contextPane.js
@@ -279,9 +279,14 @@ var ZoteroContextPane = new function () {
 
 		if (splitter.getAttribute('state') != 'collapsed') {
 			if (_panesDeck.selectedIndex == 1) {
-				var node = _notesPaneDeck.selectedPanel;
-				if (node.selectedIndex == 1) {
-					return node.querySelector('zoteronoteeditor');
+				var libraryContext = _notesPaneDeck.selectedPanel;
+				// Global note
+				if (libraryContext.selectedIndex == 1) {
+					return libraryContext.querySelector('zoteronoteeditor');
+				}
+				// Tab specific child note
+				else if (libraryContext.selectedIndex == 2) {
+					return libraryContext.querySelector('.zotero-context-pane-tab-notes-deck').selectedPanel.querySelector('zoteronoteeditor');
 				}
 			}
 		}
