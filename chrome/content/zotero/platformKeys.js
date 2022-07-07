ab6b683d901bece556efd84a3dbce20a25a8625e
Abe Jellinek <jellinek@berkeley.edu>
2022-07-07 00:04:13 -0400

    fx-compat: More progress on platform menus (#2675)
    Not able to test on non-Mac yet, but this behavior should be right.
    Removes all the #ifdefs and moves the behavior into platformKeys.js.

--- chrome/content/zotero/platformKeys.js
+++ chrome/content/zotero/platformKeys.js
@@ -1,14 +1,46 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// Don't need to depend on Zotero object here
+	let isWin = AppConstants.platform == 'win';
+	let isMac = AppConstants.platform == 'macosx';
+
 	let redoKey = document.getElementById('key_redo');
-	if (redoKey) {
-		// Don't need to depend on Zotero object here
-		if (AppConstants.platform == 'win') {
+
+	let fileQuitSeparator = document.getElementById('menu_NonMacFileQuitSeparator');
+	let fileQuitItem = document.getElementById('menu_NonMacFileQuitItem');
+
+	let editPreferencesSeparator = document.getElementById('menu_EditPreferencesSeparator');
+	let editPreferencesItem = document.getElementById('menu_EditPreferencesItem');
+
+	let applicationMenu = document.getElementById('mac_application_menu');
+	let windowMenu = document.getElementById('windowMenu');
+	let macKeyset = document.getElementById('macKeyset');
+
+	if (isWin) {
+		// Set behavior on Windows only
+		if (redoKey) {
 			redoKey.setAttribute('data-l10n-id', 'text-action-redo-shortcut');
 			redoKey.setAttribute('modifiers', 'accel');
 		}
-		else {
+	}
+	else {
+		// Set behavior on all non-Windows platforms
+		if (redoKey) {
 			redoKey.setAttribute('data-l10n-id', 'text-action-undo-shortcut');
 			redoKey.setAttribute('modifiers', 'accel,shift');
 		}
+	}
+
+	if (isMac) {
+		// Set behavior on macOS only
+		if (fileQuitSeparator) fileQuitSeparator.hidden = true;
+		if (fileQuitItem) fileQuitItem.hidden = true;
+		if (editPreferencesSeparator) editPreferencesSeparator.hidden = true;
+		if (editPreferencesItem) editPreferencesItem.hidden = true;
+	}
+	else {
+		// Set behavior on all non-macOS platforms
+		if (applicationMenu) applicationMenu.hidden = true;
+		if (windowMenu) windowMenu.hidden = true;
+		if (macKeyset) macKeyset.disabled = true;
 	}
 });
