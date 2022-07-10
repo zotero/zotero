zotero/zotero

commit a8ed30ce803a9c5b3b5c04d50f6bd6b94faeee86

Validate fields in ItemTree::getSortFields()
To avoid startup hang if a plugin does something bad:

https://forums.zotero.org/discussion/comment/411843/#Comment_411843

Fixes #2692

chrome/content/zotero/itemTree.jsx
@@ -1814,20 +1814,37 @@ var ItemTree = class ItemTree extends LibraryTree {
 		if (secondaryField) {
 			fields.push(secondaryField);
 		}
+		var fallbackFields;
 		try {
-			var fallbackFields = Zotero.Prefs.get('fallbackSort')
+			fallbackFields = Zotero.Prefs.get('fallbackSort')
 				.split(',')
-				.map((x) => x.trim())
-				.filter((x) => x !== '');
+				.map(x => x.trim())
+				.filter(x => x !== '');
+			for (let field of fallbackFields) {
+				if (!this._isValidSortField(field)) {
+					// eslint-disable-next-line no-throw-literal
+					throw `'${field}' is not a valid field in fallbackSort -- resetting`;
+				}
+			}
 		}
 		catch (e) {
-			Zotero.debug(e, 1);
 			Zotero.logError(e);
-			// This should match the default value for the fallbackSort pref
-			var fallbackFields = ['firstCreator', 'date', 'title', 'dateAdded'];
+			Zotero.Prefs.clear('fallbackSort');
+			fallbackFields = Zotero.Prefs.get('fallbackSort').split(',');
 		}
 		fields = Zotero.Utilities.arrayUnique(fields.concat(fallbackFields));
-
+		var validFields = fields.filter(x => this._isValidSortField(x));
+		if (validFields.length) {
+			fields = validFields;
+		}
+		// If no valid fields, use default fallback
+		else {
+			Zotero.logError(`No valid fields in getSortFields() (${fields.join(',')}) `
+				+ '-- resetting');
+			Zotero.Prefs.clear('fallbackSort');
+			fields = Zotero.Prefs.get('fallbackSort').split(',');
+		}
+		
 		// If date appears after year, remove it, unless it's the explicit secondary sort
 		var yearPos = fields.indexOf('year');
 		if (yearPos != -1) {
@@ -1839,7 +1856,13 @@ var ItemTree = class ItemTree extends LibraryTree {
 
 		return fields;
 	}
-
+	
+	_isValidSortField(field) {
+		return field == 'year'
+			|| !!Zotero.ItemFields.getID(field)
+			|| Zotero.Items.primaryFields.includes(field);
+	}
+	
 	/**
 	 * @param index {Integer}
 	 * @param selectAll {Boolean} Whether the selection is part of a select-all event
@@ -3710,6 +3733,13 @@ var ItemTree = class ItemTree extends LibraryTree {
 		var primaryField = this.getSortField();
 		var secondaryField = Zotero.Prefs.get('secondarySort.' + primaryField);
 		if (!secondaryField || secondaryField == primaryField) {
+			return false;
+		}
+		// Make sure specified field is valid
+		if (!this._isValidSortField(secondaryField)) {
+			Zotero.logError(`'${secondaryField}' is not a valid field in `
+				+ `secondarySort.${primaryField} -- resetting`);
+			Zotero.Prefs.clear('secondarySort.' + primaryField);
 			return false;
 		}
 		return secondaryField;

test/tests/itemTreeTest.js
@@ -151,6 +151,40 @@ describe("Zotero.ItemTree", function() {
 		})
 	})
 	
+	describe("#getSortFields()", function () {
+		before(async function () {
+			itemsView = zp.itemsView;
+			
+			// Sort by title
+			const colIndex = itemsView.tree._getColumns().findIndex(column => column.dataKey == 'title');
+			await itemsView.tree._columns.toggleSort(colIndex);
+		});
+		
+		after(function () {
+			Zotero.Prefs.clear('fallbackSort');
+		});
+		
+		it("should ignore invalid secondary-sort field", function () {
+			Zotero.Prefs.set('secondarySort.title', 'invalidField');
+			var fields = itemsView.getSortFields();
+			assert.isUndefined(Zotero.Prefs.get('secondarySort.title'));
+			// fallbackSort pref with title moved to beginning
+			assert.sameMembers(['title', 'firstCreator', 'date', 'dateAdded'], fields);
+			
+			Zotero.Prefs.clear('secondarySort.title');
+		});
+		
+		it("should ignore invalid fallback-sort field", function () {
+			Zotero.Prefs.clear('fallbackSort');
+			var originalFallback = Zotero.Prefs.get('fallbackSort');
+			Zotero.Prefs.set('fallbackSort', 'invalidField,' + originalFallback);
+			var fields = itemsView.getSortFields();
+			assert.equal(Zotero.Prefs.get('fallbackSort'), originalFallback);
+			// fallbackSort pref with title moved to beginning
+			assert.sameMembers(['title', 'firstCreator', 'date', 'dateAdded'], fields);
+		});
+	});
+	
 	describe("#notify()", function () {
 		beforeEach(function () {
 			sinon.spy(win.ZoteroPane, "itemSelected");

commit 56e7f0a10fdd30817d2803eaa96108512b7082f5

Fix sorting by Item Type and feed item itemID
Regression from a8ed30ce803

chrome/content/zotero/itemTree.jsx
@@ -1826,6 +1826,10 @@ var ItemTree = class ItemTree extends LibraryTree {
 
 	getSortFields() {
 		var fields = [this.getSortField()];
+		if (!this._isValidSortField(fields[0])) {
+			Zotero.logError(`'${fields[0]}' is not a valid sort field -- skipping`);
+			fields.shift();
+		}
 		var secondaryField = this._getSecondarySortField();
 		if (secondaryField) {
 			fields.push(secondaryField);
@@ -1849,12 +1853,8 @@ var ItemTree = class ItemTree extends LibraryTree {
 			fallbackFields = Zotero.Prefs.get('fallbackSort').split(',');
 		}
 		fields = Zotero.Utilities.arrayUnique(fields.concat(fallbackFields));
-		var validFields = fields.filter(x => this._isValidSortField(x));
-		if (validFields.length) {
-			fields = validFields;
-		}
 		// If no valid fields, use default fallback
-		else {
+		if (!fields.length) {
 			Zotero.logError(`No valid fields in getSortFields() (${fields.join(',')}) `
 				+ '-- resetting');
 			Zotero.Prefs.clear('fallbackSort');
@@ -1874,7 +1874,9 @@ var ItemTree = class ItemTree extends LibraryTree {
 	}
 	
 	_isValidSortField(field) {
-		return field == 'year'
+		return field == 'itemType'
+			|| field == 'year'
+			|| field == 'id' // feeds
 			|| !!Zotero.ItemFields.getID(field)
 			|| Zotero.Items.primaryFields.includes(field);
 	}

test/tests/itemTreeTest.js
@@ -152,7 +152,7 @@ describe("Zotero.ItemTree", function() {
 	})
 	
 	describe("#getSortFields()", function () {
-		before(async function () {
+		beforeEach(async function () {
 			itemsView = zp.itemsView;
 			
 			// Sort by title
@@ -182,6 +182,15 @@ describe("Zotero.ItemTree", function() {
 			assert.equal(Zotero.Prefs.get('fallbackSort'), originalFallback);
 			// fallbackSort pref with title moved to beginning
 			assert.sameMembers(['title', 'firstCreator', 'date', 'dateAdded'], fields);
+		});
+		
+		it("should sort by item type", async function () {
+			// Sort by item type
+			const colIndex = itemsView.tree._getColumns().findIndex(column => column.dataKey == 'itemType');
+			await itemsView.tree._columns.toggleSort(colIndex);
+			
+			var fields = itemsView.getSortFields();
+			assert.sameMembers(['itemType', 'firstCreator', 'date', 'title', 'dateAdded'], fields);
 		});
 	});
 	