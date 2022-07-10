zotero/zotero

commit 6af3b0276a5b82cdc7d40adf66b5926e3ec1f00d

Fix cookieSandbox not overriding user agent if no cookies for domain.
Potential fix for #2635

chrome/content/zotero/xpcom/cookieSandbox.js
@@ -383,18 +383,18 @@ Zotero.CookieSandbox.Observer = new function() {
 			if(trackedBy) {
 				var cookiesForURI = trackedBy.getCookiesForURI(channel.URI);
 			}
-			
+
+			if(trackedBy.userAgent) {
+				channel.setRequestHeader("User-Agent", trackedBy.userAgent, false);
+			}
+
 			if(!trackedBy || !cookiesForURI) {
 				channel.setRequestHeader("Cookie", "", false);
 				channel.setRequestHeader("Cookie2", "", false);
 				Zotero.debug("CookieSandbox: Cleared cookies to be sent to "+channelURI, 5);
 				return;
 			}
-			
-			if(trackedBy.userAgent) {
-				channel.setRequestHeader("User-Agent", trackedBy.userAgent, false);
-			}
-			
+		
 			// add cookies to be sent to this domain
 			channel.setRequestHeader("Cookie", Zotero.CookieSandbox.generateCookieString(cookiesForURI), false);
 			Zotero.debug("CookieSandbox: Added cookies for request to "+channelURI, 5);

commit 124c18d54d55873963be2632b483c9dbecfe857d

CookieSandbox: Fix error setting request cookies
Regression from 6af3b0276a, showing up as "trackedBy is undefined" error

https://forums.zotero.org/discussion/98322/no-snapshot-or-pdf-autosaved-using-zotero-connector-on-safari

chrome/content/zotero/xpcom/cookieSandbox.js
@@ -384,10 +384,10 @@ Zotero.CookieSandbox.Observer = new function() {
 				var cookiesForURI = trackedBy.getCookiesForURI(channel.URI);
 			}
 
-			if(trackedBy.userAgent) {
+			if (trackedBy && trackedBy.userAgent) {
 				channel.setRequestHeader("User-Agent", trackedBy.userAgent, false);
 			}
-
+			
 			if(!trackedBy || !cookiesForURI) {
 				channel.setRequestHeader("Cookie", "", false);
 				channel.setRequestHeader("Cookie2", "", false);