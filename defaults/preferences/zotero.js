f0e0971c69ca9c1d447d3ae69c6f46ee8b3dfeea
Martynas Bagdonas <git.martynas@gmail.com>
2022-07-11 15:23:25 +0300

    Include quotation marks for highlights by default, but allow to disable Fixes #2697

--- defaults/preferences/zotero.js
+++ defaults/preferences/zotero.js
@@ -205,7 +205,7 @@ pref("extensions.zotero.retractions.recentItems", "[]");
 
 // Annotations
 pref("extensions.zotero.annotations.noteTemplates.title", "<h1>{{title}}<br/>({{date}})</h1>");
-pref("extensions.zotero.annotations.noteTemplates.highlight", "<p>{{highlight quotes='true'}} {{citation}} {{comment}}</p>");
+pref("extensions.zotero.annotations.noteTemplates.highlight", "<p>{{highlight}} {{citation}} {{comment}}</p>");
 pref("extensions.zotero.annotations.noteTemplates.note", "<p>{{citation}} {{comment}}</p>");
 
 // Scaffold
