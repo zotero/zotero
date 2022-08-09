12ca11c11bda36ae3f9d195ccb37af77b39e65f5
Dan Stillman <dstillman@zotero.org>
2022-08-09 04:10:25 -0400

    fx-compat: Update error reporting wizard

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -5165,7 +5165,7 @@ var ZoteroPane = new function()
 			askForSteps: true
 		};
 		var io = { wrappedJSObject: { Zotero: Zotero, data:  data } };
-		var win = ww.openWindow(null, "chrome://zotero/content/error/errorReport.xhtml",
+		var win = ww.openWindow(null, "chrome://zotero/content/errorReport.xhtml",
 					"zotero-error-report", "chrome,centerscreen,modal", io);
 	}
 	
