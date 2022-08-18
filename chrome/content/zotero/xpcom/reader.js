zotero/zotero

commit 7ec54fdaa93b94941fac07f9fe297939ab216b45

Select existing unloaded tab instead of opening a new one Fixes #2740

chrome/content/zotero/xpcom/reader.js
@@ -1365,6 +1365,17 @@ class Reader {
 		this.triggerAnnotationsImportCheck(itemID);
 		let reader;
 
+		if (!allowDuplicate) {
+			let win = Zotero.getMainWindow();
+			if (win) {
+				let existingTabID = win.Zotero_Tabs.getTabIDByItemID(itemID);
+				if (existingTabID) {
+					win.Zotero_Tabs.select(existingTabID);
+					return;
+				}
+			}
+		}
+
 		if (openInWindow) {
 			reader = this._readers.find(r => r._itemID === itemID && (r instanceof ReaderWindow));
 		}

chrome/content/zotero/tabs.js
@@ -84,6 +84,11 @@ var Zotero_Tabs = new function () {
 		window.Zotero_Tooltip.stop();
 	};
 
+	this.getTabIDByItemID = function (itemID) {
+		let tab = this._tabs.find(tab => tab.data && tab.data.itemID === itemID);
+		return tab && tab.id;
+	};
+
 	this.init = function () {
 		ReactDOM.render(
 			<TabBar

commit fa32eb6790dd25afdef9aca7a32e06646ed0c5b7

Fix regression for broken "Snow in Page" Fixes #2764 Regression from 7ec54fd

chrome/content/zotero/xpcom/reader.js
@@ -1369,7 +1369,9 @@ class Reader {
 		this.triggerAnnotationsImportCheck(itemID);
 		let reader;
 
-		if (!allowDuplicate) {
+		// If duplicating is not allowed, and no reader instance is loaded for itemID,
+		// try to find an unloaded tab and select it. Zotero.Reader.open will then be called again
+		if (!allowDuplicate && !this._readers.find(r => r._itemID === itemID)) {
 			let win = Zotero.getMainWindow();
 			if (win) {
 				let existingTabID = win.Zotero_Tabs.getTabIDByItemID(itemID);