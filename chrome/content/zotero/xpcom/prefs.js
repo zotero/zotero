64897455f42063085b39b042b6fae4300182a246
Martynas Bagdonas <git.martynas@gmail.com>
2022-09-09 18:03:20 +0300

    Adjust PDF reader and context pane UI based on font size pref Fixes #2809

--- chrome/content/zotero/xpcom/prefs.js
+++ chrome/content/zotero/xpcom/prefs.js
@@ -230,6 +230,7 @@ Zotero.Prefs = new function(){
 			Zotero.setFontSize(
 				Zotero.getActiveZoteroPane().document.getElementById('zotero-pane')
 			);
+			Zotero.setFontSize(Zotero.getActiveZoteroPane().document.getElementById('zotero-context-pane'));
 			Zotero.getActiveZoteroPane().collectionsView && Zotero.getActiveZoteroPane().collectionsView.updateFontSize();
 			Zotero.getActiveZoteroPane().itemsView && Zotero.getActiveZoteroPane().itemsView.updateFontSize();
 		}],
