82b53fee8785bbff3b9c16150001a9f65953d6f1
Martynas Bagdonas <git.martynas@gmail.com>
2022-09-13 16:38:17 +0300

    Fix PDF reader sometimes showing UTC time and sometimes local
    (cherry picked from commit c13d17b5e6ca496491e926211c0e1ea7aef072ae)

--- chrome/content/zotero/xpcom/annotations.js
+++ chrome/content/zotero/xpcom/annotations.js
@@ -177,7 +177,7 @@ Zotero.Annotations = new function () {
 			o.tags = tags;
 		}
 		
-		o.dateModified = item.dateModified;
+		o.dateModified = Zotero.Date.sqlToISO8601(item.dateModified);
 		return o;
 	};
 	
