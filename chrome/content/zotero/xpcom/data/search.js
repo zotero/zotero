9f171e0f803ea6dafd8712486951016eeb8f6134
Abe Jellinek <jellinek@berkeley.edu>
2022-09-29 16:53:51 -0400

    Search: Fix noChildren matching annotated child attachments

--- chrome/content/zotero/xpcom/data/search.js
+++ chrome/content/zotero/xpcom/data/search.js
@@ -1135,6 +1135,8 @@ Zotero.Search.prototype._buildQuery = Zotero.Promise.coroutine(function* () {
 		sql += " AND (itemID NOT IN (SELECT itemID FROM itemNotes "
 			+ "WHERE parentItemID IS NOT NULL) AND itemID NOT IN "
 			+ "(SELECT itemID FROM itemAttachments "
+			+ "WHERE parentItemID IS NOT NULL) AND itemID NOT IN "
+			+ "(SELECT itemID FROM itemAnnotations "
 			+ "WHERE parentItemID IS NOT NULL))";
 	}
 	
