59cb33d2319e1c1dc85777b2316f313a4fac90b7
Dan Stillman <dstillman@zotero.org>
2022-10-29 04:39:46 -0400

    Retry only twice on error in Find Available PDF
    Not three times

--- chrome/content/zotero/xpcom/attachments.js
+++ chrome/content/zotero/xpcom/attachments.js
@@ -2032,7 +2032,7 @@ Zotero.Attachments = new function(){
 				addTriedURL(url);
 				// Backoff loop
 				let tries = 3;
-				while (tries-- >= 0) {
+				while (tries-- > 0) {
 					try {
 						await beforeRequest(url);
 						await this.downloadFile(url, path, options);
@@ -2071,7 +2071,7 @@ Zotero.Attachments = new function(){
 						
 						// Backoff loop
 						let tries = 3;
-						while (tries-- >= 0) {
+						while (tries-- > 0) {
 							try {
 								await beforeRequest(nextURL, noDelay);
 								req = await Zotero.HTTP.request(
