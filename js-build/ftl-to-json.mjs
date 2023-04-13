0de130566552f56a637c8065ec52ea93a3e5cbb3
Dan Stillman <dstillman@zotero.org>
2023-04-13 01:22:29 -0400

    Tweaks to Fluent/Transifex JSON processing scripts (#3058)
    - Use locale directories for JSON files, since that's where the
      Transifex client will interact with them
    - Skip non-locale directories (e.g., don't create an .ftl file for
      .DS_Store)
    - Other minor simplification

--- js-build/ftl-to-json.mjs
+++ js-build/ftl-to-json.mjs
@@ -5,16 +5,15 @@ import { fileURLToPath } from 'url';
 import { onError, onSuccess } from './utils.js';
 
 const ROOT = join(dirname(fileURLToPath(import.meta.url)), '..');
-const JSONDir = join(ROOT, 'tmp', 'tx');
 
 async function getJSON() {
 	const t1 = performance.now();
-	await fs.mkdirp(JSONDir);
-	const sourcefile = join(ROOT, 'chrome', 'locale', 'en-US', 'zotero', 'zotero.ftl');
-	const destFile = join(JSONDir, 'zotero_en_US.json');
-	const ftl = await fs.readFile(sourcefile, 'utf8');
+	const sourceDir = join(ROOT, 'chrome', 'locale', 'en-US', 'zotero');
+	const sourceFile = join(sourceDir, 'zotero.ftl');
+	const destFile = join(sourceDir, 'zotero.json');
+	const ftl = await fs.readFile(sourceFile, 'utf8');
 	const json = ftlToJSON(ftl, { transformTerms: false, storeTermsInJSON: false });
-	await fs.outputJSON(destFile, json);
+	await fs.outputJSON(destFile, json, { spaces: '\t' });
 	const t2 = performance.now();
 	return ({
 		action: 'ftl->json',
