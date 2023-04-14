fd744fb526bd77c174c21136f9f753ae6744150a
Adomas Venčkauskas <adomas.ven@gmail.com>
2023-04-14 15:21:52 +0300

    Fix feed update refreshing, scrolling item and collection trees
    Closes #2950

--- chrome/content/zotero/collectionTree.jsx
+++ chrome/content/zotero/collectionTree.jsx
@@ -640,7 +640,10 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 			return;
 		}
 		if (type == 'feed' && (action == 'unreadCountUpdated' || action == 'statusChanged')) {
-			this.tree.invalidate();
+			let feedRow = this.getRowIndexByID("L" + ids[0]);
+			if (feedRow !== false) {
+				this.tree.invalidateRow(feedRow);
+			}
 			return;
 		}
 		
@@ -745,7 +748,7 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 					// Invalidate parent in case it's become non-empty
 					let parentRow = this.getRowIndexByID("C" + collection.parentID);
 					if (parentRow !== false) {
-						this._treebox.invalidateRow(parentRow);
+						this.tree.invalidateRow(parentRow);
 					}
 				}
 				break;
@@ -768,6 +771,9 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 						await this.selectByID(currentTreeRow.id);
 					}
 				}
+				break;
+				
+			case 'feed':
 				break;
 			
 			default:
