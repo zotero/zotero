zotero/zotero

commit fd744fb526bd77c174c21136f9f753ae6744150a

Fix feed update refreshing, scrolling item and collection trees
Closes #2950

chrome/content/zotero/collectionTree.jsx
@@ -640,7 +640,10 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 			return;
 		}
 		if (type == 'feed' && (action == 'unreadCountUpdated' || action == 'statusChanged')) {
-			this.tree.invalidate();
+			let feedRow = this.getRowIndexByID("L" + ids[0]);
+			if (feedRow !== false) {
+				this.tree.invalidateRow(feedRow);
+			}
 			return;
 		}
 		
@@ -745,7 +748,7 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 					// Invalidate parent in case it's become non-empty
 					let parentRow = this.getRowIndexByID("C" + collection.parentID);
 					if (parentRow !== false) {
-						this._treebox.invalidateRow(parentRow);
+						this.tree.invalidateRow(parentRow);
 					}
 				}
 				break;
@@ -768,6 +771,9 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 						await this.selectByID(currentTreeRow.id);
 					}
 				}
+				break;
+				
+			case 'feed':
 				break;
 			
 			default:

chrome/content/zotero/selectItemsDialog.js
@@ -94,6 +94,10 @@ function doUnload()
 
 var onCollectionSelected = async function () {
 	if (!collectionsView.selection.count) return;
+	// Collection not changed
+	if (itemsView && itemsView.collectionTreeRow && itemsView.collectionTreeRow.id == collectionTreeRow.id) {
+		return;
+	}
 	var collectionTreeRow = collectionsView.getRow(collectionsView.selection.focused);
 	collectionTreeRow.setSearch('');
 	Zotero.Prefs.set('lastViewedFolder', collectionTreeRow.id);

commit 348083d2872540554fa18ede8c7b64dcf9dc23a9

Refresh Feeds row when feed unread count is updated
Regression from fd744fb526

chrome/content/zotero/collectionTree.jsx
@@ -640,9 +640,15 @@ var CollectionTree = class CollectionTree extends LibraryTree {
 			return;
 		}
 		if (type == 'feed' && (action == 'unreadCountUpdated' || action == 'statusChanged')) {
+			// Refresh the feed
 			let feedRow = this.getRowIndexByID("L" + ids[0]);
 			if (feedRow !== false) {
 				this.tree.invalidateRow(feedRow);
+			}
+			// Refresh the Feeds row
+			let feedsRow = this.getRowIndexByID("F1");
+			if (feedsRow !== false) {
+				this.tree.invalidateRow(feedsRow);
 			}
 			return;
 		}