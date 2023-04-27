2bc4c38f018e3061ba20154aced66b68d32c817a
Dan Stillman <dstillman@zotero.org>
2023-04-27 00:31:33 -0400

    Remove obsolete app.update prefs

--- app/assets/prefs.js
+++ app/assets/prefs.js
@@ -97,28 +97,7 @@ pref("extensions.update.url", "");
 // Don't try to load the "Get Add-ons" tab on first load of Add-ons window
 pref("extensions.ui.lastCategory", "addons://list/extension");
 
-/** The below is imported from https://developer.mozilla.org/en/XULRunner/Application_Update **/
-// Whether or not app updates are enabled
-pref("app.update.enabled", true);
-
-// This preference turns on app.update.mode and allows automatic download and
-// install to take place. We use a separate boolean toggle for this to make
-// the UI easier to construct.
 pref("app.update.auto", true);
-
-// Defines how the Application Update Service notifies the user about updates:
-//
-// AUM Set to:        Minor Releases:     Major Releases:
-// 0                  download no prompt  download no prompt
-// 1                  download no prompt  download no prompt if no incompatibilities
-// 2                  download no prompt  prompt
-//
-// See chart in nsUpdateService.js.in for more details
-//
-pref("app.update.mode", 2);
-
-// If set to true, the Update Service will present no UI for any event.
-pref("app.update.silent", false);
 
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
@@ -128,20 +107,9 @@ pref("app.update.url.manual", "https://www.zotero.org/download");
 // supplied in the "An update is available" page of the update wizard.
 pref("app.update.url.details", "https://www.zotero.org/support/changelog");
 
-// User-settable override to app.update.url for testing purposes.
-//pref("app.update.url.override", "");
-
 // Interval: Time between checks for a new version (in seconds)
 //           default=1 day
 pref("app.update.interval", 86400);
-
-// Interval: Time before prompting the user to download a new version that
-//           is available (in seconds) default=1 day
-pref("app.update.nagTimer.download", 86400);
-
-// Interval: Time before prompting the user to restart to install the latest
-//           download (in seconds) default=30 minutes
-pref("app.update.nagTimer.restart", 1800);
 
 // The minimum delay in seconds for the timer to fire.
 // default=2 minutes
@@ -152,17 +120,6 @@ pref("app.update.timerMinimumDelay", 120);
 // upgrade start page instead! Other apps may wish to show this UI, and supply
 // a whatsNewURL field in their brand.properties that contains a link to a page
 // which tells users what's new in this new update.
-
-// This needs to be disabled since it makes us error out on update for some
-// unknown reason
-pref("app.update.showInstalledUI", false);
-
-// 0 = suppress prompting for incompatibilities if there are updates available
-//     to newer versions of installed addons that resolve them.
-// 1 = suppress prompting for incompatibilities only if there are VersionInfo
-//     updates available to installed addons that resolve them, not newer
-//     versions.
-pref("app.update.incompatible.mode", 0);
 
 // update channel for this build
 pref("app.update.channel", "default");
