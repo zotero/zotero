f0bb43771fcb7efce2ba17ab885299545ee16667
Dan Stillman <dstillman@zotero.org>
2023-05-09 05:37:48 -0400

    Add whitespace below echoed status line

--- app/build.sh
+++ app/build.sh
@@ -126,12 +126,14 @@ function check_xulrunner_hash {
 
 	if [ ! -e "$CALLDIR/xulrunner/$platform_hash_file" ]; then
 		echo "xulrunner not found -- downloading"
+		echo
 		$CALLDIR/scripts/fetch_xulrunner -p $platform
 	else
 		recalculated_xulrunner_hash=$($CALLDIR/scripts/xulrunner_hash -p $platform)
 		current_xulrunner_hash=$(< "$CALLDIR/xulrunner/$platform_hash_file")
 		if [ "$current_xulrunner_hash" != "$recalculated_xulrunner_hash" ]; then 
 			echo "xulrunner hashes don't match -- redownloading"
+			echo
 			$CALLDIR/scripts/fetch_xulrunner -p $platform
 			current_xulrunner_hash=$(< "$CALLDIR/xulrunner/$platform_hash_file")
 			if [ "$current_xulrunner_hash" != "$recalculated_xulrunner_hash" ]; then
