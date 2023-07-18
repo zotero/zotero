ba919becac2465b6b7e2bf3567da7df0cb3faa4e
Bogdan Abaev <bogdan@zotero.org>
2023-07-18 13:17:14 -0700

    opened tabs menu
    Adds a toolbarbutton in the top right corner of the screen
    with a menu to view and navigate opened tabs.
    
    Fixes: #2434

--- chrome/content/zotero/tabs.js
+++ chrome/content/zotero/tabs.js
@@ -86,6 +86,7 @@ var Zotero_Tabs = new function () {
 		var { tab } = this._getTab(this._selectedID);
 		document.title = (tab.title.length ? tab.title + ' - ' : '') + Zotero.appName;
 		this._updateTabBar();
+		this._updateOpenedTabsMenuItem();
 		// Hide any tab `title` tooltips that might be open
 		window.Zotero_Tooltip.stop();
 	};
@@ -242,6 +243,34 @@ var Zotero_Tabs = new function () {
 			return;
 		}
 		tab.iconBackgroundImage = icon.style.backgroundImage;
+	};
+
+	/**
+	 * Populates the popup with opened tabs.
+	 */
+	this._updateOpenedTabsMenuItem = () => {
+		let openedTabsMenu = document.getElementById('zotero-opened-tabs-popup');
+		// Empty existing nodes
+		while (openedTabsMenu.firstChild) {
+			openedTabsMenu.removeChild(openedTabsMenu.firstChild);
+		}
+		for (let tab of this._tabs) {
+			// Skip tabs whose title wasn't added yet
+			if (tab.title == "") {
+				continue;
+			}
+			let menuitem = document.createXULElement('menuitem');
+			menuitem.setAttribute('label', tab.title);
+			// Checkbox menu item if tab with this id is selected
+			if (tab.id == this._selectedID) {
+				menuitem.setAttribute('checked', true);
+			}
+			menuitem.addEventListener("command", () => {
+				this.select(tab.id);
+			});
+			menuitem.setAttribute('id', `${tab.id}-menu`);
+			openedTabsMenu.appendChild(menuitem);
+		}
 	};
 
 	/**
