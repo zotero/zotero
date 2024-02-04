zotero/zotero

commit f8108aca456fe1e6e7feedf711e10bc67066fa55

Move "Rename Collection" and "Edit Saved Search" to Fluent
Without ellipses

And remove unused "Edit Feed…", which is in zotero.dtd too

chrome/content/zotero/zoteroPane.js
@@ -3409,7 +3409,8 @@ var ZoteroPane = new function()
 			}
 			
 			// Adjust labels
-			m.editSelectedCollection.setAttribute('label', Zotero.getString('pane.collections.menu.rename.collection'));
+			document.l10n.setAttributes(m.editSelectedCollection, 'collections-menu-rename-collection');
+			
 			m.deleteCollection.setAttribute('label', Zotero.getString('pane.collections.menu.delete.collection'));
 			m.deleteCollectionAndItems.setAttribute('label', Zotero.getString('pane.collections.menu.delete.collectionAndItems'));
 			m.exportCollection.setAttribute('label', Zotero.getString('pane.collections.menu.export.collection'));
@@ -3467,7 +3468,7 @@ var ZoteroPane = new function()
 			}
 			
 			// Adjust labels
-			m.editSelectedCollection.setAttribute('label', Zotero.getString('pane.collections.menu.edit.savedSearch'));
+			document.l10n.setAttributes(m.editSelectedCollection, 'collections-menu-modify-saved-search');
 			m.duplicate.setAttribute('label', Zotero.getString('pane.collections.menu.duplicate.savedSearch'));
 			m.deleteCollection.setAttribute('label', Zotero.getString('pane.collections.menu.delete.savedSearch'));
 			m.exportCollection.setAttribute('label', Zotero.getString('pane.collections.menu.export.savedSearch'));

chrome/locale/en-US/zotero/zotero.ftl
@@ -44,6 +44,11 @@ zotero-tabs-menu-close-button =
 toolbar-add-attachment =
     .tooltiptext = { add-attachment }
 
+collections-menu-rename-collection =
+    .label = Rename Collection
+collections-menu-edit-saved-search =
+    .label = Edit Saved Search
+
 item-creator-moveDown =
     .label = Move Down
 item-creator-moveToTop =

chrome/locale/en-US/zotero/zotero.properties
@@ -276,10 +276,7 @@ pane.collections.duplicate		= Duplicate Items
 pane.collections.removeLibrary = Remove Library
 pane.collections.removeLibrary.text = Are you sure you want to permanently remove “%S” from this computer?
 
-pane.collections.menu.rename.collection			= Rename Collection…
 pane.collections.menu.duplicate.savedSearch       = Duplicate Saved Search
-pane.collections.menu.edit.savedSearch			= Edit Saved Search…
-pane.collections.menu.edit.feed					= Edit Feed…
 pane.collections.menu.remove.library = Remove Library…
 pane.collections.menu.delete.collection			= Delete Collection…
 pane.collections.menu.delete.collectionAndItems	= Delete Collection and Items…

commit f29f87ad5b9e19cebb91b0a04f65723d2d52ad9c

Fix missing "Edit Saved Search" label
Regression from f8108aca45

https://forums.zotero.org/discussion/111486/zotero-7-beta-edit-saved-search-from-right-click-on-a-saved-search-collection

chrome/content/zotero/zoteroPane.js
@@ -3468,7 +3468,7 @@ var ZoteroPane = new function()
 			}
 			
 			// Adjust labels
-			document.l10n.setAttributes(m.editSelectedCollection, 'collections-menu-modify-saved-search');
+			document.l10n.setAttributes(m.editSelectedCollection, 'collections-menu-edit-saved-search');
 			m.duplicate.setAttribute('label', Zotero.getString('pane.collections.menu.duplicate.savedSearch'));
 			m.deleteCollection.setAttribute('label', Zotero.getString('pane.collections.menu.delete.savedSearch'));
 			m.exportCollection.setAttribute('label', Zotero.getString('pane.collections.menu.export.savedSearch'));