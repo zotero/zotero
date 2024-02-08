674e06d7fbbd0047b620dad23205f61051b0e12d
Abe Jellinek <jellinek@berkeley.edu>
2024-02-08 16:35:28 -0500

    Don't stick when too tall
    For now, at least.

--- chrome/content/zotero/elements/sticky.js
+++ chrome/content/zotero/elements/sticky.js
@@ -50,35 +50,13 @@
 			this._replacement = replacement;
 			this.append(replacement);
 			
-			let intersectionObserver = new IntersectionObserver((entries) => {
-				for (let { target, intersectionRatio } of entries) {
-					// Only pay attention to the replacement when stuck and the box when not stuck
-					if (target !== (this.classList.contains('stuck') ? replacement : this.box)) {
-						continue;
-					}
-					
-					let scrollParent = this.scrollParent;
-					let box = this.box;
+			this._intersectionObserver = new IntersectionObserver(this._handleIntersection, { threshold: 1 });
+			this._resizeObserver = new ResizeObserver(this._handleResize);
 
-					let stuck = scrollParent && box
-						&& box.getBoundingClientRect().top <= scrollParent.getBoundingClientRect().top
-						&& intersectionRatio < 1;
-					this._setStuck(stuck);
-				}
-			}, { threshold: 1 });
-
-			// Attach the observer now, and reattach it if the child is added/replaced
-			intersectionObserver.observe(replacement);
-			if (this.box) {
-				intersectionObserver.observe(this.box);
-			}
-			new MutationObserver(() => {
-				intersectionObserver.disconnect();
-				intersectionObserver.observe(replacement);
-				if (this.box) {
-					intersectionObserver.observe(this.box);
-				}
-			}).observe(this, { childList: true });
+			// Attach the observers now, and reattach them if the child is added/replaced
+			this._attachObservers();
+			new MutationObserver(() => this._attachObservers())
+				.observe(this, { childList: true });
 
 			// An element with position: fixed will eat wheel events, so dispatch them
 			// to the parent manually
@@ -87,16 +65,43 @@
 			});
 		}
 		
-		_setStuck(stuck) {
-			this.classList.toggle('stuck', stuck);
-			this._replacement.hidden = !stuck;
-			if (stuck) {
-				this._replacement.style.height = `${this.box.offsetHeight || this.box.getBoundingClientRect().height}px`;
-				this.scrollParent.addEventListener('scroll', this._handleScroll);
-				this._handleScroll();
+		_attachObservers() {
+			this._intersectionObserver.disconnect();
+			this._resizeObserver.disconnect();
+			
+			this._intersectionObserver.observe(this._replacement);
+			if (this.box) {
+				this._intersectionObserver.observe(this.box);
+				this._resizeObserver.observe(this.box);
 			}
+			if (this.scrollParent) {
+				this._resizeObserver.observe(this.scrollParent);
+			}
+			this._handleResize();
 		}
-		
+
+		_handleIntersection = (entries) => {
+			let scrollParent = this.scrollParent;
+			let box = this.box;
+
+			for (let { target, intersectionRatio } of entries) {
+				// Only pay attention to the replacement when stuck and the box when not stuck
+				if (target !== (this.classList.contains('stuck') ? this._replacement : this.box)) {
+					continue;
+				}
+
+				let stuck = scrollParent && box
+					&& box.getBoundingClientRect().top <= scrollParent.getBoundingClientRect().top
+					&& intersectionRatio < 1;
+				this._setStuck(stuck);
+			}
+		};
+
+		_handleResize = () => {
+			this.classList.toggle('long',
+				this.box && this.scrollParent && this.box.offsetHeight > this.scrollParent.offsetHeight);
+		};
+
 		_handleScroll = (event) => {
 			if (event && (event.target !== this.scrollParent || !this.classList.contains('stuck'))) {
 				this.box.style.removeProperty('--full-height');
@@ -109,6 +114,16 @@
 			this.box.style.setProperty('--scroll-distance', scrollDistance + 'px');
 			this.box.style.setProperty('--scrollbar-width', this.scrollParent.offsetWidth - this.scrollParent.clientWidth + 'px');
 		};
+
+		_setStuck(stuck) {
+			this.classList.toggle('stuck', stuck);
+			this._replacement.hidden = !stuck;
+			if (stuck) {
+				this._replacement.style.height = `${this.box.offsetHeight || this.box.getBoundingClientRect().height}px`;
+				this.scrollParent.addEventListener('scroll', this._handleScroll);
+				this._handleScroll();
+			}
+		}
 	}
 
 	customElements.define("sticky", Sticky);
