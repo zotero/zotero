4c9d85e16061276baa6f3d96388be606d3466233
Tom Najdek <tom@doppnet.com>
2024-03-29 13:33:03 +0100

    Improve error message in the importer
    * New messages for connection/certificate problems
    * Change error message color to better fit after the redesign

--- chrome/content/zotero/import/importWizard.js
+++ chrome/content/zotero/import/importWizard.js
@@ -256,8 +256,21 @@ const Zotero_Import_Wizard = { // eslint-disable-line no-unused-vars
 			}
 			catch (e) {
 				const feedbackEl = document.getElementById('mendeley-online-login-feedback');
+				if (e instanceof Zotero.HTTP.SecurityException) {
+					feedbackEl.setAttribute('data-l10n-id', 'import-online-connection-unsecure');
+					feedbackEl.setAttribute('data-l10n-args', JSON.stringify({ targetApp: "Mendeley", errorCode: e.message }));
+				}
+				else if (e instanceof Zotero.HTTP.UnexpectedStatusException && (e.status === 400 || e.status === 401 || e.status === 403)) {
+					feedbackEl.setAttribute('data-l10n-id', 'import-online-wrong-credentials');
+					feedbackEl.setAttribute('data-l10n-args', JSON.stringify({ targetApp: "Mendeley" }));
+					this.wizard.canAdvance = false; // change to login/password input will reset this
+				}
+				else {
+					feedbackEl.setAttribute('data-l10n-id', 'import-online-connection-error');
+					feedbackEl.setAttribute('data-l10n-args', JSON.stringify({ targetApp: "Mendeley" }));
+				}
+				
 				feedbackEl.style.display = '';
-				this.wizard.canAdvance = false; // change to either of the inputs will reset thi
 			}
 			finally {
 				userNameEl.disabled = false;
