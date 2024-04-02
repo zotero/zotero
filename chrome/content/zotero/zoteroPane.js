35aaa013ed1d8f1bc22bab1d2e916baef79443a3
Dan Stillman <dstillman@zotero.org>
2024-04-02 05:49:54 -0400

    Move ZoteroPane.syncAlert() to Sync.Runner.alert()
    And use for sync setup in prefs so that it includes the dialog header
    from the sync error messages

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -4798,7 +4798,7 @@ var ZoteroPane = new function()
 			catch (e) {
 				// TODO: show error somewhere else
 				Zotero.debug(e, 1);
-				ZoteroPane_Local.syncAlert(e);
+				Zotero.Sync.Runner.alert(e);
 				return;
 			}
 			
@@ -5139,53 +5139,6 @@ var ZoteroPane = new function()
 		);
 		
 		return index == 0 ? 'all' : 'one';
-	};
-	
-	
-	this.syncAlert = function (e) {
-		e = Zotero.Sync.Runner.parseError(e);
-		var ps = Services.prompt;
-		var buttonText = e.dialogButtonText;
-		var buttonCallback = e.dialogButtonCallback;
-		
-		if (e.errorType == 'warning' || e.errorType == 'error') {
-			let title = Zotero.getString('general.' + e.errorType);
-			// TODO: Display header in bold
-			let msg = (e.dialogHeader ? e.dialogHeader + '\n\n' : '') + e.message;
-			
-			if (e.errorType == 'warning' || buttonText === null) {
-				ps.alert(null, title, e.message);
-				return;
-			}
-			
-			if (!buttonText) {
-				buttonText = Zotero.getString('errorReport.reportError');
-				buttonCallback = function () {
-					ZoteroPane.reportErrors();
-				};
-			}
-			
-			let buttonFlags = ps.BUTTON_POS_0 * ps.BUTTON_TITLE_OK
-				+ ps.BUTTON_POS_1 * ps.BUTTON_TITLE_IS_STRING;
-			let index = ps.confirmEx(
-				null,
-				title,
-				msg,
-				buttonFlags,
-				"",
-				buttonText,
-				"", null, {}
-			);
-			
-			if (index == 1) {
-				setTimeout(buttonCallback, 1);
-			}
-		}
-		// Upgrade message
-		else if (e.errorType == 'upgrade') {
-			ps.alert(null, "", e.message);
-			return;
-		}
 	};
 	
 	
