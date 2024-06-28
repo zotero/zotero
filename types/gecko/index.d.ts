456bb65572e6b0d3f2e43026d952700a1e70f6d5
Abe Jellinek <jellinek@berkeley.edu>
2024-06-28 14:47:33 -0400

    Add Gecko types and a script to update them

---
+++ types/gecko/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * NOTE: Do not modify this file by hand.
+ * Run `mach ts update` to reference typelibs generated in <objdir>.
+ */
+
+/// <reference types="./lib.gecko.dom.d.ts" />
+/// <reference types="./lib.gecko.nsresult.d.ts" />
+/// <reference types="./lib.gecko.services.d.ts" />
+/// <reference types="./lib.gecko.xpcom.d.ts" />
---
+++ types/gecko/lib.gecko.services.d.ts
@@ -0,0 +1,61 @@
+/**
+ * NOTE: Do not modify this file by hand.
+ * Content was generated from services.json.
+ */
+
+interface JSServices {
+  DAPTelemetry: nsIDAPTelemetry;
+  appShell: nsIAppShellService;
+  appinfo: nsICrashReporter & nsIXULAppInfo & nsIXULRuntime;
+  blocklist: nsIBlocklistService;
+  cache2: nsICacheStorageService;
+  catMan: nsICategoryManager;
+  clearData: nsIClearDataService;
+  clipboard: nsIClipboard;
+  console: nsIConsoleService;
+  cookieBanners: nsICookieBannerService;
+  cookies: nsICookieManager & nsICookieService;
+  cpmm: ContentProcessMessageManager;
+  dirsvc: nsIDirectoryService & nsIProperties;
+  dns: nsIDNSService;
+  domStorageManager: nsIDOMStorageManager & nsILocalStorageManager;
+  droppedLinkHandler: nsIDroppedLinkHandler;
+  eTLD: nsIEffectiveTLDService;
+  els: nsIEventListenerService;
+  env: nsIEnvironment;
+  focus: nsIFocusManager;
+  fog: nsIFOG;
+  intl: mozIMozIntl;
+  io: nsIIOService & nsINetUtil & nsISpeculativeConnect;
+  loadContextInfo: nsILoadContextInfoFactory;
+  locale: mozILocaleService;
+  logins: nsILoginManager;
+  mm: ChromeMessageBroadcaster;
+  obs: nsIObserverService;
+  perms: nsIPermissionManager;
+  policies: nsIEnterprisePolicies;
+  ppmm: ParentProcessMessageManager;
+  prefs: nsIPrefBranch & nsIPrefService;
+  profiler: nsIProfiler;
+  prompt: nsIPromptService;
+  qms: nsIQuotaManagerService;
+  rfp: nsIRFPService;
+  scriptSecurityManager: nsIScriptSecurityManager;
+  scriptloader: mozIJSSubScriptLoader;
+  search: nsISearchService;
+  sessionStorage: nsISessionStorageService;
+  startup: nsIAppStartup;
+  storage: mozIStorageService;
+  strings: nsIStringBundleService;
+  sysinfo: nsIPropertyBag2 & nsISystemInfo;
+  telemetry: nsITelemetry;
+  textToSubURI: nsITextToSubURI;
+  tm: nsIThreadManager;
+  uriFixup: nsIURIFixup;
+  urlFormatter: nsIURLFormatter;
+  uuid: nsIUUIDGenerator;
+  vc: nsIVersionComparator;
+  wm: nsIWindowMediator;
+  ww: nsIWindowWatcher;
+  xulStore: nsIXULStore;
+}
