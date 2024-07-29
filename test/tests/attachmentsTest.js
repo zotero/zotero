e3057489bddb5ada61cedb5c6acd9b1a240b925e
Abe Jellinek <jellinek@berkeley.edu>
2024-07-29 11:29:36 -0400

    Update tests to test new behavior
    And update support.js importFileAttachment() to allow enabling default
    setAutoAttachmentTitle() behavior.

--- test/tests/attachmentsTest.js
+++ test/tests/attachmentsTest.js
@@ -152,7 +152,7 @@ describe("Zotero.Attachments", function() {
 				file: file,
 				parentItemID: parent.id,
 			});
-			assert.equal(attachment.getField('title'), Zotero.getString('file-type-pdf'));
+			assert.equal(attachment.getField('title'), 'test');
 			await parent.eraseTx();
 		});
 	})
