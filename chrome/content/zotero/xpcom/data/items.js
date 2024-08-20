86fdc152b2c86474467d3356477d95861f039e4a
Bogdan Abaev <bogdan@zotero.org>
2024-08-20 14:55:54 -0700

    annotations appearing in itemTree
    - Annotations are displayed in itemTree under their file attachments
    on a third level. The annotation spans the entire row.
    - The title is constructed on the go. When possible, it
    includes annotation quote and comment as pseudo-columns
    of the row. The comment occupies about twice as much space as
    the quote. Otherwise, (if there is no quote) only the annotation comment
    is included as the "title" part of the row
    - Non-CJK segments of the quote part of the annotation row are italicized.
    CJK segments are left as is. If CJK segments are present, there is more
    padding between quote and comment parts.
    - Search matches the actual attachment instead of it's parent file.
    - Can create child notes from annotations of the same item or
    standalone notes from annotations across different items from
    the context menu or the header button.
    - When an annotation (or multiple annotations) are selected, the
    annotationItemPane component is displayed where annotations are
    groupped by their top-level item in collapsible-sections. Annotations
    are displayed fully, without having their content cut off.
    - Special treatment for annotations to always prompt
    to erase the item regardless of what collectionTree row
    is selected (e.g. if a collection is selected, we
    still want one to be able to delete the annotation).
    This only applies if all selected items are annotations.
    If multiple items are selected, some annotations and
    some not, do nothing. This is until the trash is
    ready. In the future, we may send annotations to trash

--- chrome/content/zotero/xpcom/data/items.js
+++ chrome/content/zotero/xpcom/data/items.js
@@ -254,11 +254,14 @@ Zotero.Items = function() {
 				item.updateDisplayTitle()
 			}
 			catch (e) {
-				// A few item types need creators to be loaded. Instead of making
-				// updateDisplayTitle() async and loading conditionally, just catch the error
+				// A few item types need creators or tags to be loaded. Annotations need to be loaded
+				// to be displayed in itemTree.
+				// Instead of making updateDisplayTitle() async and loading conditionally, just catch the error
 				// and load on demand
 				if (e instanceof Zotero.Exception.UnloadedDataException) {
 					yield item.loadDataType('creators');
+					yield item.loadDataType('annotation');
+					yield item.loadDataType('tags');
 					item.updateDisplayTitle()
 				}
 				else {
--- chrome/content/zotero/xpcom/editorInstance.js
+++ chrome/content/zotero/xpcom/editorInstance.js
@@ -1308,7 +1308,7 @@ class EditorInstance {
 	 * @param {Integer} options.collectionID - Only valid if parentID not provided
 	 * @returns {Promise<Zotero.Item>}
 	 */
-	static async createNoteFromAnnotations(annotations, { parentID, collectionID } = {}) {
+	static async createNoteFromAnnotations(annotations, { parentID, collectionID, skipFirstSave } = {}) {
 		if (!annotations.length) {
 			throw new Error("No annotations provided");
 		}
@@ -1335,7 +1335,11 @@ class EditorInstance {
 		else if (collectionID) {
 			note.addToCollection(collectionID);
 		}
-		await note.saveTx();
+		// Each save causes change of focus, so for standalone notes from
+		// itemTree attachments, skip it to avoid blinking
+		if (!skipFirstSave) {
+			await note.saveTx();
+		}
 		let editorInstance = new EditorInstance();
 		editorInstance._item = note;
 		let jsonAnnotations = [];
