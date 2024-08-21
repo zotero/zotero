d3420e32d3449aadc4e373a4ec5cf56edcc8f25b
Abe Jellinek <jellinek@berkeley.edu>
2024-08-21 12:46:11 -0400

    Display filenames in italic
    Not sure if we actually want this.

--- chrome/content/zotero/itemTree.jsx
+++ chrome/content/zotero/itemTree.jsx
@@ -2870,6 +2870,9 @@ var ItemTree = class ItemTree extends LibraryTree {
 		}
 		let textSpanAriaLabel = [textWithFullStop, itemTypeAriaLabel, tagAriaLabel, retractedAriaLabel].join(' ');
 		textSpan.className = "cell-text";
+		if (this._rowCache[item.id].titleIsFilename) {
+			textSpan.classList.add('filename');
+		}
 		if (lazy.BIDI_BROWSER_UI) {
 			textSpan.dir = Zotero.ItemFields.getDirection(
 				item.itemTypeID, column.dataKey, item.getField('language')
@@ -3254,9 +3257,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 				&& !(treeRow.ref.isSnapshotAttachment() && /snapshot/i.test(treeRow.ref.getField('title')))
 				&& Zotero.Prefs.get('showAttachmentFilenames')) {
 			row.title = treeRow.ref.attachmentFilename;
+			row.titleIsFilename = true;
 		}
 		else {
 			row.title = treeRow.ref.getDisplayTitle();
+			row.titleIsFilename = false;
 		}
 		
 		const columns = this.getColumns();
