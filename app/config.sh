d1a82af43c0e8b3803d409cecb8da560da009cb8
Dan Stillman <dstillman@zotero.org>
2024-08-27 01:07:21 -0400

    Update Linux build to 115.14.0esr

--- app/config.sh
+++ app/config.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Version of Gecko to build with
 GECKO_VERSION_MAC="115.9.1esr"
-GECKO_VERSION_LINUX="115.9.1esr"
+GECKO_VERSION_LINUX="115.14.0esr"
 GECKO_VERSION_WIN="115.14.0esr"
 RUST_VERSION=1.69.0
 
