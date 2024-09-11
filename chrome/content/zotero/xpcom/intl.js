zotero/zotero

commit af3602124b0c3e2736a9654651ff3d89cc6e7edc

Load DTD strings lazily (#4664)

chrome/content/zotero/xpcom/intl.js
@@ -79,17 +79,6 @@ Zotero.Intl = new function () {
 		Zotero.arrowPreviousKey = Zotero.rtl ? 'ArrowRight' : 'ArrowLeft';
 		Zotero.arrowNextKey = Zotero.rtl ? 'ArrowLeft' : 'ArrowRight';
 		
-		this.strings = {};
-		const intlFiles = ['zotero.dtd', 'preferences.dtd', 'mozilla/editMenuOverlay.dtd'];
-		for (let intlFile of intlFiles) {
-			let localeXML = Zotero.File.getContentsFromURL(`chrome://zotero/locale/${intlFile}`);
-			let regexp = /<!ENTITY ([^\s]+)\s+"([^"]+)/g;
-			let regexpResult;
-			while (regexpResult = regexp.exec(localeXML)) {
-				this.strings[regexpResult[1]] = regexpResult[2];
-			}
-		}
-		
 		// Provide synchronous access to Fluent strings for getString()
 		ftl = new Localization([
 			'branding/brand.ftl',
@@ -99,6 +88,22 @@ Zotero.Intl = new function () {
 		], true);
 		Zotero.ftl = ftl;
 	};
+
+
+	ChromeUtils.defineLazyGetter(this, 'strings', () => {
+		const intlFiles = ['zotero.dtd', 'preferences.dtd', 'mozilla/editMenuOverlay.dtd'];
+
+		let strings = [];
+		for (let intlFile of intlFiles) {
+			let localeXML = Zotero.File.getContentsFromURL(`chrome://zotero/locale/${intlFile}`);
+			let regexp = /<!ENTITY ([^\s]+)\s+"([^"]+)/g;
+			let regexpResult;
+			while ((regexpResult = regexp.exec(localeXML))) {
+				strings[regexpResult[1]] = regexpResult[2];
+			}
+		}
+		return strings;
+	});
 
 
 	/**

commit fe3fdac88e1ad2238038555f85a0f157ea41a2cd

Quick fix for `Localized string not available for general.warning`
https://forums.zotero.org/discussion/117812/issue-with-installing-zotero-7

With Zotero data directory in cloud storage folder

Regression from af3602124

chrome/content/zotero/xpcom/intl.js
@@ -135,7 +135,12 @@ Zotero.Intl = new function () {
 				if (ftlString) {
 					return ftlString;
 				}
-				else if (this.strings[name]) {
+				// TEMP: The this.strings check prevents "TypeError: this.strings is undefined"
+				// from an early Zotero.getString() call, but I'm not sure why. this.strings is
+				// set using defineLazyGetter, but lazy doesn't mean asynchronous...
+				//
+				// https://forums.zotero.org/discussion/117812/issue-with-installing-zotero-7
+				else if (this.strings && this.strings[name]) {
 					return this.strings[name];
 				}
 				else {