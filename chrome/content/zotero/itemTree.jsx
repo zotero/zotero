b41030293bb00597cf4dd168272369c259b15a7f
Bogdan Abaev <bogdan@zotero.org>
2024-10-29 19:02:08 -0700

    arrowUp/down in duplicates view select the set
    When duplicates collectionTreeRow is selected, arrowUp/down within
    itemTree will select the entire previous/next set of duplicate items.
    
    Per discussion based on https://forums.zotero.org/discussion/119217/bug-zotero-7-beta-duplicates-collection-navigation-via-arrow-keys

--- chrome/content/zotero/itemTree.jsx
+++ chrome/content/zotero/itemTree.jsx
@@ -978,6 +978,23 @@ var ItemTree = class ItemTree extends LibraryTree {
 			this.collapseAllRows();
 			return false;
 		}
+		// On arrowUp/down without modifiers in duplicates view, select the entire set
+		else if (this.collectionTreeRow.isDuplicates() && ["ArrowUp", "ArrowDown"].includes(event.key)
+				&& !event.shiftKey && !event.ctrlKey && !event.metaKey && !event.altKey) {
+			// Find the first row index before/after the current set
+			let selectedArray = Array.from(this.selection.selected).sort();
+			let nextRowIndex = event.key == "ArrowUp" ? (selectedArray[0] - 1) : (selectedArray[selectedArray.length - 1] + 1);
+			if (nextRowIndex < 0 || nextRowIndex > this._rows.length - 1) return false;
+			// Set that row's item as the next set to select
+			let nextItem = this._rows[nextRowIndex].ref;
+			var setItemIDs = this.collectionTreeRow.ref.getSetItemsByItemID(nextItem.id);
+			// Set focus to the first row in the set
+			let itemRows = setItemIDs.map(itemID => this._rowMap[itemID]).sort();
+			this.selection.focused = itemRows[0];
+			
+			this.selectItems(setItemIDs);
+			return false;
+		}
 		return true;
 	}
 
