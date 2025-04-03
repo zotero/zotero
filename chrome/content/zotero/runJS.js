14107bacf93b0ed3d62b181dd03f3c064f98c632
Bogdan Abaev <bogdan@zotero.org>
2025-04-03 10:47:31 -0700

    localize message + display spinner

--- chrome/content/zotero/runJS.js
+++ chrome/content/zotero/runJS.js
@@ -14,6 +14,8 @@ async function run() {
 	var isAsync = document.getElementById('run-as-async').checked;
 	var result;
 	var resultTextbox = document.getElementById('result');
+	var spinner = document.getElementById("loading-spinner");
+	spinner.setAttribute("status", "animate");
 	try {
 		if (isAsync) {
 			code = '(async function () {' + code + '})()';
@@ -28,6 +30,12 @@ async function run() {
 		resultTextbox.textContent = e;
 		return;
 	}
+	// Hide the spinner after a small delay so it briefly appears even
+	// if the code runs fast to indicate that everything did run
+	setTimeout(() => {
+		spinner.removeAttribute("status");
+	}, 100);
+	
 	resultTextbox.classList.remove('error');
 	if (typeof result == 'string') {
 		resultTextbox.textContent = result;
@@ -36,7 +44,7 @@ async function run() {
 		resultTextbox.textContent = Zotero.Utilities.varDump(result);
 	}
 	else {
-		resultTextbox.textContent = "Completed successfully";
+		document.l10n.setAttributes(resultTextbox, "runJS-completed");
 	}
 }
 
--- chrome/locale/en-GB/zotero/zotero.ftl
+++ chrome/locale/en-GB/zotero/zotero.ftl
@@ -241,6 +241,7 @@ runJS-title = Run JavaScript
 runJS-editor-label = Code:
 runJS-run = Run
 runJS-help = { general-help }
+runJS-completed = Completed successfully
 runJS-result =
     { $type ->
         [async] Return value:
