zotero/zotero

commit 26221b6786123c4c232b34ed658fc79ea8cda6e9

Fix broken browser window on print + remove many Mozilla files (#5050)

app/build.sh
@@ -232,6 +232,47 @@ elif [ $BUILD_LINUX == 1 ]; then
 fi
 set -e
 cd $omni_dir
+
+# Remove Firefox files that we don't need
+rm actors/AboutLogins{Parent,Child}.sys.mjs
+rm actors/AboutMessagePreview{Parent,Child}.sys.mjs
+rm actors/AboutNewTab{Parent,Child}.sys.mjs
+rm actors/AboutPocket{Parent,Child}.sys.mjs
+rm actors/AboutPrivateBrowsing{Parent,Child}.sys.mjs
+rm actors/AboutProtections{Parent,Child}.sys.mjs
+rm actors/AboutReader{Parent,Child}.sys.mjs
+rm actors/AboutTabCrashed{Parent,Child}.sys.mjs
+rm actors/AboutWelcome{Parent,Child}.sys.mjs
+rm actors/ASRouter{Parent,Child}.sys.mjs
+rm actors/BackupUI{Parent,Child}.sys.mjs
+rm actors/BlockedSite{Parent,Child}.sys.mjs
+rm actors/BrowserProcessChild.sys.mjs
+rm actors/BrowserTabChild.sys.mjs
+rm actors/ClickHandler{Parent,Child}.sys.mjs
+rm actors/ContentSearch{Parent,Child}.sys.mjs
+rm actors/DecoderDoctor{Parent,Child}.sys.mjs
+rm actors/DOMFullscreen{Parent,Child}.sys.mjs
+rm actors/EncryptedMedia{Parent,Child}.sys.mjs
+rm actors/FormValidation{Parent,Child}.sys.mjs
+rm actors/LinkHandler{Parent,Child}.sys.mjs
+rm actors/MigrationWizard{Parent,Child}.sys.mjs
+rm actors/PageData{Parent,Child}.sys.mjs # Not our PageData actor
+rm actors/PageInfoChild.sys.mjs
+rm actors/PageStyle{Parent,Child}.sys.mjs
+rm actors/Plugin{Parent,Child}.sys.mjs # GMP plugins, which we remove
+rm actors/PointerLock{Parent,Child}.sys.mjs
+rm actors/RFPHelper{Parent,Child}.sys.mjs
+rm actors/ScreenshotsComponentChild.sys.mjs
+rm actors/SearchSERPTelemetry{Parent,Child}.sys.mjs
+rm actors/ShoppingSidebar{Parent,Child}.sys.mjs
+rm actors/SpeechDispatcher{Parent,Child}.sys.mjs
+rm actors/WebRTC{Parent,Child}.sys.mjs
+
+mv chrome/browser/content/browser/license.html chrome/browser_license.html
+rm -r chrome/browser # We want Firefox, just not the browser part
+
+rm modules/SearchSERPTelemetry.sys.mjs
+
 # Move some Firefox files that would be overwritten out of the way
 mv chrome.manifest chrome.manifest-fx
 mv defaults defaults-fx
@@ -292,9 +333,6 @@ elif [ $BUILD_LINUX == 1 ]; then
 	# https://bugzilla.mozilla.org/show_bug.cgi?id=1747208
 	perl -pi -e 's/pref\("general\.autoScroll", false\);/pref\("general.autoScroll", true);/' $prefs_file
 fi
-
-# Clear list of built-in add-ons
-echo '{"dictionaries": {"en-US": "dictionaries/en-US.dic"}, "system": []}' > chrome/browser/content/browser/built_in_addons.json
 
 # chrome.manifest
 mv chrome.manifest zotero.manifest
@@ -378,9 +416,6 @@ export CALLDIR && perl -pi -e 'BEGIN { local $/; open $fh, "$ENV{CALLDIR}/assets
 
 # Don't try to initialize Places, since we've removed its files
 replace_line 'this._placesInitialized = true;' 'if (true) return; this._placesInitialized = true;' modules/BrowserGlue.sys.mjs
-
-# Prevent color scheme getting reset to 'light' during printing
-replace_line 'new LightweightThemeConsumer\(document\);' '\/\/new LightweightThemeConsumer\(document\);'  chrome/browser/content/browser/browser-init.js
 
 # Move test files to root directory
 if [ $include_tests -eq 1 ]; then

app/scripts/fetch_xulrunner
@@ -90,6 +90,17 @@ function modify_omni {
 	unzip omni.ja
 	rm omni.ja
 	
+	rm -r chrome/pdfjs
+	
+	rm -r modules/narrate
+	rm -r modules/translation
+	
+	rm actors/AboutTranslations{Parent,Child}.sys.mjs
+	rm actors/CookieBanner{Parent,Child}.sys.mjs
+	rm actors/PictureInPictureChild.sys.mjs
+	rm actors/ThumbnailsChild.sys.mjs
+	rm actors/Translations{Engine,}{Parent,Child}.sys.mjs
+	
 	replace_line 'BROWSER_CHROME_URL:.+' 'BROWSER_CHROME_URL: "chrome:\/\/zotero\/content\/zoteroPane.xhtml",' modules/AppConstants.sys.mjs
 	
 	# https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/internals/preferences.html

chrome/content/zotero/browserWindowShim.js
@@ -1,0 +1,220 @@
+// Try our very hardest to look like a browser window so Firefox doesn't make its own
+// We're faking it like Thunderbird does in a couple places:
+//   https://searchfox.org/comm-esr128/rev/c0882dde78e3fb440d2c0bb730e9ac5377762cbf/mail/base/content/mailWindow.js
+//   https://searchfox.org/comm-esr128/rev/c0882dde78e3fb440d2c0bb730e9ac5377762cbf/mail/components/extensions/extensionPopup.js
+// But we can try a little less hard because our browsers will never be visible
+
+
+var { AppConstants } = ChromeUtils.importESModule(
+	"resource://gre/modules/AppConstants.sys.mjs"
+);
+var { XPCOMUtils } = ChromeUtils.importESModule(
+	"resource://gre/modules/XPCOMUtils.sys.mjs"
+);
+
+// In case PrintUtils isn't already loaded:
+XPCOMUtils.defineLazyScriptGetter(
+	window,
+	"PrintUtils",
+	"chrome://global/content/printUtils.js"
+);
+
+class nsBrowserAccess {
+	QueryInterface = ChromeUtils.generateQI(["nsIBrowserDOMWindow"]);
+
+	_openURIInNewTab() {
+		// Callers pass this method all kinds of crazy arguments,
+		// but it seems like we don't need anything complicated:
+		let browser = document.createXULElement('browser');
+		browser.hidden = true;
+		document.documentElement.appendChild(browser);
+		return browser;
+	}
+
+	createContentWindow(
+		aURI,
+		aOpenWindowInfo,
+		aWhere,
+		aFlags,
+		aTriggeringPrincipal,
+		aCsp
+	) {
+		return this.getContentWindowOrOpenURI(
+			null,
+			aOpenWindowInfo,
+			aWhere,
+			aFlags,
+			aTriggeringPrincipal,
+			aCsp,
+			true
+		);
+	}
+
+	createContentWindowInFrame(aURI, aParams, aWhere, aFlags, aName) {
+		// Passing a null-URI to only create the content window,
+		// and pass true for aSkipLoad to prevent loading of
+		// about:blank
+		return this.getContentWindowOrOpenURIInFrame(
+			null,
+			aParams,
+			aWhere,
+			aFlags,
+			aName,
+			true
+		);
+	}
+
+	openURI(aURI, aOpenWindowInfo, aWhere, aFlags, aTriggeringPrincipal, aCsp) {
+		if (!aURI) {
+			throw Components.Exception(
+				"openURI should only be called with a valid URI",
+				Cr.NS_ERROR_FAILURE
+			);
+		}
+		return this.getContentWindowOrOpenURI(
+			aURI,
+			aOpenWindowInfo,
+			aWhere,
+			aFlags,
+			aTriggeringPrincipal,
+			aCsp,
+			false
+		);
+	}
+
+	openURIInFrame(aURI, aParams, aWhere, aFlags, aName) {
+		return this.getContentWindowOrOpenURIInFrame(
+			aURI,
+			aParams,
+			aWhere,
+			aFlags,
+			aName,
+			false
+		);
+	}
+
+	getContentWindowOrOpenURI(
+		aURI,
+		aOpenWindowInfo,
+		aWhere,
+		aFlags,
+		aTriggeringPrincipal,
+		aCsp,
+		aSkipLoad
+	) {
+		if (aWhere == Ci.nsIBrowserDOMWindow.OPEN_PRINT_BROWSER) {
+			const browser =
+				PrintUtils.handleStaticCloneCreatedForPrint(aOpenWindowInfo);
+			return browser ? browser.browsingContext : null;
+		}
+
+		const isExternal = !!(aFlags & Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL);
+
+		if (aOpenWindowInfo && isExternal) {
+			throw Components.Exception(
+				"nsBrowserAccess.openURI did not expect aOpenWindowInfo to be " +
+				"passed if the context is OPEN_EXTERNAL.",
+				Cr.NS_ERROR_FAILURE
+			);
+		}
+
+		if (isExternal && aURI && aURI.schemeIs("chrome")) {
+			Services.console.logStringMessage(
+				"use -chrome command-line option to load external chrome urls\n"
+			);
+			return null;
+		}
+
+		const ReferrerInfo = Components.Constructor(
+			"@mozilla.org/referrer-info;1",
+			"nsIReferrerInfo",
+			"init"
+		);
+
+		let referrerInfo;
+		if (aFlags & Ci.nsIBrowserDOMWindow.OPEN_NO_REFERRER) {
+			referrerInfo = new ReferrerInfo(Ci.nsIReferrerInfo.EMPTY, false, null);
+		} else if (
+			aOpenWindowInfo &&
+			aOpenWindowInfo.parent &&
+			aOpenWindowInfo.parent.window
+		) {
+			referrerInfo = new ReferrerInfo(
+				aOpenWindowInfo.parent.window.document.referrerInfo.referrerPolicy,
+				true,
+				Services.io.newURI(aOpenWindowInfo.parent.window.location.href)
+			);
+		} else {
+			referrerInfo = new ReferrerInfo(Ci.nsIReferrerInfo.EMPTY, true, null);
+		}
+
+		if (aWhere != Ci.nsIBrowserDOMWindow.OPEN_NEWTAB) {
+			Services.console.logStringMessage(
+				"Opening a URI in something other than a new tab is not supported, opening in new tab instead"
+			);
+		}
+
+		const browser = this._openURIInNewTab(
+			aURI,
+			referrerInfo,
+			isExternal,
+			aOpenWindowInfo,
+			aTriggeringPrincipal,
+			aCsp,
+			aSkipLoad,
+			aOpenWindowInfo?.openerBrowser?.getAttribute("messagemanagergroup")
+		);
+
+		return browser ? browser.browsingContext : null;
+	}
+
+	getContentWindowOrOpenURIInFrame(
+		aURI,
+		aParams,
+		aWhere,
+		aFlags,
+		aName,
+		aSkipLoad
+	) {
+		if (aWhere == Ci.nsIBrowserDOMWindow.OPEN_PRINT_BROWSER) {
+			return PrintUtils.handleStaticCloneCreatedForPrint(
+				aParams.openWindowInfo
+			);
+		}
+
+		if (aWhere != Ci.nsIBrowserDOMWindow.OPEN_NEWTAB) {
+			Services.console.logStringMessage(
+				"Error: openURIInFrame can only open in new tabs or print"
+			);
+			return null;
+		}
+
+		const isExternal = !!(aFlags & Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL);
+
+		return this._openURIInNewTab(
+			aURI,
+			aParams.referrerInfo,
+			isExternal,
+			aParams.openWindowInfo,
+			aParams.triggeringPrincipal,
+			aParams.csp,
+			aSkipLoad,
+			aParams.openerBrowser?.getAttribute("messagemanagergroup")
+		);
+	}
+
+	canClose() {
+		return true;
+	}
+
+	get tabCount() {
+		return 1;
+	}
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	window.docShell.treeOwner
+		.QueryInterface(Ci.nsIInterfaceRequestor)
+		.getInterface(Ci.nsIAppWindow).XULBrowserWindow = window.XULBrowserWindow;
+	window.browserDOMWindow = new nsBrowserAccess();
+});

chrome/content/zotero/reader.xhtml
@@ -34,6 +34,7 @@
 		Services.scriptloader.loadSubScript("chrome://zotero/content/include.js", this);
 		Services.scriptloader.loadSubScript("resource://zotero/require.js", this);
 		Services.scriptloader.loadSubScript("chrome://zotero/content/platformKeys.js", this);
+		Services.scriptloader.loadSubScript("chrome://zotero/content/browserWindowShim.js", this);
 
 		// Mozilla scripts
 		Services.scriptloader.loadSubScript("chrome://global/content/globalOverlay.js", this);

chrome/content/zotero/standalone/basicViewer.xhtml
@@ -58,6 +58,7 @@
 		Services.scriptloader.loadSubScript("chrome://zotero/content/platformKeys.js", this);
 		Services.scriptloader.loadSubScript("chrome://zotero/content/editMenuOverlay.js", this);
 		Services.scriptloader.loadSubScript("chrome://zotero/content/include.js", this);
+		Services.scriptloader.loadSubScript("chrome://zotero/content/browserWindowShim.js", this);
 		
 		// Mozilla scripts
 		Services.scriptloader.loadSubScript("chrome://global/content/globalOverlay.js", this);

chrome/content/zotero/zoteroPane.xhtml
@@ -64,6 +64,7 @@
 		Services.scriptloader.loadSubScript("chrome://zotero/content/titlebar.js", this);
 		Services.scriptloader.loadSubScript("chrome://zotero/content/platformKeys.js", this);
 		Services.scriptloader.loadSubScript("chrome://zotero/content/editMenuOverlay.js", this);
+		Services.scriptloader.loadSubScript("chrome://zotero/content/browserWindowShim.js", this);
 		
 		// Mozilla scripts
 		Services.scriptloader.loadSubScript("chrome://global/content/globalOverlay.js", this);

commit 1c089bf44819c7a84367ec76f8e79d49c0d2f7b6

Fix broken spellchecking when only en-US is installed
Regression in 26221b6786123c4c232b34ed658fc79ea8cda6e9

app/build.sh
@@ -274,6 +274,8 @@ browser_keep=(
 	content/browser/license.html
 	# For web-ext
 	content/browser/parent/ext-browser.js
+	# For spellchecking
+	content/browser/built_in_addons.json
 )
 if [ $BUILD_WIN == 1 ] || [ $BUILD_LINUX == 1 ]; then
 	# Windows/Linux window controls
@@ -350,6 +352,9 @@ elif [ $BUILD_LINUX == 1 ]; then
 	# https://bugzilla.mozilla.org/show_bug.cgi?id=1747208
 	perl -pi -e 's/pref\("general\.autoScroll", false\);/pref\("general.autoScroll", true);/' $prefs_file
 fi
+
+# Clear list of built-in add-ons, but leave the en-US dictionary
+echo '{"dictionaries": {"en-US": "dictionaries/en-US.dic"}, "system": []}' > chrome/browser/content/browser/built_in_addons.json
 
 # chrome.manifest
 mv chrome.manifest zotero.manifest