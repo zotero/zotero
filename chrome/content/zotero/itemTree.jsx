zotero/zotero

commit 48fd23ccec73156855f4c59170b332af2e4357dd

annotations showing in itemTree (#3416)
- Annotations are displayed in itemTree under their file attachments
  on a third level. The annotation spans the entire row.
- The title is constructed on the go. When possible, it
  includes annotation quote and comment as pseudo-columns
  of the row. The comment occupies about twice as much space as
  the quote. Otherwise, (if there is no quote) only the annotation
  comment
  is included as the "title" part of the row
- Non-CJK segments of the quote part of the annotation row are
  italicized. CJK segments are left as is. If CJK segments are present,
  there is more padding between quote and comment parts.
- Search matches the actual attachment instead of its parent file.
- Can create child notes from annotations of the same item or
  standalone notes from annotations across different items from
  the context menu or the header button.
- When an annotation (or multiple annotations) are selected, the
  annotationItemPane component is displayed where annotations are
  grouped by their top-level item. Annotations are displayed fully,
  without having their content cut off.
- Special treatment for annotations to always prompt
  to erase the item regardless of what collectionTree row
  is selected (e.g., if a collection is selected, we
  still want one to be able to delete the annotation).
  This only applies if all selected items are annotations.
  If multiple items are selected, some annotations and
  some not, do nothing. This is until the trash is
  ready. In the future, we may send annotations to trash
- strip all HTML tags from annotation for now, until the logic to
  properly render annotation markup is copied over from the reader
  (applies to both annotation-row component and the annotation
  item rendered in the itemTree)
- Added a generalized "expandToItem" function to itemTree to
  expand all ancestors of a given item, similar to "expandToCollection"
  from collectionTree

chrome/content/zotero/itemTree.jsx
@@ -268,8 +268,6 @@ var ItemTree = class ItemTree extends LibraryTree {
 					.concat(await this.collectionTreeRow.getTrashedCollections())
 					.concat(await Zotero.Searches.getDeleted(this.collectionTreeRow.ref.libraryID));
 			}
-			// TEMP: Hide annotations
-			newSearchItems = newSearchItems.filter(item => !item.isAnnotation());
 			// Remove notes and attachments if necessary
 			if (this.props.regularOnly) {
 				newSearchItems = newSearchItems.filter((item) => {
@@ -309,7 +307,8 @@ var ItemTree = class ItemTree extends LibraryTree {
 					if (row.ref instanceof Zotero.Item && row.ref.parentID) {
 						continue;
 					}
-					let isSearchParent = newSearchParentIDs.has(row.ref.treeViewID);
+					let attachments = row.ref.isRegularItem() ? row.ref.getAttachments() : [];
+					let isSearchParent = newSearchParentIDs.has(row.ref.treeViewID) || attachments.some(id => newSearchParentIDs.has(id));
 					// If not showing children or no children match the search, close
 					if (this.props.regularOnly || !isSearchParent) {
 						row.isOpen = false;
@@ -333,8 +332,10 @@ var ItemTree = class ItemTree extends LibraryTree {
 				else if (skipChildren) {
 					continue;
 				}
-				newRows.push(row);
-				allItemIDs.add(row.ref.treeViewID);
+				if (!allItemIDs.has(row.ref.id)) {
+					newRows.push(row);
+					allItemIDs.add(row.ref.treeViewID);
+				}
 			}
 			
 			// Add new items
@@ -348,6 +349,14 @@ var ItemTree = class ItemTree extends LibraryTree {
 						continue;
 					}
 					item = Zotero.Items.get(parentItemID);
+					// Go up one more level to check for parents of annotation rows
+					let parentsParent = item.parentItemID;
+					if (parentsParent) {
+						if (allItemIDs.has(parentsParent)) {
+							continue;
+						}
+						item = Zotero.Items.get(parentsParent);
+					}
 				}
 				// Parent item may have already been added from child
 				else if (allItemIDs.has(item.treeViewID)) {
@@ -356,9 +365,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 				
 				// Add new top-level items
 				let row = new ItemTreeRow(item, 0, false);
-				newRows.push(row);
-				allItemIDs.add(item.treeViewID);
-				addedItemIDs.add(item.treeViewID);
+				if (!allItemIDs.has(item.treeViewID)) {
+					newRows.push(row);
+					allItemIDs.add(item.treeViewID);
+					addedItemIDs.add(item.treeViewID);
+				}
 			}
 			
 			this._rows = newRows;
@@ -1231,15 +1242,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 				continue;
 			}
 			
-			// Get the row of the parent, if there is one
-			let parent = item.parentItemID;
-			let parentRow = parent && this._rowMap[parent];
-			
 			// If row with id isn't visible, check to see if it's hidden under a parent
 			if (row == undefined) {
-				if (!parent || parentRow === undefined) {
-					// No parent -- it's not here
-					
+				await this.expandToItem(id);
+				if (!this._rowMap[id]) {
+					// The row is still not found
 					// Clear the quick search and tag selection and try again (once)
 					if (!noRecurse && window.ZoteroPane) {
 						let cleared1 = await window.ZoteroPane.clearQuicksearch();
@@ -1253,13 +1260,6 @@ var ItemTree = class ItemTree extends LibraryTree {
 					Zotero.debug(`Couldn't find row for item ${id} -- not selecting`);
 					continue;
 				}
-				
-				// If parent is already open and we haven't found the item, the child
-				// hasn't yet been added to the view, so close parent to allow refresh
-				await this._closeContainer(parentRow);
-				
-				// Open the parent
-				await this.toggleOpenState(parentRow);
 			}
 			
 			// Since we're opening containers, we still need to reference by id
@@ -1276,7 +1276,6 @@ var ItemTree = class ItemTree extends LibraryTree {
 			}
 			rowsToSelect.push(row);
 		}
-		
 		if (!rowsToSelect.length) {
 			return 0;
 		}
@@ -1713,10 +1712,14 @@ var ItemTree = class ItemTree extends LibraryTree {
 
 		//Get children
 		var includeTrashed = this.collectionTreeRow.isTrash();
-		var attachments = item.getAttachments(includeTrashed);
-		var notes = item.getNotes(includeTrashed);
+		var attachments = item.isRegularItem() ? item.getAttachments(includeTrashed) : [];
+		var notes = item.isRegularItem() ? item.getNotes(includeTrashed) : [];
+		var annotations = [];
 
-		var newRows;
+		if (item.isFileAttachment()) {
+			annotations = item.getAnnotations();
+		}
+		var newRows = [];
 		if (attachments.length && notes.length) {
 			newRows = notes.concat(attachments);
 		}
@@ -1726,10 +1729,14 @@ var ItemTree = class ItemTree extends LibraryTree {
 		else if (notes.length) {
 			newRows = notes;
 		}
+		if (annotations.length) {
+			newRows = newRows.concat(annotations);
+		}
 
 		if (newRows) {
-			newRows = Zotero.Items.get(newRows);
-
+			if (!item.isFileAttachment()) {
+				newRows = Zotero.Items.get(newRows);
+			}
 			for (let i = 0; i < newRows.length; i++) {
 				count++;
 				this._addRow(
@@ -1766,8 +1773,15 @@ var ItemTree = class ItemTree extends LibraryTree {
 
 		var savedSelection = this.getSelectedObjects();
 		for (var i=0; i<this.rowCount; i++) {
-			var id = this.getRow(i).ref.id;
-			if (searchParentIDs.has(id) && this.isContainer(i) && !this.isContainerOpen(i)) {
+			if (!this.isContainer(i) || this.isContainerOpen(i)) {
+				continue;
+			}
+			let item = this.getRow(i).ref;
+			let attachments = item.isRegularItem() ? item.getAttachments() : [];
+			// expand item row if it is a parent of a match
+			// OR if it has a child that is a parent of a match
+			let shouldBeOpened = searchParentIDs.has(item.id) || attachments.some(id => searchParentIDs.has(id));
+			if (shouldBeOpened) {
 				this.toggleOpenState(i, true);
 			}
 		}
@@ -1885,7 +1899,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 
 			let collectionTreeRow = this.collectionTreeRow;
 
-			if (collectionTreeRow.isBucket()) {
+			// If all selected items are annotations, for now erase them skipping trash
+			if (selectedItems.length && selectedItems.every(item => item.isAnnotation())) {
+				await Zotero.Items.erase(selectedItemIDs);
+			}
+			else if (collectionTreeRow.isBucket()) {
 				collectionTreeRow.ref.deleteItems(ids);
 			}
 			else if (collectionTreeRow.isTrash()) {
@@ -2061,8 +2079,9 @@ var ItemTree = class ItemTree extends LibraryTree {
 	};
 	
 	isContainer = (index) => {
-		return this.getRow(index).ref.isRegularItem();
-	};
+		let item = this.getRow(index).ref;
+		return item.isRegularItem() || item.isFileAttachment();
+	}
 
 	isContainerOpen = (index) => {
 		return this.getRow(index).isOpen;
@@ -2074,11 +2093,46 @@ var ItemTree = class ItemTree extends LibraryTree {
 		}
 
 		var item = this.getRow(index).ref;
+		if (item.isFileAttachment()) {
+			return item.numAnnotations() == 0;
+		}
 		if (!item.isRegularItem()) {
 			return true;
 		}
 		var includeTrashed = this.collectionTreeRow.isTrash();
 		return item.numNotes(includeTrashed) === 0 && item.numAttachments(includeTrashed) == 0;
+	};
+
+	// Expand all ancestors of the specified item id
+	expandToItem = async (id) => {
+		let item = Zotero.Items.get(id);
+		// Stop if the row already exists of if the item is not found
+		if (this._rowMap[id] || !item) return;
+		let toExpand = [];
+		// Collect all ancestors of the item
+		while (item.parentItemID) {
+			item = Zotero.Items.get(item.parentItemID);
+			toExpand.push(item.id);
+		}
+		if (!this.getRow(this._rowMap[item.id])) return;
+		// Go through ancestors starting from the top-most one
+		// and expand them if needed
+		while (toExpand.length > 0) {
+			let ancestorID = toExpand.pop();
+			let ancestorRow = this._rowMap[ancestorID];
+
+			// If the row for the next ancestor already exists, just move one
+			if (toExpand.length > 0) {
+				let nextAncestorID = toExpand[toExpand.length - 1];
+				let nextAncestorRow = this._rowMap[nextAncestorID];
+				if (this.getRow(nextAncestorRow)) continue;
+			}
+			
+			// Close and re-open the ancestor to reveal the next row until
+			// we reach the desired item
+			await this._closeContainer(ancestorRow);
+			await this.toggleOpenState(ancestorRow);
+		}
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
@@ -2269,6 +2323,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 						return false;
 					}
 
+					// Disallow drag of annotation items
+					if (item.isAnnotation()) {
+						return false;
+					}
+
 					// Disallow cross-library child drag
 					if (item.libraryID != collectionTreeRow.ref.libraryID) {
 						return false;
@@ -2288,6 +2347,11 @@ var ItemTree = class ItemTree extends LibraryTree {
 				for (let item of items) {
 					// Don't allow drag if any top-level items
 					if (item.isTopLevelItem()) {
+						return false;
+					}
+
+					// Disallow drag of annotation items
+					if (item.isAnnotation()) {
 						return false;
 					}
 
@@ -3142,8 +3206,12 @@ var ItemTree = class ItemTree extends LibraryTree {
 				: acc;
 		}, { lowestOrdinal: Infinity, firstColumn: null });
 
+		let item = this.getRow(index).ref;
 		for (let column of columns) {
 			if (column.hidden) continue;
+			// Annotation rows have a single cell, created below
+			if (item.isAnnotation()) continue;
+			
 			div.appendChild(this._renderCell(index, rowData[column.dataKey], column, column === firstColumn));
 		}
 
@@ -3188,6 +3256,46 @@ var ItemTree = class ItemTree extends LibraryTree {
 			}
 		}
 
+		// Render annotation rows as a single cell with title/text and comment
+		div.classList.toggle("annotation-row", item.isAnnotation());
+		div.classList.remove("tight");
+		if (item.isAnnotation()) {
+			// Use "title" column  as a blueprint to render the first part of annotation row
+			let titleRowData = Object.assign({}, columns.find(column => column.dataKey == "title"));
+			titleRowData.className = "title";
+			let title;
+			// Strip html tags from annotation comment and text until the algorithm
+			// for safe rendering of relevant html tags is carried over from the reader
+			let parserUtils = Cc["@mozilla.org/parserutils;1"].getService(Ci.nsIParserUtils);
+			let plainText = parserUtils.convertToPlainText(item.annotationText || "", Ci.nsIDocumentEncoder.OutputRaw, 0);
+			let plainComment = parserUtils.convertToPlainText(item.annotationComment || "", Ci.nsIDocumentEncoder.OutputRaw, 0);
+			if (["highlight", "underline"].includes(item.annotationType)) {
+				title = this._renderCell(index, plainText, titleRowData, true);
+				let titleCell = title.querySelector(".cell-text");
+				// Quote text is in italics
+				titleCell.classList.add("italics");
+				// Add quotation marks around the quoted text
+				titleCell.setAttribute("q-mark-open", Zotero.getString("punctuation.openingQMark"));
+				title.setAttribute("q-mark-close", Zotero.getString("punctuation.closingQMark"));
+				if (item.annotationComment) {
+					let comment = renderCell(null, plainComment, { className: "annotation-comment" });
+					div.appendChild(comment);
+				}
+				// only keep default wider spacing if there are CJK characters
+				let containsCJK = /[\p{Script=Han}\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Hangul}]/u.test(item.annotationText);
+				div.classList.toggle("tight", !containsCJK);
+			}
+			else if (item.annotationComment) {
+				// If there is a comment for image, ink, note annotations, use that as the title
+				title = this._renderCell(index, plainComment, titleRowData, true);
+			}
+			else {
+				// Catch all - use annotation type as the title
+				let annotationTypeName = Zotero.getString(`pdfReader.${item.annotationType}Annotation`);
+				title = this._renderCell(index, annotationTypeName, titleRowData, true);
+			}
+			div.prepend(title);
+		}
 		return div;
 	};
 	
@@ -3682,6 +3790,7 @@ var ItemTree = class ItemTree extends LibraryTree {
 	_saveOpenState(close) {
 		if (!this.tree) return [];
 		var itemIDs = [];
+		var toClose = [];
 		if (close) {
 			if (!this.selection.selectEventsSuppressed) {
 				var unsuppress = this.selection.selectEventsSuppressed = true;
@@ -3689,13 +3798,18 @@ var ItemTree = class ItemTree extends LibraryTree {
 		}
 		for (var i=0; i<this._rows.length; i++) {
 			if (this.isContainer(i) && this.isContainerOpen(i)) {
-				itemIDs.push(this.getRow(i).ref.id);
-				if (close) {
-					this._closeContainer(i, true);
+				let row = this.getRow(i);
+				itemIDs.push(row.ref.id);
+				if (close && row.level == 0) {
+					toClose.push(this.getRow(i).ref.id);
 				}
 			}
 		}
 		if (close) {
+			for (i = toClose.length - 1; i >= 0; i--) {
+				let row = this._rowMap[toClose[i]];
+				this._closeContainer(row, true);
+			}
 			this._refreshRowMap();
 			if (unsuppress) {
 				this.selection.selectEventsSuppressed = false;
@@ -3704,13 +3818,17 @@ var ItemTree = class ItemTree extends LibraryTree {
 		return itemIDs;
 	}
 
-	_rememberOpenState(itemIDs) {
+	_rememberOpenState(itemIDs, secondLevel = false) {
 		if (!this.tree) return;
 		var rowsToOpen = [];
+		var nextLevelToOpen = [];
 		for (let id of itemIDs) {
 			var row = this._rowMap[id];
 			// Item may not still exist
 			if (row == undefined) {
+				if (!secondLevel) {
+					nextLevelToOpen.push(id);
+				}
 				continue;
 			}
 			rowsToOpen.push(row);
@@ -3727,6 +3845,10 @@ var ItemTree = class ItemTree extends LibraryTree {
 			this.toggleOpenState(rowsToOpen[i], true);
 		}
 		this._refreshRowMap();
+
+		if (nextLevelToOpen.length) {
+			this._rememberOpenState(nextLevelToOpen, true);
+		}
 		if (unsuppress) {
 			this.selection.selectEventsSuppressed = false;
 		}
@@ -3940,6 +4062,17 @@ var ItemTree = class ItemTree extends LibraryTree {
 			return icon;
 		}
 		
+		if (item.isAnnotation()) {
+			let img = document.createElement("img");
+			img.className = "annotation-icon";
+			let type = item.annotationType;
+			if (type == 'image') {
+				type = 'area';
+			}
+			img.src = 'chrome://zotero/skin/16/universal/annotate-' + type + '.svg';
+			img.style.fill = item.annotationColor;
+			return img;
+		}
 		var itemType = item.getItemTypeIconName();
 		return getCSSItemTypeIcon(itemType);
 	}

chrome/content/zotero/customElements.js
@@ -68,6 +68,7 @@ Services.scriptloader.loadSubScript('chrome://zotero/content/elements/itemTreeMe
 		['attachment-row', 'chrome://zotero/content/elements/attachmentRow.js'],
 		['attachment-annotations-box', 'chrome://zotero/content/elements/attachmentAnnotationsBox.js'],
 		['annotation-row', 'chrome://zotero/content/elements/annotationRow.js'],
+		['annotation-items-pane', 'chrome://zotero/content/elements/annotationItemsPane.js'],
 		['context-notes-list', 'chrome://zotero/content/elements/contextNotesList.js'],
 		['note-row', 'chrome://zotero/content/elements/noteRow.js'],
 		['notes-context', 'chrome://zotero/content/elements/notesContext.js'],

chrome/content/zotero/elements/annotationItemsPane.js
@@ -1,0 +1,134 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2024 Corporation for Digital Scholarship
+					 Vienna, Virginia, USA
+					 https://www.zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+{
+	class AnnotationItemsPane extends XULElementBase {
+		content = MozXULElement.parseXULToFragment(`
+			<html:div class="custom-head"></html:div>
+			<html:div class="body zotero-view-item"> </html:div>
+		`);
+
+		set items(items) {
+			if (items.some(item => !item.isAnnotation())) return;
+			this._items = items;
+		}
+
+		get items() {
+			return this._items || [];
+		}
+
+		init() {
+			this._body = this.querySelector('.body');
+			this._notifierID = Zotero.Notifier.registerObserver(this, ['item']);
+		}
+
+		destroy() {
+			Zotero.Notifier.unregisterObserver(this._notifierID);
+		}
+
+		notify(action, type, ids) {
+			if (action == 'modify') {
+				for (let id of ids) {
+					let updatedItem = Zotero.Items.get(id);
+					// If a selected annotation is renamed, re-render its annotation row
+					if (updatedItem.isAnnotation()) {
+						let row = this.querySelector(`annotation-row[annotation-id="${id}"]`);
+						if (row) {
+							row.render();
+						}
+					}
+					// Update name of collapsible section if item is renamed
+					else if (updatedItem.isRegularItem()) {
+						let section = this.querySelector(`collapsible-section[data-item-id="${id}"]`);
+						if (section) {
+							section.summary = updatedItem.getDisplayTitle();
+						}
+					}
+				}
+			}
+		}
+
+		render() {
+			if (!this.initialized) return;
+
+			let topLevelItems = Zotero.Items.getTopLevel(this.items);
+
+			// Remove collapsible sections for top-level items whose annotations are no longer selected
+			for (let section of [...this.querySelectorAll("collapsible-section")]) {
+				let parentID = section.dataset.pane.split("-")[1];
+				if (!topLevelItems.some(item => item.id == parentID)) {
+					section.remove();
+				}
+			}
+			for (let parentItem of topLevelItems) {
+				let selectedAnnotations = this.items.filter(item => item.topLevelItem.id == parentItem.id);
+				// Create a collapsible section for each top-level item if it does not exist yet
+				let section = this.querySelector(`[data-pane="annotations-${parentItem.id}"]`);
+				if (!section) {
+					section = document.createXULElement("collapsible-section");
+					section.dataset.l10nId = "section-attachments-annotations";
+					section.dataset.pane = `annotations-${parentItem.id}`;
+					section.summary = parentItem.getDisplayTitle();
+
+					let sectionBody = document.createElement("div");
+					sectionBody.classList.add("body");
+
+					section.appendChild(sectionBody);
+					this._body.append(section);
+				}
+				document.l10n.setArgs(section, { count: selectedAnnotations.length });
+				// Add annotations into this collapsible section
+				for (let annotation of selectedAnnotations) {
+					// Skip rows that already exist
+					if (this.querySelector(`annotation-row[annotation-id="${annotation.id}"]`)) continue;
+					let row = document.createXULElement('annotation-row');
+					row.annotation = annotation;
+					section.querySelector('.body').append(row);
+				}
+			}
+			// Remove annotation rows for annotations that are no longer selected
+			for (let row of [...this.querySelectorAll("annotation-row")]) {
+				let rowID = row.getAttribute("annotation-id");
+				if (!this.items.some(obj => obj.id == rowID)) {
+					row.remove();
+				}
+			}
+		}
+
+		renderCustomHead(callback) {
+			let customHead = this.querySelector(".custom-head");
+			customHead.replaceChildren();
+			let append = (...args) => {
+				customHead.append(...args);
+			};
+			if (callback) callback({
+				doc: document,
+				append,
+			});
+		}
+	}
+
+	customElements.define("annotation-items-pane", AnnotationItemsPane);
+}

chrome/content/zotero/elements/annotationRow.js
@@ -88,20 +88,34 @@
 					img.src = Zotero.File.pathToFileURI(imagePath);
 					img.draggable = false;
 					this._body.append(img);
+					// if the image could not be loaded for some reason (e.g. file is not there),
+					// show a placeholder text
+					img.addEventListener('error', () => {
+						let placeholder = document.createElement('div');
+						placeholder.classList.add('comment');
+						document.l10n.setAttributes(placeholder, 'annotation-image-not-available');
+						this._body.replaceChildren(placeholder);
+					});
 				}
 			}
 			
+			// Strip all html tags from comment and text for now until the algorithm for safe
+			// rendering of relevant html tags is carried over from the reader
+			let parserUtils = Cc["@mozilla.org/parserutils;1"].getService(Ci.nsIParserUtils);
+
 			if (this._annotation.annotationText) {
 				let text = document.createElement('div');
 				text.classList.add('quote');
-				text.textContent = this._annotation.annotationText;
+				let plainQuote = parserUtils.convertToPlainText(this._annotation.annotationText, Ci.nsIDocumentEncoder.OutputRaw, 0);
+				text.textContent = plainQuote;
 				this._body.append(text);
 			}
 			
 			if (this._annotation.annotationComment) {
 				let comment = document.createElement('div');
 				comment.classList.add('comment');
-				comment.textContent = this._annotation.annotationComment;
+				let plainComment = parserUtils.convertToPlainText(this._annotation.annotationComment, Ci.nsIDocumentEncoder.OutputRaw, 0);
+				comment.textContent = plainComment;
 				this._body.append(comment);
 			}
 			
@@ -110,6 +124,9 @@
 			this._tags.textContent = tags.map(tag => tag.tag).sort(Zotero.localeCompare).join(Zotero.getString('punctuation.comma') + ' ');
 			
 			this.style.setProperty('--annotation-color', this._annotation.annotationColor);
+			// A11y - make focusable + add screen reader's labels
+			this.setAttribute("tabindex", 0);
+			this.setAttribute("aria-label", this.annotation.getDisplayTitle());
 		}
 	}
 

chrome/content/zotero/elements/collapsibleSection.js
@@ -372,11 +372,11 @@
 		}
 		
 		get _disableCollapsing() {
-			return !!this.closest('panel, menupopup, merge-pane, scaffold-item-preview');
+			return !!this.closest('panel, menupopup, merge-pane, scaffold-item-preview, annotation-items-pane');
 		}
 
 		get _disableSavingOpenState() {
-			return !!this.closest('merge-pane, scaffold-item-preview');
+			return !!this.closest('merge-pane, scaffold-item-preview, annotation-items-pane');
 		}
 
 		_handleClick = (event) => {

chrome/content/zotero/elements/itemPane.js
@@ -36,6 +36,8 @@
 					previousfocus="zotero-items-tree" />
 				
 				<duplicates-merge-pane id="zotero-duplicates-merge-pane" />
+
+				<annotation-items-pane id="zotero-annotations-pane" />
 			</deck>
 			<item-pane-sidenav id="zotero-view-item-sidenav" class="zotero-view-item-sidenav"/>
 		`);
@@ -45,6 +47,7 @@
 			this._noteEditor = this.querySelector("#zotero-note-editor");
 			this._duplicatesPane = this.querySelector("#zotero-duplicates-merge-pane");
 			this._messagePane = this.querySelector("#zotero-item-message");
+			this._annotationsPane = this.querySelector("#zotero-annotations-pane");
 			this._sidenav = this.querySelector("#zotero-view-item-sidenav");
 			this._deck = this.querySelector("#zotero-item-pane-content");
 
@@ -108,6 +111,10 @@
 			if (!this.data) return false;
 			let hideSidenav = false;
 			let renderStatus = false;
+			// Only annotations selected
+			if (this.data.length > 0 && this.data.every(item => item.isAnnotation())) {
+				return renderStatus = this.renderAnnotations(this.data);
+			}
 			// Single item selected
 			if (this.data.length == 1) {
 				let item = this.data[0];
@@ -138,6 +145,14 @@
 					this.updateReadLabel();
 				}
 			}
+		}
+
+		renderAnnotations(annotations) {
+			this.mode = "annotations";
+			let annotationsViewer = document.getElementById("zotero-annotations-pane");
+			annotationsViewer.items = annotations;
+			annotationsViewer.render();
+			return true;
 		}
 
 		renderNoteEditor(item) {
@@ -318,6 +333,11 @@
 				this.updateReadLabel();
 				return;
 			}
+			// Create note from annotations button
+			if (this.data.every(item => item.isAnnotation())) {
+				container.renderCustomHead(this.renderAnnotationsHead.bind(this));
+				return;
+			}
 
 			container.renderCustomHead();
 		}
@@ -379,6 +399,21 @@
 			append(toggleReadButton, addToButton);
 
 			this.setTranslateButton();
+		}
+
+		renderAnnotationsHead(data) {
+			let { doc, append } = data;
+			let button = doc.createXULElement("button");
+			button.id = 'zotero-item-pane-note-from-annotations';
+			if (Zotero.Items.getTopLevel(this.data).length == 1) {
+				button.label = Zotero.getString('pane.items.menu.addNoteFromAnnotations');
+				button.addEventListener("command", () => ZoteroPane.addNoteFromAnnotationsFromSelected());
+			}
+			else {
+				button.label = Zotero.getString('pane.items.menu.createNoteFromAnnotations');
+				button.addEventListener("command", () => ZoteroPane.createStandaloneNoteFromAnnotationsFromSelected());
+			}
+			append(button);
 		}
 
 		updateReadLabel() {
@@ -493,8 +528,12 @@
 		getCurrentPane(mode = undefined) {
 			if (!mode) {
 				// Guess a mode from the current data
+				// Only annotation items selected
+				if (this.data.every(item => item.isAnnotation())) {
+					mode = "annotations";
+				}
 				// No/multiple objects are selected OR selected object is a trashed collection/search
-				if (!this.data.length || this.data.length > 1
+				else if (!this.data.length || this.data.length > 1
 					|| this.data[0] instanceof Zotero.Collection || this.data[0] instanceof Zotero.Search) {
 					mode = "message";
 				}
@@ -510,6 +549,7 @@
 				item: "_itemDetails",
 				note: "_noteEditor",
 				duplicates: "_duplicatesPane",
+				annotations: "_annotationsPane"
 			};
 			return this[map[mode]];
 		}
@@ -586,6 +626,10 @@
 					if (this.previousElementSibling.localName === "splitter") {
 						this.previousElementSibling.setAttribute("state", "open");
 					}
+					break;
+				}
+				case "annotations": {
+					this._deck.selectedIndex = 4;
 					break;
 				}
 			}

chrome/content/zotero/locateMenu.js
@@ -349,7 +349,7 @@ var Zotero_LocateMenu = new function() {
 		var selectedItems = [];
 		while (selectedItems.length < 50 && allSelectedItems.length) {
 			var item = allSelectedItems.shift();
-			if (!item.isNote()) selectedItems.push(item);
+			if (!item.isNote() && !item.isAnnotation()) selectedItems.push(item);
 		}
 		return selectedItems;
 	}

commit a7964f7116fc0a386710f51ad55805d4a5c54246

Fix duplicate attachment row after retrieving metadata
https://forums.zotero.org/discussion/123939/double-pdf-file-after-doing-a-retrieve-metadata-operation-on-a-standalone-pdf-file

Regression from 48fd23ccec73156855f4c59170b332af2e4357dd

chrome/content/zotero/itemTree.jsx
@@ -688,7 +688,7 @@ var ItemTree = class ItemTree extends LibraryTree {
 						let parentIndex = this.getParentIndex(row);
 
 						// Top-level item
-						if (this.isContainer(row)) {
+						if (item.isTopLevelItem()) {
 							// If Unfiled Items and itm was added to a collection, remove from view
 							if (collectionTreeRow.isUnfiled() && item.getCollections().length) {
 								this._removeRow(row);