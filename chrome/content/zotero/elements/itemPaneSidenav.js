zotero/zotero

commit 3102b6b67a3866514e062c653c4c4d7d03f4e1fb

Always show item/context pane toggle (#5263)

chrome/content/zotero/elements/itemPaneSidenav.js
@@ -28,6 +28,14 @@
 {
 	class ItemPaneSidenav extends XULElementBase {
 		content = MozXULElement.parseXULToFragment(`
+			<!-- Standard mode only: Toggle Item/Context Pane button -->
+			<toolbarbutton class="btn"
+				data-action="toggle-pane"
+				tabindex="0"
+			/>
+			
+			<html:div class="divider"/>
+			
 			<html:div class="inherit-flex highlight-notes-inactive" tabindex="0" role="tab" data-l10n-id="sidenav-main-btn-grouping">
 				<!-- Buttons will be added dynamically -->
 			</html:div>
@@ -119,7 +127,7 @@
 		}
 
 		get _collapsed() {
-			return this.container?._collapsed;
+			return !!this.container?._collapsed;
 		}
 
 		set _collapsed(val) {
@@ -215,6 +223,11 @@
 			for (let button of this.querySelectorAll('.btn[data-action]')) {
 				let action = button.dataset.action;
 				
+				if (action === 'toggle-pane') {
+					button.addEventListener('command', () => {
+						this._collapsed = !this._collapsed;
+					});
+				}
 				if (action === 'locate') {
 					button.addEventListener('mousedown', async (event) => {
 						if (event.button !== 0 || button.open) {
@@ -313,8 +326,14 @@
 			for (let button of this.querySelectorAll('.btn[data-action]')) {
 				let action = button.dataset.action;
 				
-				if (action == 'locate') {
+				if (action == 'toggle-pane' || action == 'locate') {
 					button.parentElement.hidden = false;
+				}
+				if (action == 'toggle-pane') {
+					document.l10n.setAttributes(button,
+						Zotero_Tabs.selectedType === 'library'
+							? 'toggle-item-pane'
+							: 'toggle-context-pane');
 				}
 			}
 			
@@ -601,7 +620,7 @@
 			return {
 				index,
 				position: index === 0 ? 0 : index * (btnSize + btnGap) + btnGap / 2
-			}
+			};
 		};
 
 		handleKeyDown = (event) => {

chrome/content/zotero/contextPane.js
@@ -57,11 +57,9 @@ var ZoteroContextPane = new function () {
 			_contextPaneInner.setAttribute('collapsed', !!collapsed);
 			_contextPaneSplitter.setAttribute('state', collapsed ? 'collapsed' : 'open');
 			_contextPaneSplitterStacked.setAttribute('state', collapsed ? 'collapsed' : 'open');
-			_update();
+			this.update();
 		}
 	});
-
-	this.update = _update;
 
 	this.focus = () => {
 		return _contextPaneInner.handleFocus();
@@ -70,10 +68,6 @@ var ZoteroContextPane = new function () {
 	this.showLoadingMessage = (isShow) => {
 		_loadingMessageContainer.classList.toggle('hidden', !isShow);
 	};
-
-	this.updateAddToNote = _updateAddToNote;
-
-	this.togglePane = _togglePane;
 
 	this.init = function () {
 		if (!Zotero) {
@@ -93,18 +87,18 @@ var ZoteroContextPane = new function () {
 
 		this.context = _contextPaneInner;
 
-		window.addEventListener('resize', _update);
-		Zotero.Reader.onChangeSidebarWidth = _updatePaneWidth;
-		Zotero.Reader.onToggleSidebar = _updatePaneWidth;
+		window.addEventListener('resize', this.update);
+		Zotero.Reader.onChangeSidebarWidth = this._updatePaneWidth;
+		Zotero.Reader.onToggleSidebar = this._updatePaneWidth;
 	};
 
 	this.destroy = function () {
-		window.removeEventListener('resize', _update);
+		window.removeEventListener('resize', this.update);
 		Zotero.Reader.onChangeSidebarWidth = () => {};
 		Zotero.Reader.onToggleSidebar = () => {};
 	};
 
-	function _updateAddToNote() {
+	this.updateAddToNote = () => {
 		let reader = Zotero.Reader.getByTabID(Zotero_Tabs.selectedID);
 		if (reader) {
 			let editor = ZoteroContextPane.activeEditor;
@@ -113,34 +107,28 @@ var ZoteroContextPane = new function () {
 				&& (editor.item.deleted || editor.item.parentItem && editor.item.parentItem.deleted);
 			reader.enableAddToNote(!!editor && !libraryReadOnly && !noteReadOnly);
 		}
-	}
+	};
 	
-	function _updatePaneWidth() {
+	this._updatePaneWidth = () => {
 		let stacked = _isStacked();
-		let width = Zotero.Reader.getSidebarWidth() + 'px';
-		if (!Zotero.Reader.getSidebarOpen()) {
-			width = 0;
-		}
+		let readerSidebarWidth = (Zotero.Reader.getSidebarOpen() ? Zotero.Reader.getSidebarWidth() : 0)
+			+ 'px';
 		let contextPaneWidth = _contextPane.getAttribute("width");
 		if (contextPaneWidth && !_contextPane.style.width) {
 			_contextPane.style.width = `${contextPaneWidth}px`;
 		}
 		if (Zotero.rtl) {
 			_contextPane.style.left = 0;
-			_contextPane.style.right = stacked ? width : 'unset';
+			_contextPane.style.right = stacked ? readerSidebarWidth : 'unset';
 		}
 		else {
-			_contextPane.style.left = stacked ? width : 'unset';
+			_contextPane.style.left = stacked ? readerSidebarWidth : 'unset';
 			_contextPane.style.right = 0;
 		}
-	}
+	};
 
-	function _isStacked() {
-		return Zotero.Prefs.get('layout') == 'stacked';
-	}
-
-	function _update() {
-		if (Zotero_Tabs.selectedIndex == 0) {
+	this.update = () => {
+		if (Zotero_Tabs.selectedType === 'library') {
 			return;
 		}
 		if (_isStacked()) {
@@ -157,6 +145,9 @@ var ZoteroContextPane = new function () {
 			// needed for standard layout
 			_contextPane.style.width = 'auto';
 			_contextPaneInner.style.removeProperty("min-height");
+
+			// Propagate state to standard splitter
+			_contextPaneSplitter.setAttribute('state', this.collapsed ? 'collapsed' : 'open');
 		}
 		else {
 			_contextPaneSplitter.setAttribute('hidden', false);
@@ -172,30 +163,37 @@ var ZoteroContextPane = new function () {
 			// force it to occupy all height available
 			_contextPaneInner.style.minHeight = `100%`;
 			_contextPane.style.width = `${_contextPane.getAttribute("width")}px`;
+
+			// Propagate state to stacked splitter
+			_contextPaneSplitterStacked.setAttribute('state', this.collapsed ? 'collapsed' : 'open');
 		}
 		
-		if (Zotero_Tabs.selectedIndex > 0) {
-			var height = null;
-			if (_isStacked()) {
-				height = 0;
-				if (_contextPane.getAttribute('collapsed') != 'true') {
-					height = _contextPaneInner.getBoundingClientRect().height;
-				}
+		Zotero.Reader.setContextPaneOpen(!this.collapsed);
+		
+		var height = null;
+		if (_isStacked()) {
+			height = 0;
+			if (_contextPane.getAttribute('collapsed') != 'true') {
+				height = _contextPaneInner.getBoundingClientRect().height;
 			}
-			Zotero.Reader.setBottomPlaceholderHeight(height);
 		}
+		Zotero.Reader.setBottomPlaceholderHeight(height);
 		
-		_updatePaneWidth();
-		_updateAddToNote();
+		this._updatePaneWidth();
+		this.updateAddToNote();
 
 		ZoteroPane.updateLayoutConstraints();
-	}
-	
-	function _isLibraryReadOnly(libraryID) {
-		return !Zotero.Libraries.get(libraryID).editable;
+	};
+
+	this.togglePane = () => {
+		this.collapsed = !this.collapsed;
+	};
+
+	function _isStacked() {
+		return Zotero.Prefs.get('layout') == 'stacked';
 	}
 
-	function _togglePane() {
-		this.collapsed = !this.collapsed;
+	function _isLibraryReadOnly(libraryID) {
+		return !Zotero.Libraries.get(libraryID).editable;
 	}
 };

chrome/content/zotero/elements/contextPane.js
@@ -25,6 +25,10 @@
 
 
 {
+	let { isPaneCollapsed, setPaneCollapsed } = ChromeUtils.importESModule(
+		'chrome://zotero/content/elements/utils/collapsiblePane.mjs'
+	);
+
 	class ContextPane extends XULElementBase {
 		content = MozXULElement.parseXULToFragment(`
 			<deck id="zotero-context-pane-deck" flex="1" selectedIndex="0">
@@ -61,6 +65,14 @@
 		get activeEditor() {
 			let currentContext = this._getCurrentNotesContext();
 			return currentContext?._getCurrentEditor();
+		}
+
+		get collapsed() {
+			return isPaneCollapsed(this);
+		}
+
+		set collapsed(val) {
+			setPaneCollapsed(this, val);
 		}
 
 		init() {
@@ -329,9 +341,7 @@
 		}
 	
 		handleFocus() {
-			let splitter = ZoteroContextPane.splitter;
-	
-			if (splitter.getAttribute('state') != 'collapsed') {
+			if (!this.collapsed) {
 				if (this.mode == "item") {
 					let header = this._itemPaneDeck.selectedPanel.querySelector("item-pane-header");
 					// Focus the first focusable node after header

chrome/content/zotero/elements/itemDetails.js
@@ -157,29 +157,30 @@
 			this._paneParent.style.setProperty('--min-scroll-height', val + 'px');
 		}
 
+		/**
+		 * Convenience getter that delegates to our <item-pane> / <context-pane>
+		 * parent. This exists because the sidenav controls us, not the <item-pane>,
+		 * but needs to get/set its collapsed state.
+		 * @returns {boolean}
+		 */
 		get _collapsed() {
-			let collapsible = this.closest('splitter:not([hidden="true"]) + *');
-			if (!collapsible) return false;
-			return collapsible.getAttribute('collapsed') === 'true';
+			let parentPane = this.closest('item-pane, context-pane');
+			if (!parentPane) {
+				return false;
+			}
+			return parentPane.collapsed;
 		}
 		
+		/**
+		 * Convenience setter for collapsed state. See above.
+		 * @param {boolean} val
+		 */
 		set _collapsed(val) {
-			let collapsible = this.closest('splitter:not([hidden="true"]) + *');
-			if (!collapsible) return;
-			let splitter = collapsible.previousElementSibling;
-			if (val) {
-				collapsible.setAttribute('collapsed', 'true');
-				collapsible.removeAttribute("width");
-				collapsible.removeAttribute("height");
-				splitter.setAttribute('state', 'collapsed');
-				splitter.setAttribute('substate', 'after');
+			let parentPane = this.closest('item-pane, context-pane');
+			if (!parentPane) {
+				return;
 			}
-			else {
-				collapsible.removeAttribute('collapsed');
-				splitter.setAttribute('state', '');
-				splitter.setAttribute('substate', 'after');
-			}
-			window.dispatchEvent(new Event('resize'));
+			parentPane.collapsed = val;
 		}
 
 		get sidenav() {

chrome/content/zotero/elements/itemPane.js
@@ -25,6 +25,10 @@
 
 
 {
+	let { isPaneCollapsed, setPaneCollapsed } = ChromeUtils.importESModule(
+		'chrome://zotero/content/elements/utils/collapsiblePane.mjs'
+	);
+	
 	class ItemPane extends XULElementBase {
 		content = MozXULElement.parseXULToFragment(`
 			<deck id="zotero-item-pane-content" class="zotero-item-pane-content" selectedIndex="0" flex="1">
@@ -107,9 +111,16 @@
 			this.setAttribute("view-type", type);
 		}
 
+		get collapsed() {
+			return isPaneCollapsed(this);
+		}
+
+		set collapsed(val) {
+			setPaneCollapsed(this, val);
+		}
+
 		render() {
 			if (!this.data) return false;
-			let hideSidenav = false;
 			let renderStatus = false;
 			// Only annotations selected
 			if (this.data.length > 0 && this.data.every(item => item.isAnnotation())) {
@@ -124,7 +135,6 @@
 					renderStatus = this.renderMessage();
 				}
 				else if (item.isNote()) {
-					hideSidenav = true;
 					renderStatus = this.renderNoteEditor(item);
 				}
 				else {
@@ -135,7 +145,6 @@
 			else {
 				renderStatus = this.renderMessage();
 			}
-			this._sidenav.hidden = hideSidenav;
 			return renderStatus;
 		}
 

chrome/content/zotero/elements/utils/collapsiblePane.mjs
@@ -1,0 +1,66 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2025 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+                     https://www.zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+/**
+ * Get the collapsed state of an <item-pane> or <context-pane>.
+ *
+ * @param {XULElement} pane
+ * @returns {boolean}
+ */
+export function isPaneCollapsed(pane) {
+	let collapsibleParent = pane.closest('splitter:not([hidden="true"]) + *');
+	if (collapsibleParent.previousElementSibling?.localName !== 'splitter') {
+		return false;
+	}
+	return collapsibleParent.getAttribute('collapsed') === 'true';
+}
+
+/**
+ * Set the collapsed state of an <item-pane> or <context-pane>.
+ *
+ * @param {XULElement} pane
+ * @param {boolean} collapsed
+ */
+export function setPaneCollapsed(pane, collapsed) {
+	let collapsibleParent = pane.closest('splitter:not([hidden="true"]) + *');
+	if (!collapsibleParent) {
+		return;
+	}
+	let splitter = collapsibleParent.previousElementSibling;
+
+	if (collapsed) {
+		collapsibleParent.setAttribute('collapsed', 'true');
+		collapsibleParent.removeAttribute('width');
+		collapsibleParent.removeAttribute('height');
+		splitter.setAttribute('state', 'collapsed');
+		splitter.setAttribute('substate', 'after');
+	}
+	else {
+		collapsibleParent.removeAttribute('collapsed');
+		splitter.setAttribute('state', '');
+		splitter.setAttribute('substate', 'after');
+	}
+	pane.ownerDocument.defaultView.dispatchEvent(new Event('resize'));
+}

chrome/content/zotero/xpcom/reader.js
@@ -201,6 +201,7 @@ class ReaderInstance {
 			sidebarWidth: this._sidebarWidth,
 			sidebarOpen: this._sidebarOpen,
 			bottomPlaceholderHeight: this._bottomPlaceholderHeight,
+			contextPaneOpen: this._contextPaneOpen,
 			rtl: Zotero.rtl,
 			fontSize: Zotero.Prefs.get('fontSize'),
 			localizedStrings: {
@@ -666,6 +667,11 @@ class ReaderInstance {
 	focusFirst() {
 		// this._postMessage({ action: 'focusFirst' });
 		setTimeout(() => this._iframeWindow.focus());
+	}
+
+	async setContextPaneOpen(open) {
+		await this._initPromise;
+		this._internalReader.setContextPaneOpen(open);
 	}
 
 	async setBottomPlaceholderHeight(height) {
@@ -1236,6 +1242,7 @@ class ReaderTab extends ReaderInstance {
 		super(options);
 		this._sidebarWidth = options.sidebarWidth;
 		this._sidebarOpen = options.sidebarOpen;
+		this._contextPaneOpen = options.bottomPlaceholderHeight;
 		this._bottomPlaceholderHeight = options.bottomPlaceholderHeight;
 		this._showContextPaneToggle = true;
 		this._onToggleSidebarCallback = options.onToggleSidebar;
@@ -1388,6 +1395,7 @@ class ReaderWindow extends ReaderInstance {
 		super(options);
 		this._sidebarWidth = options.sidebarWidth;
 		this._sidebarOpen = options.sidebarOpen;
+		this._contextPaneOpen = false;
 		this._bottomPlaceholderHeight = 0;
 		this._onClose = options.onClose;
 
@@ -1800,6 +1808,7 @@ class Reader {
 	constructor() {
 		this._sidebarWidth = 240;
 		this._sidebarOpen = false;
+		this._contextPaneOpen = false;
 		this._bottomPlaceholderHeight = 0;
 		this._readers = [];
 		this._notifierID = Zotero.Notifier.registerObserver(this, ['item', 'setting', 'tab'], 'reader');
@@ -1924,6 +1933,14 @@ class Reader {
 			reader.toggleSidebar(open);
 		}
 		this._setSidebarState();
+	}
+
+	setContextPaneOpen(open) {
+		this._contextPaneOpen = open;
+		let readers = this._readers.filter(r => r instanceof ReaderTab);
+		for (let reader of readers) {
+			reader.setContextPaneOpen(open);
+		}
 	}
 	
 	setBottomPlaceholderHeight(height) {
@@ -2107,6 +2124,7 @@ class Reader {
 				background: openInBackground,
 				sidebarWidth: this._sidebarWidth,
 				sidebarOpen: this._sidebarOpen,
+				contextPaneOpen: this._contextPaneOpen,
 				bottomPlaceholderHeight: this._bottomPlaceholderHeight,
 				preventJumpback: preventJumpback,
 				onToggleSidebar: (open) => {

chrome/content/zotero/zoteroPane.js
@@ -161,14 +161,14 @@ var ZoteroPane = new function()
 	};
 
 	function setUpKeyboardNavigation() {
-		let collectionTreeToolbar = this.document.getElementById("zotero-toolbar-collection-tree");
-		let itemTreeToolbar = this.document.getElementById("zotero-toolbar-item-tree");
-		let titleBar = this.document.getElementById("zotero-title-bar");
-		let itemTree = this.document.getElementById("zotero-items-tree");
-		let collectionsTree = this.document.getElementById("zotero-collections-tree");
-		let tagSelector = this.document.getElementById("zotero-tag-selector");
-		let tagContainer = this.document.getElementById('zotero-tag-selector-container');
-		let collectionsPane = this.document.getElementById("zotero-collections-pane");
+		let collectionTreeToolbar = document.getElementById("zotero-toolbar-collection-tree");
+		let itemTreeToolbar = document.getElementById("zotero-toolbar-item-tree");
+		let titleBar = document.getElementById("zotero-title-bar");
+		let itemTree = document.getElementById("zotero-items-tree");
+		let collectionsTree = document.getElementById("zotero-collections-tree");
+		let tagSelector = document.getElementById("zotero-tag-selector");
+		let tagContainer = document.getElementById('zotero-tag-selector-container');
+		let collectionsPane = document.getElementById("zotero-collections-pane");
 
 		// function to handle actual focusing based on a given event
 		// and a mapping of event targets + keys to the focus destinations
@@ -227,7 +227,8 @@ var ZoteroPane = new function()
 				// If desired target is hidden/disabled, create a fake event
 				// and dispatch it on the hidden target to rerun moveFocus
 				// and place focus on the next non-hidden node
-				if (target.disabled || target.hidden || target.parentNode.hidden) {
+				if (target.disabled || target.hidden || target.parentNode.hidden
+						|| getComputedStyle(target).display === 'none') {
 					event.target = target;
 					let fakeEventCopy = new KeyboardEvent('keydown', {
 						key: event.key,
@@ -401,18 +402,22 @@ var ZoteroPane = new function()
 					Tab: () => document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus(),
 					ShiftTab: () => document.getElementById('zotero-tb-collections-search').click()
 				},
-				'zotero-tb-search-textbox': {
-					ShiftTab: () => {
-						document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus();
-					},
-					Tab: () => itemTree.querySelector(".virtualized-table")
-				},
 				'zotero-tb-search-dropmarker': {
 					ArrowNext: () => null,
 					ArrowPrevious: () => null,
 					Tab: () => document.getElementById("zotero-tb-search-textbox"),
 					ShiftTab: () => document.getElementById('zotero-tb-add')
-				}
+				},
+				'zotero-tb-search-textbox': {
+					Tab: () => document.getElementById("zotero-tb-toggle-item-pane-stacked"),
+					ShiftTab: () => {
+						document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus();
+					}
+				},
+				'zotero-tb-toggle-item-pane-stacked': {
+					Tab: () => itemTree.querySelector(".virtualized-table"),
+					ShiftTab: () => document.getElementById("zotero-tb-search-textbox")
+				},
 			};
 			moveFocus(actionsMap, event, true);
 		});
@@ -441,7 +446,7 @@ var ZoteroPane = new function()
 		itemTree.addEventListener("keydown", (event) => {
 			let actionsMap = {
 				'item-tree-main-default': {
-					ShiftTab: () => document.getElementById('zotero-tb-search-textbox')
+					ShiftTab: () => document.getElementById('zotero-tb-toggle-item-pane-stacked')
 				}
 			};
 			moveFocus(actionsMap, event);
@@ -6856,8 +6861,14 @@ var ZoteroPane = new function()
 		this.handleTagSelectorResize();
 
 		this.itemPane.handleResize();
-	}
-
+	};
+	
+	
+	this.toggleItemPane = function () {
+		this.itemPane.collapsed = !this.itemPane.collapsed;
+		this.updateLayoutConstraints();
+	};
+	
 	
 	// Set the label of the dynamic tooltip. Can be used when we cannot set .tooltiptext
 	// property, e.g. if we don't want the tooltip to be announced by screenreaders.

commit 910c1122421ebcc48ffed95e0d2430ade25d4ee5

Fix sidenav drop indicator position
regression after #5263

chrome/content/zotero/elements/itemPaneSidenav.js
@@ -605,6 +605,9 @@
 		 * @property {number} position Position of the drop indicator
 		 */
 		computeDropPosition(offset) {
+			let isStacked = this.classList.contains("stacked");
+			let baseSize = isStacked ? 0 : 42;
+
 			// Keep in sync with _itemPaneSidenav.scss
 			let btnSize = 28;
 			let btnGap = 6;
@@ -619,7 +622,7 @@
 			}
 			return {
 				index,
-				position: index === 0 ? 0 : index * (btnSize + btnGap) + btnGap / 2
+				position: (index === 0 ? 0 : index * (btnSize + btnGap) + btnGap / 2) + baseSize
 			};
 		};
 