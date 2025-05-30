2a31018585def1856115366f33a009ee8afa294e
Bogdan Abaev <bogdan@zotero.org>
2025-05-30 15:53:26 -0700

    preference to hide context rows during search
    hideContextRows pref, accessible via View > Show Only Matching Items,
    will hide all context rows during search that do not also have
    matching child items.
    
    Fixes #5264

--- chrome/content/zotero/standalone/standalone.js
+++ chrome/content/zotero/standalone/standalone.js
@@ -492,6 +492,12 @@ const ZoteroStandalone = new function () {
 			Zotero.Prefs.get('recursiveCollections')
 		);
 
+		// Hide context rows
+		this.updateMenuItemCheckmark(
+			'view-menuitem-hide-context-rows',
+			Zotero.Prefs.get('hideContextRows')
+		);
+
 		this.onUpdateCustomMenus(event, 'view');
 	};
 
@@ -621,6 +627,10 @@ const ZoteroStandalone = new function () {
 			
 			case 'recursive-collections':
 				this.toggleBooleanPref('recursiveCollections');
+				break;
+
+			case 'hide-context-rows':
+				this.toggleBooleanPref('hideContextRows');
 				break;
 		}
 	};
