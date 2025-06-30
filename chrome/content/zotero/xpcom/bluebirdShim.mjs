1c4aae4842ec750229ac0fa68510f23cf82af04b
Abe Jellinek <jellinek@berkeley.edu>
2025-06-30 14:15:53 -0400

    Add Promise.defer() to Bluebird shim

--- chrome/content/zotero/xpcom/bluebirdShim.mjs
+++ chrome/content/zotero/xpcom/bluebirdShim.mjs
@@ -2,4 +2,13 @@ export default class BluebirdShimPromise extends Promise {
 	static delay(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
+	
+	static defer() {
+		let deferred = {};
+		deferred.promise = new Promise((resolve, reject) => {
+			deferred.resolve = resolve;
+			deferred.reject = reject;
+		});
+		return deferred;
+	}
 }
