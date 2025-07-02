587e4227d74283ca6e522acf77601fa18a27818c
Dan Stillman <dstillman@zotero.org>
2025-07-02 02:49:30 -0400

    Update Windows build to Firefox 140.0esr

--- app/config.sh
+++ app/config.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Version of Gecko to build with
 GECKO_VERSION_MAC="140.0esr"
 GECKO_VERSION_LINUX="140.0esr"
-GECKO_VERSION_WIN="128.6.0esr"
+GECKO_VERSION_WIN="140.0esr"
 RUST_VERSION=1.86.0
 
 # URL prefix for custom builds of Firefox components
