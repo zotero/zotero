135246eea7e2675545a050a5cb2601b39418c035
Tom Najdek <tom@doppnet.com>
2025-07-11 16:49:04 +0200

    Re-implement "Create Parent Dialog" without using React
    * Fixed an issue where the window size did not match the content
    * Replaced the semi-transparent progress bar with a spinner
    * Fixed a problem that allowed triggering a search while one was already in progress
    * Reduced code complexity

--- chrome/content/zotero/createParentDialog.js
+++ chrome/content/zotero/createParentDialog.js
@@ -1,90 +1,95 @@
 /*
-    ***** BEGIN LICENSE BLOCK *****
-
-    Copyright © 2009 Center for History and New Media
-                     George Mason University, Fairfax, Virginia, USA
-                     http://zotero.org
-
-    This file is part of Zotero.
-
-    Zotero is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Zotero is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-
-    ***** END LICENSE BLOCK *****
+	***** BEGIN LICENSE BLOCK *****
+    
+	Copyright © 2025 Corporation for Digital Scholarship
+					 Vienna, Virginia, USA
+					 https://www.zotero.org
+    
+	This file is part of Zotero.
+    
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+    
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+    
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+	***** END LICENSE BLOCK *****
 */
 
-"use strict";
+/* global Zotero_Lookup: false */
 
-import ReactDOM from "react-dom";
+const ZoteroCreateParentDialog = { // eslint-disable-line no-unused-vars
+	init() {
+		this.io = window.arguments[0];
 
-var io;
-let createParent;
-let root;
+		this.inputEl = document.getElementById('parent-item-identifier');
+		this.progressEl = document.getElementById('progress');
+		this.acceptBtnEl = document.querySelector('dialog').getButton("accept");
+		this.manualEntryBtnEl = document.querySelector('dialog').getButton("extra2");
 
-function toggleAccept(enabled) {
-	document.querySelector('dialog').getButton("accept").disabled = !enabled;
-}
+		// Set font size from pref
+		Zotero.UIProperties.registerRoot(
+			document.getElementById('zotero-create-parent-container')
+		);
 
-function doLoad() {
-	// Set font size from pref
-	let sbc = document.getElementById('zotero-create-parent-container');
-	Zotero.UIProperties.registerRoot(sbc);
+		this.inputEl.addEventListener('input', this.handleInput.bind(this));
+		document.addEventListener('dialogaccept', this.handleAcceptClick.bind(this));
+		document.addEventListener('dialogextra2', this.handleManualEntry.bind(this));
 
-	io = window.arguments[0];
+		document.getElementById('title').textContent = this.io.dataIn.item.attachmentFilename;
+		this.inputEl.focus();
+	},
 
-	createParent = document.getElementById('create-parent');
-	root = ReactDOM.createRoot(createParent);
-	Zotero.CreateParent.render(root, {
-		loading: false,
-		item: io.dataIn.item,
-		toggleAccept
-	});
+	async performLookup() {
+		let newItems = await Zotero_Lookup.addItemsFromIdentifier(
+			this.inputEl,
+			this.io.dataIn.item,
+			this.handleStatusChange.bind(this)
+		);
 
-	document.addEventListener('dialogaccept', (event) => {
-		doAccept();
-		event.preventDefault();
-	});
-	document.addEventListener('dialogextra2', doManualEntry);
-}
-
-function doUnload() {
-	root.unmount();
-}
-
-async function doAccept() {
-	let textBox = document.getElementById('parent-item-identifier');
-	let childItem = io.dataIn.item;
-	let newItems = await Zotero_Lookup.addItemsFromIdentifier(
-		textBox,
-		childItem,
-		(on) => {
-			// Render react again with correct loading value
-			Zotero.CreateParent.render(root, {
-				loading: on,
-				item: childItem,
-				toggleAccept
-			});
+		// If we successfully created a parent, return it
+		if (newItems.length) {
+			this.io.dataOut = { parent: newItems[0] };
+			window.close();
 		}
-	);
+	},
 
-	// If we successfully created a parent, return it
-	if (newItems.length) {
-		io.dataOut = { parent: newItems[0] };
+	handleInput(event) {
+		const input = event.target.value.trim();
+		this.acceptBtnEl.disabled = input === '';
+	},
+
+	handleStatusChange(isLookingUp)	{
+		this.inputEl.disabled = isLookingUp;
+		this.acceptBtnEl.disabled = isLookingUp;
+		this.manualEntryBtnEl.disabled = isLookingUp;
+		if (isLookingUp) {
+			this.progressEl.setAttribute("status", "animate");
+		}
+		else {
+			this.progressEl.removeAttribute("status");
+		}
+	},
+
+	handleAcceptClick(ev) {
+		ev.preventDefault();
+
+		if (this.inputEl.value.trim() === '') {
+			return;
+		}
+
+		this.performLookup();
+	},
+
+	handleManualEntry() {
+		this.io.dataOut = { parent: false };
 		window.close();
 	}
-}
-
-function doManualEntry() {
-	io.dataOut = { parent: false };
-	window.close();
-}
+};
