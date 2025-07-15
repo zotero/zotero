ba3a0376b73095bc59c28948311e22d7e597083e
Dan Stillman <dstillman@zotero.org>
2025-07-14 21:20:12 -0400

    Revert copyright block whitespace change

--- chrome/content/zotero/createParentDialog.js
+++ chrome/content/zotero/createParentDialog.js
@@ -1,26 +1,26 @@
 /*
-	***** BEGIN LICENSE BLOCK *****
-    
-	Copyright © 2025 Corporation for Digital Scholarship
-					 Vienna, Virginia, USA
-					 https://www.zotero.org
-    
-	This file is part of Zotero.
-    
-	Zotero is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-    
-	Zotero is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-    
-	You should have received a copy of the GNU Affero General Public License
-	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-    
-	***** END LICENSE BLOCK *****
+    ***** BEGIN LICENSE BLOCK *****
+
+    Copyright © 2009 Center for History and New Media
+                     George Mason University, Fairfax, Virginia, USA
+                     http://zotero.org
+
+    This file is part of Zotero.
+
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+
+    ***** END LICENSE BLOCK *****
 */
 
 /* global Zotero_Lookup: false */
