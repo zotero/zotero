71d308a13c328540388638f6e88c9267ee05c63d
Dan Stillman <dstillman@zotero.org>
2025-07-29 00:47:33 -0400

    BluebirdShimPromise: Add value() and fix isResolved() semantics
    - value() is used in noWait export translation
    
    - isResolved() technically means fulfilled or rejected -- unclear if we
      ever depended on that, but it might as well be correct

--- chrome/content/zotero/xpcom/bluebirdShim.mjs
+++ chrome/content/zotero/xpcom/bluebirdShim.mjs
@@ -20,16 +20,32 @@ export class BluebirdShimPromise extends Promise {
 				let returnValue = fn.apply(this, args);
 				let promise;
 				let isResolved = false;
+				let isFulfilled = false;
 				if (returnValue && returnValue.then) {
 					promise = returnValue;
 				}
 				else {
 					promise = Promise.resolve(returnValue);
 					isResolved = true;
+					isFulfilled = true;
 				}
 				if (typeof promise.isResolved === 'undefined') {
-					promise.then(() => isResolved = true, () => {});
+					promise.then(
+						() => {
+							isResolved = true;
+							isFulfilled = true;
+						},
+						() => isResolved = true
+					);
 					promise.isResolved = () => isResolved;
+				}
+				if (promise.value === undefined) {
+					promise.value = () => {
+						if (!isFulfilled) {
+							throw new Error("value() called on unfulfilled promise");
+						}
+						return returnValue;
+					};
 				}
 				return promise;
 			}
