5bbf4fb664fbb9c8d70c4dc56d346d34a8479344
abaevbog <bogdan@zotero.org>
2025-08-15 00:40:01 -0500

    avoid relation to self after merging related items (#5488)
    When copying over relations during merge of items
    that are related to one another, avoid relating the
    chosen main item to itself.
    
    Fixes: #5487

--- test/tests/duplicatesTest.js
+++ test/tests/duplicatesTest.js
@@ -81,5 +81,33 @@ describe("Duplicate Items", function () {
 			assert.isTrue(collection1.hasItem(item1.id));
 			assert.isTrue(collection2.hasItem(item1.id));
 		});
+
+		it("should not create a relation to self if related items are merged", async function () {
+			// Create 3 items related to each other
+			// item1 <-> item2, item2 <-> item3
+			var item1 = await createDataObject('item', { setTitle: true });
+			var item2 = item1.clone();
+			var item3 = item1.clone();
+			await item2.saveTx();
+			await item3.saveTx();
+
+			item1.addRelatedItem(item2);
+			item2.addRelatedItem(item1);
+			
+			item2.addRelatedItem(item3);
+			item3.addRelatedItem(item2);
+
+			await item1.saveTx();
+			await item2.saveTx();
+			await item3.saveTx();
+
+			// Merge all 3 items into item1
+			await merge(item1.id);
+			
+			// Item 1 should now be related to item2 and item3
+			assert.sameMembers(item1.relatedItems, [item2.key, item3.key]);
+			assert.sameMembers(item2.relatedItems, [item1.key]);
+			assert.sameMembers(item3.relatedItems, [item1.key]);
+		});
 	});
 });
