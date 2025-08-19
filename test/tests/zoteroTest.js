ba65b1dcc744acefa20f0ab78645884ec6e9932b
Abe Jellinek <jellinek@berkeley.edu>
2025-08-19 15:56:57 -0400

    Keep Zotero in the User-Agent, add Firefox

--- test/tests/zoteroTest.js
+++ test/tests/zoteroTest.js
@@ -12,18 +12,18 @@ describe("Zotero", function() {
 			it("should replace app name with Firefox", function () {
 				var platformVersion = Services.appinfo.platformVersion.match(/^\d+/)[0] + '.0';
 				var ua1 = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 ${Zotero.clientName}/${Zotero.version}`;
-				var ua2 = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/${platformVersion}`;
-				assert.equal(Zotero.VersionHeader.update(ua1, ZOTERO_CONFIG.CLIENT_NAME), ua2);
+				var ua2 = `${ua1} Firefox/${platformVersion}`;
+				assert.equal(Zotero.VersionHeader.update(ua1), ua2);
 			});
 			
 			it("should show Chrome user agent unchanged", function () {
 				var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36';
-				assert.equal(Zotero.VersionHeader.update(ua, ZOTERO_CONFIG.CLIENT_NAME), ua);
+				assert.equal(Zotero.VersionHeader.update(ua), ua);
 			});
 				
 			it("should show Firefox user agent unchanged", function () {
 				var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0';
-				assert.equal(Zotero.VersionHeader.update(ua, ZOTERO_CONFIG.CLIENT_NAME), ua);
+				assert.equal(Zotero.VersionHeader.update(ua), ua);
 			});
 		});
 	});
