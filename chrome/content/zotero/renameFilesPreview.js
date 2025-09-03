52b34c45bbe2c651ad16497388d916dbea377058
Tom Najdek <tom@doppnet.com>
2025-09-03 12:37:46 +0200

    Toggle `autoRenameFiles.done` when a dry run for file renaming finds no files to rename

--- chrome/content/zotero/renameFilesPreview.js
+++ chrome/content/zotero/renameFilesPreview.js
@@ -82,6 +82,8 @@ var RenameFilesPreview = { // eslint-disable-line no-unused-vars
 			this.introEl.dataset.l10nId = 'rename-files-preview-no-files';
 			this.cancelBtnEl.label = await document.l10n.formatValue('general-done');
 			this.acceptBtnEl.remove();
+			// There is nothing that would be renamed; ensure the “Rename Files” button is hidden in the preferences pane.
+			Zotero.Prefs.set('autoRenameFiles.done', true);
 		}
 		else {
 			this.rows = rows.flatMap(obj => [{ name: obj.oldName }, { name: obj.newName }]);
