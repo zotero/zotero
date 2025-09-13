0125de3d064388c342e493791301f9b3be53522a
Dan Stillman <dstillman@zotero.org>
2025-09-13 02:31:13 -0400

    Don't include "Settings…" link in file-renaming announcement banner

--- chrome/content/zotero/zoteroPane.js
+++ chrome/content/zotero/zoteroPane.js
@@ -6715,12 +6715,6 @@ var ZoteroPane = new function () {
 			Zotero.launchURL("https://www.zotero.org/support/file_renaming");
 		};
 		
-		document.getElementById('file-renaming-settings-link').onclick = () => {
-			Zotero.Utilities.Internal.openPreferences('zotero-prefpane-general', {
-				scrollTo: '#zotero-prefpane-file-renaming-groupbox'
-			});
-		};
-		
 		this.document.getElementById('file-renaming-banner-close').onclick = () => {
 			this.hideFileRenamingBanner();
 		};
