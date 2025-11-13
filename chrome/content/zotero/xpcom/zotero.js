f192c6d6222993d99d275fef6d51e2c834904ecd
Abe Jellinek <jellinek@berkeley.edu>
2025-11-13 12:12:56 -0500

    Don't prepend http:// to System Preferences URIs

--- chrome/content/zotero/xpcom/zotero.js
+++ chrome/content/zotero/xpcom/zotero.js
@@ -1116,7 +1116,9 @@ const { CommandLineOptions } = ChromeUtils.importESModule("chrome://zotero/conte
 	this.launchURL = function (url) {
 		if (!Zotero.Utilities.isHTTPURL(url)) {
 			if (Zotero.Utilities.isHTTPURL(url, true)) {
-				url = 'http://' + url;
+				if (!url.startsWith('x-apple.systempreferences:')) {
+					url = 'http://' + url;
+				}
 			}
 			// Launch non-HTTP URLs
 			else {
