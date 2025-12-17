86823e45efe673847807f348e968db2d8951bd24
Dan Stillman <dstillman@zotero.org>
2025-12-17 15:37:23 -0500

    Update to Firefox 140.6.0esr

--- app/config.sh
+++ app/config.sh
@@ -1,9 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Version of Gecko to build with
-GECKO_VERSION_MAC="140.3.0esr"
-GECKO_VERSION_LINUX="140.0esr"
-GECKO_VERSION_WIN="140.0esr"
+GECKO_VERSION_MAC="140.6.0esr"
+GECKO_VERSION_LINUX="140.6.0esr"
+GECKO_VERSION_WIN="140.6.0esr"
 RUST_VERSION=1.86.0
 
 # URL prefix for custom builds of Firefox components
