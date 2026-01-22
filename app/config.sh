5dcb1a4bbadb198b0c7b0465c4d8fe836144145e
Dan Stillman <dstillman@zotero.org>
2026-01-22 15:24:20 -0500

    Update Mac and Linux builds to Firefox 140.7.0esr

--- app/config.sh
+++ app/config.sh
@@ -1,8 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Version of Gecko to build with
-GECKO_VERSION_MAC="140.6.0esr"
-GECKO_VERSION_LINUX="140.6.0esr"
+GECKO_VERSION_MAC="140.7.0esr"
+GECKO_VERSION_LINUX="140.7.0esr"
 GECKO_VERSION_WIN="140.6.0esr"
 RUST_VERSION=1.86.0
 
