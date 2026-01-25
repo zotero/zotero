zotero/zotero

commit 48fd23ccec73156855f4c59170b332af2e4357dd

annotations showing in itemTree (#3416)
- Annotations are displayed in itemTree under their file attachments
  on a third level. The annotation spans the entire row.
- The title is constructed on the go. When possible, it
  includes annotation quote and comment as pseudo-columns
  of the row. The comment occupies about twice as much space as
  the quote. Otherwise, (if there is no quote) only the annotation
  comment
  is included as the "title" part of the row
- Non-CJK segments of the quote part of the annotation row are
  italicized. CJK segments are left as is. If CJK segments are present,
  there is more padding between quote and comment parts.
- Search matches the actual attachment instead of its parent file.
- Can create child notes from annotations of the same item or
  standalone notes from annotations across different items from
  the context menu or the header button.
- When an annotation (or multiple annotations) are selected, the
  annotationItemPane component is displayed where annotations are
  grouped by their top-level item. Annotations are displayed fully,
  without having their content cut off.
- Special treatment for annotations to always prompt
  to erase the item regardless of what collectionTree row
  is selected (e.g., if a collection is selected, we
  still want one to be able to delete the annotation).
  This only applies if all selected items are annotations.
  If multiple items are selected, some annotations and
  some not, do nothing. This is until the trash is
  ready. In the future, we may send annotations to trash
- strip all HTML tags from annotation for now, until the logic to
  properly render annotation markup is copied over from the reader
  (applies to both annotation-row component and the annotation
  item rendered in the itemTree)
- Added a generalized "expandToItem" function to itemTree to
  expand all ancestors of a given item, similar to "expandToCollection"
  from collectionTree

chrome/content/zotero/xpcom/data/items.js
@@ -254,11 +254,14 @@ Zotero.Items = function() {
 				item.updateDisplayTitle()
 			}
 			catch (e) {
-				// A few item types need creators to be loaded. Instead of making
-				// updateDisplayTitle() async and loading conditionally, just catch the error
+				// A few item types need creators or tags to be loaded. Annotations need to be loaded
+				// to be displayed in itemTree.
+				// Instead of making updateDisplayTitle() async and loading conditionally, just catch the error
 				// and load on demand
 				if (e instanceof Zotero.Exception.UnloadedDataException) {
 					yield item.loadDataType('creators');
+					yield item.loadDataType('annotation');
+					yield item.loadDataType('tags');
 					item.updateDisplayTitle()
 				}
 				else {

chrome/content/zotero/customElements.js
@@ -68,6 +68,7 @@ Services.scriptloader.loadSubScript('chrome://zotero/content/elements/itemTreeMe
 		['attachment-row', 'chrome://zotero/content/elements/attachmentRow.js'],
 		['attachment-annotations-box', 'chrome://zotero/content/elements/attachmentAnnotationsBox.js'],
 		['annotation-row', 'chrome://zotero/content/elements/annotationRow.js'],
+		['annotation-items-pane', 'chrome://zotero/content/elements/annotationItemsPane.js'],
 		['context-notes-list', 'chrome://zotero/content/elements/contextNotesList.js'],
 		['note-row', 'chrome://zotero/content/elements/noteRow.js'],
 		['notes-context', 'chrome://zotero/content/elements/notesContext.js'],

chrome/content/zotero/elements/annotationItemsPane.js
@@ -1,0 +1,134 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2024 Corporation for Digital Scholarship
+					 Vienna, Virginia, USA
+					 https://www.zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+{
+	class AnnotationItemsPane extends XULElementBase {
+		content = MozXULElement.parseXULToFragment(`
+			<html:div class="custom-head"></html:div>
+			<html:div class="body zotero-view-item"> </html:div>
+		`);
+
+		set items(items) {
+			if (items.some(item => !item.isAnnotation())) return;
+			this._items = items;
+		}
+
+		get items() {
+			return this._items || [];
+		}
+
+		init() {
+			this._body = this.querySelector('.body');
+			this._notifierID = Zotero.Notifier.registerObserver(this, ['item']);
+		}
+
+		destroy() {
+			Zotero.Notifier.unregisterObserver(this._notifierID);
+		}
+
+		notify(action, type, ids) {
+			if (action == 'modify') {
+				for (let id of ids) {
+					let updatedItem = Zotero.Items.get(id);
+					// If a selected annotation is renamed, re-render its annotation row
+					if (updatedItem.isAnnotation()) {
+						let row = this.querySelector(`annotation-row[annotation-id="${id}"]`);
+						if (row) {
+							row.render();
+						}
+					}
+					// Update name of collapsible section if item is renamed
+					else if (updatedItem.isRegularItem()) {
+						let section = this.querySelector(`collapsible-section[data-item-id="${id}"]`);
+						if (section) {
+							section.summary = updatedItem.getDisplayTitle();
+						}
+					}
+				}
+			}
+		}
+
+		render() {
+			if (!this.initialized) return;
+
+			let topLevelItems = Zotero.Items.getTopLevel(this.items);
+
+			// Remove collapsible sections for top-level items whose annotations are no longer selected
+			for (let section of [...this.querySelectorAll("collapsible-section")]) {
+				let parentID = section.dataset.pane.split("-")[1];
+				if (!topLevelItems.some(item => item.id == parentID)) {
+					section.remove();
+				}
+			}
+			for (let parentItem of topLevelItems) {
+				let selectedAnnotations = this.items.filter(item => item.topLevelItem.id == parentItem.id);
+				// Create a collapsible section for each top-level item if it does not exist yet
+				let section = this.querySelector(`[data-pane="annotations-${parentItem.id}"]`);
+				if (!section) {
+					section = document.createXULElement("collapsible-section");
+					section.dataset.l10nId = "section-attachments-annotations";
+					section.dataset.pane = `annotations-${parentItem.id}`;
+					section.summary = parentItem.getDisplayTitle();
+
+					let sectionBody = document.createElement("div");
+					sectionBody.classList.add("body");
+
+					section.appendChild(sectionBody);
+					this._body.append(section);
+				}
+				document.l10n.setArgs(section, { count: selectedAnnotations.length });
+				// Add annotations into this collapsible section
+				for (let annotation of selectedAnnotations) {
+					// Skip rows that already exist
+					if (this.querySelector(`annotation-row[annotation-id="${annotation.id}"]`)) continue;
+					let row = document.createXULElement('annotation-row');
+					row.annotation = annotation;
+					section.querySelector('.body').append(row);
+				}
+			}
+			// Remove annotation rows for annotations that are no longer selected
+			for (let row of [...this.querySelectorAll("annotation-row")]) {
+				let rowID = row.getAttribute("annotation-id");
+				if (!this.items.some(obj => obj.id == rowID)) {
+					row.remove();
+				}
+			}
+		}
+
+		renderCustomHead(callback) {
+			let customHead = this.querySelector(".custom-head");
+			customHead.replaceChildren();
+			let append = (...args) => {
+				customHead.append(...args);
+			};
+			if (callback) callback({
+				doc: document,
+				append,
+			});
+		}
+	}
+
+	customElements.define("annotation-items-pane", AnnotationItemsPane);
+}

chrome/content/zotero/elements/annotationRow.js
@@ -88,20 +88,34 @@
 					img.src = Zotero.File.pathToFileURI(imagePath);
 					img.draggable = false;
 					this._body.append(img);
+					// if the image could not be loaded for some reason (e.g. file is not there),
+					// show a placeholder text
+					img.addEventListener('error', () => {
+						let placeholder = document.createElement('div');
+						placeholder.classList.add('comment');
+						document.l10n.setAttributes(placeholder, 'annotation-image-not-available');
+						this._body.replaceChildren(placeholder);
+					});
 				}
 			}
 			
+			// Strip all html tags from comment and text for now until the algorithm for safe
+			// rendering of relevant html tags is carried over from the reader
+			let parserUtils = Cc["@mozilla.org/parserutils;1"].getService(Ci.nsIParserUtils);
+
 			if (this._annotation.annotationText) {
 				let text = document.createElement('div');
 				text.classList.add('quote');
-				text.textContent = this._annotation.annotationText;
+				let plainQuote = parserUtils.convertToPlainText(this._annotation.annotationText, Ci.nsIDocumentEncoder.OutputRaw, 0);
+				text.textContent = plainQuote;
 				this._body.append(text);
 			}
 			
 			if (this._annotation.annotationComment) {
 				let comment = document.createElement('div');
 				comment.classList.add('comment');
-				comment.textContent = this._annotation.annotationComment;
+				let plainComment = parserUtils.convertToPlainText(this._annotation.annotationComment, Ci.nsIDocumentEncoder.OutputRaw, 0);
+				comment.textContent = plainComment;
 				this._body.append(comment);
 			}
 			
@@ -110,6 +124,9 @@
 			this._tags.textContent = tags.map(tag => tag.tag).sort(Zotero.localeCompare).join(Zotero.getString('punctuation.comma') + ' ');
 			
 			this.style.setProperty('--annotation-color', this._annotation.annotationColor);
+			// A11y - make focusable + add screen reader's labels
+			this.setAttribute("tabindex", 0);
+			this.setAttribute("aria-label", this.annotation.getDisplayTitle());
 		}
 	}
 

chrome/content/zotero/elements/collapsibleSection.js
@@ -372,11 +372,11 @@
 		}
 		
 		get _disableCollapsing() {
-			return !!this.closest('panel, menupopup, merge-pane, scaffold-item-preview');
+			return !!this.closest('panel, menupopup, merge-pane, scaffold-item-preview, annotation-items-pane');
 		}
 
 		get _disableSavingOpenState() {
-			return !!this.closest('merge-pane, scaffold-item-preview');
+			return !!this.closest('merge-pane, scaffold-item-preview, annotation-items-pane');
 		}
 
 		_handleClick = (event) => {

chrome/content/zotero/elements/itemPane.js
@@ -36,6 +36,8 @@
 					previousfocus="zotero-items-tree" />
 				
 				<duplicates-merge-pane id="zotero-duplicates-merge-pane" />
+
+				<annotation-items-pane id="zotero-annotations-pane" />
 			</deck>
 			<item-pane-sidenav id="zotero-view-item-sidenav" class="zotero-view-item-sidenav"/>
 		`);
@@ -45,6 +47,7 @@
 			this._noteEditor = this.querySelector("#zotero-note-editor");
 			this._duplicatesPane = this.querySelector("#zotero-duplicates-merge-pane");
 			this._messagePane = this.querySelector("#zotero-item-message");
+			this._annotationsPane = this.querySelector("#zotero-annotations-pane");
 			this._sidenav = this.querySelector("#zotero-view-item-sidenav");
 			this._deck = this.querySelector("#zotero-item-pane-content");
 
@@ -108,6 +111,10 @@
 			if (!this.data) return false;
 			let hideSidenav = false;
 			let renderStatus = false;
+			// Only annotations selected
+			if (this.data.length > 0 && this.data.every(item => item.isAnnotation())) {
+				return renderStatus = this.renderAnnotations(this.data);
+			}
 			// Single item selected
 			if (this.data.length == 1) {
 				let item = this.data[0];
@@ -138,6 +145,14 @@
 					this.updateReadLabel();
 				}
 			}
+		}
+
+		renderAnnotations(annotations) {
+			this.mode = "annotations";
+			let annotationsViewer = document.getElementById("zotero-annotations-pane");
+			annotationsViewer.items = annotations;
+			annotationsViewer.render();
+			return true;
 		}
 
 		renderNoteEditor(item) {
@@ -318,6 +333,11 @@
 				this.updateReadLabel();
 				return;
 			}
+			// Create note from annotations button
+			if (this.data.every(item => item.isAnnotation())) {
+				container.renderCustomHead(this.renderAnnotationsHead.bind(this));
+				return;
+			}
 
 			container.renderCustomHead();
 		}
@@ -379,6 +399,21 @@
 			append(toggleReadButton, addToButton);
 
 			this.setTranslateButton();
+		}
+
+		renderAnnotationsHead(data) {
+			let { doc, append } = data;
+			let button = doc.createXULElement("button");
+			button.id = 'zotero-item-pane-note-from-annotations';
+			if (Zotero.Items.getTopLevel(this.data).length == 1) {
+				button.label = Zotero.getString('pane.items.menu.addNoteFromAnnotations');
+				button.addEventListener("command", () => ZoteroPane.addNoteFromAnnotationsFromSelected());
+			}
+			else {
+				button.label = Zotero.getString('pane.items.menu.createNoteFromAnnotations');
+				button.addEventListener("command", () => ZoteroPane.createStandaloneNoteFromAnnotationsFromSelected());
+			}
+			append(button);
 		}
 
 		updateReadLabel() {
@@ -493,8 +528,12 @@
 		getCurrentPane(mode = undefined) {
 			if (!mode) {
 				// Guess a mode from the current data
+				// Only annotation items selected
+				if (this.data.every(item => item.isAnnotation())) {
+					mode = "annotations";
+				}
 				// No/multiple objects are selected OR selected object is a trashed collection/search
-				if (!this.data.length || this.data.length > 1
+				else if (!this.data.length || this.data.length > 1
 					|| this.data[0] instanceof Zotero.Collection || this.data[0] instanceof Zotero.Search) {
 					mode = "message";
 				}
@@ -510,6 +549,7 @@
 				item: "_itemDetails",
 				note: "_noteEditor",
 				duplicates: "_duplicatesPane",
+				annotations: "_annotationsPane"
 			};
 			return this[map[mode]];
 		}
@@ -586,6 +626,10 @@
 					if (this.previousElementSibling.localName === "splitter") {
 						this.previousElementSibling.setAttribute("state", "open");
 					}
+					break;
+				}
+				case "annotations": {
+					this._deck.selectedIndex = 4;
 					break;
 				}
 			}

commit 2ffd6a893ba8c868f3cf0636f5e11a34b26b7947

Don't load annotations or tags for items individually at startup
Load tags and annotations before itemData, since `_loadItemData()`
calls `updateDisplayTitle()`, which needs creators, tags, and
annotations to be loaded for some item types.

This should fix slow startup times in Zotero 8 in libraries with many
(e.g., tens of thousands) of annotations.

The slowdown here was introduced by #3416 loading annotations in the
`catch`, but it was already incorrect for tags to be loaded that way at
startup. Creators were loaded before itemData, but tags weren't.

The `catch` might not be necessary at all now that the loading order for
`loadAllData()` is fixed, but we'll need to test further to confirm
that.

Fixes #5724

chrome/content/zotero/xpcom/data/item.js
@@ -101,14 +101,16 @@ Zotero.defineProperty(Zotero.Item.prototype, 'ContainerObjectsClass', {
 });
 
 Zotero.Item.prototype._dataTypes = Zotero.Item._super.prototype._dataTypes.concat([
+	// Load creators/tags/annotations before itemData. _loadItemData() updates the cached display
+	// title, which needs that data for some item types.
 	'creators',
+	'tags',
+	'annotation',
 	'itemData',
 	'note',
-	'annotation',
 	'annotationDeferred',
 	'childItems',
 //	'relatedItems', // TODO: remove
-	'tags',
 	'collections',
 	'relations'
 ]);

chrome/content/zotero/xpcom/data/items.js
@@ -259,6 +259,7 @@ Zotero.Items = function () {
 				// Instead of making updateDisplayTitle() async and loading conditionally, just catch the error
 				// and load on demand
 				if (e instanceof Zotero.Exception.UnloadedDataException) {
+					Zotero.debug(`Unloaded data for item ${item.libraryKey} updating display title`);
 					if (item.isRegularItem()) {
 						await item.loadDataType('creators');
 						await item.loadDataType('tags');