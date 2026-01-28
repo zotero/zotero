zotero/zotero

commit 37ce3c0ed7570bcfc6640dd9f14a3e5f471e8ab1

Fix note editor toolbar window dragging

chrome/content/zotero/elements/noteEditor.js
@@ -88,6 +88,7 @@
 				this._initialized = true;
 			});
 			this.append(content);
+			this._iframe.docShell.windowDraggingAllowed = true;
 
 			this._notifierID = Zotero.Notifier.registerObserver(this, ['item', 'file'], 'noteEditor');
 			this.notitle = !!this.getAttribute('notitle');

commit aef4beee89bf25551caf35436cd67604aa17c667

Fix note editor iframe docShell undefined error
Regression after 37ce3c0

chrome/content/zotero/elements/noteEditor.js
@@ -89,10 +89,10 @@
 						'zotero/item': event.dataTransfer.getData('zotero/item')
 					}, this._iframe.contentWindow);
 				}, true);
+				this._iframe.docShell.windowDraggingAllowed = true;
 				this._initialized = true;
 			});
 			this.append(content);
-			this._iframe.docShell.windowDraggingAllowed = true;
 
 			this._notifierID = Zotero.Notifier.registerObserver(this, ['item', 'file'], 'noteEditor');
 			this.notitle = !!this.getAttribute('notitle');