zotero/zotero

commit 00527332c4e3ec9ddc80a2de8bc962bf7a678b40

Move Advanced Search and saved search editing to the main window (#5658)
---------

Co-authored-by: Dan Stillman <dstillman@zotero.org>

chrome/content/zotero/elements/advancedSearchPane.js
@@ -1,0 +1,209 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2025 Corporation for Digital Scholarship
+					 Vienna, Virginia, USA
+					 https://www.zotero.org
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+"use strict";
+
+{
+	class AdvancedSearchPane extends XULElementBase {
+		content = MozXULElement.parseXULToFragment(`
+			<hbox class="saved-search-name-row">
+				<label control="saved-search-name" data-l10n-id="new-collection-name"/>
+				<html:input type="text" id="saved-search-name"/>
+			</hbox>
+			<zoterosearch/>
+			<hbox class="advanced-search-buttons">
+				<button class="cancel-button" data-l10n-id="cancel-button"/>
+				<button class="search-button" data-l10n-id="search-button" default="true"/>
+				<button class="clear-button" data-l10n-id="clear-button"/>
+				<button class="save-button" data-l10n-id="save-search-button"/>
+			</hbox>
+		`);
+
+		_active = false;
+
+		init() {
+			this._nameField = this.querySelector('#saved-search-name');
+			this._searchElem = this.querySelector('zoterosearch');
+			this._cancelButton = this.querySelector('.cancel-button');
+			this._searchButton = this.querySelector('.search-button');
+			this._clearButton = this.querySelector('.clear-button');
+			this._saveButton = this.querySelector('.save-button');
+
+			this._searchElem.addEventListener('input', () => this._searchElem.updateSearch());
+			this._searchElem.addEventListener('command', () => this._searchElem.updateSearch());
+			
+			this._cancelButton.addEventListener('command', () => this.cancel());
+			this._searchButton.addEventListener('command', () => this.submit());
+			this._clearButton.addEventListener('command', () => this.clear());
+			this._saveButton.addEventListener('command', () => this.save());
+			
+			if (!['temporary', 'saved'].includes(this.type)) {
+				throw new Error(`Invalid type: ${this.type}`);
+			}
+			
+			if (this.type === 'saved') {
+				this._saveButton.setAttribute('default', 'true');
+			}
+
+			this.addEventListener('keydown', this._handleKeyDown);
+		}
+
+		_handleKeyDown = async (event) => {
+			this._searchElem.updateSearch();
+			if (event.key === 'Enter') {
+				if (this.type === 'temporary') {
+					await this.submit();
+				}
+				else {
+					await this.save();
+				}
+			}
+		};
+
+		/**
+		 * @returns {'temporary' | 'saved'}
+		 */
+		get type() {
+			return this.getAttribute('type');
+		}
+		
+		get search() {
+			return this._search;
+		}
+		
+		/**
+		 * Whether the search has been submitted and should filter the items list
+		 */
+		get active() {
+			return this._active;
+		}
+		
+		set search(search) {
+			this._active = false;
+			if (this.type === 'saved') {
+				if (!search?.id) {
+					throw new Error('Cannot edit unsaved search');
+				}
+				this._searchID = search.id;
+				this._search = search.clone();
+				this._nameField.value = search.name;
+			}
+			else if (search) {
+				this._search = search.clone();
+			}
+			else {
+				this._search = new Zotero.Search();
+				this._search.addCondition('title', 'contains', '');
+			}
+			this._searchElem.search = this._search;
+		}
+		
+		_ensureSearch() {
+			if (!this._search && this.type === 'temporary') {
+				this.search = null;
+			}
+		}
+
+		refresh() {
+			this._ensureSearch();
+			let libraryID = ZoteroPane.getSelectedLibraryID();
+			// Keep the previous library when the selected row doesn't have one (e.g., Feeds)
+			if (libraryID) {
+				this._search.libraryID = libraryID;
+			}
+			this._searchElem.search = this._search;
+			// There would be no way to scope a saved search to anything but the library root,
+			// so disable the option to save.
+			// Somewhat unfortunate - revisit when/if we support nested condition sets in the UI.
+			this._saveButton.disabled = this.type === 'temporary' && !ZoteroPane.getCollectionTreeRow().isLibrary();
+		}
+
+		async cancel() {
+			await ZoteroPane.setSavedSearchEditorState('closed');
+		}
+
+		async submit() {
+			if (this.type === 'saved') {
+				throw new Error('submit() is unsupported for saved search');
+			}
+			
+			this._searchElem.updateSearch();
+			this._active = true;
+			await ZoteroPane.itemsView.setFilter('advanced-search', this._search);
+			ZoteroPane.itemsView.focus();
+		}
+		
+		async clear() {
+			if (this.type === 'saved') {
+				throw new Error('clear() is unsupported for saved search');
+			}
+
+			this.search = null;
+			await ZoteroPane.itemsView.setFilter('advanced-search', null);
+		}
+
+		async save() {
+			this._searchElem.updateSearch();
+			
+			if (this.type === 'saved') {
+				let search = Zotero.Searches.get(this._searchID);
+				if (!search) {
+					throw new Error('Missing search');
+				}
+				search.fromJSON(this._search.toJSON());
+				search.name = this._nameField.value;
+				await search.saveTx();
+				await ZoteroPane.setSavedSearchEditorState('closed');
+				Zotero_Tabs.rename('zotero-pane', search.name);
+				return;
+			}
+
+			let collectionTreeRow = ZoteroPane.getCollectionTreeRow();
+			if (!collectionTreeRow.isLibrary()) {
+				throw new Error('Can only save in library root');
+			}
+			this._ensureSearch();
+			
+			let libraryID = collectionTreeRow.ref.libraryID;
+			let searches = await Zotero.Searches.getAll(libraryID);
+			let prefix = Zotero.getString('pane.collections.untitled');
+			let name = Zotero.Utilities.Internal.getNextName(
+				prefix,
+				searches.map(s => s.name).filter(n => n.startsWith(prefix))
+			);
+			
+			let search = this._search.clone(libraryID);
+			search.name = name;
+			await search.saveTx();
+
+			await ZoteroPane.setAdvancedSearchState('closed');
+		}
+		
+		focus(options) {
+			this._searchElem.querySelector('#conditionsmenu').focus(options);
+		}
+	}
+	customElements.define("advanced-search-pane", AdvancedSearchPane);
+}

chrome/content/zotero/zoteroPane.js
@@ -364,7 +364,7 @@ var ZoteroPane = new function () {
 				'zotero-tb-add': {
 					ArrowNext: () => document.getElementById("zotero-tb-lookup"),
 					ArrowPrevious: () => null,
-					Tab: () => document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus(),
+					Tab: () => document.getElementById("zotero-tb-search").focus(),
 					ShiftTab: () => {
 						if (collectionsPane.getAttribute("collapsed")) {
 							return document.getElementById('zotero-tb-sync');
@@ -378,7 +378,7 @@ var ZoteroPane = new function () {
 				'zotero-tb-lookup': {
 					ArrowNext: () => document.getElementById("zotero-tb-attachment-add"),
 					ArrowPrevious: () => document.getElementById("zotero-tb-add"),
-					Tab: () => document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus(),
+					Tab: () => document.getElementById("zotero-tb-search").focus(),
 					ShiftTab: () => document.getElementById('zotero-tb-collections-search').click(),
 					Enter: () => Zotero_Lookup.showPanel(event.target),
 					' ': () => Zotero_Lookup.showPanel(event.target)
@@ -386,13 +386,13 @@ var ZoteroPane = new function () {
 				'zotero-tb-attachment-add': {
 					ArrowNext: () => document.getElementById("zotero-tb-note-add"),
 					ArrowPrevious: () => document.getElementById("zotero-tb-lookup"),
-					Tab: () => document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus(),
+					Tab: () => document.getElementById("zotero-tb-search").focus(),
 					ShiftTab: () => document.getElementById('zotero-tb-collections-search').click()
 				},
 				'zotero-tb-note-add': {
 					ArrowNext: () => null,
 					ArrowPrevious: () => document.getElementById("zotero-tb-attachment-add"),
-					Tab: () => document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus(),
+					Tab: () => document.getElementById("zotero-tb-search").focus(),
 					ShiftTab: () => document.getElementById('zotero-tb-collections-search').click()
 				},
 				'zotero-tb-search-dropmarker': {
@@ -403,9 +403,7 @@ var ZoteroPane = new function () {
 				},
 				'zotero-tb-search-textbox': {
 					Tab: () => document.getElementById("zotero-tb-toggle-item-pane-stacked"),
-					ShiftTab: () => {
-						document.getElementById("zotero-tb-search")._searchModePopup.flattenedTreeParentNode.focus();
-					}
+					ShiftTab: () => document.getElementById("zotero-tb-search").focus()
 				},
 				'zotero-tb-toggle-item-pane-stacked': {
 					Tab: () => itemTree.querySelector(".virtualized-table"),
@@ -1554,58 +1552,8 @@ var ZoteroPane = new function () {
 		this.loadURI(Zotero.Groups.addGroupURL);
 	}
 	
-	
-	this.newSearch = async function () {
-		if (Zotero.DB.inTransaction()) {
-			await Zotero.DB.waitForTransaction();
-		}
-		
-		var libraryID = this.getSelectedLibraryID();
-		
-		var s = new Zotero.Search();
-		s.libraryID = libraryID;
-		s.addCondition('title', 'contains', '');
-		
-		var searches = await Zotero.Searches.getAll(libraryID)
-		var prefix = Zotero.getString('pane.collections.untitled');
-		var name = Zotero.Utilities.Internal.getNextName(
-			prefix,
-			searches.map(s => s.name).filter(n => n.startsWith(prefix))
-		);
-		
-		var io = { dataIn: { search: s, name }, dataOut: null };
-		window.openDialog('chrome://zotero/content/searchDialog.xhtml','','chrome,modal,centerscreen',io);
-		if (!io.dataOut) {
-			return false;
-		}
-		s.fromJSON(io.dataOut.json);
-		await s.saveTx();
-		return s.id;
-	};
-	
 	this.setVirtual = function (libraryID, type, show, select) {
 		return this.collectionsView.toggleVirtualCollection(libraryID, type, show, select);
-	};
-	
-	this.openAdvancedSearchWindow = function () {
-		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-					.getService(Components.interfaces.nsIWindowMediator);
-		var enumerator = wm.getEnumerator('zotero:search');
-		while (enumerator.hasMoreElements()) {
-			var win = enumerator.getNext();
-		}
-		
-		if (win) {
-			win.focus();
-			return;
-		}
-		
-		var s = new Zotero.Search();
-		s.libraryID = this.getSelectedLibraryID();
-		s.addCondition('title', 'contains', '');
-		
-		var io = {dataIn: {search: s}, dataOut: null};
-		window.openDialog('chrome://zotero/content/advancedSearch.xhtml', '', 'chrome,dialog=no,centerscreen', io);
 	};
 
 	this.initItemsTree = async function () {
@@ -1688,7 +1636,7 @@ var ZoteroPane = new function () {
 			this.tagSelector.handleResize();
 		}
 		if (this.collectionsView) {
-			this.collectionsView.updateHeight();
+			this.collectionsView.updateHeightDebounced();
 		}
 	}, 100);
 	
@@ -1756,7 +1704,6 @@ var ZoteroPane = new function () {
 		}
 	};
 	
-	
 	this.onCollectionSelected = Zotero.serial(async function () {
 		var collectionTreeRow = this.getCollectionTreeRow();
 		if (!collectionTreeRow) {
@@ -1772,13 +1719,43 @@ var ZoteroPane = new function () {
 			return;
 		}
 		
+		let advancedSearchDeck = document.getElementById('zotero-advanced-search-pane-deck');
+		if (this.itemsView.collectionTreeRow?.isSearch()
+				&& advancedSearchDeck.state === 'open'
+				&& advancedSearchDeck.selectedSearchType === 'saved') {
+			let result = Services.prompt.confirmEx(window,
+				Zotero.getString('saved-search-close-confirmation-title'),
+				Zotero.getString('saved-search-close-confirmation-body'),
+				Ci.nsIPromptService.BUTTON_POS_0_DEFAULT
+					| Ci.nsIPrompt.BUTTON_TITLE_SAVE * Ci.nsIPrompt.BUTTON_POS_0
+					| Ci.nsIPrompt.BUTTON_TITLE_CANCEL * Ci.nsIPrompt.BUTTON_POS_1
+					| Ci.nsIPrompt.BUTTON_TITLE_DONT_SAVE * Ci.nsIPrompt.BUTTON_POS_2,
+				null, null, null,
+				null, {});
+			switch (result) {
+				case 0:
+					await advancedSearchDeck.pane.save();
+					return;
+				case 1:
+					this.collectionsView.selection.selectEventsSuppressed = true;
+					try {
+						await this.collectionsView.selectByID(this.itemsView.collectionTreeRow.id);
+					}
+					finally {
+						this.collectionsView.selection.selectEventsSuppressed = false;
+					}
+					return;
+				case 2:
+					await advancedSearchDeck.pane.cancel();
+					break;
+			}
+		}
+		
 		// Rename tab
 		Zotero_Tabs.rename('zotero-pane', collectionTreeRow.getName());
 		
-		let type = Zotero.Libraries.get(collectionTreeRow.ref.libraryID).libraryType;
-		
 		// Clear quick search and tag selector when switching views
-		document.getElementById('zotero-tb-search-textbox').value = "";
+		document.getElementById('zotero-tb-search').onCollectionSelected();
 		if (ZoteroPane.tagSelector) {
 			ZoteroPane.tagSelector.clearTagSelection();
 		}
@@ -1788,6 +1765,7 @@ var ZoteroPane = new function () {
 			collectionTreeRow.setTags(ZoteroPane.tagSelector.getTagSelection());
 		}
 		
+		this._refreshAdvancedSearchPane(collectionTreeRow);
 		this._updateEnabledActionsForRow(collectionTreeRow);
 
 		// If item data not yet loaded for library, load it now.
@@ -1815,6 +1793,125 @@ var ZoteroPane = new function () {
 		
 		Zotero.Prefs.set('lastViewedFolder', collectionTreeRow.id);
 	});
+
+
+	/**
+	 * @param {Zotero.CollectionTreeRow} [collectionTreeRow] - During collection selection,
+	 *     the newly selected row, which isn't in the items view yet
+	 */
+	this._refreshAdvancedSearchPane = function (collectionTreeRow) {
+		let deck = document.getElementById('zotero-advanced-search-pane-deck');
+
+		deck.pane.refresh();
+
+		let search = deck.state === 'closed' || deck.selectedSearchType !== 'temporary' || !deck.pane.active
+			? null
+			: deck.pane.search;
+		if (collectionTreeRow) {
+			collectionTreeRow.setAdvancedSearch(search);
+			return undefined;
+		}
+		// Apply via the items view, whose row can be a different object from the
+		// collection tree's current row
+		return this.itemsView.setFilter('advanced-search', search);
+	};
+
+
+	/**
+	 * @param {'open' | 'collapsed' | 'closed'} state
+	 */
+	this.setAdvancedSearchState = async function (state) {
+		let deck = document.getElementById('zotero-advanced-search-pane-deck');
+		let oldState = deck.state;
+		deck.selectedSearchType = 'temporary';
+		deck.state = state;
+		
+		let advancedSearchPane = deck.pane;
+
+		document.getElementById('zotero-tb-search').updateMode();
+		let refreshPromise;
+		if (state === 'open' && oldState === 'collapsed'
+				|| state === 'collapsed' && oldState === 'open') {
+			// State change only causes visual refresh - update the tree height
+			this.itemsView.updateHeight();
+		}
+		else {
+			// State change changes displayed items - refresh the tree
+			refreshPromise = this._refreshAdvancedSearchPane();
+		}
+		
+		// Update the pane state synchronously, so that a state change initiated
+		// while the refresh below is pending doesn't see a stale search
+		if (state === 'closed') {
+			advancedSearchPane.search = null;
+		}
+		else if (state === 'open') {
+			Zotero_Tabs.select('zotero-pane');
+			advancedSearchPane.focus();
+		}
+		
+		await refreshPromise;
+	};
+	
+	
+	/**
+	 * @param {'open' | 'collapsed' | 'closed'} state
+	 */
+	this.toggleAdvancedSearchState = async function (state) {
+		let deck = document.getElementById('zotero-advanced-search-pane-deck');
+		if (state === deck.state && deck.selectedSearchType !== 'saved') {
+			// If we're trying to open the pane, and it's already open but not focused,
+			// focus it
+			if (state === 'open' && !deck.pane.matches(':focus-within')) {
+				deck.pane.focus();
+				return;
+			}
+			
+			// Flip the state
+			switch (state) {
+				case 'open':
+					state = 'closed';
+					break;
+				case 'collapsed':
+				case 'closed':
+					state = 'open';
+					break;
+			}
+		}
+		await this.setAdvancedSearchState(state);
+	};
+	
+	
+	/**
+	 * @param {'open' | 'closed'} state
+	 */
+	this.setSavedSearchEditorState = async function (state) {
+		let collectionTreeRow = this.getCollectionTreeRow();
+		if (state === 'open' && !collectionTreeRow.isSearch()) {
+			throw new Error('Cannot show saved search editor outside search row');
+		}
+		
+		let deck = document.getElementById('zotero-advanced-search-pane-deck');
+		deck.selectedSearchType = 'saved';
+		deck.state = state;
+		
+		if (state === 'open') {
+			deck.pane.search = collectionTreeRow.ref;
+		}
+
+		document.getElementById('zotero-tb-search').updateMode();
+		let refreshPromise = this._refreshAdvancedSearchPane();
+		if (state === 'open') {
+			deck.pane.focus();
+		}
+		await refreshPromise;
+	};
+
+
+	this.openAdvancedSearchWindow = function () {
+		Zotero.debug(`ZoteroPane.openAdvancedSearchWindow() is deprecated -- use ZoteroPane.toggleAdvancedSearchState() instead`);
+		this.toggleAdvancedSearchState('open');
+	};
 	
 	
 	/**
@@ -1828,7 +1925,6 @@ var ZoteroPane = new function () {
 			"menu_noteAdd",
 			
 			"cmd_zotero_newCollection",
-			"cmd_zotero_newSavedSearch",
 			"cmd_zotero_import",
 			"cmd_zotero_importFromClipboard",
 			
@@ -2580,28 +2676,7 @@ var ZoteroPane = new function () {
 				this.collectionsView.startEditing(row);
 			}
 			else {
-				let s = row.ref.clone();
-				let groups = [];
-				// Promises don't work in the modal dialog, so get the group name here, if
-				// applicable, and pass it in. We only need the group that this search belongs
-				// to, if any, since the library drop-down is disabled for saved searches.
-				if (Zotero.Libraries.get(s.libraryID).libraryType == 'group') {
-					groups.push(Zotero.Groups.getByLibraryID(s.libraryID));
-				}
-				var io = {
-					dataIn: {
-						search: s,
-						name: row.getName(),
-						groups: groups
-					},
-					dataOut: null
-				};
-				window.openDialog('chrome://zotero/content/searchDialog.xhtml','','chrome,modal,centerscreen',io);
-				if (io.dataOut) {
-					row.ref.fromJSON(io.dataOut.json);
-					await row.ref.saveTx();
-					Zotero_Tabs.rename("zotero-pane", row.ref.name);
-				}
+				this.setSavedSearchEditorState('open');
 			}
 		}
 	};
@@ -3275,10 +3350,6 @@ var ZoteroPane = new function () {
 			command: "cmd_zotero_newCollection"
 		},
 		{
-			id: "newSavedSearch",
-			command: "cmd_zotero_newSavedSearch"
-		},
-		{
 			id: "newSubcollection",
 			oncommand: () => {
 				this.newCollection(this.getSelectedCollection().key);
@@ -3564,8 +3635,7 @@ var ZoteroPane = new function () {
 				show.push(
 					'sync',
 					'sep1',
-					'newCollection',
-					'newSavedSearch'
+					'newCollection'
 				);
 			}
 				// Only show "Show Duplicates", "Show Unfiled Items", and "Show Retracted" if rows are hidden
@@ -6784,7 +6854,7 @@ var ZoteroPane = new function () {
 		if (ZoteroPane.itemsView) {
 			// Need to immediately rerender the items here without any debouncing
 			// since tree height will have changed
-			ZoteroPane.itemsView._updateHeight();
+			ZoteroPane.itemsView.updateHeight();
 		}
 		ZoteroContextPane.update();
 		Zotero_Tabs.updateSidebarLayout();
@@ -6970,7 +7040,7 @@ var ZoteroPane = new function () {
 		var collectionsPaneWidth = collectionsPane.getBoundingClientRect().width;
 		tagSelector.style.maxWidth = collectionsPaneWidth + 'px';
 		if (ZoteroPane.itemsView) {
-			ZoteroPane.itemsView.updateHeight();
+			ZoteroPane.itemsView.updateHeightDebounced();
 		}
 
 		this.handleTagSelectorResize();

chrome/content/zotero/advancedSearch.js
@@ -1,194 +1,0 @@
-/*
-    ***** BEGIN LICENSE BLOCK *****
-    
-    Copyright © 2009 Center for History and New Media
-                     George Mason University, Fairfax, Virginia, USA
-                     http://zotero.org
-    
-    This file is part of Zotero.
-    
-    Zotero is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    
-    Zotero is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-    
-    You should have received a copy of the GNU Affero General Public License
-    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-    
-    ***** END LICENSE BLOCK *****
-*/
-
-
-import CollectionViewItemTree from 'zotero/collectionViewItemTree';
-import { COLUMNS } from 'zotero/itemTreeColumns';
-
-
-var ZoteroAdvancedSearch = new function () {
-	this.onLoad = onLoad;
-	this.search = search;
-	this.clear = clear;
-	this.onItemActivate = onItemActivate;
-	
-	this.itemsView = false;
-	this._loadedDeferred = Zotero.Promise.defer();
-
-	var _searchBox;
-	var _libraryID;
-	var _searchCounter = 0;
-	
-	async function onLoad() {
-		_searchBox = document.getElementById('zotero-search-box');
-		
-		// Set font size from pref
-		var sbc = document.getElementById('zotero-search-box-container');
-		Zotero.UIProperties.registerRoot(sbc);
-		
-		_searchBox.onLibraryChange = this.onLibraryChange;
-		var io = window.arguments[0];
-		
-		io.dataIn.search.loadPrimaryData()
-		.then(function () {
-			_searchBox.search = io.dataIn.search;
-		});
-		
-		var elem = document.getElementById('zotero-items-tree');
-		const columns = COLUMNS.map((column) => {
-			column = Object.assign({}, column);
-			column.hidden = !['title', 'firstCreator', 'year', 'hasAttachment'].includes(column.dataKey);
-			return column;
-		});
-		this.itemsView = await CollectionViewItemTree.init(elem, {
-			id: "advanced-search",
-			dragAndDrop: true,
-			columnPicker: true,
-			onActivate: this.onItemActivate.bind(this),
-			columns,
-		});
-
-		await this.itemsView.changeCollectionTreeRow({
-			id: 'advanced-search-' + _searchCounter++,
-			ref: _searchBox.search,
-			visibilityGroup: 'default',
-			isSearchMode: () => true,
-			getItems: async () => [],
-			isLibrary: () => false,
-			isCollection: () => false,
-			isSearch: () => true,
-			isPublications: () => false,
-			isDuplicates: () => false,
-			isFeed: () => false,
-			isFeeds: () => false,
-			isFeedsOrFeed: () => false,
-			isRecentlyRead: () => false,
-			isSortable: () => true,
-			isShare: () => false,
-			isTrash: () => false,
-			isSearch: () => true
-		});
-
-		// Focus the first field in the window
-		Services.focus.moveFocus(window, null, Services.focus.MOVEFOCUS_FORWARD, 0);
-		this._loadedDeferred.resolve();
-	}
-	
-	this.onUnload = function () {
-		this.itemsView.unregister();
-	}
-	
-	function search() {
-		_searchBox.updateSearch();
-		_searchBox.active = true;
-		
-		return this.itemsView.changeCollectionTreeRow({
-			id: 'advanced-search-' + _searchCounter++,
-			ref: _searchBox.search,
-			visibilityGroup: 'default',
-			isSearchMode: () => true,
-			isSearch: () => true,
-			getItems: async function () {
-				await Zotero.Libraries.get(_libraryID).waitForDataLoad('item');
-
-				var search = _searchBox.search.clone();
-				search.libraryID = _libraryID;
-				var ids = await search.search();
-				return Zotero.Items.get(ids);
-			}
-		});
-	}
-	
-	
-	function clear() {
-		this.itemsView.changeCollectionTreeRow(null);
-		
-		var s = new Zotero.Search();
-		// Don't clear the selected library
-		s.libraryID = _searchBox.search.libraryID;
-		s.addCondition('title', 'contains', '');
-		_searchBox.search = s;
-		_searchBox.active = false;
-	}
-	
-	
-	this.save = async function () {
-		_searchBox.updateSearch();
-		
-		var promptService = Services.prompt;
-		
-		var libraryID = _searchBox.search.libraryID;
-		
-		var searches = await Zotero.Searches.getAll(libraryID);
-		var prefix = Zotero.getString('pane.collections.untitled');
-		var name = Zotero.Utilities.Internal.getNextName(
-			prefix,
-			searches.map(s => s.name).filter(n => n.startsWith(prefix))
-		);
-		
-		name = { value: name };
-		var result = promptService.prompt(window,
-			Zotero.getString('pane.collections.newSavedSeach'),
-			Zotero.getString('pane.collections.savedSearchName'), name, "", {});
-		
-		if (!result) {
-			return;
-		}
-		
-		if (!name.value) {
-			name.value = 'untitled';
-		}
-		
-		var s = _searchBox.search.clone();
-		s.name = name.value;
-		await s.saveTx();
-		
-		window.close();
-	};
-	
-	
-	this.onLibraryChange = function (libraryID) {
-		_libraryID = libraryID;
-		var library = Zotero.Libraries.get(libraryID);
-		var isEditable = library.editable && library.libraryType != 'publications';
-		document.getElementById('zotero-search-save').disabled = !isEditable;
-	}
-	
-	
-	function onItemActivate(event, items)
-	{
-		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-					   .getService(Components.interfaces.nsIWindowMediator);
-		
-		var lastWin = wm.getMostRecentWindow("navigator:browser");
-		
-		if (!lastWin) {
-			return;
-		}
-		
-		lastWin.ZoteroPane.selectItems(items.map(item => item.id));
-		lastWin.focus();
-	}
-}

chrome/content/zotero/advancedSearch.xhtml
@@ -1,58 +1,0 @@
-<?xml version="1.0"?>
-<?xml-stylesheet href="chrome://global/skin/"?>
-<?xml-stylesheet href="chrome://zotero/skin/zotero.css" type="text/css"?>
-<?xml-stylesheet href="chrome://zotero/skin/overlay.css" type="text/css"?>
-<?xml-stylesheet href="chrome://zotero-platform/content/overlay.css"?>
-<?xml-stylesheet href="chrome://zotero-platform/content/zotero.css"?>
-
-<!DOCTYPE window [
-	<!ENTITY % zoteroDTD SYSTEM "chrome://zotero/locale/zotero.dtd">
-	%zoteroDTD;
-	<!ENTITY % searchboxDTD SYSTEM "chrome://zotero/locale/searchbox.dtd">
-	%searchboxDTD;
-]>
-
-<window
-	id="zotero-advanced-search-dialog"
-	title="&zotero.toolbar.advancedSearch;"
-	orient="vertical"
-	persist="screenX screenY width height"
-	onload="ZoteroAdvancedSearch.onLoad()"
-	onunload="ZoteroAdvancedSearch.onUnload();"
-	xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
-	xmlns:html="http://www.w3.org/1999/xhtml"
-	windowtype="zotero:search"
-	style="display: flex;">
-
-	<script src="include.js"/>
-	<script>
-	</script>
-	<script src="customElements.js"/>
-	<script src="advancedSearch.js"/>
-
-	<popupset>
-		<panel is="autocomplete-richlistbox-popup"
-			id="search-autocomplete-popup"
-			type="autocomplete-richlistbox"
-			noautofocus="true"/>
-	</popupset>
-	
-	<vbox id="zotero-search-box-container" flex="1">
-		<vbox id="zotero-search-box-controls">
-			<zoterosearch id="zotero-search-box" oncommand="if (this.active) { ZoteroAdvancedSearch.search(); }" flex="1"/>
-			
-			<hbox id="zotero-search-buttons">
-				<button label="&zotero.search.search;" default="true" oncommand="ZoteroAdvancedSearch.search()"/>
-				<button label="&zotero.search.clear;" oncommand="ZoteroAdvancedSearch.clear()"/>
-				<button id="zotero-search-save" label="&zotero.search.saveSearch;" oncommand="ZoteroAdvancedSearch.save()"/>
-			</hbox>
-		</vbox>
-		<hbox class="virtualized-table-container" flex="1">
-			<html:div id="zotero-items-tree"/>
-		</hbox>
-	</vbox>
-	
-	<keyset>
-		<key id="key_close" key="W" modifiers="accel" oncommand="window.close()"/>
-	</keyset>
-</window>

chrome/content/zotero/collectionViewItemTree.jsx
@@ -152,6 +152,9 @@ class CollectionViewItemTreeRowProvider extends ItemTreeRowProvider {
 			case 'citation-search':
 				changed = this.collectionTreeRow.setSearch(data, 'fields');
 				break;
+			case 'advanced-search':
+				changed = this.collectionTreeRow.setAdvancedSearch(data);
+				break;
 			case 'tags':
 				changed = this.collectionTreeRow.setTags(data);
 				break;

commit 057966000cbf0695775992ccf488afe23cd54fb8

Advanced search: Don't move focus to the results when running the search
Behavior regression in #5658

https://forums.zotero.org/discussion/132526/10-0-beta-8-number-of-results-in-the-new-advanced-search

chrome/content/zotero/elements/advancedSearchPane.js
@@ -193,7 +193,7 @@
 			await ZoteroPane.setSavedSearchEditorState('closed');
 		}
 
-		async submit({ focusResults = true } = {}) {
+		async submit() {
 			if (this.type === 'saved') {
 				throw new Error('submit() is unsupported for saved search');
 			}
@@ -201,12 +201,6 @@
 			this._searchElem.updateSearch();
 			this._active = true;
 			await ZoteroPane.itemsView.setFilter('advanced-search', this._search);
-			// Running the search normally moves focus to the results, but a caller that
-			// wants to keep focus in the search builder (e.g. quick search prefill) can
-			// opt out
-			if (focusResults) {
-				ZoteroPane.itemsView.focus();
-			}
 		}
 		
 		async clear() {

chrome/content/zotero/zoteroPane.js
@@ -2060,9 +2060,8 @@ var ZoteroPane = new function () {
 		this.itemsView.collectionTreeRow.setSearch('');
 
 		Zotero_Tabs.select('zotero-pane');
-		// Keep focus in the builder (on the first condition) rather than the results,
-		// so the user can refine the seeded search
-		await deck.pane.submit({ focusResults: false });
+		await deck.pane.submit();
+		// Focus the first condition so the user can refine the seeded search
 		deck.pane.focus();
 	};
 